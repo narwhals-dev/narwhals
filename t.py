4af408697072d425a72076a01dbf0a08f7e61c58
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-14 18:18:56 +0000

    other examples

--- t.py
+++ t.py
@@ -4,18 +4,13 @@ import polars as pl
 from datetime import datetime
 
 df = pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]})
-
 dfx, plx = polars_api_compat.convert(df, api_version='0.20')
-
-print(dfx.with_columns((plx.col('a') + plx.col('b')).alias('c')).dataframe)
-print(dfx.with_columns((plx.col('a') - plx.col('a').mean()).alias('c')).dataframe)
-print(dfx.with_columns(
-    (plx.col('a') - plx.col('a').mean()).alias('c'),
-    (plx.col('a') + plx.col('a').mean()).alias('d'),
-).dataframe)
-print(dfx.with_columns([
-    (plx.col('a') - plx.col('a').mean()).alias('c'),
-    (plx.col('a') + plx.col('a').mean()).alias('d'),
-]).dataframe)
-print(dfx.select(e=plx.col('a') - plx.col('a').mean()).dataframe)
-print(dfx.select(plx.all() + 1).dataframe)
+result = dfx.with_columns(
+    c = plx.col('a') + plx.col('b'),
+    d = plx.col('a') - plx.col('a').mean(),
+)
+print(result.dataframe)
+result = dfx.with_columns(
+    plx.all() * 2,
+)
+print(result.dataframe)
