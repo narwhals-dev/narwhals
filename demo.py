138b67797714df2474a1003518f30a609887a853
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-16 19:43:16 +0000

    better typing will help your mental model, stick with it!

---
+++ demo.py
@@ -0,0 +1,27 @@
+from great_tables.data import sp500
+from polars_api_compat import convert
+
+# Define the start and end dates for the data range
+start_date = "2010-06-07"
+end_date = "2010-06-14"
+
+# Filter sp500 using Pandas to dates between `start_date` and `end_date`
+# sp500_mini = sp500[(sp500["date"] >= start_date) & (sp500["date"] <= end_date)]
+
+
+def dataframe_agnostic_filter(df, start_date, end_date):
+    # opt-in to Polars API
+    dfx, plx = convert(df, version="0.20")
+
+    # Use (supported subset of) Polars API
+    dfx = dfx.filter(
+        plx.col("date") >= start_date,
+        plx.col("date") <= end_date,
+    )
+
+    # Return underlying dataframe (same class passed by user)
+    return dfx.dataframe
+
+
+sp500_mini = dataframe_agnostic_filter(sp500, start_date, end_date)
+print(sp500_mini)
