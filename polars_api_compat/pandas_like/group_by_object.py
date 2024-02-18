7bd458afe4c7423a957841f382e968650753e462
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-18 10:37:08 +0000

    wip

---
+++ polars_api_compat/pandas_like/group_by_object.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import collections
+from typing import Any
+from typing import Iterable
+
+import pandas as pd
+
+from polars_api_compat.pandas_like.dataframe_object import LazyFrame
+from polars_api_compat.spec import DataFrame as DataFrameT
+from polars_api_compat.spec import GroupBy as GroupByT
+from polars_api_compat.spec import IntoExpr
+from polars_api_compat.spec import LazyFrame as LazyFrameT
+from polars_api_compat.spec import LazyGroupBy as LazyGroupByT
+from polars_api_compat.utils import parse_into_exprs
+
+
+class GroupBy(GroupByT):
+    def __init__(self, df: DataFrameT, keys: list[str], api_version: str) -> None:
+        self._df = df
+        self._keys = list(keys)
+        self.api_version = api_version
+
+    def agg(
+        self,
+        *aggs: IntoExpr | Iterable[IntoExpr],
+        **named_aggs: IntoExpr,
+    ) -> DataFrameT:
+        return (
+            LazyGroupBy(self._df.lazy(), self._keys, self.api_version)
+            .agg(*aggs, **named_aggs)
+            .collect()
+        )
+
+
+class LazyGroupBy(LazyGroupByT):
+    def __init__(self, df: LazyFrameT, keys: list[str], api_version: str) -> None:
+        self._df = df
+        self._keys = list(keys)
+        self.api_version = api_version
+
+    def agg(
+        self,
+        *aggs: IntoExpr | Iterable[IntoExpr],
+        **named_aggs: IntoExpr,
+    ) -> LazyFrameT:
+        exprs = parse_into_exprs(
+            self._df.__lazyframe_namespace__(),
+            *aggs,
+            **named_aggs,
+        )
+        grouped = self._df.dataframe.groupby(
+            list(self._keys),
+            sort=False,
+            as_index=False,
+        )
+
+        # Do some fastpaths, if possible
+        new_cols: list[pd.DataFrame] = []
+        to_remove: list[int] = []
+        for i, expr in enumerate(exprs):
+            if (
+                expr.function_name is not None
+                and expr.depth is not None
+                and expr.depth <= 2
+                # todo: this one shouldn't be necessary?
+                and expr.root_names is not None
+            ):
+                # We must have a simple aggregation, such as
+                #     .agg(mean=pl.col('a').mean())
+                # or
+                #     .agg(pl.col('a').mean())
+                if expr.root_names is None or expr.output_names is None:
+                    msg = "Unreachable code, please report a bug"
+                    raise AssertionError(msg)
+                if len(expr.root_names) != len(expr.output_names):
+                    msg = "Unreachable code, please report a bug"
+                    raise AssertionError(msg)
+                new_names = dict(zip(expr.root_names, expr.output_names))
+                new_cols.append(
+                    getattr(grouped[expr.root_names], expr.function_name)()[
+                        expr.root_names
+                    ].rename(columns=new_names),
+                )
+                to_remove.append(i)
+        exprs = [expr for i, expr in enumerate(exprs) if i not in to_remove]
+
+        out: dict[str, list[Any]] = collections.defaultdict(list)
+        for key, _df in grouped:
+            for _key, _name in zip(key, self._keys):
+                out[_name].append(_key)
+            for expr in exprs:
+                result = expr.call(
+                    LazyFrame(
+                        _df,
+                        api_version=self.api_version,
+                        implementation=self._df._implementation,  # type: ignore[attr-defined]
+                    )
+                )
+                for _result in result:
+                    out[_result.name].append(_result.item())
+        result = pd.DataFrame(out)
+        result = pd.concat([result, *new_cols], axis=1, copy=False)
+        return LazyFrame(
+            result, api_version=self.api_version, implementation=self._df._implementation
+        )
---
+++ polars_api_compat/translate.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Any
+
+if TYPE_CHECKING:
+    from polars_api_compat.spec import DataFrame
+    from polars_api_compat.spec import LazyFrame
+    from polars_api_compat.spec import Namespace
+
+
+def to_polars_api(df: Any, version: str) -> tuple[LazyFrame, Namespace]:  # noqa: ARG001
+    if hasattr(df, "__polars_api_compat__"):
+        return df.__polars_api_compat__()
+    try:
+        import polars as pl
+    except ModuleNotFoundError:
+        pass
+    else:
+        if isinstance(df, pl.DataFrame):
+            return df.lazy(), pl  # type: ignore[return-value]
+        if isinstance(df, pl.LazyFrame):
+            return df, pl  # type: ignore[return-value]
+    try:
+        import pandas as pd
+    except ModuleNotFoundError:
+        pass
+    else:
+        if isinstance(df, pd.DataFrame):
+            from polars_api_compat.pandas_like import translate
+
+            return translate(df, implementation="pandas")
+    try:
+        import cudf
+    except ModuleNotFoundError:
+        pass
+    else:
+        if isinstance(df, cudf.DataFrame):
+            from polars_api_compat.pandas_like import translate
+
+            return translate(df, implementation="cudf")
+    msg = f"Could not translate DataFrame {type(df)}, please open a feature request."
+    raise TypeError(msg)
+
+
+def to_original_api(df: DataFrame | LazyFrame) -> Any:
+    try:
+        import polars as pl
+    except ModuleNotFoundError:
+        pass
+    else:
+        if isinstance(df, (pl.DataFrame, pl.LazyFrame)):
+            return df
+    return df.dataframe
