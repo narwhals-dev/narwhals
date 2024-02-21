6bc911647d7608cb1b18a9cc5b5b588e71e88ec1
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-21 19:13:29 +0000

    start adding tests

---
+++ tests/tpch_test.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Any
+
+import polars
+import pytest
+
+from narwhals import to_polars_api
+
+
+def compare_dicts(result: dict[str, Any], expected: dict[str, Any]) -> None:
+    for key in result:
+        for lhs, rhs in zip(result[key], expected[key]):
+            if isinstance(lhs, float):
+                assert abs(lhs - rhs) < 1e-6
+            else:
+                assert lhs == rhs
+
+
+@pytest.mark.parametrize(
+    "df_raw",
+    [
+        (polars.read_parquet("tests/data/lineitem.parquet").to_pandas()),
+        polars.scan_parquet("tests/data/lineitem.parquet"),
+    ],
+)
+def test_q1(df_raw: Any) -> None:
+    var_1 = datetime(1998, 9, 2)
+    df, pl = to_polars_api(df_raw, version="0.20")
+    result = (
+        df.filter(pl.col("l_shipdate") <= var_1)
+        .group_by(["l_returnflag", "l_linestatus"])
+        .agg(
+            [
+                pl.sum("l_quantity").alias("sum_qty"),
+                pl.sum("l_extendedprice").alias("sum_base_price"),
+                (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
+                .sum()
+                .alias("sum_disc_price"),
+                (
+                    pl.col("l_extendedprice")
+                    * (1.0 - pl.col("l_discount"))
+                    * (1.0 + pl.col("l_tax"))
+                )
+                .sum()
+                .alias("sum_charge"),
+                pl.mean("l_quantity").alias("avg_qty"),
+                pl.mean("l_extendedprice").alias("avg_price"),
+                pl.mean("l_discount").alias("avg_disc"),
+                pl.len().alias("count_order"),
+            ],
+        )
+        .sort(["l_returnflag", "l_linestatus"])
+    )
+    result = result.collect().to_dict(as_series=False)
+    expected = {
+        "l_returnflag": ["A", "N", "N", "R"],
+        "l_linestatus": ["F", "F", "O", "F"],
+        "sum_qty": [2109.0, 29.0, 3682.0, 1876.0],
+        "sum_base_price": [3114026.44, 39824.83, 5517101.99, 2947892.16],
+        "sum_disc_price": [2954950.8082, 39028.3334, 5205468.4852, 2816542.4816999994],
+        "sum_charge": [
+            3092840.4194289995,
+            39808.900068,
+            5406966.873419,
+            2935797.8313019997,
+        ],
+        "avg_qty": [27.75, 29.0, 25.047619047619047, 26.422535211267604],
+        "avg_price": [
+            40974.032105263155,
+            39824.83,
+            37531.30605442177,
+            41519.607887323946,
+        ],
+        "avg_disc": [0.05039473684210526, 0.02, 0.05537414965986395, 0.04507042253521127],
+        "count_order": [76, 1, 147, 71],
+    }
+    compare_dicts(result, expected)
