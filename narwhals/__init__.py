a576218c726ab163b150035540e39ccafbd8f63b
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-24 14:00:59 +0000

    translate_any

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -1,7 +1,8 @@
 from narwhals import containers
+from narwhals.translate import translate_any
 from narwhals.translate import translate_frame
 from narwhals.translate import translate_series
 
 __version__ = "0.1.13"
 
-__all__ = ["translate_frame", "translate_series", "containers"]
+__all__ = ["translate_frame", "translate_series", "translate_any", "containers"]
