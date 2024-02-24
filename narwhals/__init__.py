283f9235aed06c298b9c26047f635c46cd66bdb3
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-24 13:44:22 +0000

    Bump version to 0.1.13

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -2,6 +2,6 @@ from narwhals import containers
 from narwhals.translate import to_original_object
 from narwhals.translate import translate_frame
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 __all__ = ["translate_frame", "to_original_object", "containers"]
