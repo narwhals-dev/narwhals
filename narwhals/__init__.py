c994522791404f546f55ce21628566152459a763
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-24 14:29:08 +0000

    Bump version to 0.1.14

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -3,6 +3,6 @@ from narwhals.translate import translate_any
 from narwhals.translate import translate_frame
 from narwhals.translate import translate_series
 
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 
 __all__ = ["translate_frame", "translate_series", "translate_any", "containers"]
