277c3cf16d09074a102859fe7f0043489ff5a263
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-10 18:28:37 +0000

    Bump version to 0.3.0

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -9,7 +9,7 @@ from narwhals.translate import translate_any
 from narwhals.translate import translate_frame
 from narwhals.translate import translate_series
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "translate_frame",
