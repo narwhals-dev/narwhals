57b2373c501e5683893f66fed5ff94a85bc8fdba
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-17 14:13:11 +0000

    extra coverage

--- tpch/q1.py
+++ tpch/q1.py
@@ -1,7 +1,7 @@
 # ruff: noqa
 from typing import Any
 from datetime import datetime
-from narwhals import translate_frame
+import narwhals as nw
 import pandas as pd
 import polars
 
@@ -10,33 +10,33 @@ polars.Config.set_tbl_cols(10)
 
 def q1(df_raw: Any) -> Any:
     var_1 = datetime(1998, 9, 2)
-    df, pl = translate_frame(df_raw, is_lazy=True)
+    df = nw.LazyFrame(df_raw)
     result = (
-        df.filter(pl.col("l_shipdate") <= var_1)
+        df.filter(nw.col("l_shipdate") <= var_1)
         .group_by(["l_returnflag", "l_linestatus"])
         .agg(
             [
-                pl.sum("l_quantity").alias("sum_qty"),
-                pl.sum("l_extendedprice").alias("sum_base_price"),
-                (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
+                nw.sum("l_quantity").alias("sum_qty"),
+                nw.sum("l_extendedprice").alias("sum_base_price"),
+                (nw.col("l_extendedprice") * (1 - nw.col("l_discount")))
                 .sum()
                 .alias("sum_disc_price"),
                 (
-                    pl.col("l_extendedprice")
-                    * (1.0 - pl.col("l_discount"))
-                    * (1.0 + pl.col("l_tax"))
+                    nw.col("l_extendedprice")
+                    * (1.0 - nw.col("l_discount"))
+                    * (1.0 + nw.col("l_tax"))
                 )
                 .sum()
                 .alias("sum_charge"),
-                pl.mean("l_quantity").alias("avg_qty"),
-                pl.mean("l_extendedprice").alias("avg_price"),
-                pl.mean("l_discount").alias("avg_disc"),
-                pl.len().alias("count_order"),
+                nw.mean("l_quantity").alias("avg_qty"),
+                nw.mean("l_extendedprice").alias("avg_price"),
+                nw.mean("l_discount").alias("avg_disc"),
+                nw.len().alias("count_order"),
             ],
         )
         .sort(["l_returnflag", "l_linestatus"])
     )
-    return result.collect().to_native()
+    return nw.to_native(result.collect())
 
 
 # df = pd.read_parquet("../tpch-data/lineitem.parquet")
