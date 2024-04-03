3f5d637270322842525c1ed4c227299d5fa51980
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-04-03 12:32:57 +0300

    Bump version to 0.7.8

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -15,7 +15,7 @@ from narwhals.series import Series
 from narwhals.translate import from_native
 from narwhals.translate import to_native
 
-__version__ = "0.7.7"
+__version__ = "0.7.8"
 
 __all__ = [
     "concat",
