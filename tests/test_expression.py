f285f21be2693b593088b89f99cf9021d93267cc
Dea María Léon <deamarialeon@gmail.com>
2024-04-04 13:24:39 +0200

    hypothesis testing int & floats

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -4,50 +4,47 @@ import pandas as pd
 import polars as pl
 from hypothesis import given
 from hypothesis import strategies as st
+from numpy.testing import assert_allclose
 
 import narwhals as nw
 
 
 @given(
-    st.lists(st.integers(), min_size=3, max_size=3),
-    st.lists(st.datetimes(), min_size=3, max_size=3),
-    st.lists(st.floats(), min_size=3, max_size=3),
-    st.lists(st.text(min_size=1), min_size=3, max_size=3),
+    st.lists(
+        st.integers(min_value=-9223372036854775807, max_value=9223372036854775807),
+        min_size=3,
+        max_size=3,
+    ),
+    st.lists(
+        st.floats(min_value=-9223372036854775807.0, max_value=9223372036854775807.0),
+        min_size=3,
+        max_size=3,
+    ),
 )
-def test_isnull(
-    integers: st.SearchStrategy,
-    datetimes: st.SearchStrategy,
-    floats: st.SearchStrategy,
-    text: st.SearchStrategy,
+def test_mean(
+    integer: st.SearchStrategy[list[int]],
+    floats: st.SearchStrategy[float],
 ) -> None:
     dfpd = pd.DataFrame(
         {
-            "integer": integers,
-            "date": datetimes,
+            "integer": integer,
             "floats": floats,
-            "string": text,
         }
     )
     dfpl = pl.DataFrame(
         {
-            "integer": integers,
-            "date": datetimes,
+            "integer": integer,
             "floats": floats,
-            "string": text,
-        }
+        },
     )
     df_nw1 = nw.DataFrame(dfpd)
     df_nw2 = nw.DataFrame(dfpl)
 
-    assert df_nw1.select(nw.col("integer").is_null()) == df_nw2.select(
-        nw.col("integer").is_null()
+    assert_allclose(
+        nw.to_native(df_nw1.select(nw.col("integer").mean())),
+        nw.to_native(df_nw2.select(nw.col("integer").mean())),
     )
-    assert df_nw1.select(nw.col("date").is_null()) == df_nw2.select(
-        nw.col("date").is_null()
-    )
-    assert df_nw1.select(nw.col("floats").is_null()) == df_nw2.select(
-        nw.col("floats").is_null()
-    )
-    assert df_nw1.select(nw.col("strings").is_null()) == df_nw2.select(
-        nw.col("strings").is_null()
+    assert_allclose(
+        nw.to_native(df_nw1.select(nw.col("floats").mean())),
+        nw.to_native(df_nw2.select(nw.col("floats").mean())),
     )
