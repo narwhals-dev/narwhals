64522285c522cde24a8b4ccc3749549520d8b455
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-04-25 09:23:05 +0100

    cov

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,9 @@ def pytest_collection_modifyitems(config: Any, items: Any) -> Any:
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_slow = pytest.mark.skip(
+        reason="need --runslow option to run"
+    )  # pragma: no cover
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
