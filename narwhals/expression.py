e543c36de6958edc18370947cff8cfe9b2685292
Nwabueze Ugoh <126014542+brentomagic@users.noreply.github.com>
2024-05-03 18:10:37 +0100

    Added docstring for Expr.max

--- narwhals/expression.py
+++ narwhals/expression.py
@@ -271,10 +271,6 @@ class Expr:
             ...    df = df.select(nw.max('a', 'b'))
             ...    return nw.to_native(df)
 
-                df = nw.from_native(df_any)
-                df = df.select(nw.max('a', 'b'))
-                return nw.to_native(df)
-
             We can then pass either pandas or polars to `func`:
 
             >>> func(df_pd)
