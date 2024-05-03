701fa80d09b9abfac3984b4ff611ae63dd07f800
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-03 17:08:51 +0100

    fixup coverage

--- narwhals/typing.py
+++ narwhals/typing.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import TYPE_CHECKING  # pragma: no cover
 from typing import TypeVar  # pragma: no cover
 from typing import Union  # pragma: no cover
