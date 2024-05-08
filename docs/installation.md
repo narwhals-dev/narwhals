57d4123f7401fc8fed07af866c9b3c1d585e4d46
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-08 17:57:46 +0100

    Bump version to 0.8.4

--- docs/installation.md
+++ docs/installation.md
@@ -11,6 +11,6 @@ Then, if you start the Python REPL and see the following:
 ```python
 >>> import narwhals
 >>> narwhals
-'0.8.3'
+'0.8.4'
 ```
 then installation worked correctly!
--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -27,7 +27,7 @@ from narwhals.series import Series
 from narwhals.translate import from_native
 from narwhals.translate import to_native
 
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 
 __all__ = [
     "concat",
