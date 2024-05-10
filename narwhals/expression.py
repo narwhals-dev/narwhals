bd0c5071cb5611177d652329d31195289aa98521
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-10 09:53:37 +0100

    docs

--- narwhals/expression.py
+++ narwhals/expression.py
@@ -626,17 +626,17 @@ class Expr:
 
     def shift(self, n: int) -> Expr:
         """
-        Returns the difference between each element and the previous one.
+        Shift values by `n` positions.
 
         Notes:
             pandas may change the dtype here, for example when introducing missing
             values in an integer column. To ensure, that the dtype doesn't change,
-            you may want to use `fill_null` and `cast`. For example, to calculate
-            the diff and fill missing values with `0` in a Int64 column, you could
+            you may want to use `fill_null` and `cast`. For example, to shift
+            and fill missing values with `0` in a Int64 column, you could
             do:
 
             ```python
-            nw.col('a').diff().fill_null(0).cast(nw.Int64)
+            nw.col('a').shift(1).fill_null(0).cast(nw.Int64)
             ```
 
         Examples:
@@ -650,31 +650,31 @@ class Expr:
 
             >>> def func(df_any):
             ...    df = nw.from_native(df_any)
-            ...    df = df.select(a_diff=nw.col('a').diff())
+            ...    df = df.select(a_shift=nw.col('a').shift(n=1))
             ...    return nw.to_native(df)
 
             We can then pass either pandas or Polars to `func`:
 
             >>> func(df_pd)
-               a_diff
-            0     NaN
-            1     0.0
-            2     2.0
-            3     2.0
-            4     0.0
+               a_shift
+            0      NaN
+            1      1.0
+            2      1.0
+            3      3.0
+            4      5.0
             >>> func(df_pl)
             shape: (5, 1)
-            ┌────────┐
-            │ a_diff │
-            │ ---    │
-            │ i64    │
-            ╞════════╡
-            │ null   │
-            │ 0      │
-            │ 2      │
-            │ 2      │
-            │ 0      │
-            └────────┘
+            ┌─────────┐
+            │ a_shift │
+            │ ---     │
+            │ i64     │
+            ╞═════════╡
+            │ null    │
+            │ 1       │
+            │ 1       │
+            │ 3       │
+            │ 5       │
+            └─────────┘
         """
         return self.__class__(lambda plx: self._call(plx).shift(n))
 
