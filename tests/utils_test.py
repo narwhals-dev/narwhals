784f32065f3e70aee16ce017b4876aa8eb46ed58
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-12 12:58:41 +0100

    cov

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import polars as pl
 from pandas.testing import assert_frame_equal
 from pandas.testing import assert_series_equal
 
 import narwhals as nw
 
 
-def test_maybe_align_index() -> None:
+def test_maybe_align_index_pandas() -> None:
     df = nw.from_native(pd.DataFrame({"a": [1, 2, 3]}, index=[1, 2, 0]))
     s = nw.from_native(pd.Series([1, 2, 3], index=[2, 1, 0]), series_only=True)
     result = nw.maybe_align_index(df, s)
@@ -20,3 +21,10 @@ def test_maybe_align_index() -> None:
     result = nw.maybe_align_index(df, df.sort("a", descending=True))
     expected = pd.DataFrame({"a": [3, 2, 1]}, index=[0, 2, 1])
     assert_frame_equal(nw.to_native(result), expected)
+
+
+def test_maybe_align_index_polars() -> None:
+    df = nw.from_native(pl.DataFrame({"a": [1, 2, 3]}))
+    s = nw.from_native(pl.Series([1, 2, 3]), series_only=True)
+    result = nw.maybe_align_index(df, s)
+    assert result is df
