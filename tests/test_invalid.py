b4418ca9a0570142871fe7ac4cbe7465236de0a2
Dea María Léon <deamarialeon@gmail.com>
2024-05-16 16:36:39 +0200

    pre-commit fixes

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -14,3 +14,12 @@ def test_native_vs_non_native() -> None:
     df = pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     with pytest.raises(TypeError, match="Perhaps you forgot"):
         nw.from_native(df).filter(s > 1)
+
+
+def test_validate_lazines() -> None:
+    df = pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    with pytest.raises(
+        NotImplementedError,
+        match=("The items to concatenate should either all be eager, or all lazy"),
+    ):
+        nw.concat([nw.DataFrame(df), nw.LazyFrame(df)])
