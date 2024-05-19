2bd5dd0f19178acd7675734520137248496b8fe9
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-19 18:24:26 +0200

    docs

--- narwhals/selectors.py
+++ narwhals/selectors.py
@@ -5,6 +5,47 @@ from narwhals.utils import flatten
 
 
 def by_dtype(*dtypes: DType) -> Expr:
+    """
+    Select columns based on their dtype.
+
+    Arguments:
+        dtypes: one or data types to select
+
+    Examples:
+        >>> import narwhals as nw
+        >>> import narwhals.selectors as ncs
+        >>> import pandas as pd
+        >>> import polars as pl
+        >>>
+        >>> data = {'a': [1, 2], 'b': ['x', 'y'], 'c': [4.1, 2.3]}
+        >>> df_pd = pd.DataFrame(data)
+        >>> df_pl = pl.DataFrame(data)
+
+        Let's define a dataframe-agnostic function to select int64 and float64
+        dtypes and multiplies each value by 2:
+
+        >>> def func(df_any):
+        ...     df = nw.from_native(df_any)
+        ...     df = df.select(ncs.by_dtype(nw.Int64, nw.Float64)*2)
+        ...     return nw.to_native(df)
+
+        We can then pass either pandas or Polars dataframes:
+
+        >>> func(df_pd)
+           a    c
+        0  2  8.2
+        1  4  4.6
+        >>> func(df_pl)
+        shape: (2, 2)
+        ┌─────┬─────┐
+        │ a   ┆ c   │
+        │ --- ┆ --- │
+        │ i64 ┆ f64 │
+        ╞═════╪═════╡
+        │ 2   ┆ 8.2 │
+        │ 4   ┆ 4.6 │
+        └─────┴─────┘
+    """
     return Expr(
         lambda plx: plx.selectors.by_dtype(
             [translate_dtype(plx, dtype) for dtype in flatten(dtypes)]
