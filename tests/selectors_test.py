b7e8d86ab65043e51310c460e793290cba524442
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-05-20 09:48:49 +0200

    coverage

--- tests/selectors_test.py
+++ tests/selectors_test.py
@@ -6,6 +6,7 @@ import pytest
 
 import narwhals as nw
 from narwhals.selectors import by_dtype
+from narwhals.selectors import numeric
 from tests.utils import compare_dicts
 
 data = {"a": [1, 1, 2], "b": ["a", "b", "c"], "c": [4.0, 5.0, 6.0]}
@@ -15,5 +16,13 @@ data = {"a": [1, 1, 2], "b": ["a", "b", "c"], "c": [4.0, 5.0, 6.0]}
 def test_selecctors(constructor: Any) -> None:
     df = nw.from_native(constructor(data))
     result = nw.to_native(df.select(by_dtype([nw.Int64, nw.Float64]) + 1))
+    expected = {"a": [2, 2, 3], "c": [5.0, 6.0, 7.0]}
+    compare_dicts(result, expected)
+
+
+@pytest.mark.parametrize("constructor", [pd.DataFrame, pl.DataFrame])
+def test_numeric(constructor: Any) -> None:
+    df = nw.from_native(constructor(data))
+    result = nw.to_native(df.select(numeric() + 1))
     expected = {"a": [2, 2, 3], "c": [5.0, 6.0, 7.0]}
     compare_dicts(result, expected)
