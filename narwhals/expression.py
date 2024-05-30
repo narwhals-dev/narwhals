c674d6ff4c024b60d76ea0b6cc87abc4c72f9f0d
MatthewMec <144720119+MatthewMec@users.noreply.github.com>
2024-05-30 09:04:04 -0600

    added mean docstring

--- narwhals/expression.py
+++ narwhals/expression.py
@@ -2417,7 +2417,38 @@ def sum(*columns: str) -> Expr:
 
 def mean(*columns: str) -> Expr:
     """
-    Instantiate an expression representing the mean of one or more columns, similar to `polars.mean`.
+    Get the mean value
+
+    Example:
+    >>> import pandas as pd
+    >>> import polars as pl
+    >>> import narwhals as nw
+    >>> df_pl = pl.DataFrame({"a": [1, 8, 3]})
+    >>> df_pd = pd.DataFrame({"a": [1, 8, 3]})
+
+    We define a dataframe agnostic function:
+
+    >>> def func(df_any):
+    ...     df = nw.from_native(df_any)
+    ...     df = df.select(nw.mean('a'))
+    ...     return nw.to_native(df)
+
+    We can then pass either pandas or Polars to `func`:
+
+    >>> func(df_pd)
+         a
+    0  4.0
+    >>> func(df_pl)
+    shape: (1, 1)
+    ┌─────┐
+    │ a   │
+    │ --- │
+    │ f64 │
+    ╞═════╡
+    │ 4.0 │
+    └─────┘
+
+
     """
     return Expr(lambda plx: plx.mean(*columns))
 
