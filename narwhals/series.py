0b9a363fc48f2293a5f0ea331d32ea135b88b2e8
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-06-16 19:01:36 +0100

    Update narwhals/series.py

--- narwhals/series.py
+++ narwhals/series.py
@@ -1720,7 +1720,7 @@ class Series:
             pandas rounds to the nearest even value (e.g. -0.5 and 0.5 round to 0.0, 1.5 and 2.5 round to 2.0, 3.5 and
             4.5 to 4.0, etc..).
 
-            Polars rounds towards the larger integer (e.g. -0.5 to -1.0, 0.5 to 1.0, 1.5 to 2.0, 2.5 to 3.0, etc..).
+            Polars rounds away from 0 (e.g. -0.5 to -1.0, 0.5 to 1.0, 1.5 to 2.0, 2.5 to 3.0, etc..).
 
         Examples:
             >>> import narwhals as nw
