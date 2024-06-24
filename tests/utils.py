4da698990eb9519fec751bab062df1c7dd06935b
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-06-24 21:41:01 +0200

    rename zip_strict

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,7 @@ from typing import Iterator
 from typing import Sequence
 
 
-def zip_longest(left: Sequence[Any], right: Sequence[Any]) -> Iterator[Any]:
+def zip_strict(left: Sequence[Any], right: Sequence[Any]) -> Iterator[Any]:
     if len(left) != len(right):
         raise ValueError(
             "left len != right len", len(left), len(right)
@@ -21,7 +21,7 @@ def compare_dicts(result: Any, expected: dict[str, Any]) -> None:
         for key in result.columns:
             assert key in expected
     for key in expected:
-        for lhs, rhs in zip_longest(result[key], expected[key]):
+        for lhs, rhs in zip_strict(result[key], expected[key]):
             if isinstance(lhs, float) and not math.isnan(lhs):
                 assert math.isclose(lhs, rhs, rel_tol=0, abs_tol=1e-6), (lhs, rhs)
             elif isinstance(lhs, float) and math.isnan(lhs):
