f2a619fc8b30cdefee5d18399a7cfd7262392977
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-06-26 15:03:05 +0200

    merge main, tests

--- tests/translate/narwhalify_test.py
+++ tests/translate/narwhalify_test.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
+from contextlib import nullcontext as does_not_raise
 from typing import Any
 
 import pandas as pd
+import polars as pl
 import pytest
 
 import narwhals as nw
+
+data = {"a": [2, 3, 4]}
 
 
 def test_narwhalify() -> None:
@@ -15,9 +19,9 @@ def test_narwhalify() -> None:
 
     df = pd.DataFrame({"a": [1, 2, 3]})
     result = func(df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
     result = func(df=df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
 
 
 def test_narwhalify_method() -> None:
@@ -28,9 +32,9 @@ def test_narwhalify_method() -> None:
 
     df = pd.DataFrame({"a": [1, 2, 3]})
     result = Foo().func(df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
     result = Foo().func(a=1, df=df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
 
 
 def test_narwhalify_method_called() -> None:
@@ -41,11 +45,11 @@ def test_narwhalify_method_called() -> None:
 
     df = pd.DataFrame({"a": [1, 2, 3]})
     result = Foo().func(df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
     result = Foo().func(df=df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
     result = Foo().func(a=1, df=df)
-    pd.testing.assert_frame_equal(result, pd.DataFrame({"a": [2, 3, 4]}))
+    pd.testing.assert_frame_equal(result, pd.DataFrame(data))
 
 
 def test_narwhalify_method_invalid() -> None:
@@ -69,3 +73,37 @@ def test_narwhalify_invalid() -> None:
 
     with pytest.raises(TypeError):
         func()
+
+
+@pytest.mark.parametrize(
+    ("arg1", "arg2", "context"),
+    [
+        (pd.DataFrame(data), pd.Series(data["a"]), does_not_raise()),
+        (pl.DataFrame(data), pl.Series(data["a"]), does_not_raise()),
+        (
+            pd.DataFrame(data),
+            pl.DataFrame(data),
+            pytest.raises(
+                ValueError,
+                match="Found multiple backends. Make sure that all dataframe/series inputs come from the same backend.",
+            ),
+        ),
+        (
+            pl.DataFrame(data),
+            pd.Series(data["a"]),
+            pytest.raises(
+                ValueError,
+                match="Found multiple backends. Make sure that all dataframe/series inputs come from the same backend.",
+            ),
+        ),
+    ],
+)
+def test_narwhalify_backends(arg1: Any, arg2: Any, context: Any) -> None:
+    @nw.narwhalify
+    def func(
+        arg1: Any, arg2: Any, extra: int = 1
+    ) -> tuple[Any, Any, int]:  # pragma: no cover
+        return arg1, arg2, extra
+
+    with context:
+        func(arg1, arg2)
