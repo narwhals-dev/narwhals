807d34b133eaba0ba267376c75511d8a31ac5dd5
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-06-30 14:55:03 +0100

    Bump version to 0.9.18

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -39,7 +39,7 @@ from narwhals.utils import maybe_align_index
 from narwhals.utils import maybe_convert_dtypes
 from narwhals.utils import maybe_set_index
 
-__version__ = "0.9.17"
+__version__ = "0.9.18"
 
 __all__ = [
     "selectors",
