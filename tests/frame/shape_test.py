85c02324715318c7ead3c47f9050017b2712171f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-01 13:37:49 +0100

    more pyarrow

--- tests/frame/shape_test.py
+++ tests/frame/shape_test.py
@@ -3,9 +3,9 @@ from typing import Any
 import narwhals as nw
 
 
-def test_shape(constructor: Any) -> None:
+def test_shape(constructor_with_pyarrow: Any) -> None:
     result = nw.from_native(
-        constructor({"a": [1, 2], "b": [4, 5], "c": [7, 8]}), eager_only=True
+        constructor_with_pyarrow({"a": [1, 2], "b": [4, 5], "c": [7, 8]}), eager_only=True
     ).shape
     expected = (2, 3)
     assert result == expected
