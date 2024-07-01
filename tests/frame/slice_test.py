9637e04eba827ec64316e1904efdc7d1b50dddc2
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-01 13:39:35 +0100

    more pyarrow

--- tests/frame/slice_test.py
+++ tests/frame/slice_test.py
@@ -13,17 +13,17 @@ data = {
 }
 
 
-def test_slice_column(constructor: Any) -> None:
-    result = nw.from_native(constructor(data))["a"]
+def test_slice_column(constructor_with_pyarrow: Any) -> None:
+    result = nw.from_native(constructor_with_pyarrow(data))["a"]
     assert isinstance(result, nw.Series)
     assert result.to_numpy().tolist() == [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 
 
-def test_slice_rows(constructor: Any) -> None:
-    result = nw.from_native(constructor(data))[1:]
+def test_slice_rows(constructor_with_pyarrow: Any) -> None:
+    result = nw.from_native(constructor_with_pyarrow(data))[1:]
     compare_dicts(result, {"a": [2.0, 3.0, 4.0, 5.0, 6.0], "b": [12, 13, 14, 15, 16]})
 
-    result = nw.from_native(constructor(data))[2:4]
+    result = nw.from_native(constructor_with_pyarrow(data))[2:4]
     compare_dicts(result, {"a": [3.0, 4.0], "b": [13, 14]})
 
 
@@ -44,6 +44,6 @@ def test_slice_lazy_fails() -> None:
         _ = nw.from_native(pl.LazyFrame(data))[1:]
 
 
-def test_slice_int_fails(constructor: Any) -> None:
+def test_slice_int_fails(constructor_with_pyarrow: Any) -> None:
     with pytest.raises(TypeError, match="Expected str or slice, got: <class 'int'>"):
-        _ = nw.from_native(constructor(data))[1]  # type: ignore[call-overload,index]
+        _ = nw.from_native(constructor_with_pyarrow(data))[1]  # type: ignore[call-overload,index]
