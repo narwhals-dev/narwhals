0689c9b1a6dcc48d5f22cc4a745b093c2fa1a37a
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-07-05 18:21:25 +0100

    Update tests/frame/select_test.py

--- tests/frame/select_test.py
+++ tests/frame/select_test.py
@@ -8,6 +8,5 @@ def test_select(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
     result = df.select("a")
-    result_native = nw.to_native(result)
     expected = {"a": [1, 3, 2]}
-    compare_dicts(result_native, expected)
+    compare_dicts(result, expected)
