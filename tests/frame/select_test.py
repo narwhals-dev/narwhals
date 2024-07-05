fc9c5cce2fc8f7aaa85da30e84291d63808c625c
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-07-05 18:20:00 +0100

    Update tests/frame/select_test.py

--- tests/frame/select_test.py
+++ tests/frame/select_test.py
@@ -4,9 +4,9 @@ import narwhals as nw
 from tests.utils import compare_dicts
 
 
-def test_select(constructor: Any) -> None:
+def test_select(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
-    df = nw.from_native(constructor(data), eager_only=True)
+    df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
     result = df.select("a")
     result_native = nw.to_native(result)
     expected = {"a": [1, 3, 2]}
