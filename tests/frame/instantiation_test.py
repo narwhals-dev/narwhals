35e1adbc84697e6888828772c31c3f9b573f02df
Bruno Conde Kind <condekind@users.noreply.github.com>
2024-07-07 13:59:27 -0300

    Use pyarrow constructor; Added LazyFrame to lazy instantiation test

--- tests/frame/instantiation_test.py
+++ tests/frame/instantiation_test.py
@@ -7,9 +7,9 @@ import narwhals as nw
 from tests.utils import compare_dicts
 
 
-def test_lazy_instantiation(constructor: Any) -> None:
+def test_lazy_instantiation() -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
-    result = nw.from_native(constructor(data))
+    result = nw.from_native(pl.LazyFrame(data))
     expected = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     compare_dicts(result, expected)
 
@@ -22,8 +22,8 @@ def test_lazy_instantiation_error() -> None:
         _ = nw.DataFrame(df_lazy, is_polars=True, backend_version=(0,)).shape
 
 
-def test_eager_instantiation(constructor: Any) -> None:
+def test_eager_instantiation(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
-    result = nw.from_native(constructor(data), eager_only=True)
+    result = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
     expected = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     compare_dicts(result, expected)
