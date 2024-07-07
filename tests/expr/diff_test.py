f21db8ed24d6f0a7fbb72fb88fc5fc98873a5571
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-07 17:42:18 +0100

    old versions

--- tests/expr/diff_test.py
+++ tests/expr/diff_test.py
@@ -1,6 +1,10 @@
 from typing import Any
 
+import pyarrow as pa
+import pytest
+
 import narwhals as nw
+from narwhals.utils import parse_version
 from tests.utils import compare_dicts
 
 data = {
@@ -10,7 +14,10 @@ data = {
 }
 
 
-def test_diff(constructor_with_pyarrow: Any) -> None:
+def test_diff(constructor_with_pyarrow: Any, request: Any) -> None:
+    if parse_version(pa.__version__) < (13,):
+        # pc.pairwisediff is available since pyarrow 13.0.0
+        request.applymarker(pytest.mark.xfail)
     df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
     result = df.with_columns(c_diff=nw.col("c").diff())[1:]
     expected = {
