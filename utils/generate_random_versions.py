f911b8f74ef75e9f7366dd16bac7b615377934f1
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-08 10:05:12 +0100

    add random ci jobs test

---
+++ utils/generate_random_versions.py
@@ -0,0 +1,109 @@
+import random
+
+PANDAS_VERSION = [
+    "0.25.3",
+    "1.0.0",
+    "1.0.1",
+    "1.0.2",
+    "1.0.3",
+    "1.0.4",
+    "1.0.5",
+    "1.1.0",
+    "1.1.1",
+    "1.1.2",
+    "1.1.3",
+    "1.1.4",
+    "1.1.5",
+    "1.2.0",
+    "1.2.1",
+    "1.2.2",
+    "1.2.3",
+    "1.2.4",
+    "1.2.5",
+    "1.3.0",
+    "1.3.1",
+    "1.3.2",
+    "1.3.3",
+    "1.3.4",
+    "1.3.5",
+    "1.4.0",
+    "1.4.1",
+    "1.4.2",
+    "1.4.3",
+    "1.4.4",
+    "1.5.0",
+    "1.5.1",
+    "1.5.2",
+    "1.5.3",
+    "2.0.0",
+    "2.0.1",
+    "2.0.2",
+    "2.0.3",
+    "2.1.0",
+    "2.1.1",
+    "2.1.2",
+    "2.1.3",
+    "2.1.4",
+    "2.2.0",
+    "2.2.1",
+    "2.2.2",
+]
+POLARS_VERSION = [
+    "0.20.3",
+    "0.20.4",
+    "0.20.5",
+    "0.20.6",
+    "0.20.7",
+    "0.20.8",
+    "0.20.9",
+    "0.20.10",
+    "0.20.13",
+    "0.20.14",
+    "0.20.15",
+    "0.20.16",
+    "0.20.17",
+    "0.20.18",
+    "0.20.19",
+    "0.20.21",
+    "0.20.22",
+    "0.20.23",
+    "0.20.25",
+    "0.20.26",
+    "0.20.30",
+    "0.20.31",
+    "1.0.0",
+    "1.1.0",
+]
+PYARROW_VERSION = [
+    "11.0.0",
+    "12.0.0",
+    "12.0.1",
+    "13.0.0",
+    "14.0.0",
+    "14.0.1",
+    "14.0.2",
+    "15.0.0",
+    "15.0.1",
+    "15.0.2",
+    "16.0.0",
+    "16.1.0",
+]
+
+pandas_version = random.choice(PANDAS_VERSION)
+polars_version = random.choice(POLARS_VERSION)
+pyarrow_version = random.choice(PYARROW_VERSION)
+
+content = (
+    f"pandas=={pandas_version}\npolars=={polars_version}\npyarrow=={pyarrow_version}\n"
+)
+with open("random-requirements.txt", "w") as fd:
+    fd.write(content)
+
+with open("pyproject.toml") as fd:
+    content = fd.read()
+content = content.replace(
+    'filterwarnings = [\n  "error",\n]',
+    "filterwarnings = [\n  \"error\",\n  'ignore:distutils Version classes are deprecated:DeprecationWarning',\n]",
+)
+with open("pyproject.toml", "w") as fd:
+    fd.write(content)
