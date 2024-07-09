0d15ace09a4a408f499ab2a57655c171a874575f
Nwabueze Ugoh <126014542+brentomagic@users.noreply.github.com>
2024-07-09 18:43:30 +0100

    Update reductions_test.py

--- tests/expr/reductions_test.py
+++ tests/expr/reductions_test.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+from typing import Any
+
+import narwhals.stable.v1 as nw
+from tests.utils import compare_dicts
+
+
 def test_expr_min_max(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
