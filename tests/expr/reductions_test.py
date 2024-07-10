bd17f72f7324a5bcdd4ba787ee197e64d5cb2fbb
Nwabueze Ugoh <126014542+brentomagic@users.noreply.github.com>
2024-07-10 15:41:36 +0100

    Update tests/expr/reductions_test.py
    Co-authored-by: Marco Edward Gorelli <marcogorelli@protonmail.com>

--- tests/expr/reductions_test.py
+++ tests/expr/reductions_test.py
@@ -9,8 +9,8 @@ from tests.utils import compare_dicts
 def test_expr_min_max(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
-    result_min = nw.to_native(df.select(nw.min("a", "b", "z")))
-    result_max = nw.to_native(df.select(nw.max("a", "b", "z")))
+    result_min = nw.to_native(df.select(nw.col("a", "b", "z").min()))
+    result_max = nw.to_native(df.select(nw.col("a", "b", "z").max()))
     expected_min = {"a": [1], "b": [4], "z": [7]}
     expected_max = {"a": [3], "b": [6], "z": [9]}
     compare_dicts(result_min, expected_min)
