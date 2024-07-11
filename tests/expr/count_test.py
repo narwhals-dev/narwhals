af20c7271272cb5da531d252e76deaa385eb67b6
ugohuche <ucugooh@gmail.com>
2024-07-11 21:39:28 +0100

    Modified test

--- tests/expr/count_test.py
+++ tests/expr/count_test.py
@@ -4,10 +4,9 @@ import narwhals.stable.v1 as nw
 from tests.utils import compare_dicts
 
 
-def test_count(constructor: Any) -> None:
+def test_count(constructor_with_pyarrow: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, None, 6], "z": [7.0, None, None]}
-    df = nw.from_native(constructor(data), eager_only=True)
-    result = df.select(nw.all().count())
-    result_native = nw.to_native(result)
+    df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
+    result = df.select(nw.col("a", "b", "z").count())
     expected = {"a": [3], "b": [2], "z": [1]}
-    compare_dicts(result_native, expected)
+    compare_dicts(result, expected)
