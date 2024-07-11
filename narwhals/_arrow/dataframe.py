78020d37a74f13622dee33b8aa73b784654d6fed
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-07-11 09:37:13 +0200

    feat: to_numpy,to_dict,pipe,with_row_index

--- narwhals/_arrow/dataframe.py
+++ narwhals/_arrow/dataframe.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 from typing import Any
+from typing import Callable
 from typing import Iterable
 from typing import Sequence
 from typing import overload
@@ -194,6 +195,35 @@ class ArrowDataFrame:
 
     def to_pandas(self) -> Any:
         return self._native_dataframe.to_pandas()
+
+    def to_numpy(self) -> Any:
+        import numpy as np
+
+        return np.column_stack([col.to_numpy() for col in self._native_dataframe.columns])
+
+    def to_dict(self, *, as_series: bool) -> Any:
+        df = self._native_dataframe
+
+        names_and_values = zip(df.column_names, df.columns)
+        if as_series:
+            from narwhals._arrow.series import ArrowSeries
+
+            return {
+                name: ArrowSeries(col, name=name, backend_version=self._backend_version)
+                for name, col in names_and_values
+            }
+        else:
+            return {name: col.to_pylist() for name, col in names_and_values}
+
+    def pipe(self, function: Callable[[Any], Self], *args: Any, **kwargs: Any) -> Self:
+        return function(self, *args, **kwargs)
+
+    def with_row_index(self, name: str) -> Self:
+        pa = get_pyarrow()
+        df = self._native_dataframe
+
+        row_indices = pa.array(range(df.num_rows))
+        return self._from_native_dataframe(df.append_column(name, row_indices))
 
     def lazy(self) -> Self:
         return self
