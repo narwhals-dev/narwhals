b1a5063aa40cce5d5d5b5943588a32cf8859f92f
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-07-12 22:19:30 +0200

    col name typo

--- tests/expr/sum_horizontal_test.py
+++ tests/expr/sum_horizontal_test.py
@@ -10,11 +10,11 @@ from tests.utils import compare_dicts
 def test_sumh(constructor_with_pyarrow: Any, col_expr: Any) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
     df = nw.from_native(constructor_with_pyarrow(data), eager_only=True)
-    result = df.with_columns(horizonal_sum=nw.sum_horizontal(col_expr, nw.col("b")))
+    result = df.with_columns(horizontal_sum=nw.sum_horizontal(col_expr, nw.col("b")))
     expected = {
         "a": [1, 3, 2],
         "b": [4, 4, 6],
         "z": [7.0, 8.0, 9.0],
-        "horizonal_sum": [5, 7, 8],
+        "horizontal_sum": [5, 7, 8],
     }
     compare_dicts(result, expected)
