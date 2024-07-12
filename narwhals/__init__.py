057c00c0a624a23ba20953ef6abf38bc6f0750a0
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-07-12 22:13:49 +0100

    Bump version to 1.0.6 (#504)

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -44,7 +44,7 @@ from narwhals.utils import maybe_align_index
 from narwhals.utils import maybe_convert_dtypes
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 __all__ = [
     "selectors",
