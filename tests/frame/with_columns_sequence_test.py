c1bc50c388cbab8fc1942c2adc319b6fcf16f497
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-07-13 12:39:51 +0200

    another failing test

--- tests/frame/with_columns_sequence_test.py
+++ tests/frame/with_columns_sequence_test.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import numpy as np
+import pytest
 
 import narwhals.stable.v1 as nw
 from tests.utils import compare_dicts
@@ -11,7 +12,14 @@ data = {
 }
 
 
-def test_with_columns(constructor: Any) -> None:
-    result = nw.from_native(constructor(data)).with_columns(d=np.array([4, 5]))
-    expected = {"a": ["foo", "bars"], "ab": ["foo", "bars"], "d": [4, 5]}
+def test_with_columns(constructor_with_pyarrow: Any, request: Any) -> None:
+    if "table" in str(constructor_with_pyarrow):
+        request.applymarker(pytest.mark.xfail)
+
+    result = (
+        nw.from_native(constructor_with_pyarrow(data))
+        .with_columns(d=np.array([4, 5]))
+        .with_columns(d2=nw.col("d") ** 2)
+    )
+    expected = {"a": ["foo", "bars"], "ab": ["foo", "bars"], "d": [4, 5], "d2": [16, 25]}
     compare_dicts(result, expected)
