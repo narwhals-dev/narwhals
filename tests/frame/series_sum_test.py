3af98338a9b1f4b6e195f1bf3b9a6591668c24df
mfonekpo <mfonekpo34@gmail.com>
2024-07-15 11:00:30 +0100

    cleaning up code

--- tests/frame/series_sum_test.py
+++ tests/frame/series_sum_test.py
@@ -6,30 +6,17 @@ import narwhals.stable.v1 as nw
 from tests.utils import compare_dicts
 
 
-def test_series_sum(constructor_with_pyarrow: Any) -> None:
+def test_series_sum(constructor: Any) -> None:
     data = {
         "a": [0, 1, 2, 3, 4],
         "b": [1, 2, 3, 5, 3],
         "c": [5, 4, None, 2, 1],
     }
-    df = nw.from_native(
-        constructor_with_pyarrow(data), eager_only=True, allow_series=True
-    )
+    df = nw.from_native(constructor(data), eager_only=True, allow_series=True)
 
     # Calculating the sum for each column seperately
-    result_sum_a = df.select(nw.col("a").sum())
-    result_sum_b = df.select(nw.col("b").sum())
-    result_sum_c = df.select(nw.col("c").sum())
+    result = df.select(nw.col("a", "b", "c").sum())
 
-    # Convert the results to Narwhals Native Frame
-    result_native_a = nw.to_native(result_sum_a)
-    result_native_b = nw.to_native(result_sum_b)
-    result_native_c = nw.to_native(result_sum_c)
+    expected_sum = {"a": [10], "b": [14], "c": [12]}
 
-    expected_sum_a = {"a": [10]}
-    expected_sum_b = {"b": [14]}
-    expected_sum_c = {"c": [12]}
-
-    compare_dicts(result_sum_a, expected_sum_a)
-    compare_dicts(result_sum_b, expected_sum_b)
-    compare_dicts(result_sum_c, expected_sum_c)
+    compare_dicts(result, expected_sum)
