499864745d754d669d44d26f7d65375d623b20fc
mfonekpo <mfonekpo34@gmail.com>
2024-07-15 12:01:57 +0100

    all test cases passed on CI

--- tests/frame/series_sum_test.py
+++ tests/frame/series_sum_test.py
@@ -12,9 +12,10 @@ def test_series_sum(constructor: Any) -> None:
         "b": [1, 2, 3, 5, 3],
         "c": [5, 4, None, 2, 1],
     }
-    df = nw.from_native(constructor(data), eager_only=True, allow_series=True)
+    df = nw.from_native(
+        constructor(data), strict=False, eager_only=True, allow_series=True
+    )
 
-    # Calculating the sum for each column seperately
     result = df.select(nw.col("a", "b", "c").sum())
 
     expected_sum = {"a": [10], "b": [14], "c": [12]}
