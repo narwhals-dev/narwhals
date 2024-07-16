36aa1d2921871129a8c0cfec00ef72691dc1f603
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-07-16 09:35:22 +0200

    old versions cast

--- tests/expr/cat/get_categories_test.py
+++ tests/expr/cat/get_categories_test.py
@@ -2,19 +2,41 @@ from __future__ import annotations
 
 from typing import Any
 
+import pyarrow as pa
+import pytest
+
 import narwhals.stable.v1 as nw
 from tests.utils import compare_dicts
 
 data = {"a": ["one", "two", "two"]}
 
 
-def test_get_categories(constructor: Any) -> None:
+def test_get_categories(request: Any, constructor: Any) -> None:
+    if "pyarrow_table" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+
     df = nw.from_native(constructor(data), eager_only=True)
     df = df.select(nw.col("a").cast(nw.Categorical))
     expected = {"a": ["one", "two"]}
 
-    result = df.select(nw.col("a").cat.get_categories())
-    compare_dicts(result, expected)
+    result_expr = df.select(nw.col("a").cat.get_categories())
+    compare_dicts(result_expr, expected)
 
-    result = df.select(df["a"].cat.get_categories())
-    compare_dicts(result, expected)
+    result_series = df["a"].cat.get_categories().to_list()
+    assert set(result_series) == set(expected["a"])
+
+
+def test_get_categories_pyarrow() -> None:
+    # temporary test until we have `cast` in pyarrow - later, fuse
+    # this with test above
+    table = pa.table(
+        {"a": pa.array(["a", "b", None, "d"], pa.dictionary(pa.int64(), pa.utf8()))}
+    )
+    df = nw.from_native(table, eager_only=True)
+    expected = {"a": ["a", "b", "d"]}
+
+    result_expr = df.select(nw.col("a").cat.get_categories())
+    compare_dicts(result_expr, expected)
+
+    result_series = df["a"].cat.get_categories().to_list()
+    assert result_series == expected["a"]
