ff6a616c7b4b40db517b5137deaf5905a1992070
mfonekpo <mfonekpo34@gmail.com>
2024-07-20 01:48:01 +0100

    refactoring everything to a single line

--- tests/series_only/drop_nulls_test.py
+++ tests/series_only/drop_nulls_test.py
@@ -18,16 +18,13 @@ def test_drop_nulls(constructor: Any) -> None:
         constructor(data), strict=False, eager_only=True, allow_series=True
     )
 
-    result_a = df.select(nw.col("A").drop_nulls())
-    result_b = df.select(nw.col("B").drop_nulls())
-    result_c = df.select(nw.col("C").drop_nulls())
-    result_d = df.select(nw.col("D").drop_nulls())
-    expected_a = {"A": [1.0, 2.0, 4.0]}
-    expected_b = {"B": [5, 6, 7, 8]}
-    expected_c = {"C": []}
-    expected_d = {"D": [9, 10, 11, 12]}
+    result = df.select(nw.col("A", "B", "C", "D").drop_nulls())
+    expected = {
+        "A": [1.0, 2.0, 4.0],
+        "B": [5, 6, 8],
+        "C": [],
+        "D": [9, 10, 12],
+    }
 
-    compare_dicts(result_a, expected_a)
-    compare_dicts(result_b, expected_b)
-    compare_dicts(result_c, expected_c)
-    compare_dicts(result_d, expected_d)
+ 
+    compare_dicts(result, expected)
