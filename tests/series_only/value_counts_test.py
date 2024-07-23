0633b3f9daf81ca63ec75b8028287ec4d4a7dd0a
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-23 17:58:00 +0100

    fix the random ci jobs test too

--- tests/series_only/value_counts_test.py
+++ tests/series_only/value_counts_test.py
@@ -1,19 +1,20 @@
-import sys
 from typing import Any
 
+import pandas as pd
 import pytest
 
 import narwhals.stable.v1 as nw
+from narwhals.utils import parse_version
 from tests.utils import compare_dicts
 
 data = {"a": [4, 4, 6, 4, 1, 1]}
 
 
 def test_value_counts(request: Any, constructor: Any) -> None:
-    if "pandas_nullable_constructor" in str(constructor) and sys.version_info < (
-        3,
-        9,
-    ):  # fails for py3.8
+    if "pandas_nullable_constructor" in str(constructor) and parse_version(
+        pd.__version__
+    ) < (2, 2):
+        # bug in old pandas
         request.applymarker(pytest.mark.xfail)
 
     series = nw.from_native(constructor(data), eager_only=True)["a"]
