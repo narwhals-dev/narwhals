3a6aaa00725d0df3a4c55f155aea4a6d539053ae
Magdalena Kowalczuk <74981211+anopsy@users.noreply.github.com>
2024-07-24 09:05:46 +0000

    arrow isbetween

---
+++ tests/series_only/is_between_test.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pandas as pd
+import pytest
+
+import narwhals.stable.v1 as nw
+from narwhals.utils import parse_version
+from tests.utils import compare_dicts
+
+data = [1, 4, 2, 5]
+
+
+@pytest.mark.parametrize(
+    ("closed", "expected"),
+    [
+        ("left", [True, True, True, False]),
+        ("right", [False, True, True, True]),
+        ("both", [True, True, True, True]),
+        ("none", [False, True, True, False]),
+    ],
+)
+def test_is_between(
+    request: Any, constructor_series: Any, closed: str, expected: list[bool]
+) -> None:
+    if "pandas_series_nullable_constructor" in str(constructor_series) and parse_version(
+        pd.__version__
+    ) < (2, 2):
+        # bug in old pandas
+        request.applymarker(pytest.mark.xfail)
+    ser = nw.from_native(constructor_series(data), series_only=True)
+    result = ser.is_between(1, 5, closed=closed)
+    compare_dicts({"a": result}, {"a": expected})
