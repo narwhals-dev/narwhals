f85420e0a802f2f634fad7f3e1ed4f49992ee17b
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-07-25 10:01:22 +0200

    forgot about lazy :(

--- tests/expr_and_series/gather_every_test.py
+++ tests/expr_and_series/gather_every_test.py
@@ -10,8 +10,8 @@ data = {"a": list(range(10))}
 
 @pytest.mark.parametrize("n", [1, 2, 3])
 @pytest.mark.parametrize("offset", [1, 2, 3])
-def test_gather_every_expr(constructor: Any, n: int, offset: int) -> None:
-    df = nw.from_native(constructor(data), eager_only=True)
+def test_gather_every_expr(constructor_with_lazy: Any, n: int, offset: int) -> None:
+    df = nw.from_native(constructor_with_lazy(data))
 
     result = df.select(nw.col("a").gather_every(n=n, offset=offset))
     expected = {"a": data["a"][offset::n]}
--- tests/frame/gather_every_test.py
+++ tests/frame/gather_every_test.py
@@ -10,8 +10,8 @@ data = {"a": list(range(10))}
 
 @pytest.mark.parametrize("n", [1, 2, 3])
 @pytest.mark.parametrize("offset", [1, 2, 3])
-def test_gather_every(constructor: Any, n: int, offset: int) -> None:
-    df = nw.from_native(constructor(data), eager_only=True)
+def test_gather_every(constructor_with_lazy: Any, n: int, offset: int) -> None:
+    df = nw.from_native(constructor_with_lazy(data))
     result = df.gather_every(n=n, offset=offset)
     expected = {"a": data["a"][offset::n]}
     compare_dicts(result, expected)
