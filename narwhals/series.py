5e34f8e33d590290f586853ea7ef52f6d8885d52
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2024-07-25 16:08:52 +0000

    removed the explicit function and switched to higher order function.

--- narwhals/series.py
+++ narwhals/series.py
@@ -118,6 +118,7 @@ class Series:
             ...     return s_any.pipe(lambda x: x**2)
 
             Now apply it to the series
+
             >>> func(spl)
             shape: (4,)
             Series: '' [i64]
--- tests/expr_and_series/pipe_test.py
+++ tests/expr_and_series/pipe_test.py
@@ -3,19 +3,13 @@ from typing import Any
 import narwhals as nw
 from tests.utils import compare_dicts
 
-
-@nw.narwhalify()
-def func(s: Any) -> Any:
-    return s.pipe(lambda x: x**2)
-
-
 input_list = {"a": [2, 4, 6, 8]}
 expected = [4, 16, 36, 64]
 
 
 def test_pipe_expr(constructor: Any) -> None:
     df = nw.from_native(constructor(input_list))
-    e = df.select(func(nw.col("a")))
+    e = df.select(nw.col("a").pipe(lambda x: x**2))
     compare_dicts(e, {"a": expected})
 
 
@@ -23,4 +17,4 @@ def test_pipe_series(
     constructor_series: Any,
 ) -> None:
     s = nw.from_native(constructor_series(input_list["a"]), series_only=True)
-    assert func(s).to_list() == expected
+    assert s.pipe(lambda x: x**2).to_list() == expected
