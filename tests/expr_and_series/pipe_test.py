126bdcdebd4363e603fca9fece7f86501a3a2221
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2024-07-25 14:13:26 +0000

    Fix the test being so the function is being used correctly.

--- tests/expr_and_series/pipe_test.py
+++ tests/expr_and_series/pipe_test.py
@@ -10,12 +10,12 @@ def func(s: Any) -> Any:
 
 
 input_list = {"a": [2, 4, 6, 8]}
-expected = [i**2 for i in input_list["a"]]
+expected = [4, 16, 36, 64]
 
 
 def test_pipe_expr(constructor: Any) -> None:
     df = nw.from_native(constructor(input_list))
-    e = df.select(nw.col("a").pipe(func))
+    e = df.select(func(nw.col("a")))
     compare_dicts(e, {"a": expected})
 
 
@@ -23,4 +23,4 @@ def test_pipe_series(
     constructor_series: Any,
 ) -> None:
     s = nw.from_native(constructor_series(input_list["a"]), series_only=True)
-    assert s.pipe(func).to_list() == expected
+    assert func(s).to_list() == expected
