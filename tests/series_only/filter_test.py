d3cf7440940e6c790ac0a06c4f205592d0134ff1
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-07-27 22:15:00 +0100

    chore: replace constructor_series with constructor (#649)
    * ref
    
    * chore: replace constructor_series with constructor

--- tests/series_only/filter_test.py
+++ tests/series_only/filter_test.py
@@ -7,9 +7,9 @@ import numpy as np
 import narwhals as nw
 
 
-def test_filter(constructor_series: Any) -> None:
+def test_filter(constructor: Any) -> None:
     data = [1, 3, 2]
-    series = nw.from_native(constructor_series(data), series_only=True)
+    series = nw.from_native(constructor({"a": data}), eager_only=True)["a"]
     result = series.filter(series > 1)
     expected = np.array([3, 2])
     assert (result.to_numpy() == expected).all()
