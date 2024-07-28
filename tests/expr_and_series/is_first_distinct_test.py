4abde0b6edb14c02a06eb8b6ddbdf231f758793b
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-28 11:01:20 +0100

    fixup

--- tests/expr_and_series/is_first_distinct_test.py
+++ tests/expr_and_series/is_first_distinct_test.py
@@ -12,7 +12,10 @@ data = {
 }
 
 
-def test_is_first_distinct_expr(constructor: Any) -> None:
+def test_is_first_distinct_expr(constructor: Any, request: Any) -> None:
+    if "modin" in str(constructor):
+        # TODO(unassigned): why is Modin failing here?
+        request.applymarker(pytest.mark.xfail)
     df = nw.from_native(constructor(data), eager_only=True)
     result = df.select(nw.all().is_first_distinct())
     expected = {
@@ -22,10 +25,7 @@ def test_is_first_distinct_expr(constructor: Any) -> None:
     compare_dicts(result, expected)
 
 
-def test_is_first_distinct_series(constructor: Any, request: Any) -> None:
-    if "modin" in str(constructor):
-        # TODO(unassigned): why is Modin failing here?
-        request.applymarker(pytest.mark.xfail)
+def test_is_first_distinct_series(constructor: Any) -> None:
     series = nw.from_native(constructor(data), eager_only=True)["a"]
     result = series.is_first_distinct()
     expected = np.array([True, False, True, True, False])
