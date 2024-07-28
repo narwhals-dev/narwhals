fa4fa08abd46d9fb8cacbc62c2eae8659424c0cc
montanarograziano <graziano.montanaro98@gmail.com>
2024-07-28 15:34:38 +0200

    Swapped constructor_eager fixture with constructor in test_std

--- tests/frame/std_test.py
+++ tests/frame/std_test.py
@@ -6,8 +6,8 @@ from tests.utils import compare_dicts
 data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
 
 
-def test_std(constructor_eager: Any) -> None:
-    df = nw.from_native(constructor_eager(data))
+def test_std(constructor: Any) -> None:
+    df = nw.from_native(constructor(data))
     result = df.select(
         nw.col("a").std().alias("a_ddof_default"),
         nw.col("a").std(ddof=1).alias("a_ddof_1"),
