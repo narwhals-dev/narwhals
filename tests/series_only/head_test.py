f02debf170d6abd6453707028e720dadc7e2c7fc
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-07-28 09:07:54 +0100

    add collect for dask

--- tests/series_only/head_test.py
+++ tests/series_only/head_test.py
@@ -8,7 +8,7 @@ import narwhals as nw
 
 
 @pytest.mark.parametrize("n", [2, -1])
-def test_head(constructor_series: Any, n: int) -> None:
-    s = nw.from_native(constructor_series([1, 2, 3]), series_only=True)
+def test_head(constructor: Any, n: int) -> None:
+    s = nw.from_native(constructor({"a": [1, 2, 3]}), eager_only=True)["a"]
 
     assert s.head(n).to_list() == [1, 2]
