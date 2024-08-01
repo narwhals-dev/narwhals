dfc6fe3b37cbb52921c42153111c41880761fc83
montanarograziano <graziano.montanaro98@gmail.com>
2024-08-01 12:37:03 +0200

    Swapped constructor_eager fixture with constructor in test_abs_series

--- tests/expr_and_series/abs_test.py
+++ tests/expr_and_series/abs_test.py
@@ -11,8 +11,8 @@ def test_abs(constructor: Any) -> None:
     compare_dicts(result, expected)
 
 
-def test_abs_series(constructor_eager: Any) -> None:
-    df = nw.from_native(constructor_eager({"a": [1, 2, 3, -4, 5]}), eager_only=True)
-    result = {"b": df["a"].abs()}
+def test_abs_series(constructor: Any) -> None:
+    df = nw.from_native(constructor({"a": [1, 2, 3, -4, 5]}))
+    result = df.with_columns(b=nw.col("a").abs()).select("b")
     expected = {"b": [1, 2, 3, 4, 5]}
     compare_dicts(result, expected)
