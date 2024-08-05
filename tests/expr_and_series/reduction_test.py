7220f886bf2a2e001d95c43c2ae980fc80a00000
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-08-05 20:05:26 +0200

    test refactor

--- tests/expr_and_series/reduction_test.py
+++ tests/expr_and_series/reduction_test.py
@@ -8,73 +8,59 @@ import narwhals.stable.v1 as nw
 from tests.utils import compare_dicts
 
 
-def test_scalar_reduction_select(request: Any, constructor: Any) -> None:
-    if "dask" in str(constructor):
+@pytest.mark.parametrize(
+    ("expr", "expected"),
+    [
+        (
+            [nw.col("a").min().alias("min"), nw.col("a", "b").mean()],
+            {"min": [1], "a": [2], "b": [5]},
+        ),
+        ([(nw.col("a") + nw.col("b").max()).alias("x")], {"x": [7, 8, 9]}),
+        ([nw.col("a"), nw.col("b").min()], {"a": [1, 2, 3], "b": [4, 4, 4]}),
+        ([nw.col("a").max(), nw.col("b")], {"a": [3, 3, 3], "b": [4, 5, 6]}),
+        (
+            [nw.col("a"), nw.col("b").min().alias("min")],
+            {"a": [1, 2, 3], "min": [4, 4, 4]},
+        ),
+    ],
+    ids=range(5),
+)
+def test_scalar_reduction_select(
+    request: Any, constructor: Any, expr: list[Any], expected: dict[str, list[Any]]
+) -> None:
+    if "dask" in str(constructor) and int(request.node.callspec.id[-1]) != 1:
         request.applymarker(pytest.mark.xfail)
 
     data = {"a": [1, 2, 3], "b": [4, 5, 6]}
     df = nw.from_native(constructor(data))
-    result = df.select(
-        nw.col("a").min().alias("min"),
-        nw.col("b").max().alias("max"),
-        nw.col("a", "b").mean(),
-    )
-    expected = {"min": [1], "max": [6], "a": [2], "b": [5]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.select((nw.col("a") + nw.col("b").max()).alias("x"))
-    expected = {"x": [7, 8, 9]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.select(nw.col("a"), nw.col("b").min())
-    expected = {"a": [1, 2, 3], "b": [4, 4, 4]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.select(nw.col("a").max(), nw.col("b"))
-    expected = {"a": [3, 3, 3], "b": [4, 5, 6]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.select(nw.col("a"), nw.col("b").min().alias("min"))
-    expected = {"a": [1, 2, 3], "min": [4, 4, 4]}
+    result = df.select(*expr)
     compare_dicts(result, expected)
 
 
-def test_scalar_reduction_with_columns(request: Any, constructor: Any) -> None:
-    if "dask" in str(constructor):
+@pytest.mark.parametrize(
+    ("expr", "expected"),
+    [
+        (
+            [nw.col("a").min().alias("min"), nw.col("a", "b").mean()],
+            {"min": [1, 1, 1], "a": [2, 2, 2], "b": [5, 5, 5]},
+        ),
+        ([(nw.col("a") + nw.col("b").max()).alias("x")], {"x": [7, 8, 9]}),
+        ([nw.col("a"), nw.col("b").min()], {"a": [1, 2, 3], "b": [4, 4, 4]}),
+        ([nw.col("a").max(), nw.col("b")], {"a": [3, 3, 3], "b": [4, 5, 6]}),
+        (
+            [nw.col("a"), nw.col("b").min().alias("min")],
+            {"a": [1, 2, 3], "min": [4, 4, 4]},
+        ),
+    ],
+    ids=range(5),
+)
+def test_scalar_reduction_with_columns(
+    request: Any, constructor: Any, expr: list[Any], expected: dict[str, list[Any]]
+) -> None:
+    if "dask" in str(constructor) and int(request.node.callspec.id[-1]) != 1:
         request.applymarker(pytest.mark.xfail)
 
     data = {"a": [1, 2, 3], "b": [4, 5, 6]}
     df = nw.from_native(constructor(data))
-    result = df.with_columns(
-        nw.col("a").min().alias("min"),
-        nw.col("b").max().alias("max"),
-        nw.col("a", "b").mean(),
-    )
-    expected = {"min": [1, 1, 1], "max": [6, 6, 6], "a": [2, 2, 2], "b": [5, 5, 5]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.with_columns((nw.col("a") + nw.col("b").max()).alias("x")).select("x")
-    expected = {"x": [7, 8, 9]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.with_columns(nw.col("a"), nw.col("b").min())
-    expected = {"a": [1, 2, 3], "b": [4, 4, 4]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.with_columns(nw.col("a").max(), nw.col("b"))
-    expected = {"a": [3, 3, 3], "b": [4, 5, 6]}
-    compare_dicts(result, expected)
-
-    df = nw.from_native(constructor(data))
-    result = df.with_columns(nw.col("a"), nw.col("b").min().alias("min")).select(
-        "a", "min"
-    )
-    expected = {"a": [1, 2, 3], "min": [4, 4, 4]}
+    result = df.with_columns(*expr).select(*expected.keys())
     compare_dicts(result, expected)
