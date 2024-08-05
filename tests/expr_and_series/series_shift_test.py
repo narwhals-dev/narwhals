d646b839d6940aaf22d7b974b2c87afe07bd14e1
mfonekpo <mfonekpo34@gmail.com>
2024-08-05 18:23:00 +0100

    I hope this works finally

--- tests/expr_and_series/series_shift_test.py
+++ tests/expr_and_series/series_shift_test.py
@@ -37,12 +37,35 @@ def test_shift(constructor_eager: Any) -> None:
 def test_shift_multi_chunk_pyarrow() -> None:
     tbl = pa.table({"a": [1, 2, 3]})
     tbl = pa.concat_tables([tbl, tbl, tbl])
-    tbl = tbl.combine_chunks()
     df = nw.from_native(tbl, eager_only=True)
-    result_a = df.select(nw.col("a").shift(1))
-    result_b = df.select(nw.col("a").shift(-1))
-    expected_a = {"a": [None, 1, 2, 3, 1, 2, 3, 1, 2]}
-    expected_b = {"a": [2, 3, 1, 2, 3, 1, 2, 3, None]}
 
-    compare_dicts(result_a, expected_a)
-    compare_dicts(result_b, expected_b)
+    a_type = tbl.schema.field("a").type
+
+    # Shift right by 1
+    result_a = df.select(nw.col("a").shift(1))
+    # Prepend a null and exclude the last element
+    expected_a = pa.concat_arrays(
+        [pa.nulls(1, a_type)]
+        + [chunk.slice(0, len(chunk) - 1) for chunk in tbl.column(0).chunks]
+    )
+    expected_a_dict = {"a": expected_a.to_pylist()}
+
+    # Shift left by -1
+    result_b = df.select(nw.col("a").shift(-1))
+    # Exclude the first element and append a null
+    expected_b = pa.concat_arrays(
+        [chunk.slice(1, len(chunk) - 1) for chunk in tbl.column(0).chunks]
+        + [pa.nulls(1, a_type)]
+    )
+    expected_b_dict = {"a": expected_b.to_pylist()}
+
+    # when shift is 0
+    result_c = df.select(nw.col("a").shift(0))
+    expected_c = tbl.column(0)
+    expected_c_dict = {
+        "a": [item for chunk in expected_c.chunks for item in chunk.to_pylist()]
+    }
+
+    compare_dicts(result_a, expected_a_dict)
+    compare_dicts(result_b, expected_b_dict)
+    compare_dicts(result_c, expected_c_dict)
