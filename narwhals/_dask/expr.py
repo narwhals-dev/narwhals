3c6fa8c3133f39c49f4c7eac04d08c617bea33f9
Magdalena Kowalczuk <74981211+anopsy@users.noreply.github.com>
2024-08-08 15:55:49 +0200

    feat: add NotImplementedError error for index changing methods Dask (#744)
    * add notImplemented error for index changing methods Dask
    
    * add NotImplementedError to index changing methods Dask
    
    * add NotImplementedError in Dask drop_null/count
    
    * Update expr.py
    
    ---------
    
    Co-authored-by: Marco Edward Gorelli <marcogorelli@protonmail.com>

--- narwhals/_dask/expr.py
+++ narwhals/_dask/expr.py
@@ -4,6 +4,7 @@ from copy import copy
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import Callable
+from typing import NoReturn
 
 from narwhals.dependencies import get_dask
 
@@ -419,6 +420,16 @@ class DaskExpr:
             decimals,
             returns_scalar=False,
         )
+
+    def drop_nulls(self) -> NoReturn:
+        # We can't (yet?) allow methods which modify the index
+        msg = "`Expr.drop_nulls` is not supported for the Dask backend. Please use `LazyFrame.drop_nulls` instead."
+        raise NotImplementedError(msg)
+
+    def sort(self, *, descending: bool = False, nulls_last: bool = False) -> NoReturn:
+        # We can't (yet?) allow methods which modify the index
+        msg = "`Expr.sort` is not supported for the Dask backend. Please use `LazyFrame.sort` instead."
+        raise NotImplementedError(msg)
 
     def abs(self) -> Self:
         return self._from_call(
