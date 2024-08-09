0af19dcfb79d4872f274607c7ac6dff8875d66b0
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-08-09 19:54:08 +0100

    perf: remove some lambdas from _dask backend

--- narwhals/_dask/dataframe.py
+++ narwhals/_dask/dataframe.py
@@ -130,11 +130,10 @@ class DaskLazyFrame:
     def with_row_index(self: Self, name: str) -> Self:
         # Implementation is based on the following StackOverflow reply:
         # https://stackoverflow.com/questions/60831518/in-dask-how-does-one-add-a-range-of-integersauto-increment-to-a-new-column/60852409#60852409
-        return self._from_native_dataframe(
-            self._native_dataframe.assign(**{name: 1}).assign(
-                **{name: lambda t: t[name].cumsum(method="blelloch") - 1}
-            )
-        )
+        df = self._native_dataframe
+        df = df.assign(**{name: 1})
+        df = df.assign(**{name: df[name].cumsum(method="blelloch") - 1})
+        return self._from_native_dataframe(df)
 
     def rename(self: Self, mapping: dict[str, str]) -> Self:
         return self._from_native_dataframe(self._native_dataframe.rename(columns=mapping))
@@ -156,14 +155,9 @@ class DaskLazyFrame:
         if keep == "none":
             subset = subset or self.columns
             token = generate_unique_token(n_bytes=8, columns=subset)
-            unique = (
-                native_frame.groupby(subset)
-                .size()
-                .rename(token)
-                .loc[lambda t: t == 1]
-                .reset_index()
-                .drop(columns=token)
-            )
+            ser = native_frame.groupby(subset).size().rename(token)
+            ser = ser.loc[ser == 1]
+            unique = ser.reset_index().drop(columns=token)
             result = native_frame.merge(unique, on=subset, how="inner")
         else:
             mapped_keep = {"any": "first"}.get(keep, keep)
@@ -224,16 +218,15 @@ class DaskLazyFrame:
                 )
                 .drop_duplicates()
             )
+            df = self._native_dataframe.merge(
+                other_native,
+                how="outer",
+                indicator=indicator_token,
+                left_on=left_on,
+                right_on=left_on,
+            )
             return self._from_native_dataframe(
-                self._native_dataframe.merge(
-                    other_native,
-                    how="outer",
-                    indicator=indicator_token,
-                    left_on=left_on,
-                    right_on=left_on,
-                )
-                .loc[lambda t: t[indicator_token] == "left_only"]
-                .drop(columns=[indicator_token])
+                df.loc[df[indicator_token] == "left_only"].drop(columns=[indicator_token])
             )
 
         if how == "semi":
