bf1fdcb675da65e7f34c9f6cf24a17b65ab66dc5
Luciano Scarpulla <scarpulla53@gmail.com>
2024-08-10 19:15:12 +0200

    test: refactor

--- tests/expr_and_series/str/replace_test.py
+++ tests/expr_and_series/str/replace_test.py
@@ -5,30 +5,52 @@ from typing import Any
 import pytest
 
 import narwhals.stable.v1 as nw
+from tests.utils import compare_dicts
+
+replace_data = [
+    (
+        {"a": ["123abc", "abc456"]},
+        r"abc\b",
+        "ABC",
+        1,
+        False,
+        {"a": ["123ABC", "abc456"]},
+    ),
+    ({"a": ["abc abc", "abc456"]}, r"abc", "", 1, False, {"a": [" abc", "456"]}),
+    ({"a": ["abc abc abc", "456abc"]}, r"abc", "", -1, False, {"a": ["  ", "456"]}),
+    (
+        {"a": ["Dollar $ign", "literal"]},
+        r"$",
+        "S",
+        -1,
+        True,
+        {"a": ["Dollar Sign", "literal"]},
+    ),
+]
+
+replace_all_data = [
+    (
+        {"a": ["123abc", "abc456"]},
+        r"abc\b",
+        "ABC",
+        False,
+        {"a": ["123ABC", "abc456"]},
+    ),
+    ({"a": ["abc abc", "abc456"]}, r"abc", "", False, {"a": [" ", "456"]}),
+    ({"a": ["abc abc abc", "456abc"]}, r"abc", "", False, {"a": ["  ", "456"]}),
+    (
+        {"a": ["Dollar $ign", "literal"]},
+        r"$",
+        "S",
+        True,
+        {"a": ["Dollar Sign", "literal"]},
+    ),
+]
 
 
 @pytest.mark.parametrize(
     ("data", "pattern", "value", "n", "literal", "expected"),
-    [
-        (
-            {"a": ["123abc", "abc456"]},
-            r"abc\b",
-            "ABC",
-            1,
-            False,
-            {"a": ["123ABC", "abc456"]},
-        ),
-        ({"a": ["abc abc", "abc456"]}, r"abc", "", 1, False, {"a": [" abc", "456"]}),
-        ({"a": ["abc abc abc", "456abc"]}, r"abc", "", -1, False, {"a": ["  ", "456"]}),
-        (
-            {"a": ["Dollar $ign", "literal"]},
-            r"$",
-            "S",
-            -1,
-            True,
-            {"a": ["Dollar Sign", "literal"]},
-        ),
-    ],
+    replace_data,
 )
 def test_str_replace_series(
     constructor_eager: Any,
@@ -49,24 +71,7 @@ def test_str_replace_series(
 
 @pytest.mark.parametrize(
     ("data", "pattern", "value", "literal", "expected"),
-    [
-        (
-            {"a": ["123abc", "abc456"]},
-            r"abc\b",
-            "ABC",
-            False,
-            {"a": ["123ABC", "abc456"]},
-        ),
-        ({"a": ["abc abc", "abc456"]}, r"abc", "", False, {"a": [" ", "456"]}),
-        ({"a": ["abc abc abc", "456abc"]}, r"abc", "", False, {"a": ["  ", "456"]}),
-        (
-            {"a": ["Dollar $ign", "literal"]},
-            r"$",
-            "S",
-            True,
-            {"a": ["Dollar Sign", "literal"]},
-        ),
-    ],
+    replace_all_data,
 )
 def test_str_replace_all_series(
     constructor_eager: Any,
@@ -84,29 +89,10 @@ def test_str_replace_all_series(
 
 @pytest.mark.parametrize(
     ("data", "pattern", "value", "n", "literal", "expected"),
-    [
-        (
-            {"a": ["123abc", "abc456"]},
-            r"abc\b",
-            "ABC",
-            1,
-            False,
-            {"a": ["123ABC", "abc456"]},
-        ),
-        ({"a": ["abc abc", "abc456"]}, r"abc", "", 1, False, {"a": [" abc", "456"]}),
-        ({"a": ["abc abc abc", "456abc"]}, r"abc", "", -1, False, {"a": ["  ", "456"]}),
-        (
-            {"a": ["Dollar $ign", "literal"]},
-            r"$",
-            "S",
-            -1,
-            True,
-            {"a": ["Dollar Sign", "literal"]},
-        ),
-    ],
+    replace_data,
 )
 def test_str_replace_expr(
-    constructor_eager: Any,
+    constructor: Any,
     data: dict[str, list[str]],
     pattern: str,
     value: str,
@@ -114,46 +100,31 @@ def test_str_replace_expr(
     literal: bool,  # noqa: FBT001
     expected: dict[str, list[str]],
 ) -> None:
-    df = nw.from_native(constructor_eager(data), eager_only=True)
+    df = nw.from_native(constructor(data))
 
-    result_series = df.select(
+    result_df = df.select(
         nw.col("a").str.replace(pattern=pattern, value=value, n=n, literal=literal)
     )
-    assert result_series["a"].to_list() == expected["a"]
+    compare_dicts(result_df, expected)
 
 
 @pytest.mark.parametrize(
     ("data", "pattern", "value", "literal", "expected"),
-    [
-        (
-            {"a": ["123abc", "abc456"]},
-            r"abc\b",
-            "ABC",
-            False,
-            {"a": ["123ABC", "abc456"]},
-        ),
-        ({"a": ["abc abc", "abc456"]}, r"abc", "", False, {"a": [" ", "456"]}),
-        ({"a": ["abc abc abc", "456abc"]}, r"abc", "", False, {"a": ["  ", "456"]}),
-        (
-            {"a": ["Dollar $ign", "literal"]},
-            r"$",
-            "S",
-            True,
-            {"a": ["Dollar Sign", "literal"]},
-        ),
-    ],
+    replace_all_data,
 )
 def test_str_replace_all_expr(
-    constructor_eager: Any,
+    constructor: Any,
     data: dict[str, list[str]],
     pattern: str,
     value: str,
     literal: bool,  # noqa: FBT001
     expected: dict[str, list[str]],
 ) -> None:
-    df = nw.from_native(constructor_eager(data), eager_only=True)
+    df = nw.from_native(constructor(data))
 
-    result_series = df.select(
-        nw.col("a").str.replace_all(pattern=pattern, value=value, literal=literal)
+    result_df = nw.to_native(
+        df.select(
+            nw.col("a").str.replace_all(pattern=pattern, value=value, literal=literal)
+        )
     )
-    assert result_series["a"].to_list() == expected["a"]
+    compare_dicts(result_df, expected)
