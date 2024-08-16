89fb6d70f1069c95683621040110dd355e2d3182
Zhengbo Wang <lw7205675@gmail.com>
2024-08-16 11:47:55 +0800

    test: Improve `duration_attribute_tests`
    - Add constructor_lazy and ensure it fails as expected.
    - Combine two separate tests into one.

--- tests/expr_and_series/dt/datetime_duration_test.py
+++ tests/expr_and_series/dt/datetime_duration_test.py
@@ -23,79 +23,54 @@ data = {
         timedelta(milliseconds=1, microseconds=300),
     ],
     "c": np.array([None, 20], dtype="timedelta64[ns]"),
-    "d": [
-        timedelta(milliseconds=2),
-        timedelta(seconds=1),
-    ],
 }
 
 
 @pytest.mark.parametrize(
-    ("attribute", "expected_a", "expected_b"),
+    ("attribute", "expected_a", "expected_b", "expected_c"),
     [
-        ("total_minutes", [0, 1], [0, 0]),
-        ("total_seconds", [0, 61], [0, 0]),
-        ("total_milliseconds", [0, 61001], [2, 1]),
+        ("total_minutes", [0, 1], [0, 0], None),
+        ("total_seconds", [0, 61], [0, 0], None),
+        ("total_milliseconds", [0, 61001], [2, 1], None),
+        ("total_microseconds", None, [2000, 1300], [0, 0]),
+        ("total_nanoseconds", None, [2000000, 1300000], [0, 20]),
     ],
 )
 def test_duration_attributes(
     request: Any,
-    constructor_eager: Any,
+    constructor: Any,
     attribute: str,
-    expected_a: list[int],
-    expected_b: list[int],
+    expected_a: list[int] | None,
+    expected_b: list[int] | None,
+    expected_c: list[int] | None,
 ) -> None:
-    if parse_version(pd.__version__) < (2, 2) and "pandas_pyarrow" in str(
-        constructor_eager
+    if "lazy" in str(constructor) or (
+        parse_version(pd.__version__) < (2, 2) and "pandas_pyarrow" in str(constructor)
     ):
         request.applymarker(pytest.mark.xfail)
 
-    df = nw.from_native(constructor_eager(data), eager_only=True)
-    result_a = df.select(getattr(nw.col("a").dt, attribute)().fill_null(0))
-    compare_dicts(result_a, {"a": expected_a})
+    df = nw.from_native(constructor(data), eager_only=True)
 
-    result_a = df.select(getattr(df["a"].dt, attribute)().fill_null(0))
-    compare_dicts(result_a, {"a": expected_a})
+    if expected_a is not None:
+        result_a = df.select(getattr(nw.col("a").dt, attribute)().fill_null(0))
+        compare_dicts(result_a, {"a": expected_a})
 
-    result_b = df.select(getattr(nw.col("b").dt, attribute)().fill_null(0))
-    compare_dicts(result_b, {"b": expected_b})
+        result_a = df.select(getattr(df["a"].dt, attribute)().fill_null(0))
+        compare_dicts(result_a, {"a": expected_a})
 
-    result_b = df.select(getattr(df["b"].dt, attribute)().fill_null(0))
-    compare_dicts(result_b, {"b": expected_b})
+    if expected_b is not None:
+        result_b = df.select(getattr(nw.col("b").dt, attribute)().fill_null(0))
+        compare_dicts(result_b, {"b": expected_b})
 
+        result_b = df.select(getattr(df["b"].dt, attribute)().fill_null(0))
+        compare_dicts(result_b, {"b": expected_b})
 
-@pytest.mark.parametrize(
-    ("attribute", "expected_b", "expected_c"),
-    [
-        ("total_microseconds", [2000, 1300], [0, 0]),
-        ("total_nanoseconds", [2000000, 1300000], [0, 20]),
-    ],
-)
-def test_duration_micro_nano(
-    request: Any,
-    constructor_eager: Any,
-    attribute: str,
-    expected_b: list[int],
-    expected_c: list[int],
-) -> None:
-    if parse_version(pd.__version__) < (2, 2) and "pandas_pyarrow" in str(
-        constructor_eager
-    ):
-        request.applymarker(pytest.mark.xfail)
+    if expected_c is not None:
+        result_c = df.select(getattr(nw.col("c").dt, attribute)().fill_null(0))
+        compare_dicts(result_c, {"c": expected_c})
 
-    df = nw.from_native(constructor_eager(data), eager_only=True)
-
-    result_b = df.select(getattr(nw.col("b").dt, attribute)().fill_null(0))
-    compare_dicts(result_b, {"b": expected_b})
-
-    result_b = df.select(getattr(df["b"].dt, attribute)().fill_null(0))
-    compare_dicts(result_b, {"b": expected_b})
-
-    result_c = df.select(getattr(nw.col("c").dt, attribute)().fill_null(0))
-    compare_dicts(result_c, {"c": expected_c})
-
-    result_c = df.select(getattr(df["c"].dt, attribute)().fill_null(0))
-    compare_dicts(result_c, {"c": expected_c})
+        result_c = df.select(getattr(df["c"].dt, attribute)().fill_null(0))
+        compare_dicts(result_c, {"c": expected_c})
 
 
 @pytest.mark.parametrize("unit", ["s", "ms", "us", "ns"])
