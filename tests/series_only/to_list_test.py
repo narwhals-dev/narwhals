165e875a453cc6d39402fd4f3b5239c980377851
Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>
2024-08-22 12:18:01 +0200

    test: remove `to_numpy` and `to_list` in tests (#842)
    * test: rm to_numpy and to_list
    
    * to_list test

--- tests/series_only/to_list_test.py
+++ tests/series_only/to_list_test.py
@@ -12,4 +12,4 @@ def test_to_list(constructor_eager: Any, request: Any) -> None:
     if "cudf" in str(constructor_eager):  # pragma: no cover
         request.applymarker(pytest.mark.xfail)
     s = nw.from_native(constructor_eager({"a": data}), eager_only=True)["a"]
-    compare_dicts({"a": s}, {"a": [1, 2, 3]})
+    compare_dicts({"a": s.to_list()}, {"a": [1, 2, 3]})
