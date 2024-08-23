2bc5cc0534bcdb491d0742e9cdaed53908deec64
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-08-23 21:56:13 +0100

    nw.when

--- tests/expr_and_series/when_test.py
+++ tests/expr_and_series/when_test.py
@@ -6,7 +6,6 @@ import numpy as np
 import pytest
 
 import narwhals.stable.v1 as nw
-from narwhals.stable.v1 import when
 from tests.utils import compare_dicts
 
 data = {
@@ -22,7 +21,7 @@ def test_when(request: Any, constructor: Any) -> None:
         request.applymarker(pytest.mark.xfail)
 
     df = nw.from_native(constructor(data))
-    result = df.select(when(nw.col("a") == 1).then(value=3).alias("a_when"))
+    result = df.select(nw.when(nw.col("a") == 1).then(value=3).alias("a_when"))
     expected = {
         "a_when": [3, np.nan, np.nan],
     }
@@ -34,7 +33,7 @@ def test_when_otherwise(request: Any, constructor: Any) -> None:
         request.applymarker(pytest.mark.xfail)
 
     df = nw.from_native(constructor(data))
-    result = df.select(when(nw.col("a") == 1).then(3).otherwise(6).alias("a_when"))
+    result = df.select(nw.when(nw.col("a") == 1).then(3).otherwise(6).alias("a_when"))
     expected = {
         "a_when": [3, 6, 6],
     }
@@ -46,7 +45,9 @@ def test_multiple_conditions(request: Any, constructor: Any) -> None:
         request.applymarker(pytest.mark.xfail)
 
     df = nw.from_native(constructor(data))
-    result = df.select(when(nw.col("a") < 3, nw.col("c") < 5.0).then(3).alias("a_when"))
+    result = df.select(
+        nw.when(nw.col("a") < 3, nw.col("c") < 5.0).then(3).alias("a_when")
+    )
     expected = {
         "a_when": [3, np.nan, np.nan],
     }
@@ -59,7 +60,7 @@ def test_no_arg_when_fail(request: Any, constructor: Any) -> None:
 
     df = nw.from_native(constructor(data))
     with pytest.raises((TypeError, ValueError)):
-        df.select(when().then(value=3).alias("a_when"))
+        df.select(nw.when().then(value=3).alias("a_when"))
 
 
 def test_value_numpy_array(request: Any, constructor: Any) -> None:
@@ -70,7 +71,7 @@ def test_value_numpy_array(request: Any, constructor: Any) -> None:
     import numpy as np
 
     result = df.select(
-        when(nw.col("a") == 1).then(np.asanyarray([3, 4, 5])).alias("a_when")
+        nw.when(nw.col("a") == 1).then(np.asanyarray([3, 4, 5])).alias("a_when")
     )
     expected = {
         "a_when": [3, np.nan, np.nan],
@@ -86,7 +87,7 @@ def test_value_series(request: Any, constructor_eager: Any) -> None:
     s_data = {"s": [3, 4, 5]}
     s = nw.from_native(constructor_eager(s_data))["s"]
     assert isinstance(s, nw.Series)
-    result = df.select(when(nw.col("a") == 1).then(s).alias("a_when"))
+    result = df.select(nw.when(nw.col("a") == 1).then(s).alias("a_when"))
     expected = {
         "a_when": [3, np.nan, np.nan],
     }
@@ -98,7 +99,7 @@ def test_value_expression(request: Any, constructor: Any) -> None:
         request.applymarker(pytest.mark.xfail)
 
     df = nw.from_native(constructor(data))
-    result = df.select(when(nw.col("a") == 1).then(nw.col("a") + 9).alias("a_when"))
+    result = df.select(nw.when(nw.col("a") == 1).then(nw.col("a") + 9).alias("a_when"))
     expected = {
         "a_when": [10, np.nan, np.nan],
     }
@@ -113,7 +114,7 @@ def test_otherwise_numpy_array(request: Any, constructor: Any) -> None:
     import numpy as np
 
     result = df.select(
-        when(nw.col("a") == 1)
+        nw.when(nw.col("a") == 1)
         .then(-1)
         .otherwise(np.asanyarray([0, 9, 10]))
         .alias("a_when")
@@ -132,7 +133,7 @@ def test_otherwise_series(request: Any, constructor_eager: Any) -> None:
     s_data = {"s": [0, 9, 10]}
     s = nw.from_native(constructor_eager(s_data))["s"]
     assert isinstance(s, nw.Series)
-    result = df.select(when(nw.col("a") == 1).then(-1).otherwise(s).alias("a_when"))
+    result = df.select(nw.when(nw.col("a") == 1).then(-1).otherwise(s).alias("a_when"))
     expected = {
         "a_when": [-1, 9, 10],
     }
@@ -145,7 +146,7 @@ def test_otherwise_expression(request: Any, constructor: Any) -> None:
 
     df = nw.from_native(constructor(data))
     result = df.select(
-        when(nw.col("a") == 1).then(-1).otherwise(nw.col("a") + 7).alias("a_when")
+        nw.when(nw.col("a") == 1).then(-1).otherwise(nw.col("a") + 7).alias("a_when")
     )
     expected = {
         "a_when": [-1, 9, 10],
