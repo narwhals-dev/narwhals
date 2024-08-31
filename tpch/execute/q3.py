afb061661d012e7865b1b41a3342e1c1812ec701
Zhengbo Wang <lw7205675@gmail.com>
2024-08-31 23:38:35 +0800

    remove pandas-native and ibis

--- tpch/execute/q3.py
+++ tpch/execute/q3.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import ibis
 import pandas as pd
 import polars as pl
 from queries import q3
@@ -8,9 +7,6 @@ from queries import q3
 pd.options.mode.copy_on_write = True
 pd.options.future.infer_string = True
 
-
-con_pd = ibis.pandas.connect()
-con_pl = ibis.polars.connect()
 
 customer = Path("data") / "customer.parquet"
 lineitem = Path("data") / "lineitem.parquet"
@@ -21,25 +17,10 @@ IO_FUNCS = {
     "pandas[pyarrow]": lambda x: pd.read_parquet(
         x, engine="pyarrow", dtype_backend="pyarrow"
     ),
-    "pandas[pyarrow][ibis]": lambda x: con_pd.read_parquet(
-        x, engine="pyarrow", dtype_backend="pyarrow"
-    ),
     "polars[eager]": lambda x: pl.read_parquet(x),
     "polars[lazy]": lambda x: pl.scan_parquet(x),
-    "polars[lazy][ibis]": lambda x: con_pl.read_parquet(x),
 }
 
-tool = "pandas[pyarrow][ibis]"
-fn = IO_FUNCS[tool]
-print(q3.query_ibis(fn(customer), fn(lineitem), fn(orders), tool="pandas"))
-
-tool = "polars[lazy][ibis]"
-fn = IO_FUNCS[tool]
-print(q3.query_ibis(fn(customer), fn(lineitem), fn(orders), tool="polars"))
-
-tool = "pandas[pyarrow]"
-fn = IO_FUNCS[tool]
-print(q3.query_pandas_native(fn(customer), fn(lineitem), fn(orders)))
 
 tool = "pandas"
 fn = IO_FUNCS[tool]
--- tpch/queries/q3.py
+++ tpch/queries/q3.py
@@ -1,38 +1,7 @@
-from datetime import date
 from datetime import datetime
 from typing import Any
 
-import ibis
-
 import narwhals as nw
-
-
-def query_pandas_native(
-    customer_ds: Any,
-    line_item_ds: Any,
-    orders_ds: Any,
-) -> Any:
-    var1 = "BUILDING"
-    var2 = date(1995, 3, 15)
-
-    fcustomer = customer_ds[customer_ds["c_mktsegment"] == var1]
-
-    jn1 = fcustomer.merge(orders_ds, left_on="c_custkey", right_on="o_custkey")
-    jn2 = jn1.merge(line_item_ds, left_on="o_orderkey", right_on="l_orderkey")
-
-    jn2 = jn2[jn2["o_orderdate"] < var2]
-    jn2 = jn2[jn2["l_shipdate"] > var2]
-    jn2["revenue"] = jn2.l_extendedprice * (1 - jn2.l_discount)
-
-    gb = jn2.groupby(["o_orderkey", "o_orderdate", "o_shippriority"], as_index=False)
-    agg = gb["revenue"].sum()
-
-    sel = agg.loc[:, ["o_orderkey", "revenue", "o_orderdate", "o_shippriority"]]
-    sel = sel.rename({"o_orderkey": "l_orderkey"}, axis="columns")
-
-    sorted = sel.sort_values(by=["revenue", "o_orderdate"], ascending=[False, True])
-
-    return sorted.head(10)  # type: ignore[no-any-return]
 
 
 def query(
@@ -73,43 +42,3 @@ def query(
     )
 
     return nw.to_native(q_final)
-
-
-def query_ibis(
-    customer: Any,
-    lineitem: Any,
-    orders: Any,
-    *,
-    tool: str,
-) -> Any:
-    var1 = "BUILDING"
-    var2 = date(1995, 3, 15)
-
-    q_final = (
-        customer.filter(customer["c_mktsegment"] == var1)
-        .join(orders, customer["c_custkey"] == orders["o_custkey"])
-        .join(lineitem, orders["o_orderkey"] == lineitem["l_orderkey"])
-        .filter(ibis._["o_orderdate"] < var2)
-        .filter(ibis._["l_shipdate"] > var2)
-        .mutate(revenue=(lineitem["l_extendedprice"] * (1 - lineitem["l_discount"])))
-        .group_by(
-            "o_orderkey",
-            "o_orderdate",
-            "o_shippriority",
-        )
-        .agg(revenue=ibis._["revenue"].sum())
-        .select(
-            ibis._["o_orderkey"].name("o_orderkey"),
-            "revenue",
-            "o_orderdate",
-            "o_shippriority",
-        )
-        .order_by(ibis.desc("revenue"), "o_orderdate")
-        .limit(10)
-    )
-    if tool == "pandas":
-        return q_final.to_pandas()
-    if tool == "polars":
-        return q_final.to_polars()
-    msg = "expected pandas or polars"
-    raise ValueError(msg)
