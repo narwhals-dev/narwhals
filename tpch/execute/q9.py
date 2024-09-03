fa7bae410d3715b0cf40212cee82d5b525e15437
Zhengbo Wang <lw7205675@gmail.com>
2024-09-03 09:34:09 +0800

    Add q9 of tpch

---
+++ tpch/execute/q9.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import pandas as pd
+import polars as pl
+from queries import q9
+
+part = Path("data") / "part.parquet"
+partsupp = Path("data") / "partsupp.parquet"
+nation = Path("data") / "nation.parquet"
+lineitem = Path("data") / "lineitem.parquet"
+orders = Path("data") / "orders.parquet"
+supplier = Path("data") / "supplier.parquet"
+IO_FUNCS = {
+    "pandas": lambda x: pd.read_parquet(x, engine="pyarrow"),
+    "pandas[pyarrow]": lambda x: pd.read_parquet(
+        x, engine="pyarrow", dtype_backend="pyarrow"
+    ),
+    "polars[eager]": lambda x: pl.read_parquet(x),
+    "polars[lazy]": lambda x: pl.scan_parquet(x),
+}
+
+tool = "pandas"
+fn = IO_FUNCS[tool]
+print(
+    q9.query(fn(part), fn(partsupp), fn(nation), fn(lineitem), fn(orders), fn(supplier))
+)
+
+tool = "pandas[pyarrow]"
+fn = IO_FUNCS[tool]
+print(
+    q9.query(fn(part), fn(partsupp), fn(nation), fn(lineitem), fn(orders), fn(supplier))
+)
+
+tool = "polars[eager]"
+fn = IO_FUNCS[tool]
+print(
+    q9.query(fn(part), fn(partsupp), fn(nation), fn(lineitem), fn(orders), fn(supplier))
+)
+
+tool = "polars[lazy]"
+fn = IO_FUNCS[tool]
+print(
+    q9.query(
+        fn(part), fn(partsupp), fn(nation), fn(lineitem), fn(orders), fn(supplier)
+    ).collect()
+)
---
+++ tpch/queries/q9.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+import narwhals as nw
+from narwhals.typing import FrameT
+
+pd.options.mode.copy_on_write = True
+pd.options.future.infer_string = True
+
+
+@nw.narwhalify
+def query(
+    part_ds: FrameT,
+    partsupp_ds: FrameT,
+    nation_ds: FrameT,
+    lineitem_ds: FrameT,
+    orders_ds: FrameT,
+    supplier_ds: FrameT,
+) -> FrameT:
+    return (
+        part_ds.join(partsupp_ds, left_on="p_partkey", right_on="ps_partkey")
+        .join(supplier_ds, left_on="ps_suppkey", right_on="s_suppkey")
+        .join(
+            lineitem_ds,
+            left_on=["p_partkey", "ps_suppkey"],
+            right_on=["l_partkey", "l_suppkey"],
+        )
+        .join(orders_ds, left_on="l_orderkey", right_on="o_orderkey")
+        .join(nation_ds, left_on="s_nationkey", right_on="n_nationkey")
+        .filter(nw.col("p_name").str.contains("green"))
+        .select(
+            nw.col("n_name").alias("nation"),
+            nw.col("o_orderdate").dt.year().alias("o_year"),
+            (
+                nw.col("l_extendedprice") * (1 - nw.col("l_discount"))
+                - nw.col("ps_supplycost") * nw.col("l_quantity")
+            ).alias("amount"),
+        )
+        .group_by("nation", "o_year")
+        .agg(nw.sum("amount").alias("sum_profit"))
+        .sort(by=["nation", "o_year"], descending=[False, True])
+    )
