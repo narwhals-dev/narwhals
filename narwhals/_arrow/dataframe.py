cbcc1c5d937bc05fa38d72168285d35a7493eb5f
raisadz <34237447+raisadz@users.noreply.github.com>
2024-09-08 11:32:58 +0100

    remove convertion of string keys to list

--- narwhals/_arrow/dataframe.py
+++ narwhals/_arrow/dataframe.py
@@ -275,11 +275,6 @@ class ArrowDataFrame:
         left_on: str | list[str] | None = None,
         right_on: str | list[str] | None = None,
     ) -> Self:
-        if isinstance(left_on, str):
-            left_on = [left_on]
-        if isinstance(right_on, str):
-            right_on = [right_on]
-
         how_to_join_map = {
             "anti": "left anti",
             "semi": "left semi",
