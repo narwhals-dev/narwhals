f6d3f57f9ae40de84d1ca38c26bae277fb040826
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-09-11 10:11:53 +0100

    xfail modin

--- tests/series_only/scatter_test.py
+++ tests/series_only/scatter_test.py
@@ -1,10 +1,17 @@
+from __future__ import annotations
+
 from typing import Any
+
+import pytest
 
 import narwhals as nw
 from tests.utils import compare_dicts
 
 
-def test_scatter(constructor_eager: Any) -> None:
+def test_scatter(constructor_eager: Any, request: pytest.FixtureRequest) -> None:
+    if "modin" in str(constructor_eager):
+        # https://github.com/modin-project/modin/issues/7392
+        request.applymarker(pytest.mark.xfail)
     df = nw.from_native(
         constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
     )
