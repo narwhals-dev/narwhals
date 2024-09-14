644393e80c00046c6e73ae2653d5402b5f7140b6
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-09-14 17:15:38 +0200

    ci: add codspeed for tpch queries

--- tpch/tests/test_queries.py
+++ tpch/tests/test_queries.py
@@ -1,16 +1,23 @@
+from __future__ import annotations
+
 import subprocess
 import sys
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytest_codspeed.plugin import BenchmarkFixture
 
 
-def test_execute_scripts() -> None:
+def test_execute_scripts(benchmark: BenchmarkFixture) -> None:
     root = Path(__file__).resolve().parent.parent
     # directory containing all the queries
     execute_dir = root / "execute"
 
     for script_path in execute_dir.glob("q[1-9]*.py"):
         print(f"executing query {script_path.stem}")  # noqa: T201
-        result = subprocess.run(  # noqa: S603
+        result = benchmark(
+            subprocess.run,
             [sys.executable, "-m", f"execute.{script_path.stem}"],
             capture_output=True,
             text=True,
