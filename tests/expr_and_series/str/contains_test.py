f8ae4504e8b8ead7acf7a68736303ac5bdad0699
Liam Connors <connorsl@tcd.ie>
2024-09-15 19:05:49 -0400

    xfail case insensitive tests for cuDF

--- tests/expr_and_series/str/contains_test.py
+++ tests/expr_and_series/str/contains_test.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import pandas as pd
 import polars as pl
+import pytest
 
 import narwhals.stable.v1 as nw
 from tests.utils import Constructor
@@ -13,7 +14,10 @@ df_pandas = pd.DataFrame(data)
 df_polars = pl.DataFrame(data)
 
 
-def test_contains(constructor: Constructor) -> None:
+def test_contains(constructor: Constructor, request: pytest.FixtureRequest) -> None:
+    if "cudf" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+
     df = nw.from_native(constructor(data))
     result = df.with_columns(
         nw.col("pets").str.contains("(?i)parrot|Dove").alias("result")
@@ -25,7 +29,10 @@ def test_contains(constructor: Constructor) -> None:
     compare_dicts(result, expected)
 
 
-def test_contains_series(constructor_eager: Any) -> None:
+def test_contains_series(constructor_eager: Any, request: pytest.FixtureRequest) -> None:
+    if "cudf" in str(constructor_eager):
+        request.applymarker(pytest.mark.xfail)
+
     df = nw.from_native(constructor_eager(data), eager_only=True)
     result = df.with_columns(
         case_insensitive_match=df["pets"].str.contains("(?i)parrot|Dove")
