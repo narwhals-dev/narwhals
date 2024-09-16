994bb07c8d577aae1f011ed0e06a8ff31181cf74
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-09-16 13:19:14 +0200

    almost there with tests

--- tests/expr_and_series/sort_test.py
+++ tests/expr_and_series/sort_test.py
@@ -3,6 +3,7 @@ from typing import Any
 import pytest
 
 import narwhals.stable.v1 as nw
+from tests.utils import compare_dicts
 
 data = {"a": [0, 0, 2, -1], "b": [1, 3, 2, None]}
 
@@ -10,34 +11,27 @@ data = {"a": [0, 0, 2, -1], "b": [1, 3, 2, None]}
 @pytest.mark.parametrize(
     ("descending", "nulls_last", "expected"),
     [
-        (True, True, {"b": [3, 2, 1, None]}),
-        (True, False, {"b": [None, 3, 2, 1]}),
-        (False, True, {"b": [1, 2, 3, None]}),
-        (False, False, {"b": [None, 1, 2, 3]}),
+        (True, True, {"b": [3, 2, 1, float("nan")]}),
+        (True, False, {"b": [float("nan"), 3, 2, 1]}),
+        (False, True, {"b": [1, 2, 3, float("nan")]}),
+        (False, False, {"b": [float("nan"), 1, 2, 3]}),
     ],
 )
 def test_sort_single_expr(
     constructor: Any, descending: Any, nulls_last: Any, expected: Any
 ) -> None:
-    df = nw.from_native(constructor(data)).lazy()
-    result = nw.to_native(
-        df.select(
-            nw.col("b").sort(descending=descending, nulls_last=nulls_last),
-        ).collect()
-    )
-
-    expected_df = nw.to_native(nw.from_native(constructor(expected)).lazy().collect())
-    result = nw.maybe_align_index(result, expected_df)
-    assert result.equals(expected_df)
+    df = nw.from_native(constructor(data))
+    result = df.select(nw.col("b").sort(descending=descending, nulls_last=nulls_last))
+    compare_dicts(result, expected)
 
 
 @pytest.mark.parametrize(
     ("descending", "nulls_last", "expected"),
     [
-        (True, True, {"a": [0, 0, 2, -1], "b": [3, 2, 1, None]}),
-        (True, False, {"a": [0, 0, 2, -1], "b": [None, 3, 2, 1]}),
-        (False, True, {"a": [0, 0, 2, -1], "b": [1, 2, 3, None]}),
-        (False, False, {"a": [0, 0, 2, -1], "b": [None, 1, 2, 3]}),
+        (True, True, {"a": [0, 0, 2, -1], "b": [3, 2, 1, float("nan")]}),
+        (True, False, {"a": [0, 0, 2, -1], "b": [float("nan"), 3, 2, 1]}),
+        (False, True, {"a": [0, 0, 2, -1], "b": [1, 2, 3, float("nan")]}),
+        (False, False, {"a": [0, 0, 2, -1], "b": [float("nan"), 1, 2, 3]}),
     ],
 )
 def test_sort_multiple_expr(
@@ -46,16 +40,12 @@ def test_sort_multiple_expr(
     if "dask" in str(constructor):
         request.applymarker(pytest.mark.xfail)
 
-    df = nw.from_native(constructor(data)).lazy()
-    result = nw.to_native(
-        df.select(
-            "a",
-            nw.col("b").sort(descending=descending, nulls_last=nulls_last),
-        ).collect()
+    df = nw.from_native(constructor(data))
+    result = df.select(
+        "a",
+        nw.col("b").sort(descending=descending, nulls_last=nulls_last),
     )
-
-    expected_df = nw.to_native(nw.from_native(constructor(expected)).lazy().collect())
-    assert result.equals(expected_df)
+    compare_dicts(result, expected)
 
 
 @pytest.mark.parametrize(
