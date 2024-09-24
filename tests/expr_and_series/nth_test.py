ebbd4d7a04a5cc9efd2f45018ff666600ceaf073
Cheukting <cheukting.ho@gmail.com>
2024-09-24 10:27:38 +0100

    refactor tests

--- tests/expr_and_series/nth_test.py
+++ tests/expr_and_series/nth_test.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import polars as pl
 import pytest
 
 import narwhals.stable.v1 as nw
-from narwhals.dependencies import get_polars
+from narwhals.utils import parse_version
 from tests.utils import Constructor
 from tests.utils import compare_dicts
 
@@ -18,21 +19,43 @@ data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.1, 8, 9]}
         ([0, 2], {"a": [1, 3, 2], "z": [7.1, 8, 9]}),
     ],
 )
-def test_nth(
-    constructor: Constructor, idx: int | list[int], expected: dict[str, list[int]]
+def test_nth_not_polars(
+    not_polars_constructor: Constructor,
+    idx: int | list[int],
+    expected: dict[str, list[int]],
 ) -> None:
-    df = nw.from_native(constructor(data))
-    pl = get_polars()
-    is_polars = isinstance(df.to_native(), (pl.DataFrame, pl.LazyFrame))
-    if pl.build_info().get("version") is not None:
-        pl_version = tuple(int(i) for i in pl.build_info()["version"].split("."))
-    else:
-        pl_version = (0, 0, 0)
-    if is_polars and pl_version < (0, 20, 26):
-        with pytest.raises(
-            AttributeError, match="`nth` is only supported for Polars>=0.20.26."
-        ):
-            df.select(nw.nth(idx))
-    else:
-        result = df.select(nw.nth(idx))
-        compare_dicts(result, expected)
+    df = nw.from_native(not_polars_constructor(data))
+    result = df.select(nw.nth(idx))
+    compare_dicts(result, expected)
+
+
+@pytest.mark.xfail(
+    parse_version(pl.__version__) < parse_version("0.20.26"),
+    reason="nth not supported for Polars versions < 0.20.26",
+)
+@pytest.mark.parametrize(
+    ("idx", "expected"),
+    [
+        (0, {"a": [1, 3, 2]}),
+        ([0, 1], {"a": [1, 3, 2], "b": [4, 4, 6]}),
+        ([0, 2], {"a": [1, 3, 2], "z": [7.1, 8, 9]}),
+    ],
+)
+def test_nth_polars(
+    polar_constructor: Constructor, idx: int | list[int], expected: dict[str, list[int]]
+) -> None:
+    df = nw.from_native(polar_constructor(data))
+    result = df.select(nw.nth(idx))
+    compare_dicts(result, expected)
+
+
+@pytest.mark.skipif(
+    parse_version(pl.__version__) >= parse_version("0.20.26"),
+    reason="only riase error for Polars versions < 0.20.26",
+)
+def test_nth_not_supported() -> None:
+    df = nw.from_native(pl.DataFrame(data))
+    with pytest.raises(
+        AttributeError, match="`nth` is only supported for Polars>=0.20.26."
+    ):
+        df.select(nw.nth(0))
--- tests/translate/select_test.py
+++ tests/translate/select_test.py
@@ -7,6 +7,7 @@ import polars as pl
 import pytest
 
 import narwhals.stable.v1 as nw
+from narwhals.utils import parse_version
 
 if TYPE_CHECKING:
     from narwhals.typing import IntoDataFrameT
@@ -32,18 +33,17 @@ def test_nth(idx: int | list[int], expected: dict[str, list[int]]) -> None:
     pd.testing.assert_frame_equal(result, pd.DataFrame(expected))
 
 
+@pytest.mark.skipif(
+    parse_version(pl.__version__) >= parse_version("0.20.26"),
+    reason="only riase error for Polars versions < 0.20.26",
+)
 def test_nth_old_polars() -> None:
     @nw.narwhalify
     def func(df: nw.DataFrame[IntoDataFrameT]) -> nw.DataFrame[IntoDataFrameT]:
         return df.select(nw.nth(0))
 
     df = pl.DataFrame(data)
-    if pl.build_info().get("version") is not None:
-        pl_version = tuple(int(i) for i in pl.build_info()["version"].split("."))
-    else:
-        pl_version = (0, 0, 0)
-    if pl_version < (0, 20, 26):
-        with pytest.raises(
-            AttributeError, match="`nth` is only supported for Polars>=0.20.26."
-        ):
-            func(df)
+    with pytest.raises(
+        AttributeError, match="`nth` is only supported for Polars>=0.20.26."
+    ):
+        func(df)
