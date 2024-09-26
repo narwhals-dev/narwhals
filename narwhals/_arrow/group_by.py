8c9246ea4b13e61fcc3bdca2b896075e9276d7f4
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-09-26 22:53:46 +0200

    fix: pyarrow group_by unique

--- narwhals/_arrow/group_by.py
+++ narwhals/_arrow/group_by.py
@@ -20,6 +20,7 @@ POLARS_TO_ARROW_AGGREGATIONS = {
     "n_unique": "count_distinct",
     "std": "stddev",
     "var": "variance",  # currently unused, we don't have `var` yet
+    "unique": "distinct",
 }
 
 
@@ -32,6 +33,7 @@ def get_function_name_option(function_name: str) -> Any | None:
         "count_distinct": pc.CountOptions(mode="all"),
         "stddev": pc.VarianceOptions(ddof=1),
         "variance": pc.VarianceOptions(ddof=1),
+        "distinct": pc.CountOptions(mode="all"),
     }
     return function_name_to_options.get(function_name)
 
