76c8e8eaedfb69d26ade543c986f2dcb551e8bdd
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-09-27 21:40:41 +0200

    release: Bump version to 1.8.4 (#1084)

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -57,7 +57,7 @@ from narwhals.utils import maybe_convert_dtypes
 from narwhals.utils import maybe_get_index
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.8.3"
+__version__ = "1.8.4"
 
 __all__ = [
     "dependencies",
