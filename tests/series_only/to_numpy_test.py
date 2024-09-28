fbcdf1c09778d02a484ed2edbb45b61e005f873c
Liam Connors <connorsl@tcd.ie>
2024-09-28 14:50:22 -0400

    xfail to_numpy test with nulls

--- tests/series_only/to_numpy_test.py
+++ tests/series_only/to_numpy_test.py
@@ -11,7 +11,7 @@ import narwhals.stable.v1 as nw
 
 def test_to_numpy(constructor_eager: Any, request: pytest.FixtureRequest) -> None:
     if "pandas_constructor" in str(constructor_eager) or "modin_constructor" in str(
-        constructor_eager
+        constructor_eager or "cudf_constructor" in str(constructor_eager)
     ):
         request.applymarker(pytest.mark.xfail)
 
