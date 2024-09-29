d2b747b6e144ec51847196a12be441a2dc7f427d
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-09-29 18:41:53 +0100

    wip

--- narwhals/_dask/selectors.py
+++ narwhals/_dask/selectors.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING
 from typing import Any
 from typing import NoReturn
 
-from narwhals import dtypes
 from narwhals._dask.expr import DaskExpr
 
 if TYPE_CHECKING:
@@ -41,27 +40,27 @@ class DaskSelectorNamespace:
     def numeric(self: Self) -> DaskSelector:
         return self.by_dtype(
             [
-                dtypes.Int64,
-                dtypes.Int32,
-                dtypes.Int16,
-                dtypes.Int8,
-                dtypes.UInt64,
-                dtypes.UInt32,
-                dtypes.UInt16,
-                dtypes.UInt8,
-                dtypes.Float64,
-                dtypes.Float32,
+                self._dtypes.Int64,
+                self._dtypes.Int32,
+                self._dtypes.Int16,
+                self._dtypes.Int8,
+                self._dtypes.UInt64,
+                self._dtypes.UInt32,
+                self._dtypes.UInt16,
+                self._dtypes.UInt8,
+                self._dtypes.Float64,
+                self._dtypes.Float32,
             ],
         )
 
     def categorical(self: Self) -> DaskSelector:
-        return self.by_dtype([dtypes.Categorical])
+        return self.by_dtype([self._dtypes.Categorical])
 
     def string(self: Self) -> DaskSelector:
-        return self.by_dtype([dtypes.String])
+        return self.by_dtype([self._dtypes.String])
 
     def boolean(self: Self) -> DaskSelector:
-        return self.by_dtype([dtypes.Boolean])
+        return self.by_dtype([self._dtypes.Boolean])
 
     def all(self: Self) -> DaskSelector:
         def func(df: DaskLazyFrame) -> list[Any]:
