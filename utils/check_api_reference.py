7b791439bbef8e999c18992c5611c8bd3e9a3c89
AlessandroMiola <alessandro1.miola@gmail.com>
2024-10-06 20:30:28 +0200

    chore: remove extra blanks

--- utils/check_api_reference.py
+++ utils/check_api_reference.py
@@ -163,21 +163,17 @@ series_cat_methods = [
     for i in nw.from_native(pl.Series(), series_only=True).cat.__dir__()
     if not i[0].isupper() and i[0] != "_"
 ]
-
 with open("docs/api-reference/series_cat.md") as fd:
     content = fd.read()
-
 documented = [
     remove_prefix(i, "        - ")
     for i in content.splitlines()
     if i.startswith("        - ") and not i.startswith("        - _")
 ]
-
 if missing := set(series_cat_methods).difference(documented):
     print("Series.cat: not documented")  # noqa: T201
     print(missing)  # noqa: T201
     ret = 1
-
 if extra := set(documented).difference(series_cat_methods):
     print("Series.cat: outdated")  # noqa: T201
     print(extra)  # noqa: T201
@@ -189,21 +185,17 @@ series_dt_methods = [
     for i in nw.from_native(pl.Series(), series_only=True).dt.__dir__()
     if not i[0].isupper() and i[0] != "_"
 ]
-
 with open("docs/api-reference/series_dt.md") as fd:
     content = fd.read()
-
 documented = [
     remove_prefix(i, "        - ")
     for i in content.splitlines()
     if i.startswith("        - ") and not i.startswith("        - _")
 ]
-
 if missing := set(series_dt_methods).difference(documented):
     print("Series.dt: not documented")  # noqa: T201
     print(missing)  # noqa: T201
     ret = 1
-
 if extra := set(documented).difference(series_dt_methods):
     print("Series.dt: outdated")  # noqa: T201
     print(extra)  # noqa: T201
@@ -215,21 +207,17 @@ series_str_methods = [
     for i in nw.from_native(pl.Series(), series_only=True).str.__dir__()
     if not i[0].isupper() and i[0] != "_"
 ]
-
 with open("docs/api-reference/series_str.md") as fd:
     content = fd.read()
-
 documented = [
     remove_prefix(i, "        - ")
     for i in content.splitlines()
     if i.startswith("        - ") and not i.startswith("        - _")
 ]
-
 if missing := set(series_str_methods).difference(documented):
     print("Series.str: not documented")  # noqa: T201
     print(missing)  # noqa: T201
     ret = 1
-
 if extra := set(documented).difference(series_str_methods):
     print("Series.str: outdated")  # noqa: T201
     print(extra)  # noqa: T201
