b0dffadc9be3ee8e42e064bec5e6f7c71aad0b27
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-10-06 12:35:36 +0200

    placeholder pyspark test

---
+++ tests/pypsark_test.py
@@ -0,0 +1,15 @@
+"""
+PySpark support in Narwhals is still _very_ limited.
+Start with a simple test file whilst we develop the basics.
+Once we're a bit further along, we can integrate PySpark tests into the main test suite.
+"""
+
+# import pandas as pd
+# import pytest
+
+# import narwhals.stable.v1 as nw
+# from tests.utils import compare_dicts
+
+
+def test_with_columns() -> None:
+    raise NotImplementedError
