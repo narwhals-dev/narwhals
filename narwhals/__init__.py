e92dac4ff423699af53fdc6678cd5ec85d41ef23
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-10-09 09:27:41 +0200

    release: Bump version to 1.9.2

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -59,7 +59,7 @@ from narwhals.utils import maybe_get_index
 from narwhals.utils import maybe_reset_index
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.9.1"
+__version__ = "1.9.2"
 
 __all__ = [
     "dependencies",
