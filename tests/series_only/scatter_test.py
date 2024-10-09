41f6233d960e38bb3af6319cea8d0fc23b9c59f3
Cheukting <cheukting.ho@gmail.com>
2024-10-09 20:39:51 +0100

    scatter always return new series
    Co-authored-by: Marco Alabruzzo <marco.alabruzzo@gmail.com>

--- tests/series_only/scatter_test.py
+++ tests/series_only/scatter_test.py
@@ -24,3 +24,23 @@ def test_scatter(constructor_eager: Any, request: pytest.FixtureRequest) -> None
         "b": [142, 132, 124],
     }
     compare_dicts(result, expected)
+
+
+def test_scatter_unchanged(
+    constructor_eager: Any, request: pytest.FixtureRequest
+) -> None:
+    if "modin" in str(constructor_eager):
+        # https://github.com/modin-project/modin/issues/7392
+        request.applymarker(pytest.mark.xfail)
+    df = nw.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [142, 124, 132]}), eager_only=True
+    )
+    df.with_columns(
+        df["a"].scatter([0, 1], [999, 888]),
+        df["b"].scatter([0, 2, 1], df["b"]),
+    )
+    expected = {
+        "a": [1, 2, 3],
+        "b": [142, 124, 132],
+    }
+    compare_dicts(df, expected)
