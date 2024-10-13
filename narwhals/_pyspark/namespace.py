d7b275292076a8e199adb196888e61563cd9516d
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-10-13 14:31:51 +0200

    fix all_horizontal

--- narwhals/_pyspark/namespace.py
+++ narwhals/_pyspark/namespace.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
+import operator
 from functools import reduce
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import Callable
 from typing import NoReturn
 
+from narwhals._expression_parsing import combine_root_names
 from narwhals._expression_parsing import parse_into_exprs
+from narwhals._expression_parsing import reduce_output_names
 from narwhals._pyspark.expr import PySparkExpr
+from narwhals._pyspark.utils import get_column_name
 
 if TYPE_CHECKING:
     from pyspark.sql import Column
@@ -62,7 +66,22 @@ class PySparkNamespace:
         )
 
     def all_horizontal(self, *exprs: IntoPySparkExpr) -> PySparkExpr:
-        return reduce(lambda x, y: x & y, parse_into_exprs(*exprs, namespace=self))
+        parsed_exprs = parse_into_exprs(*exprs, namespace=self)
+
+        def func(df: PySparkLazyFrame) -> list[Column]:
+            cols = [c for _expr in parsed_exprs for c in _expr._call(df)]
+            col_name = get_column_name(df, cols[0])
+            return [reduce(operator.and_, cols).alias(col_name)]
+
+        return PySparkExpr(
+            call=func,
+            depth=max(x._depth for x in parsed_exprs) + 1,
+            function_name="all_horizontal",
+            root_names=combine_root_names(parsed_exprs),
+            output_names=reduce_output_names(parsed_exprs),
+            returns_scalar=False,
+            dtypes=self._dtypes,
+        )
 
     def col(self, *column_names: str) -> PySparkExpr:
         return PySparkExpr.from_column_names(*column_names, dtypes=self._dtypes)
