8fc2099124cc84a872da6a2a918bfbcf928da843
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-10-16 09:15:05 +0200

    fixup

--- tests/from_pycapsule_test.py
+++ tests/from_pycapsule_test.py
@@ -21,8 +21,9 @@ def test_from_pycapsule_to_arrow() -> None:
 
 @pytest.mark.xfail(parse_version(pa.__version__) < (14,), reason="too old")
 def test_from_pycapsule_to_polars(monkeypatch: pytest.MonkeyPatch) -> None:
+    tbl = pa.table({"ab": [1, 2, 3], "ba": [4, 5, 6]})
     monkeypatch.delitem(sys.modules, "pandas")
-    df = nw.from_native(pa.table({"ab": [1, 2, 3], "ba": [4, 5, 6]}), eager_only=True)
+    df = nw.from_native(tbl, eager_only=True)
     result = nw.from_pycapsule(df, native_namespace=pl)
     assert isinstance(result.to_native(), pl.DataFrame)
     expected = {"ab": [1, 2, 3], "ba": [4, 5, 6]}
