44795ffb7884e671b4e4301fe240e1dcf55bcd5d
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-10-17 19:02:51 +0200

    simplify test_to_py_scalar_arrays_series

--- tests/translate/to_py_scalar_test.py
+++ tests/translate/to_py_scalar_test.py
@@ -1,21 +1,26 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import narwhals.stable.v1 as nw
 from narwhals.dependencies import get_cudf
 from narwhals.dependencies import get_numpy
-from tests.utils import Constructor
+
+if TYPE_CHECKING:
+    from tests.utils import ConstructorEager
 
 
-def test_to_py_scalar(constructor_eager: Constructor) -> None:
+def test_to_py_scalar(constructor_eager: ConstructorEager) -> None:
     df = nw.from_native(constructor_eager({"a": [1, 2, 3]}))
     assert nw.to_py_scalar(df["a"].item(0)) == 1
 
 
-def test_to_py_scalar_cudf_series() -> None:
+def test_to_py_scalar_arrays_series() -> None:
     if cudf := get_cudf():  # pragma: no cover
         df = nw.from_native(cudf.DataFrame({"a": [1, 2, 3]}))
         cudf_series = nw.to_native(nw.to_py_scalar(df["a"]))
         assert isinstance(cudf_series, cudf.Series)
-    if np := get_numpy():  # pragma: no cover
-        array = np.array([1, 2, 3])
-        assert isinstance(nw.to_py_scalar(array), np.ndarray)
+
+    np = get_numpy()
+    array = np.array([1, 2, 3])
+    assert isinstance(nw.to_py_scalar(array), np.ndarray)
