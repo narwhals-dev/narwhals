c19be3b7acb968bf85e5aa26453178544f2df19a
Liam Connors <connorsl@tcd.ie>
2024-10-17 05:59:23 -0400

    test: update cuDF tests (#1196)
    * unxfail test_to_dummies_drop_first
    
    * xfail timezone tests for cuDF
    
    * fix typo

--- tests/series_only/to_dummy_test.py
+++ tests/series_only/to_dummy_test.py
@@ -18,11 +18,7 @@ def test_to_dummies(constructor_eager: Any, sep: str) -> None:
 
 
 @pytest.mark.parametrize("sep", ["_", "-"])
-def test_to_dummies_drop_first(
-    request: pytest.FixtureRequest, constructor_eager: Any, sep: str
-) -> None:
-    if "cudf" in str(constructor_eager):
-        request.applymarker(pytest.mark.xfail)
+def test_to_dummies_drop_first(constructor_eager: Any, sep: str) -> None:
     s = nw.from_native(constructor_eager({"a": data}), eager_only=True)["a"].alias("a")
     result = s.to_dummies(drop_first=True, separator=sep)
     expected = {f"a{sep}2": [0, 1, 0], f"a{sep}3": [0, 0, 1]}
