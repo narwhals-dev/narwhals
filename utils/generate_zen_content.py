990577fec837ce4604168ce08168e4c89940b7ce
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-10-24 12:00:48 +0200

    docs: zen doc page

---
+++ utils/generate_zen_content.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Final
+
+from narwhals.this import ZEN
+
+DESTINATION_PATH: Final[Path] = Path("docs") / "this.md"
+
+content = f"""
+# The Zen of Narwhals
+
+The well famous Python easter egg `import this` will reveal The Zen of Python, by Tim Peters.
+
+Narwhals took inspiration from _this_ and created its own Zen.
+
+```py
+import narwhals.this
+```
+
+```terminal
+{ZEN}
+```
+"""
+
+with DESTINATION_PATH.open(mode="w") as destination:
+    destination.write(content)
