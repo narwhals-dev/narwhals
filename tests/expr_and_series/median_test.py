a138981b911f3ae30fefdaca8dada1ecf8020cd3
AlessandroMiola <alessandro1.miola@gmail.com>
2024-10-24 19:30:00 +0200

    test: test median ignores null values

--- tests/expr_and_series/median_test.py
+++ tests/expr_and_series/median_test.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
-from typing import Any
-
 import pytest
 
 import narwhals.stable.v1 as nw
 from tests.utils import Constructor
-from tests.utils import compare_dicts
+from tests.utils import ConstructorEager
+from tests.utils import assert_equal_data
 
-data = {"a": [3, 8, 2], "b": [5, 5, 7], "z": [7.0, 8, 9]}
+data = {
+    "a": [3, 8, 2, None],
+    "b": [5, 5, None, 7],
+    "z": [7.0, 8, 9, None],
+    "s": ["f", "a", "x", "x"],
+}
 
 
 @pytest.mark.parametrize(
     "expr", [nw.col("a", "b", "z").median(), nw.median("a", "b", "z")]
 )
-def test_expr_median_expr(
+def test_median_expr(
     constructor: Constructor, expr: nw.Expr, request: pytest.FixtureRequest
 ) -> None:
     if "dask_lazy_p2" in str(constructor):
@@ -22,11 +26,13 @@ def test_expr_median_expr(
     df = nw.from_native(constructor(data))
     result = df.select(expr)
     expected = {"a": [3.0], "b": [5.0], "z": [8.0]}
-    compare_dicts(result, expected)
+    assert_equal_data(result, expected)
 
 
 @pytest.mark.parametrize(("col", "expected"), [("a", 3.0), ("b", 5.0), ("z", 8.0)])
-def test_expr_median_series(constructor_eager: Any, col: str, expected: float) -> None:
+def test_median_series(
+    constructor_eager: ConstructorEager, col: str, expected: float
+) -> None:
     series = nw.from_native(constructor_eager(data), eager_only=True)[col]
     result = series.median()
-    compare_dicts({col: [result]}, {col: [expected]})
+    assert_equal_data({col: [result]}, {col: [expected]})
