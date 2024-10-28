e9e8c280d0c58c0fbae98f21ac9b8b44d734071b
Dea María Léon <deamarialeon@gmail.com>
2024-10-28 10:14:25 +0100

    better test?

--- tests/expr_and_series/map_batches_test.py
+++ tests/expr_and_series/map_batches_test.py
@@ -9,15 +9,13 @@ from tests.utils import assert_equal_data
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6], "z": [7.0, 8.0, 9.0]}
 
-expected = [2, 3, 4]
-
 
 def test_map_batches_expr(constructor: Constructor) -> None:
     if is_dask_dataframe(constructor(data)):  # Remove
         pytest.skip()
     df = nw.from_native(constructor(data))
-    e = df.select(nw.col("a").map_batches(lambda s: s + 1))
-    assert_equal_data(e, {"a": expected})
+    e = df.select(nw.col("a", "b").map_batches(lambda s: s + 1))
+    assert_equal_data(e, {"a": [2, 3, 4], "b": [5, 6, 7]})
 
 
 def test_map_batches_expr_numpy(constructor: Constructor) -> None:
@@ -25,6 +23,8 @@ def test_map_batches_expr_numpy(constructor: Constructor) -> None:
         pytest.skip()
     df = nw.from_native(constructor(data))
     e = df.select(
-        nw.col("a").map_batches(lambda s: s.to_numpy() + 1, return_dtype=nw.Float64).sum()
+        nw.col("a")
+        .map_batches(lambda s: s.to_numpy() + 1, return_dtype=nw.Float64())
+        .sum()
     )
     assert_equal_data(e, {"a": [9.0]})
