9dedbb2c43a307ed885ee6da9b3f69c93a55b23c
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-11-03 14:43:11 +0000

    release: Bump version to 1.13.1

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -67,7 +67,7 @@ from narwhals.utils import maybe_get_index
 from narwhals.utils import maybe_reset_index
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.13.0"
+__version__ = "1.13.1"
 
 __all__ = [
     "dependencies",
