c243c788fd2af7192123ca4488f2c8e6214aef9e
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-11-11 18:32:26 +0100

    parametrize tests

--- tpch/tests/queries_test.py
+++ tpch/tests/queries_test.py
@@ -4,20 +4,22 @@ import subprocess
 import sys
 from pathlib import Path
 
+import pytest
 
-def test_execute_scripts() -> None:
-    root = Path(__file__).resolve().parent.parent
-    # directory containing all the queries
-    execute_dir = root / "execute"
+ROOT_PATH = Path(__file__).resolve().parent.parent
+# directory containing all the script to execute queries
+EXECUTE_DIR = ROOT_PATH / "execute"
 
-    for script_path in execute_dir.glob("q[1-9]*.py"):
-        print(f"executing query {script_path.stem}")  # noqa: T201
-        result = subprocess.run(  # noqa: S603
-            [sys.executable, "-m", f"execute.{script_path.stem}"],
-            capture_output=True,
-            text=True,
-            check=False,
-        )
-        assert (
-            result.returncode == 0
-        ), f"Script {script_path} failed with error: {result.stderr}"
+
+@pytest.mark.parametrize("script_path", EXECUTE_DIR.glob("q[1-9]*.py"))
+def test_execute_scripts(script_path: Path) -> None:
+    print(f"executing query {script_path.stem}")  # noqa: T201
+    result = subprocess.run(  # noqa: S603
+        [sys.executable, "-m", f"execute.{script_path.stem}"],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+    assert (
+        result.returncode == 0
+    ), f"Script {script_path} failed with error: {result.stderr}"
