2dae73ffab9704b683f90379f32b7ed25d8c013f
Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>
2024-11-12 19:11:20 +0100

    fix: pandas and arrow `to_dummies` with nulls (#1040)
    ---------
    
    Co-authored-by: Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- tests/series_only/to_dummy_test.py
+++ tests/series_only/to_dummy_test.py
@@ -6,14 +6,15 @@ import narwhals.stable.v1 as nw
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
 
-data = [1, 2, 3]
+data = ["x", "y", "z"]
+data_na = ["x", "y", None]
 
 
 @pytest.mark.parametrize("sep", ["_", "-"])
 def test_to_dummies(constructor_eager: ConstructorEager, sep: str) -> None:
     s = nw.from_native(constructor_eager({"a": data}), eager_only=True)["a"].alias("a")
     result = s.to_dummies(separator=sep)
-    expected = {f"a{sep}1": [1, 0, 0], f"a{sep}2": [0, 1, 0], f"a{sep}3": [0, 0, 1]}
+    expected = {f"a{sep}x": [1, 0, 0], f"a{sep}y": [0, 1, 0], f"a{sep}z": [0, 0, 1]}
 
     assert_equal_data(result, expected)
 
@@ -22,6 +23,31 @@ def test_to_dummies(constructor_eager: ConstructorEager, sep: str) -> None:
 def test_to_dummies_drop_first(constructor_eager: ConstructorEager, sep: str) -> None:
     s = nw.from_native(constructor_eager({"a": data}), eager_only=True)["a"].alias("a")
     result = s.to_dummies(drop_first=True, separator=sep)
-    expected = {f"a{sep}2": [0, 1, 0], f"a{sep}3": [0, 0, 1]}
+    expected = {f"a{sep}y": [0, 1, 0], f"a{sep}z": [0, 0, 1]}
 
     assert_equal_data(result, expected)
+
+
+@pytest.mark.parametrize("sep", ["_", "-"])
+def test_to_dummies_with_nulls(constructor_eager: ConstructorEager, sep: str) -> None:
+    if "pandas_nullable_constructor" not in str(constructor_eager):
+        pytest.skip()
+    s = nw.from_native(constructor_eager({"a": data_na}), eager_only=True)["a"].alias("a")
+    result = s.to_dummies(separator=sep)
+    expected = {f"a{sep}null": [0, 0, 1], f"a{sep}x": [1, 0, 0], f"a{sep}y": [0, 1, 0]}
+
+    assert_equal_data(result, expected)
+
+
+@pytest.mark.parametrize("sep", ["_", "-"])
+def test_to_dummies_drop_first_na(
+    request: pytest.FixtureRequest, constructor_eager: ConstructorEager, sep: str
+) -> None:
+    if "cudf" in str(constructor_eager):
+        request.applymarker(pytest.mark.xfail)
+    s = nw.from_native(constructor_eager({"a": data_na}), eager_only=True)["a"].alias("a")
+    result = s.to_dummies(drop_first=True, separator=sep)
+    expected = {f"a{sep}null": [0, 0, 1], f"a{sep}y": [0, 1, 0]}
+
+    assert_equal_data(result, expected)
+    assert result.schema == {f"a{sep}null": nw.Int8, f"a{sep}y": nw.Int8}
