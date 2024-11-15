72589d6afd37d5bc9c6f6817be858901f7e40489
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-11-15 15:19:23 +0000

    release: Bump version to 1.13.5 (#1368)

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -68,7 +68,7 @@ from narwhals.utils import maybe_get_index
 from narwhals.utils import maybe_reset_index
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.13.4"
+__version__ = "1.13.5"
 
 __all__ = [
     "dependencies",
