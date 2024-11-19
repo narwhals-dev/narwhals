33c0f8493d94862db14babff6c3d446896ab23b1
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-11-19 10:38:04 +0000

    release: Bump version to 1.14.1

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -69,7 +69,7 @@ from narwhals.utils import maybe_get_index
 from narwhals.utils import maybe_reset_index
 from narwhals.utils import maybe_set_index
 
-__version__ = "1.14.0"
+__version__ = "1.14.1"
 
 __all__ = [
     "Array",
