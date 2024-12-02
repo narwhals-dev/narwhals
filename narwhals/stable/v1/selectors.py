b7f6cb0575cde53a1546156963b914e871d74c62
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-12-02 21:50:52 +0000

    fixup

--- narwhals/stable/v1/selectors.py
+++ narwhals/stable/v1/selectors.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from narwhals.selectors import all
+from narwhals.selectors import all  # noqa: A004
 from narwhals.selectors import boolean
 from narwhals.selectors import by_dtype
 from narwhals.selectors import categorical
