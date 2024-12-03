0e5cf1aa42294d0e6322e6a19cc2716a8f39370c
MUKESHRAJ M <87431738+MUKESHRAJMAHENDRAN@users.noreply.github.com>
2024-12-03 19:10:29 +0000

    Added the test for exp max, min and series max, min

--- narwhals/series.py
+++ narwhals/series.py
@@ -685,7 +685,7 @@ class Series(Generic[IntoSeriesT]):
             >>> my_library_agnostic_function(s_pl)
             0
         """
-        return self._compliant_series.idxmax()
+        return self._compliant_series.argmin()
 
     def argmax(self) -> Any:
         """Get the index of the maximum value in this Series.
@@ -712,7 +712,7 @@ class Series(Generic[IntoSeriesT]):
             >>> my_library_agnostic_function(s_pl)
             2
         """
-        return self._compliant_series.idxmax()
+        return self._compliant_series.argmax()
 
     def skew(self: Self) -> Any:
         """Calculate the sample skewness of the Series.
