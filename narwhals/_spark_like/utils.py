d8e60647da7952c9edf09576f7c9a14f218fdd42
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2024-12-05 08:01:42 +0100

    extract _columns_from_expr

--- narwhals/_spark_like/utils.py
+++ narwhals/_spark_like/utils.py
@@ -57,28 +57,29 @@ def get_column_name(df: SparkLikeLazyFrame, column: Column) -> str:
     return str(df._native_frame.select(column).columns[0])
 
 
+def _columns_from_expr(df: SparkLikeLazyFrame, expr: IntoSparkLikeExpr) -> list[Column]:
+    if isinstance(expr, str):  # pragma: no cover
+        from pyspark.sql import functions as F  # noqa: N812
+
+        return [F.col(expr)]
+    elif hasattr(expr, "__narwhals_expr__"):
+        col_output_list = expr._call(df)
+        if expr._output_names is not None and (
+            len(col_output_list) != len(expr._output_names)
+        ):  # pragma: no cover
+            msg = "Safety assertion failed, please report a bug to https://github.com/narwhals-dev/narwhals/issues"
+            raise AssertionError(msg)
+        return col_output_list
+    else:
+        raise InvalidIntoExprError.from_invalid_type(type(expr))
+
+
 def parse_exprs_and_named_exprs(
     df: SparkLikeLazyFrame, *exprs: IntoSparkLikeExpr, **named_exprs: IntoSparkLikeExpr
 ) -> dict[str, Column]:
-    def _columns_from_expr(expr: IntoSparkLikeExpr) -> list[Column]:
-        if isinstance(expr, str):  # pragma: no cover
-            from pyspark.sql import functions as F  # noqa: N812
-
-            return [F.col(expr)]
-        elif hasattr(expr, "__narwhals_expr__"):
-            col_output_list = expr._call(df)
-            if expr._output_names is not None and (
-                len(col_output_list) != len(expr._output_names)
-            ):  # pragma: no cover
-                msg = "Safety assertion failed, please report a bug to https://github.com/narwhals-dev/narwhals/issues"
-                raise AssertionError(msg)
-            return col_output_list
-        else:
-            raise InvalidIntoExprError.from_invalid_type(type(expr))
-
     result_columns: dict[str, list[Column]] = {}
     for expr in exprs:
-        column_list = _columns_from_expr(expr)
+        column_list = _columns_from_expr(df, expr)
         if isinstance(expr, str):  # pragma: no cover
             output_names = [expr]
         elif expr._output_names is None:
@@ -87,7 +88,7 @@ def parse_exprs_and_named_exprs(
             output_names = expr._output_names
         result_columns.update(zip(output_names, column_list))
     for col_alias, expr in named_exprs.items():
-        columns_list = _columns_from_expr(expr)
+        columns_list = _columns_from_expr(df, expr)
         if len(columns_list) != 1:  # pragma: no cover
             msg = "Named expressions must return a single column"
             raise AssertionError(msg)
