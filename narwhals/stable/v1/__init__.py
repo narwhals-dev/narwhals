a5189f35ee696f35eab53f6d8fae809818e79c7e
Dea María Léon <deamarialeon@gmail.com>
2024-12-20 19:21:06 +0100

    forgot v1

--- narwhals/stable/v1/__init__.py
+++ narwhals/stable/v1/__init__.py
@@ -2106,7 +2106,7 @@ def to_native(
             **Deprecated** (v1.13.0):
                 Please use `pass_through` instead. Note that `strict` is still available
                 (and won't emit a deprecation warning) if you use `narwhals.stable.v1`,
-                see [perfect backwards compatibility policy]../backcompat.md/).
+                see [perfect backwards compatibility policy](../backcompat.md/).
         pass_through: Determine what happens if `narwhals_object` isn't a Narwhals class:
 
             - `False` (default): raise an error
@@ -2370,7 +2370,7 @@ def nth(*indices: int | Sequence[int]) -> Expr:
 
     Notes:
         `nth` is not supported for Polars version<1.0.0. Please use
-        [`col`][] instead.
+        [`narwhals.col`][] instead.
 
     Arguments:
         indices: One or more indices representing the columns to retrieve.
@@ -3610,7 +3610,7 @@ def from_dict(
     """Instantiate DataFrame from dictionary.
 
     Indexes (if present, for pandas-like backends) are aligned following
-    the [left-hand-rule](https://narwhals-dev.github.io/narwhals/pandas_like_concepts/pandas_index/).
+    the [left-hand-rule](../pandas_like_concepts/pandas_index.md/).
 
     Notes:
         For pandas-like dataframes, conversion to schema is applied after dataframe
