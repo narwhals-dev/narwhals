d2d299c7dd6e61bf4bcf51e7f98b4e0bf383e0e9
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-12-20 22:27:09 +0100

    feat: add some spark-like frame methods

--- narwhals/_spark_like/dataframe.py
+++ narwhals/_spark_like/dataframe.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
+import warnings
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import Iterable
+from typing import Literal
 from typing import Sequence
 
 from narwhals._spark_like.utils import native_to_narwhals_dtype
 from narwhals._spark_like.utils import parse_exprs_and_named_exprs
 from narwhals.utils import Implementation
+from narwhals.utils import find_stacklevel
 from narwhals.utils import flatten
 from narwhals.utils import parse_columns_to_drop
 from narwhals.utils import parse_version
@@ -173,16 +176,87 @@ class SparkLikeLazyFrame:
 
         flat_by = flatten([*flatten([by]), *more_by])
         if isinstance(descending, bool):
-            descending = [descending]
+            descending = [descending] * len(flat_by)
 
         if nulls_last:
-            sort_funcs = [
+            sort_funcs = (
                 F.desc_nulls_last if d else F.asc_nulls_last for d in descending
-            ]
+            )
         else:
-            sort_funcs = [
+            sort_funcs = (
                 F.desc_nulls_first if d else F.asc_nulls_first for d in descending
-            ]
+            )
 
         sort_cols = [sort_f(col) for col, sort_f in zip(flat_by, sort_funcs)]
         return self._from_native_frame(self._native_frame.sort(*sort_cols))
+
+    def with_row_index(self: Self, name: str) -> Self:
+        import pyspark.sql.functions as F  # noqa: N812
+
+        return self._from_native_frame(
+            self._native_frame.withColumn(name, F.monotonically_increasing_id())
+        )
+
+    def drop_nulls(self: Self, subset: str | list[str] | None) -> Self:
+        return self._from_native_frame(self._native_frame.dropna(subset=subset))
+
+    def rename(self: Self, mapping: dict[str, str]) -> Self:
+        return self._from_native_frame(
+            self._native_frame.withColumnsRenamed(colsMap=mapping)
+        )
+
+    def unique(
+        self: Self,
+        subset: str | list[str] | None = None,
+        *,
+        keep: Literal["any", "first", "last", "none"],
+        maintain_order: bool,
+    ) -> Self:
+        if keep != "any":
+            msg = "`LazyFrame.unique` with PySpark backend only supports `keep='any'`."
+            raise ValueError(msg)
+
+        if maintain_order is True:
+            warnings.warn(
+                "Argument `maintain_order=True` is unused in `LazyFrame.unique` with "
+                "PySpark backend",
+                UserWarning,
+                stacklevel=find_stacklevel(),
+            )
+        subset = [subset] if isinstance(subset, str) else subset
+        return self._from_native_frame(self._native_frame.dropDuplicates(subset=subset))
+
+    def join(
+        self,
+        other: Self,
+        how: Literal["inner", "left", "cross", "semi", "anti"],
+        left_on: str | list[str] | None,
+        right_on: str | list[str] | None,
+        suffix: str,
+    ) -> Self:
+        self_native = self._native_frame
+        other_native = other._native_frame
+
+        left_columns = self.columns
+        right_columns = other.columns
+
+        if isinstance(left_on, str):
+            left_on = [left_on]
+        if isinstance(right_on, str):
+            right_on = [right_on]
+
+        # create a mapping for columns on other
+        # `right_on` columns will be renamed as `left_on`
+        # the remaining columns will be either added the suffix or left unchanged.
+        rename_mapping = {
+            **dict(zip(right_on or [], left_on or [])),
+            **{
+                colname: f"{colname}{suffix}" if colname in left_columns else colname
+                for colname in list(set(right_columns).difference(set(right_on or [])))
+            },
+        }
+        return self._from_native_frame(
+            self_native.join(
+                other=other_native.withColumnsRenamed(rename_mapping), on=left_on, how=how
+            )
+        )
