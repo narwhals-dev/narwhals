95dd8d31d870599c1da9d05701b04a1bb4b36ce3
Dhanunjaya-Elluri <dhanunjayet@gmail.com>
2024-12-23 07:27:12 +0100

    feat: add `.collect_schema` to interchange-level df `duckdb` & `ibis`

--- narwhals/_ibis/dataframe.py
+++ narwhals/_ibis/dataframe.py
@@ -129,3 +129,10 @@ class IbisInterchangeFrame:
 
     def _from_native_frame(self: Self, df: Any) -> Self:
         return self.__class__(df, version=self._version)
+
+    def collect_schema(self) -> dict[str, DType]:
+        """Return the schema of the Ibis table."""
+        return {
+            column_name: native_to_narwhals_dtype(ibis_dtype, self._version)
+            for column_name, ibis_dtype in self._native_frame.schema().items()
+        }
