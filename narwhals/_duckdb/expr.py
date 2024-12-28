83cdfcf996588e7641cf81eae19659cdc0b9b53e
raisadz <34237447+raisadz@users.noreply.github.com>
2024-12-28 16:34:32 +0000

    simplify slice

--- narwhals/_duckdb/expr.py
+++ narwhals/_duckdb/expr.py
@@ -436,29 +436,20 @@ class DuckDBExprStringNamespace:
         from duckdb import ConstantExpression
         from duckdb import FunctionExpression
 
-        if length is None:
-            return self._compliant_expr._from_call(
-                lambda _input: FunctionExpression(
-                    "array_slice",
-                    _input,
-                    ConstantExpression(offset + 1)
-                    if offset >= 0
-                    else FunctionExpression("length", _input) + offset + 1,
-                    FunctionExpression("length", _input),
-                ),
-                "slice",
-                returns_scalar=False,
+        def func(_input: duckdb.Expression) -> duckdb.Expression:
+            return FunctionExpression(
+                "array_slice",
+                _input,
+                ConstantExpression(offset + 1)
+                if offset >= 0
+                else FunctionExpression("length", _input) + offset + 1,
+                FunctionExpression("length", _input)
+                if length is None
+                else ConstantExpression(length) + offset,
             )
-        else:
-            return self._compliant_expr._from_call(
-                lambda _input: FunctionExpression(
-                    "array_slice",
-                    _input,
-                    ConstantExpression(offset + 1)
-                    if offset >= 0
-                    else FunctionExpression("length", _input) + offset + 1,
-                    ConstantExpression(length) + offset,
-                ),
-                "slice",
-                returns_scalar=False,
-            )
+
+        return self._compliant_expr._from_call(
+            func,
+            "slice",
+            returns_scalar=False,
+        )
