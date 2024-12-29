57030942036609081b3fb0b59dfd290b739007a3
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-12-29 21:31:35 +0000

    fix: fix is_duplicated and is_unique for pyarrow

--- tests/expr_and_series/is_unique_test.py
+++ tests/expr_and_series/is_unique_test.py
@@ -5,14 +5,13 @@ from tests.utils import Constructor
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
 
-data = {
-    "a": [1, 1, 2],
-    "b": [1, 2, 3],
-    "index": [0, 1, 2],
-}
-
 
 def test_is_unique_expr(constructor: Constructor) -> None:
+    data = {
+        "a": [1, 1, 2],
+        "b": [1, 2, 3],
+        "index": [0, 1, 2],
+    }
     df = nw.from_native(constructor(data))
     result = df.select(nw.col("a", "b").is_unique(), "index").sort("index")
     expected = {
@@ -23,7 +22,28 @@ def test_is_unique_expr(constructor: Constructor) -> None:
     assert_equal_data(result, expected)
 
 
+def test_is_unique_w_nulls_expr(constructor: Constructor) -> None:
+    data = {
+        "a": [None, 1, 2],
+        "b": [None, 2, None],
+        "index": [0, 1, 2],
+    }
+    df = nw.from_native(constructor(data))
+    result = df.select(nw.col("a", "b").is_unique(), "index").sort("index")
+    expected = {
+        "a": [True, True, True],
+        "b": [False, True, False],
+        "index": [0, 1, 2],
+    }
+    assert_equal_data(result, expected)
+
+
 def test_is_unique_series(constructor_eager: ConstructorEager) -> None:
+    data = {
+        "a": [1, 1, 2],
+        "b": [1, 2, 3],
+        "index": [0, 1, 2],
+    }
     series = nw.from_native(constructor_eager(data), eager_only=True)["a"]
     result = series.is_unique()
     expected = {
