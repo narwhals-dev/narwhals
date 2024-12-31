cdacd8c33d8eacbe601475e7c598e348e2038c93
Cameron Riddell <cameron@dutc.io>
2024-12-31 13:47:17 -0800

    xfail pyarrow backed dates tzconvert on windows

--- tests/series_only/to_numpy_test.py
+++ tests/series_only/to_numpy_test.py
@@ -42,7 +42,8 @@ def test_to_numpy_tz_aware(
     if (
         ("pyarrow_table" in str(constructor_eager) and PYARROW_VERSION < (12,))
         or ("pandas_pyarrow" in str(constructor_eager) and PANDAS_VERSION < (2, 2))
-        or ("modin_pyarrow" in str(constructor_eager) and is_windows())
+        or ("modin_pyarrow" in str(constructor_eager) and PANDAS_VERSION < (2, 2))
+        or ("pyarrow" in str(constructor_eager) and is_windows())
     ):
         request.applymarker(pytest.mark.xfail)
         request.applymarker(pytest.mark.xfail)
