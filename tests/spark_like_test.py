c688c67719fd454506b3e9612d567e5b0818655d
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-01-02 09:37:02 +0100

    sumh sort, sum_expr constructor

--- tests/spark_like_test.py
+++ tests/spark_like_test.py
@@ -313,11 +313,11 @@ def test_sumh(pyspark_constructor: Constructor, col_expr: Any) -> None:
 
 
 def test_sumh_nullable(pyspark_constructor: Constructor) -> None:
-    data = {"a": [1, 8, 3], "b": [4, 5, None]}
+    data = {"a": [1, 8, 3], "b": [4, 5, None], "idx": [0, 1, 2]}
     expected = {"hsum": [5, 13, 3]}
 
     df = nw.from_native(pyspark_constructor(data))
-    result = df.select(hsum=nw.sum_horizontal("a", "b"))
+    result = df.select("idx", hsum=nw.sum_horizontal("a", "b")).sort("idx").drop("idx")
     assert_equal_data(result, expected)
 
 
@@ -388,9 +388,9 @@ def test_expr_min_expr(pyspark_constructor: Constructor) -> None:
 
 # copied from tests/expr_and_series/min_test.py
 @pytest.mark.parametrize("expr", [nw.col("a", "b", "z").sum(), nw.sum("a", "b", "z")])
-def test_expr_sum_expr(constructor: Constructor, expr: nw.Expr) -> None:
+def test_expr_sum_expr(pyspark_constructor: Constructor, expr: nw.Expr) -> None:
     data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8, 9]}
-    df = nw.from_native(constructor(data))
+    df = nw.from_native(pyspark_constructor(data))
     result = df.select(expr)
     expected = {"a": [6], "b": [14], "z": [24.0]}
     assert_equal_data(result, expected)
