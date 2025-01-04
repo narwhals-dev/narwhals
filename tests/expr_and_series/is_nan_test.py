48989885b32b844b8f78122500a9885017f0407c
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-01-04 11:17:15 +0000

    catch ComputeError from is_nan for cudf

--- tests/expr_and_series/is_nan_test.py
+++ tests/expr_and_series/is_nan_test.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from contextlib import nullcontext as does_not_raise
 from typing import Any
 
 import pytest
+from polars.exceptions import ComputeError
 
 import narwhals.stable.v1 as nw
 from tests.conftest import dask_lazy_p1_constructor
@@ -48,7 +50,15 @@ def test_nan(constructor: Constructor) -> None:
             "float_na": [True, False, None],
         }
 
-    assert_equal_data(result, expected)
+    context = (
+        pytest.raises(
+            ComputeError, match="NAN is not supported in a Non-floating point type column"
+        )
+        if "cudf" in str(constructor)
+        else does_not_raise()
+    )
+    with context:
+        assert_equal_data(result, expected)
 
 
 def test_nan_series(constructor_eager: ConstructorEager) -> None:
