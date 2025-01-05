fix: fix `PYSPARK_VERSION` for `median` calculation

narwhals/_spark_like/expr.py
@@ -10,6 +10,7 @@ from narwhals._spark_like.utils import get_column_name
 from narwhals._spark_like.utils import maybe_evaluate
 from narwhals.typing import CompliantExpr
 from narwhals.utils import Implementation
+from narwhals.utils import get_module_version_as_tuple
 from narwhals.utils import parse_version
 
 if TYPE_CHECKING:
@@ -19,6 +20,8 @@ if TYPE_CHECKING:
     from narwhals._spark_like.dataframe import SparkLikeLazyFrame
     from narwhals._spark_like.namespace import SparkLikeNamespace
     from narwhals.utils import Version
+
+PYSPARK_VERSION: tuple[int, ...] = get_module_version_as_tuple("pyspark")
 
 
 class SparkLikeExpr(CompliantExpr["Column"]):
@@ -222,9 +225,16 @@ class SparkLikeExpr(CompliantExpr["Column"]):
         return self._from_call(F.mean, "mean", returns_scalar=True)
 
     def median(self) -> Self:
-        from pyspark.sql import functions as F  # noqa: N812
+        def _median(_input: Column) -> Column:
+            from pyspark.sql import functions as F  # noqa: N812
 
-        return self._from_call(F.median, "median", returns_scalar=True)
+            if PYSPARK_VERSION < (3, 4):
+                # Use percentile_approx with default accuracy parameter (10000)
+                return F.percentile_approx(_input.cast("double"), 0.5)
+
+            return F.median(_input)
+
+        return self._from_call(_median, "median", returns_scalar=True)
 
     def min(self) -> Self:
         from pyspark.sql import functions as F  # noqa: N812