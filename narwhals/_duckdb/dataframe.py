c8dcad6f89528ff1776ed9f93ce56ede5a80442f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-01-09 14:56:48 +0000

    fix: duckdb column names with spaces

--- narwhals/_duckdb/dataframe.py
+++ narwhals/_duckdb/dataframe.py
@@ -243,7 +243,7 @@ class DuckDBLazyFrame:
             assert right_on is not None  # noqa: S101
 
             conditions = [
-                f"lhs.{left} = rhs.{right}" for left, right in zip(left_on, right_on)
+                f'lhs."{left}" = rhs."{right}"' for left, right in zip(left_on, right_on)
             ]
             condition = " and ".join(conditions)
             rel = self._native_frame.set_alias("lhs").join(
@@ -251,16 +251,16 @@ class DuckDBLazyFrame:
             )
 
         if how in ("inner", "left", "cross"):
-            select = [f"lhs.{x}" for x in self._native_frame.columns]
+            select = [f'lhs."{x}"' for x in self._native_frame.columns]
             for col in other._native_frame.columns:
                 if col in self._native_frame.columns and (
                     right_on is None or col not in right_on
                 ):
-                    select.append(f"rhs.{col} as {col}{suffix}")
+                    select.append(f'rhs."{col}" as "{col}{suffix}"')
                 elif right_on is None or col not in right_on:
                     select.append(col)
         else:  # semi
-            select = [f"lhs.{x}" for x in self._native_frame.columns]
+            select = ["lhs.*"]
 
         res = rel.select(", ".join(select)).set_alias(original_alias)
         return self._from_native_frame(res)
