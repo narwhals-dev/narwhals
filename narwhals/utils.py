80840f4bf3c5ed66ced8d7fa94992aba6ac019f0
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-01-27 13:50:12 +0000

    feat: increase terminal width for ipython

--- narwhals/utils.py
+++ narwhals/utils.py
@@ -1035,11 +1035,11 @@ def generate_repr(header: str, native_repr: str) -> str:
     try:
         terminal_width = os.get_terminal_size().columns
     except OSError:
-        terminal_width = 80
+        terminal_width = int(os.getenv("COLUMNS", 80))  # noqa: PLW1508
     native_lines = native_repr.splitlines()
     max_native_width = max(len(line) for line in native_lines)
 
-    if max_native_width + 2 < terminal_width:
+    if max_native_width + 2 <= terminal_width:
         length = max(max_native_width, len(header))
         output = f"┌{'─'*length}┐\n"
         header_extra = length - len(header)
