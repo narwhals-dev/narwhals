8f1c5563a9d49bc69218ce53622abdbfeacdc303
Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>
2025-02-09 16:31:23 +0100

    ci: validate tpch query results (#1971)
    * ci: validate tpch query results
    
    * LazyFrame -> FrameT
    
    * parenthesis

--- tpch/queries/q16.py
+++ tpch/queries/q16.py
@@ -8,7 +8,6 @@ if TYPE_CHECKING:
     from narwhals.typing import FrameT
 
 
-@nw.narwhalify
 def query(part_ds: FrameT, partsupp_ds: FrameT, supplier_ds: FrameT) -> FrameT:
     var1 = "Brand#45"
 
