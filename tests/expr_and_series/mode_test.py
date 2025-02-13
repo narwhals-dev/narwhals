97d93d10619389bab5d2b9c7413690b977b4dcec
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-02-13 11:10:04 +0000

    wip

--- tests/expr_and_series/mode_test.py
+++ tests/expr_and_series/mode_test.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pytest
+
 import narwhals.stable.v1 as nw
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
@@ -10,20 +12,24 @@ data = {
 }
 
 
-def test_mode_single_expr(constructor_eager: ConstructorEager) -> None:
+def test_mode_single_expr(
+    constructor_eager: ConstructorEager, request: pytest.FixtureRequest
+) -> None:
     if "pyarrow" in str(constructor_eager):
         # TODO(unassigned): reimplement mode for pyarrow
-        return
+        request.applymarker(pytest.mark.xfail)
     df = nw.from_native(constructor_eager(data))
     result = df.select(nw.col("a").mode()).sort("a")
     expected = {"a": [1, 2]}
     assert_equal_data(result, expected)
 
 
-def test_mode_series(constructor_eager: ConstructorEager) -> None:
+def test_mode_series(
+    constructor_eager: ConstructorEager, request: pytest.FixtureRequest
+) -> None:
     if "pyarrow" in str(constructor_eager):
         # TODO(unassigned): reimplement mode for pyarrow
-        return
+        request.applymarker(pytest.mark.xfail)
     series = nw.from_native(constructor_eager(data), eager_only=True)["a"]
     result = series.mode().sort()
     expected = {"a": [1, 2]}
