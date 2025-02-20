e4292780e2b6a3b34b6cdb9b11a293be895bd93d
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2025-02-20 08:33:15 +0100

    add more tests

--- tests/frame/unpivot_test.py
+++ tests/frame/unpivot_test.py
@@ -106,3 +106,27 @@ def test_unpivot_mixed_types(
     result = df.unpivot(on=["a", "b"], index="idx")
 
     assert result.collect_schema().dtypes() == expected_dtypes
+
+
+def test_unpivot_index_none(constructor: Constructor) -> None:
+    df = nw.from_native(constructor(data))
+    expected_data = {
+        "variable": ["b", "b", "b", "c", "c", "c"],
+        "value": [1, 3, 5, 2, 4, 6],
+    }
+    result = df.unpivot(on=["b", "c"], index=None)
+    assert result.collect_schema().names() == ["variable", "value"]
+    assert_equal_data(result, expected_data)
+
+
+def test_unpivot_on_none(constructor: Constructor) -> None:
+    df = nw.from_native(constructor(data))
+    expected_data = {
+        "a": ["x", "y", "z"],
+        "b": [1, 3, 5],
+        "variable": ["c", "c", "c"],
+        "value": [2, 4, 6],
+    }
+    result = df.unpivot(on=None, index=["a", "b"])
+    assert result.collect_schema().names() == ["a", "b", "variable", "value"]
+    assert_equal_data(result, expected_data)
