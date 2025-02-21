2ab3305064bb093b5d98422c88b9a583c86f11a7
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-02-21 16:23:35 +0000

    feat(typing): provide `PandasLikeSeries` to `PandasLikeDataFrame`
    Experimenting with `pandas` only to start

--- narwhals/_pandas_like/dataframe.py
+++ narwhals/_pandas_like/dataframe.py
@@ -84,7 +84,7 @@ CLASSICAL_NUMPY_DTYPES: frozenset[np.dtype] = frozenset(
 )
 
 
-class PandasLikeDataFrame(CompliantDataFrame, CompliantLazyFrame):
+class PandasLikeDataFrame(CompliantDataFrame["PandasLikeSeries"], CompliantLazyFrame):
     # --- not in the spec ---
     def __init__(
         self: Self,
