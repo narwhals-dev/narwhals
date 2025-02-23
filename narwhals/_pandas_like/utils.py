b473d33b5fdbd65e6ba2127dcf9af84333ff4b4b
Dan Redding <125183946+dangotbanned@users.noreply.github.com>
2025-02-23 21:00:23 +0000

    chore(typing): Re-enable `mypy` strict (#2077)

--- narwhals/_pandas_like/utils.py
+++ narwhals/_pandas_like/utils.py
@@ -102,7 +102,7 @@ def align_and_extract_native(
     lhs_index = lhs._native_series.index
 
     if isinstance(rhs, PandasLikeDataFrame):
-        return NotImplemented  # type: ignore[no-any-return]
+        return NotImplemented
 
     if lhs._broadcast and isinstance(rhs, PandasLikeSeries) and not rhs._broadcast:
         return lhs._native_series.iloc[0], rhs._native_series
@@ -309,7 +309,7 @@ def set_index(
         kwargs["copy"] = False
     else:  # pragma: no cover
         pass
-    return obj.set_axis(index, axis=0, **kwargs)  # type: ignore[attr-defined, no-any-return]
+    return obj.set_axis(index, axis=0, **kwargs)  # type: ignore[attr-defined]
 
 
 def set_columns(
@@ -339,7 +339,7 @@ def set_columns(
         kwargs["copy"] = False
     else:  # pragma: no cover
         pass
-    return obj.set_axis(columns, axis=1, **kwargs)  # type: ignore[attr-defined, no-any-return]
+    return obj.set_axis(columns, axis=1, **kwargs)  # type: ignore[attr-defined]
 
 
 def rename(
@@ -353,8 +353,8 @@ def rename(
     if implementation is Implementation.PANDAS and (
         backend_version >= (3,)
     ):  # pragma: no cover
-        return obj.rename(*args, **kwargs)  # type: ignore[attr-defined, no-any-return]
-    return obj.rename(*args, **kwargs, copy=False)  # type: ignore[attr-defined, no-any-return]
+        return obj.rename(*args, **kwargs)  # type: ignore[attr-defined]
+    return obj.rename(*args, **kwargs, copy=False)  # type: ignore[attr-defined]
 
 
 @lru_cache(maxsize=16)
@@ -698,7 +698,7 @@ def int_dtype_mapper(dtype: Any) -> str:
 
 
 def convert_str_slice_to_int_slice(
-    str_slice: slice, columns: pd.Index
+    str_slice: slice, columns: pd.Index[str]
 ) -> tuple[int | None, int | None, int | None]:
     # We can safely cast to int because we know that `columns` doesn't contain duplicates.
     start = (
@@ -716,8 +716,8 @@ def convert_str_slice_to_int_slice(
 
 
 def calculate_timestamp_datetime(
-    s: pd.Series, original_time_unit: str, time_unit: str
-) -> pd.Series:
+    s: pd.Series[int], original_time_unit: str, time_unit: str
+) -> pd.Series[int]:
     if original_time_unit == "ns":
         if time_unit == "ns":
             result = s
@@ -752,7 +752,7 @@ def calculate_timestamp_datetime(
     return result
 
 
-def calculate_timestamp_date(s: pd.Series, time_unit: str) -> pd.Series:
+def calculate_timestamp_date(s: pd.Series[int], time_unit: str) -> pd.Series[int]:
     s = s * 86_400  # number of seconds in a day
     if time_unit == "ns":
         result = s * 1_000_000_000
@@ -787,9 +787,9 @@ def select_columns_by_name(
             raise ColumnNotFoundError.from_missing_and_available_column_names(
                 missing_columns, available_columns
             )
-        return df.loc[:, column_names]  # type: ignore[no-any-return, attr-defined]
+        return df.loc[:, column_names]  # type: ignore[attr-defined]
     try:
-        return df[column_names]  # type: ignore[no-any-return, index]
+        return df[column_names]  # type: ignore[index]
     except KeyError as e:
         available_columns = df.columns.tolist()  # type: ignore[attr-defined]
         missing_columns = [x for x in column_names if x not in available_columns]
@@ -833,7 +833,7 @@ def pivot_table(
     return result
 
 
-def check_column_names_are_unique(columns: pd.Index) -> None:
+def check_column_names_are_unique(columns: pd.Index[str]) -> None:
     try:
         len_unique_columns = len(columns.drop_duplicates())
     except Exception:  # noqa: BLE001  # pragma: no cover
--- narwhals/expr.py
+++ narwhals/expr.py
@@ -177,7 +177,7 @@ class Expr:
         )
 
     def __rand__(self: Self, other: Any) -> Self:
-        return (self & other).alias("literal")
+        return (self & other).alias("literal")  # type: ignore[no-any-return]
 
     def __or__(self: Self, other: Any) -> Self:
         return self.__class__(
@@ -188,7 +188,7 @@ class Expr:
         )
 
     def __ror__(self: Self, other: Any) -> Self:
-        return (self | other).alias("literal")
+        return (self | other).alias("literal")  # type: ignore[no-any-return]
 
     def __add__(self: Self, other: Any) -> Self:
         return self.__class__(
@@ -199,7 +199,7 @@ class Expr:
         )
 
     def __radd__(self: Self, other: Any) -> Self:
-        return (self + other).alias("literal")
+        return (self + other).alias("literal")  # type: ignore[no-any-return]
 
     def __sub__(self: Self, other: Any) -> Self:
         return self.__class__(
@@ -250,7 +250,7 @@ class Expr:
         )
 
     def __rmul__(self: Self, other: Any) -> Self:
-        return (self * other).alias("literal")
+        return (self * other).alias("literal")  # type: ignore[no-any-return]
 
     def __le__(self: Self, other: Any) -> Self:
         return self.__class__(
@@ -1145,12 +1145,12 @@ class Expr:
             ub: CompliantExpr[Any, Any],
         ) -> CompliantExpr[Any, Any]:
             if closed == "left":
-                return (compliant_expr >= lb) & (compliant_expr < ub)  # type: ignore[no-any-return]
+                return (compliant_expr >= lb) & (compliant_expr < ub)
             elif closed == "right":
-                return (compliant_expr > lb) & (compliant_expr <= ub)  # type: ignore[no-any-return]
+                return (compliant_expr > lb) & (compliant_expr <= ub)
             elif closed == "none":
-                return (compliant_expr > lb) & (compliant_expr < ub)  # type: ignore[no-any-return]
-            return (compliant_expr >= lb) & (compliant_expr <= ub)  # type: ignore[no-any-return]
+                return (compliant_expr > lb) & (compliant_expr < ub)
+            return (compliant_expr >= lb) & (compliant_expr <= ub)
 
         return self.__class__(
             lambda plx: apply_n_ary_operation(
