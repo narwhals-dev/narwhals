e76d05f40593598d61bc576553f7053e609e3d79
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-02-26 15:32:19 +0000

    test: add `test_iter_columns`

--- tests/frame/columns_test.py
+++ tests/frame/columns_test.py
@@ -8,6 +8,9 @@ import narwhals.stable.v1 as nw
 
 if TYPE_CHECKING:
     from tests.utils import Constructor
+    from tests.utils import ConstructorEager
+
+data = {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8.0, 9.0]}
 
 
 @pytest.mark.filterwarnings("ignore:Determining|Resolving.*")
@@ -16,4 +19,11 @@ def test_columns(constructor: Constructor) -> None:
     df = nw.from_native(constructor(data))
     result = df.columns
     expected = ["a", "b", "z"]
+    assert result == expected
+
+
+def test_iter_columns(constructor_eager: ConstructorEager) -> None:
+    df = nw.from_native(constructor_eager(data), eager_only=True)
+    expected = df.to_dict(as_series=True)
+    result = {series.name: series for series in df.iter_columns()}
     assert result == expected
