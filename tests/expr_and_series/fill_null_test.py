fbb347e815e0d808aa546ef0effbbbeced250a69
Jan Pipek <jan.pipek@gmail.com>
2025-02-27 06:30:34 +0100

    failing tests

--- tests/expr_and_series/fill_null_test.py
+++ tests/expr_and_series/fill_null_test.py
@@ -31,6 +31,25 @@ def test_fill_null(request: pytest.FixtureRequest, constructor: Constructor) -> 
     assert_equal_data(result, expected)
 
 
+def test_fill_null_with_expression(request: pytest.FixtureRequest, constructor: Constructor) -> None:
+    if "pyspark" in str(constructor):
+        request.applymarker(pytest.mark.xfail)
+    data = {
+        "a": [0.0, None, 2.0, 3.0, 4.0],
+        "b": [1.0, None, None, 5.0, 3.0],
+        "c": [5.0, 2.0, None, 2.0, 1.0],
+    }
+    df = nw.from_native(constructor(data))
+
+    result = df.with_columns(nw.col("a", "b").fill_null(nw.col("c")))
+    expected = {
+        "a": [0.0, 2, 2, 3, 4],
+        "b": [1.0, 2, None, 5, 3],
+        "c": [5.0, 2, 3, 2, 1],
+    }
+    assert_equal_data(result, expected)
+
+
 def test_fill_null_exceptions(constructor: Constructor) -> None:
     data = {
         "a": [0.0, None, 2.0, 3.0, 4.0],
