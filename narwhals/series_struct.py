e54663c6f6bea002f17cd4961464ca6e31545970
osoucy <osoucy.transactions@gmail.com>
2025-03-05 01:37:59 -0500

    Addressed PR comment

--- narwhals/series_struct.py
+++ narwhals/series_struct.py
@@ -37,16 +37,12 @@ class SeriesStructNamespace(Generic[SeriesT]):
             ... )
             >>> s = nw.from_native(s_native, series_only=True)
             >>> s.struct.field("name").to_native()
-            ┌────────────────────┐
-            |  Narwhals Series   |
-            |--------------------|
-            |shape: (2,)         |
-            |Series: 'name' [str]|
-            |[                   |
-            |	"john"           |
-            |	"jane"           |
-            |]                   |
-            └────────────────────┘
+            shape: (2,)
+            Series: 'name' [str]
+            [
+                "john"
+                "jane"
+            ]
         """
         return self._narwhals_series._from_compliant_series(
             self._narwhals_series._compliant_series.struct.field(name)
