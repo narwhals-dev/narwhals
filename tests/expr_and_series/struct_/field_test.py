59fb5673a87ddb49c5236338fb35e2761463ac9a
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-09 16:40:33 +0000

    remove unnecessary aliases

--- tests/expr_and_series/struct_/field_test.py
+++ tests/expr_and_series/struct_/field_test.py
@@ -7,14 +7,11 @@ import pytest
 import narwhals.stable.v1 as nw
 from tests.utils import PANDAS_VERSION
 from tests.utils import Constructor
+from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
 
-data = {"user": [{"id": "0", "name": "john"}, {"id": "1", "name": "jane"}]}
-expected = {"id": ["0", "1"], "name": ["john", "jane"]}
 
-
-@pytest.mark.skipif(PANDAS_VERSION < (2, 2, 0), reason="old pandas")
-def test_get_field(
+def test_get_field_expr(
     request: pytest.FixtureRequest,
     constructor: Constructor,
 ) -> None:
@@ -22,8 +19,10 @@ def test_get_field(
         backend in str(constructor) for backend in ("dask", "modin", "cudf", "sqlframe")
     ):
         request.applymarker(pytest.mark.xfail)
+    if "pandas" in str(constructor) and PANDAS_VERSION < (2, 2, 0):
+        pytest.skip()
+    data = {"user": [{"id": "0", "name": "john"}, {"id": "1", "name": "jane"}]}
 
-    _expected = expected.copy()
     df_native = constructor(data)
 
     if "pandas" in str(constructor):
@@ -42,37 +41,44 @@ def test_get_field(
         nw.col("user").struct.field("id"),
         nw.col("user").struct.field("name"),
     )
+    expected = {"id": ["0", "1"], "name": ["john", "jane"]}
+    assert_equal_data(result, expected)
+    result = nw.from_native(df).select(
+        nw.col("user").struct.field("id").name.keep(),
+    )
+    expected = {"user": ["0", "1"]}
+    assert_equal_data(result, expected)
 
+
+def test_get_field_series(
+    request: pytest.FixtureRequest,
+    constructor_eager: ConstructorEager,
+) -> None:
+    if any(backend in str(constructor_eager) for backend in ("modin", "cudf")):
+        request.applymarker(pytest.mark.xfail)
+    if "pandas" in str(constructor_eager) and PANDAS_VERSION < (2, 2, 0):
+        pytest.skip()
+    data = {"user": [{"id": "0", "name": "john"}, {"id": "1", "name": "jane"}]}
+    expected = {"id": ["0", "1"], "name": ["john", "jane"]}
+
+    _expected = expected.copy()
+    df_native = constructor_eager(data)
+
+    if "pandas" in str(constructor_eager):
+        df_native = df_native.assign(  # type: ignore[union-attr]
+            user=pd.Series(
+                data["user"],
+                dtype=pd.ArrowDtype(
+                    pa.struct([("id", pa.string()), ("name", pa.string())])
+                ),
+            )
+        )
+
+    df = nw.from_native(df_native, eager_only=True)
+
+    result = nw.from_native(df).select(
+        df["user"].struct.field("id"),
+        df["user"].struct.field("name"),
+    )
+    expected = {"id": ["0", "1"], "name": ["john", "jane"]}
     assert_equal_data(result, _expected)
-
-
-def test_polars_series_get_field() -> None:
-    import polars as pl
-
-    import narwhals as nw
-
-    s_native = pl.Series(
-        [
-            {"id": "0", "name": "john"},
-            {"id": "1", "name": "jane"},
-        ]
-    )
-    s = nw.from_native(s_native, series_only=True)
-    assert s.struct.field("name").to_list() == ["john", "jane"]
-
-
-def test_pandas_object_series() -> None:
-    import pandas as pd
-
-    import narwhals as nw
-
-    s_native = pd.Series(
-        data=[
-            {"id": "0", "name": "john"},
-            {"id": "1", "name": "jane"},
-        ]
-    )
-    s = nw.from_native(s_native, series_only=True)
-
-    with pytest.raises(TypeError):
-        s.struct.field("name")
