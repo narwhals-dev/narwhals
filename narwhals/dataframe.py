1f67693055251e0d8cd2b6530bf1b5a1026dba23
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-13 16:06:46 +0000

    chore: force github to let me start a thread

--- narwhals/dataframe.py
+++ narwhals/dataframe.py
@@ -432,7 +432,7 @@ class DataFrame(BaseFrame[DataFrameT]):
         if isinstance(arg, Series):
             return arg._compliant_series._to_expr()
         if isinstance(arg, Expr):
-            return arg._to_compliant_expr(self.__narwhals_namespace__())
+            return arg._to_compliant_expr(self.__narwhals_namespace__())  # comment
         if isinstance(arg, str):
             return plx.col(arg)
         if get_polars() is not None and "polars" in str(type(arg)):  # pragma: no cover
