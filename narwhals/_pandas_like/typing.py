b114d47b536c975520efc053d1a3e19409734ef5
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-16 20:34:28 +0000

    chore(typing): Add constrained `TypeVar`(s) for `pandas`

--- narwhals/_pandas_like/typing.py
+++ narwhals/_pandas_like/typing.py
@@ -5,13 +5,40 @@ from typing import Union  # pragma: no cover
 
 if TYPE_CHECKING:
     import sys
+    from typing import Any
+
+    if sys.version_info >= (3, 13):
+        from typing import TypeVar
+    else:
+        from typing_extensions import TypeVar
 
     if sys.version_info >= (3, 10):
         from typing import TypeAlias
     else:
         from typing_extensions import TypeAlias
 
+    import cudf
+    import modin.pandas as mpd
+    import pandas as pd
+
     from narwhals._pandas_like.expr import PandasLikeExpr
     from narwhals._pandas_like.series import PandasLikeSeries
 
     IntoPandasLikeExpr: TypeAlias = Union[PandasLikeExpr, PandasLikeSeries]
+
+    DataFrameT = TypeVar(
+        "DataFrameT", pd.DataFrame, mpd.DataFrame, cudf.DataFrame, default=pd.DataFrame
+    )
+    SeriesT = TypeVar(
+        "SeriesT", pd.Series[Any], mpd.Series, cudf.Series[Any], default=pd.Series[Any]
+    )
+    NDFrameT = TypeVar(
+        "NDFrameT",
+        pd.DataFrame,
+        mpd.DataFrame,
+        cudf.DataFrame,
+        pd.Series[Any],
+        mpd.Series,
+        cudf.Series[Any],
+        default=pd.DataFrame,
+    )
