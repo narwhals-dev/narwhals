54e2e9f81bacaac04e89c6a929e3c0769b6b9c9e
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-17 19:10:55 +0000

    fix: Mark `ArrowSeries.ewm_mean` as `not_implemented`

--- narwhals/_arrow/series.py
+++ narwhals/_arrow/series.py
@@ -32,6 +32,7 @@ from narwhals.exceptions import InvalidOperationError
 from narwhals.utils import Implementation
 from narwhals.utils import generate_temporary_column_name
 from narwhals.utils import import_dtypes_module
+from narwhals.utils import not_implemented
 from narwhals.utils import validate_backend_version
 
 if TYPE_CHECKING:
@@ -1207,3 +1208,5 @@ class ArrowSeries(EagerSeries["ArrowChunkedArray"]):
     @property
     def struct(self: Self) -> ArrowSeriesStructNamespace:
         return ArrowSeriesStructNamespace(self)
+
+    ewm_mean = not_implemented()
