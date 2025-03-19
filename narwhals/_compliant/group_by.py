16a9230996788bfdb1332ba54db81c86afe8ba0a
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-19 18:56:06 +0000

    fix: `3.8` compat
    Needed for `__init__` support

--- narwhals/_compliant/group_by.py
+++ narwhals/_compliant/group_by.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 import re
+import sys
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import ClassVar
 from typing import Iterator
 from typing import Mapping
-from typing import Protocol
 from typing import Sequence
 
 from narwhals._compliant.typing import CompliantDataFrameT
@@ -23,13 +23,23 @@ if TYPE_CHECKING:
 
     Frame: TypeAlias = "CompliantDataFrame[Any, Any, NativeFrameT_co] | CompliantLazyFrame[Any, NativeFrameT_co]"
 
+if not TYPE_CHECKING:  # pragma: no cover
+    if sys.version_info >= (3, 9):
+        from typing import Protocol as Protocol38
+    else:
+        from typing import Generic as Protocol38
+else:  # pragma: no cover
+    # TODO @dangotbanned: Remove after dropping `3.8` (#2084)
+    # - https://github.com/narwhals-dev/narwhals/pull/2064#discussion_r1965921386
+    from typing import Protocol as Protocol38
+
 __all__ = ["CompliantGroupBy", "EagerGroupBy"]
 
 
 # NOTE: Type checkers disagree
 # - `pyright` wants invariant `*Expr`
 # - `mypy` want contravariant `*Expr`
-class CompliantGroupBy(Protocol[CompliantFrameT, CompliantExprT]):  # type: ignore[misc]
+class CompliantGroupBy(Protocol38[CompliantFrameT, CompliantExprT]):  # type: ignore[misc]
     _NARWHALS_TO_NATIVE_AGGREGATIONS: ClassVar[Mapping[str, Any]]
     _compliant_frame: CompliantFrameT
     _keys: Sequence[str]
@@ -60,7 +70,7 @@ class CompliantGroupBy(Protocol[CompliantFrameT, CompliantExprT]):  # type: igno
                 and re.sub(r"(\w+->)", "", expr._function_name)
                 in self._NARWHALS_TO_NATIVE_AGGREGATIONS
             ):
-                # NOTE: Need to define `_implementation` in both protocols
+                # NOTE: Need to define `_implementation` in both protocols (#2251)
                 name = self.compliant._implementation.name.lower()  # type: ignore  # noqa: PGH003
                 msg = (
                     f"Non-trivial complex aggregation found.\n\n"
@@ -77,6 +87,6 @@ class CompliantGroupBy(Protocol[CompliantFrameT, CompliantExprT]):  # type: igno
 
 class EagerGroupBy(  # type: ignore[misc]
     CompliantGroupBy[CompliantDataFrameT, CompliantExprT],
-    Protocol[CompliantDataFrameT, CompliantExprT],
+    Protocol38[CompliantDataFrameT, CompliantExprT],
 ):
     def __iter__(self) -> Iterator[tuple[Any, CompliantDataFrameT]]: ...
