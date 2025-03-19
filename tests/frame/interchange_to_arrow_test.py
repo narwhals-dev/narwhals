63b9e2a047a43206274a5359eb3a73112fbb8b84
Michał Górny <mgorny@gentoo.org>
2025-03-19 12:45:23 +0100

    Fix ruff

--- tests/frame/interchange_to_arrow_test.py
+++ tests/frame/interchange_to_arrow_test.py
@@ -12,11 +12,11 @@ data: Mapping[str, Any] = {"a": [1, 2, 3], "b": [4.0, 5.0, 6.1], "z": ["x", "y",
 pytest.importorskip("polars")
 pytest.importorskip("pyarrow")
 
-import polars as pl
-import pyarrow as pa
-
 
 def test_interchange_to_arrow() -> None:
+    import polars as pl
+    import pyarrow as pa
+
     df_pl = pl.DataFrame(data)
     df = nw.from_native(df_pl.__dataframe__(), eager_or_interchange_only=True)
     result = df.to_arrow()
@@ -28,7 +28,10 @@ def test_interchange_ibis_to_arrow(
     tmpdir: pytest.TempdirFactory, request: pytest.FixtureRequest
 ) -> None:  # pragma: no cover
     pytest.importorskip("ibis")
+
     import ibis
+    import polars as pl
+    import pyarrow as pa
 
     try:
         ibis.set_backend("duckdb")
@@ -48,7 +51,10 @@ def test_interchange_ibis_to_arrow(
 
 def test_interchange_duckdb_to_arrow() -> None:
     pytest.importorskip("duckdb")
+
     import duckdb
+    import polars as pl
+    import pyarrow as pa
 
     df_pl = pl.DataFrame(data)  # noqa: F841
     rel = duckdb.sql("select * from df_pl")
