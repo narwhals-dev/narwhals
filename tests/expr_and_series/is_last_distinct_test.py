a3098936269cabde85b2e48a4706bdfa17bade56
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-22 22:46:46 +0000

    skip for old polars

--- tests/expr_and_series/is_last_distinct_test.py
+++ tests/expr_and_series/is_last_distinct_test.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import pytest
+
 import narwhals.stable.v1 as nw
+from tests.utils import POLARS_VERSION
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
 
@@ -22,6 +25,8 @@ def test_is_last_distinct_expr(constructor_eager: ConstructorEager) -> None:
 
 def test_is_last_distinct_expr_all(constructor_eager: ConstructorEager) -> None:
     # https://github.com/narwhals-dev/narwhals/issues/2268
+    if "polars" in str(constructor_eager) and POLARS_VERSION < (1, 9):
+        pytest.skip(reason="too old version")
     data = {"a": [1, 1, 2, 3, 2], "b": [1, 2, 3, 2, 1], "i": [0, 1, 2, 3, 4]}
     df = nw.from_native(constructor_eager(data))
     result = df.select(nw.all().is_last_distinct().over(_order_by="i"))
