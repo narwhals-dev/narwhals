f2149e6423b3d6c73aedd868edd49afa63f4eda6
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-22 12:48:00 +0000

    Make annotations readable again

--- narwhals/_compliant/when_then.py
+++ narwhals/_compliant/when_then.py
@@ -5,12 +5,13 @@ from typing import TYPE_CHECKING
 from typing import Any
 from typing import Callable
 from typing import Sequence
+from typing import TypeVar
 from typing import cast
 
 from narwhals._compliant.expr import CompliantExpr
-from narwhals._compliant.typing import CompliantExprT
-from narwhals._compliant.typing import CompliantFrameT
-from narwhals._compliant.typing import CompliantSeriesOrNativeExprT
+from narwhals._compliant.typing import CompliantExprAny
+from narwhals._compliant.typing import CompliantFrameAny
+from narwhals._compliant.typing import CompliantSeriesOrNativeExprAny
 
 if TYPE_CHECKING:
     from typing_extensions import Self
@@ -30,39 +31,38 @@ else:  # pragma: no cover
     # - https://github.com/narwhals-dev/narwhals/pull/2064#discussion_r1965921386
     from typing import Protocol as Protocol38
 
-_Scalar: TypeAlias = Any
-
 __all__ = ["CompliantThen", "CompliantWhen"]
 
+ExprT = TypeVar("ExprT", bound=CompliantExprAny)
+SeriesT = TypeVar("SeriesT", bound=CompliantSeriesOrNativeExprAny)
+FrameT = TypeVar("FrameT", bound=CompliantFrameAny)
 
-class CompliantWhen(
-    Protocol38[CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT]
-):
-    _condition: CompliantExprT
-    _then_value: CompliantExprT | CompliantSeriesOrNativeExprT | _Scalar
-    _otherwise_value: CompliantExprT | CompliantSeriesOrNativeExprT | _Scalar
+Scalar: TypeAlias = Any
+"""A native or python literal value."""
+
+IntoExpr: TypeAlias = "SeriesT | ExprT | Scalar"
+"""Anything that is convertable into a `CompliantExpr`."""
+
+
+class CompliantWhen(Protocol38[FrameT, SeriesT, ExprT]):
+    _condition: ExprT
+    _then_value: IntoExpr[SeriesT, ExprT]
+    _otherwise_value: IntoExpr[SeriesT, ExprT]
     _implementation: Implementation
     _backend_version: tuple[int, ...]
     _version: Version
 
     @property
-    def _then(
-        self,
-    ) -> type[
-        CompliantThen[CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT]
-    ]: ...
-
-    def __call__(
-        self, compliant_frame: CompliantFrameT, /
-    ) -> Sequence[CompliantSeriesOrNativeExprT]: ...
+    def _then(self) -> type[CompliantThen[FrameT, SeriesT, ExprT]]: ...
+    def __call__(self, compliant_frame: FrameT, /) -> Sequence[SeriesT]: ...
 
     def then(
-        self, value: CompliantExprT | CompliantSeriesOrNativeExprT | _Scalar, /
-    ) -> CompliantThen[CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT]:
+        self, value: IntoExpr[SeriesT, ExprT], /
+    ) -> CompliantThen[FrameT, SeriesT, ExprT]:
         return self._then.from_when(self, value)
 
     @classmethod
-    def from_expr(cls, condition: CompliantExprT, /, *, context: _FullContext) -> Self:
+    def from_expr(cls, condition: ExprT, /, *, context: _FullContext) -> Self:
         obj = cls.__new__(cls)
         obj._condition = condition
         obj._then_value = None
@@ -73,14 +73,9 @@ class CompliantWhen(
         return obj
 
 
-class CompliantThen(
-    CompliantExpr[CompliantFrameT, CompliantSeriesOrNativeExprT],
-    Protocol38[CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT],
-):
-    _call: Callable[[CompliantFrameT], Sequence[CompliantSeriesOrNativeExprT]]
-    _when_value: CompliantWhen[
-        CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT
-    ]
+class CompliantThen(CompliantExpr[FrameT, SeriesT], Protocol38[FrameT, SeriesT, ExprT]):
+    _call: Callable[[FrameT], Sequence[SeriesT]]
+    _when_value: CompliantWhen[FrameT, SeriesT, ExprT]
     _function_name: str
     _implementation: Implementation
     _backend_version: tuple[int, ...]
@@ -90,10 +85,8 @@ class CompliantThen(
     @classmethod
     def from_when(
         cls,
-        when: CompliantWhen[
-            CompliantFrameT, CompliantSeriesOrNativeExprT, CompliantExprT
-        ],
-        then: CompliantExprT | CompliantSeriesOrNativeExprT | _Scalar,
+        when: CompliantWhen[FrameT, SeriesT, ExprT],
+        then: IntoExpr[SeriesT, ExprT],
         /,
     ) -> Self:
         when._then_value = then
@@ -112,9 +105,7 @@ class CompliantThen(
         obj._call_kwargs = {}
         return obj
 
-    def otherwise(
-        self, otherwise: CompliantExprT | CompliantSeriesOrNativeExprT | _Scalar, /
-    ) -> CompliantExprT:
+    def otherwise(self, otherwise: IntoExpr[SeriesT, ExprT], /) -> ExprT:
         self._when_value._otherwise_value = otherwise
         self._function_name = "whenotherwise"
-        return cast("CompliantExprT", self)
+        return cast("ExprT", self)
