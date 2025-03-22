e293e0e06a2f864cf4eb5c25534e9ddc7e0f575c
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-22 22:38:17 +0000

    fix: safety assertion was being thrown for Expr.is_last_distinct with over and _order_by by pandas and pyarrow

--- tests/expr_and_series/is_last_distinct_test.py
+++ tests/expr_and_series/is_last_distinct_test.py
@@ -20,6 +20,19 @@ def test_is_last_distinct_expr(constructor_eager: ConstructorEager) -> None:
     assert_equal_data(result, expected)
 
 
+def test_is_last_distinct_expr_all(constructor_eager: ConstructorEager) -> None:
+    # https://github.com/narwhals-dev/narwhals/issues/2268
+    data = {"a": [1, 1, 2, 3, 2], "b": [1, 2, 3, 2, 1], "i": [0, 1, 2, 3, 4]}
+    df = nw.from_native(constructor_eager(data))
+    result = df.select(nw.all().is_last_distinct().over(_order_by="i"))
+    expected = {
+        "a": [False, True, False, True, True],
+        "b": [False, False, True, True, True],
+        "i": [True, True, True, True, True],
+    }
+    assert_equal_data(result, expected)
+
+
 def test_is_last_distinct_series(constructor_eager: ConstructorEager) -> None:
     series = nw.from_native(constructor_eager(data), eager_only=True)["a"]
     result = series.is_last_distinct()
