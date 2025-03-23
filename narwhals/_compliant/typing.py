6ec7272f2818899b4a6231125ea0438ed4bb4ef2
Dan Redding <125183946+dangotbanned@users.noreply.github.com>
2025-03-23 18:37:46 +0000

    chore(typing): Move `DepthTracking*` typing (#2272)

--- narwhals/_compliant/typing.py
+++ narwhals/_compliant/typing.py
@@ -13,6 +13,7 @@ if TYPE_CHECKING:
     from narwhals._compliant.dataframe import CompliantLazyFrame
     from narwhals._compliant.dataframe import EagerDataFrame
     from narwhals._compliant.expr import CompliantExpr
+    from narwhals._compliant.expr import DepthTrackingExpr
     from narwhals._compliant.expr import EagerExpr
     from narwhals._compliant.expr import LazyExpr
     from narwhals._compliant.expr import NativeExpr
@@ -39,6 +40,8 @@ CompliantSeriesOrNativeExprAny: TypeAlias = "CompliantSeriesAny | NativeExpr"
 CompliantDataFrameAny: TypeAlias = "CompliantDataFrame[Any, Any, Any]"
 CompliantLazyFrameAny: TypeAlias = "CompliantLazyFrame[Any, Any]"
 CompliantFrameAny: TypeAlias = "CompliantDataFrameAny | CompliantLazyFrameAny"
+
+DepthTrackingExprAny: TypeAlias = "DepthTrackingExpr[Any, Any]"
 
 EagerDataFrameAny: TypeAlias = "EagerDataFrame[Any, Any, Any]"
 EagerSeriesAny: TypeAlias = "EagerSeries[Any]"
@@ -81,6 +84,11 @@ CompliantLazyFrameT_co = TypeVar(
     "CompliantLazyFrameT_co", bound=CompliantLazyFrameAny, covariant=True
 )
 IntoCompliantExpr: TypeAlias = "CompliantExpr[CompliantFrameT, CompliantSeriesOrNativeExprT_co] | CompliantSeriesOrNativeExprT_co"
+
+DepthTrackingExprT = TypeVar("DepthTrackingExprT", bound=DepthTrackingExprAny)
+DepthTrackingExprT_contra = TypeVar(
+    "DepthTrackingExprT_contra", bound=DepthTrackingExprAny, contravariant=True
+)
 
 EagerExprT = TypeVar("EagerExprT", bound=EagerExprAny)
 EagerExprT_contra = TypeVar("EagerExprT_contra", bound=EagerExprAny, contravariant=True)
