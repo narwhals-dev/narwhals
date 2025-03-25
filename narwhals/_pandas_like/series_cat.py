f4c0cd6a3e42dd31e88beb20e629e7151c74f786
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-25 18:52:01 +0000

    feat(DRAFT): Implement `PandasLikeSeries.cat`
    Pushing to try out `3.8`& `3.10` bugs in CI again

--- narwhals/_pandas_like/series_cat.py
+++ narwhals/_pandas_like/series_cat.py
@@ -1,19 +1,15 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from typing import Any
+
+from narwhals._compliant.series import EagerSeriesCatNamespace
 
 if TYPE_CHECKING:
-    from typing_extensions import Self
-
     from narwhals._pandas_like.series import PandasLikeSeries
 
 
-class PandasLikeSeriesCatNamespace:
-    def __init__(self: Self, series: PandasLikeSeries) -> None:
-        self._compliant_series = series
-
-    def get_categories(self: Self) -> PandasLikeSeries:
-        s = self._compliant_series._native_series
-        return self._compliant_series._from_native_series(
-            s.__class__(s.cat.categories, name=s.name)
-        )
+class PandasLikeSeriesCatNamespace(EagerSeriesCatNamespace["PandasLikeSeries", Any]):
+    def get_categories(self) -> PandasLikeSeries:
+        s = self.native
+        return self.from_native(type(s)(s.cat.categories, name=s.name))
