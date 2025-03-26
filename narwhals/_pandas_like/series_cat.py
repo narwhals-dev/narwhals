afd9fc6eae13cd227ad0f6f2bac7efd9e2f2adc5
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-26 17:32:57 +0000

    fix(typing): Fill incorrect `Any`
    I forgot that one isn't for `pd.Series`

--- narwhals/_pandas_like/series_cat.py
+++ narwhals/_pandas_like/series_cat.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from typing import Any
 
 from narwhals._compliant.any_namespace import CatNamespace
 from narwhals._pandas_like.utils import PandasLikeSeriesNamespace
@@ -10,7 +9,9 @@ if TYPE_CHECKING:
     from narwhals._pandas_like.series import PandasLikeSeries
 
 
-class PandasLikeSeriesCatNamespace(PandasLikeSeriesNamespace, CatNamespace[Any]):
+class PandasLikeSeriesCatNamespace(
+    PandasLikeSeriesNamespace, CatNamespace["PandasLikeSeries"]
+):
     def get_categories(self) -> PandasLikeSeries:
         s = self.native
         return self.from_native(type(s)(s.cat.categories, name=s.name))
