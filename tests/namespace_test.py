eae54c4b1fc7444831a47eec0dd1f4c9fd0ad3ce
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-03-31 21:37:46 +0100

    test: Add `test_namespace_from_native_object`

--- tests/namespace_test.py
+++ tests/namespace_test.py
@@ -4,10 +4,12 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+import narwhals as nw
 from narwhals._namespace import Namespace
 
 if TYPE_CHECKING:
     from narwhals._namespace import BackendName
+    from tests.utils import Constructor
 
 
 @pytest.mark.parametrize(
@@ -28,3 +30,11 @@ def test_namespace_from_backend_name(name: BackendName) -> None:
     pytest.importorskip(name)
     namespace = Namespace.from_backend(name)
     assert namespace.implementation.name.lower() == name
+
+
+def test_namespace_from_native_object(constructor: Constructor) -> None:
+    data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+    frame = constructor(data)
+    namespace = Namespace.from_native_object(frame)
+    nw_frame = nw.from_native(frame)
+    assert namespace.implementation == nw_frame.implementation
