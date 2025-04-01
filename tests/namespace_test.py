571bd6540bc27a1f133865924f3d104b5af604ba
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-04-01 13:07:10 +0100

    test: Deeper integration test
    These are the *kinds* of tasks I'm hoping to simplify and introduce typing to

--- tests/namespace_test.py
+++ tests/namespace_test.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+from collections import deque
 from typing import TYPE_CHECKING
+from typing import Any
+from typing import Callable
+from typing import Iterable
+from typing import Sequence
 
 import pytest
 
@@ -8,6 +13,7 @@ import narwhals as nw
 from narwhals._namespace import Namespace
 
 if TYPE_CHECKING:
+    from typing_extensions import TypeAlias
     from typing_extensions import assert_type
 
     from narwhals._arrow.namespace import ArrowNamespace  # noqa: F401
@@ -16,6 +22,8 @@ if TYPE_CHECKING:
     from narwhals._pandas_like.namespace import PandasLikeNamespace  # noqa: F401
     from narwhals._polars.namespace import PolarsNamespace  # noqa: F401
     from tests.utils import Constructor
+
+IntoIterable: TypeAlias = Callable[[Sequence[Any]], Iterable[Any]]
 
 _EAGER_ALLOWED = "polars", "pandas", "pyarrow", "modin", "cudf"
 _LAZY_ONLY = "dask", "duckdb", "pyspark", "sqlframe"
@@ -56,3 +64,25 @@ def test_namespace_from_backend_typing(backend: _eager_allowed) -> None:
         "Namespace[PandasLikeNamespace]",
         "Namespace[ArrowNamespace]",
     }
+
+
+@pytest.mark.parametrize("into_iter", [list, tuple, deque, iter])
+@eager_allowed
+def test_namespace_series_from_iterable(
+    backend: _eager_allowed, into_iter: IntoIterable
+) -> None:
+    pytest.importorskip(backend)
+    data = 1, 2, 3
+    namespace = Namespace.from_backend(backend)
+    compliant = namespace.compliant
+    iterable = into_iter(data)
+    compliant_series = compliant._series.from_iterable(
+        iterable, context=compliant, name="hello"
+    )
+    # BUG: `@overload`(s) for `from_native` fail to mentioned `Compliant` support
+    # Fix *should be*:
+    #     `IntoSeries: TypeAlias = Series[Any] | NativeSeries | CompliantSeries[Any]`
+    # Can't do that as, `PolarsSeries` is not compliant yet
+    series = nw.from_native(compliant_series, series_only=True)  # pyright: ignore[reportCallIssue, reportArgumentType]
+    assert isinstance(series, nw.Series)
+    assert series.to_list() == list(data)
