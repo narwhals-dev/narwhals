21e80efd5e90f428c828219c31142c7326d3fbc9
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-04-07 15:23:40 +0100

    fix: Implement `to_numpy` to catch args
    https://docs.pola.rs/api/python/stable/reference/dataframe/api/polars.DataFrame.to_numpy.html

--- narwhals/_polars/dataframe.py
+++ narwhals/_polars/dataframe.py
@@ -76,7 +76,6 @@ class PolarsDataFrame:
     select: Method[Self]
     sort: Method[Self]
     to_arrow: Method[pa.Table]
-    to_numpy: Method[_2DArray]
     to_pandas: Method[pd.DataFrame]
     unique: Method[Self]
     with_columns: Method[Self]
@@ -230,6 +229,9 @@ class PolarsDataFrame:
         if self._backend_version < (0, 20, 28):
             return self.native.__array__(dtype)
         return self.native.__array__(dtype)
+
+    def to_numpy(self, dtype: Any = None, *, copy: bool | None = None) -> _2DArray:
+        return self.native.to_numpy()
 
     def collect_schema(self: Self) -> dict[str, DType]:
         if self._backend_version < (1,):
