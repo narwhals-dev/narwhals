0d05cf9bcbb209bb85adc9ad99aa253a92afb912
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-04-11 18:19:25 +0100

    chore!: use Python API more for DuckDB support

--- narwhals/_duckdb/dataframe.py
+++ narwhals/_duckdb/dataframe.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 from functools import reduce
 from operator import and_
 from typing import TYPE_CHECKING
@@ -21,6 +22,7 @@ from narwhals.dependencies import get_duckdb
 from narwhals.exceptions import ColumnNotFoundError
 from narwhals.exceptions import InvalidOperationError
 from narwhals.typing import CompliantDataFrame
+from narwhals.typing import CompliantLazyFrame
 from narwhals.utils import Implementation
 from narwhals.utils import Version
 from narwhals.utils import generate_temporary_column_name
@@ -48,7 +50,8 @@ if TYPE_CHECKING:
     from narwhals.typing import LazyUniqueKeepStrategy
     from narwhals.utils import _FullContext
 
-from narwhals.typing import CompliantLazyFrame
+with contextlib.suppress(ImportError):  # requires duckdb>=1.3.0
+    from duckdb import SQLExpression  # type: ignore[attr-defined, unused-ignore]
 
 
 class DuckDBLazyFrame(CompliantLazyFrame["DuckDBExpr", "duckdb.DuckDBPyRelation"]):
@@ -359,6 +362,12 @@ class DuckDBLazyFrame(CompliantLazyFrame["DuckDBExpr", "duckdb.DuckDBPyRelation"
         self, subset: Sequence[str] | None, *, keep: LazyUniqueKeepStrategy
     ) -> Self:
         if subset_ := subset if keep == "any" else (subset or self.columns):
+            if self._backend_version < (1, 3):
+                msg = (
+                    "At least version 1.3 of DuckDB is required for `unique` operation\n"
+                    "with `subset` specified."
+                )
+                raise NotImplementedError(msg)
             # Sanitise input
             if any(x not in self.columns for x in subset_):
                 msg = f"Columns {set(subset_).difference(self.columns)} not found in {self.columns}."
@@ -366,16 +375,17 @@ class DuckDBLazyFrame(CompliantLazyFrame["DuckDBExpr", "duckdb.DuckDBPyRelation"
             idx_name = generate_temporary_column_name(8, self.columns)
             count_name = generate_temporary_column_name(8, [*self.columns, idx_name])
             partition_by_sql = generate_partition_by_sql(*(subset_))
-            rel = self.native  # noqa: F841
-            query = f"""
-                select *,
-                        row_number() over ({partition_by_sql}) as "{idx_name}",
-                        count(*) over ({partition_by_sql}) as "{count_name}"
-                from rel
-                """  # noqa: S608
             name = count_name if keep == "none" else idx_name
             return self._with_native(
-                duckdb.sql(query)
+                self.native.select(
+                    StarExpression(),
+                    SQLExpression(
+                        f"{FunctionExpression('row_number')} over ({partition_by_sql})"
+                    ).alias(idx_name),
+                    SQLExpression(
+                        f"{FunctionExpression('count', StarExpression())} over ({partition_by_sql})"
+                    ).alias(count_name),
+                )
                 .filter(col(name) == lit(1))
                 .select(StarExpression(exclude=[count_name, idx_name]))
             )
