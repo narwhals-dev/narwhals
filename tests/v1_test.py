73c7d768579eaa365002a9bfe38eda1388f819ac
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-04-12 15:46:42 +0100

    io

--- tests/v1_test.py
+++ tests/v1_test.py
@@ -1,6 +1,11 @@
 # Test assorted functions which we overwrite in stable.v1
 from __future__ import annotations
 
+from typing import Any
+
+import numpy as np
+import pandas as pd
+
 import narwhals.stable.v1 as nw_v1
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
@@ -33,3 +38,28 @@ def test_agg_shorthands(constructor_eager: ConstructorEager) -> None:
         "len": [3, 3, 3],
     }
     assert_equal_data(result, expected)
+
+
+def test_when_then(constructor_eager: ConstructorEager) -> None:
+    df = nw_v1.from_native(
+        constructor_eager({"a": [1, 2, 3], "b": [4, 5, 6], "c": [6, 7, 8]})
+    )
+    result = df.select(nw_v1.when(nw_v1.col("a") > 1).then("b").otherwise("c"))
+    expected = {"b": [6, 5, 6]}
+    assert_equal_data(result, expected)
+
+
+def test_constructors() -> None:
+    assert nw_v1.new_series("a", [1, 2, 3], backend="pandas").to_list() == [1, 2, 3]
+    arr: np.ndarray[tuple[int, int], Any] = np.array([[1, 2], [3, 4]])  # pyright: ignore[reportAssignmentType]
+    assert_equal_data(
+        nw_v1.from_numpy(arr, schema=["a", "b"], backend="pandas"),
+        {"a": [1, 3], "b": [2, 4]},
+    )
+    assert_equal_data(
+        nw_v1.from_dict({"a": [1, 2, 3]}, backend="pandas"), {"a": [1, 2, 3]}
+    )
+    assert_equal_data(
+        nw_v1.from_arrow(pd.DataFrame({"a": [1, 2, 3]}), backend="pandas"),
+        {"a": [1, 2, 3]},
+    )
