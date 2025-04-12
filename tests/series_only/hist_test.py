939e45098fbbef483566e41766c6d3da76daa8f3
Cameron <96146940+camriddell@users.noreply.github.com>
2025-04-12 01:53:16 -0700

    Fix/hist new polars (#2374)
    * enh hist to always include lowest bin
    
    * fix hist(bin_count=…) when data is empty
    
    * add shuffling to simple smoke tests
    
    * add hist fastpaths for no data and minimal bins
    
    * simplify hist hypothesis tests
    
    * add coverage for unreachable code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/series_only/hist_test.py
+++ tests/series_only/hist_test.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from random import Random
 from typing import Any
 
 import hypothesis.strategies as st
@@ -13,13 +14,13 @@ from tests.utils import PYARROW_VERSION
 from tests.utils import ConstructorEager
 from tests.utils import assert_equal_data
 
-xfail_hist = pytest.mark.xfail(
-    reason="https://github.com/narwhals-dev/narwhals/issues/2348", strict=False
-)
+rnd = Random(0)  # noqa: S311
 
-
-data = {
+data: dict[str, list[int | float]] = {
     "int": [0, 1, 2, 3, 4, 5, 6],
+    "float": [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+    "int_shuffled": [1, 0, 2, 3, 6, 5, 4],
+    "float_shuffled": [1.0, 0.0, 2.0, 3.0, 6.0, 5.0, 4.0],
 }
 
 bins_and_expected = [
@@ -29,11 +30,11 @@ bins_and_expected = [
     },
     {
         "bins": [1.0, 2.5, 5.5, float("inf")],
-        "expected": [1, 3, 1],
+        "expected": [2, 3, 1],
     },
     {
         "bins": [1.0, 2.5, 5.5],
-        "expected": [1, 3],
+        "expected": [2, 3],
     },
     {
         "bins": [-10.0, -1.0, 2.5, 5.5],
@@ -41,23 +42,27 @@ bins_and_expected = [
     },
     {
         "bins": [1.0, 2.0625],
-        "expected": [1],
+        "expected": [2],
     },
     {
         "bins": [1],
         "expected": [],
     },
+    {
+        "bins": [0, 10],
+        "expected": [7],
+    },
 ]
 counts_and_expected = [
     {
         "bin_count": 4,
-        "expected_bins": [-0.006, 1.5, 3.0, 4.5, 6.0],
+        "expected_bins": [0, 1.5, 3.0, 4.5, 6.0],
         "expected_count": [2, 2, 1, 2],
     },
     {
         "bin_count": 12,
         "expected_bins": [
-            -0.006,
+            0,
             0.5,
             1.0,
             1.5,
@@ -74,6 +79,11 @@ counts_and_expected = [
         "expected_count": [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
     },
     {
+        "bin_count": 1,
+        "expected_bins": [0, 6],
+        "expected_count": [7],
+    },
+    {
         "bin_count": 0,
         "expected_bins": [],
         "expected_count": [],
@@ -81,7 +91,6 @@ counts_and_expected = [
 ]
 
 
-@xfail_hist
 @pytest.mark.parametrize("params", bins_and_expected)
 @pytest.mark.parametrize("include_breakpoint", [True, False])
 @pytest.mark.filterwarnings(
@@ -121,8 +130,7 @@ def test_hist_bin(
         assert_equal_data(result, expected)
 
         # result size property
-        if len(bins) < 2:
-            assert len(result) == 0
+        assert len(result) == max(len(bins) - 1, 0)
 
     # shift bins property
     shift_by = 10
@@ -167,7 +175,6 @@ def test_hist_bin(
         assert_equal_data(result, expected)
 
 
-@xfail_hist
 @pytest.mark.parametrize("params", counts_and_expected)
 @pytest.mark.parametrize("include_breakpoint", [True, False])
 @pytest.mark.filterwarnings(
@@ -208,9 +215,9 @@ def test_hist_count(
         assert_equal_data(result, expected)
 
         # result size property
-        if bin_count < 2:
-            assert len(result) == 0
-        else:
+
+        assert len(result) == bin_count
+        if bin_count > 0:
             assert result["count"].sum() == df[col].count()
 
     # missing/nan results
@@ -231,15 +238,13 @@ def test_hist_count(
         assert_equal_data(result, expected)
 
         # result size property
-        if bin_count < 2:
-            assert len(result) == 0
-        else:
+        assert len(result) == bin_count
+        if bin_count > 0:
             assert (
                 result["count"].sum() == (~(df[col].is_nan() | df[col].is_null())).sum()
             )
 
 
-@xfail_hist
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature."
 )
@@ -276,7 +281,6 @@ def test_hist_count_no_spread(
     assert_equal_data(result, expected)
 
 
-@xfail_hist
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature."
 )
@@ -292,7 +296,7 @@ def test_hist_bin_and_bin_count() -> None:
         s.hist(bins=[1, 3], bin_count=4)
 
 
-@xfail_hist
+@pytest.mark.parametrize("include_breakpoint", [True, False])
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature."
 )
@@ -300,6 +304,7 @@ def test_hist_no_data(
     constructor_eager: ConstructorEager,
     *,
     request: pytest.FixtureRequest,
+    include_breakpoint: bool,
 ) -> None:
     if "cudf" in str(constructor_eager):
         # TODO(unassigned): too many spurious failures, report and revisit
@@ -309,15 +314,20 @@ def test_hist_no_data(
     s = nw.from_native(constructor_eager({"values": []})).select(
         nw.col("values").cast(nw.Float64)
     )["values"]
-    for include_breakpoint in [True, False]:
-        result = s.hist(bin_count=10, include_breakpoint=include_breakpoint)
-        assert len(result) == 10
+    for bin_count in [1, 10]:
+        result = s.hist(bin_count=bin_count, include_breakpoint=include_breakpoint)
+        assert len(result) == bin_count
         assert result["count"].sum() == 0
 
-    for include_breakpoint in [True, False]:
-        result = s.hist(bins=[1, 5, 10], include_breakpoint=include_breakpoint)
-        assert len(result) == 2
-        assert result["count"].sum() == 0
+        if include_breakpoint:
+            bps = result["breakpoint"].to_list()
+            assert bps[0] == (1 / bin_count)
+            if bin_count > 1:
+                assert bps[-1] == 1
+
+    result = s.hist(bins=[1, 5, 10], include_breakpoint=include_breakpoint)
+    assert len(result) == 2
+    assert result["count"].sum() == 0
 
 
 @pytest.mark.filterwarnings(
@@ -341,7 +351,6 @@ def test_hist_small_bins(
         s["values"].hist(bins=[1, 3], bin_count=4)
 
 
-@xfail_hist
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature."
 )
@@ -362,12 +371,16 @@ def test_hist_non_monotonic(constructor_eager: ConstructorEager) -> None:
     data=st.lists(
         # Bug in Polars <= 1.21; computing histograms with NaN data and passed bins can be unreliable
         #   this leads to flaky hypothesis testing https://github.com/pola-rs/polars/issues/21082
-        # min_value/max_value from PyArrows min/max boundaries
-        st.floats(
-            min_value=-9007199254740992,
-            max_value=9007199254740992,
-            allow_nan=False,
-            allow_infinity=False,
+        # min_value/max_value; avoid issues with floating point value imprecision
+        # small floats around 0 are incorrectly binned by Polars
+        st.one_of(
+            st.just(0.0),
+            st.floats(
+                min_value=-1e10, max_value=-1e-4, allow_nan=False, allow_subnormal=False
+            ),
+            st.floats(
+                min_value=1e-4, max_value=1e10, allow_nan=False, allow_subnormal=False
+            ),
         ),
         min_size=0,
         max_size=100,
@@ -376,14 +389,13 @@ def test_hist_non_monotonic(constructor_eager: ConstructorEager) -> None:
         st.floats(min_value=0.001, max_value=1_000, allow_nan=False), max_size=50
     ),
 )
-@xfail_hist
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature.",
     "ignore:invalid value encountered in cast:RuntimeWarning",
 )
 @pytest.mark.skipif(
-    POLARS_VERSION < (1, 15),
-    reason="hist(bins=...) cannot be used for compatibility checks since narwhals aims to mimic polars>=1.2.0 behavior",
+    POLARS_VERSION < (1, 27),
+    reason="polars cannot be used for compatibility checks since narwhals aims to mimic polars>=1.27 behavior",
 )
 @pytest.mark.slow
 def test_hist_bin_hypotheis(
@@ -421,22 +433,16 @@ def test_hist_bin_hypotheis(
 
 @given(
     data=st.lists(
-        # min_value/max_value from PyArrows min/max boundaries
-        st.floats(
-            min_value=-9007199254740992,
-            max_value=9007199254740992,
-            allow_nan=False,
-            allow_infinity=False,
-        ),
-        min_size=0,
+        st.integers(
+            min_value=-int(1e9), max_value=int(1e9)
+        ),  # dynamic bin creation & small floating point value distances creates lots of noise
         max_size=100,
     ),
-    bin_count=st.integers(min_value=0, max_value=1_000),
+    bin_count=st.integers(min_value=1, max_value=20),
 )
-@xfail_hist
 @pytest.mark.skipif(
-    POLARS_VERSION < (1, 15),
-    reason="hist(bin_count=...) behavior significantly changed after this version",
+    POLARS_VERSION < (1, 27),
+    reason="polars cannot be used for compatibility checks since narwhals aims to mimic polars>=1.27 behavior",
 )
 @pytest.mark.filterwarnings(
     "ignore:`Series.hist` is being called from the stable API although considered an unstable feature.",
@@ -480,7 +486,10 @@ def test_hist_count_hypothesis(
     # Bug in Polars <= 1.21; hist becomes unreliable when passing bin_counts
     #   for data with a wide range and a large number of passed bins
     #   https://github.com/pola-rs/polars/issues/20879
-    if expected["count"].sum() != expected_data.count() and "polars" not in str(
+
+    if expected[
+        "count"
+    ].sum() != expected_data.is_not_nan().sum() and "polars" not in str(
         constructor_eager
     ):
         request.applymarker(pytest.mark.xfail)
