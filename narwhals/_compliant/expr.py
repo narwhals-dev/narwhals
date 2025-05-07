3fe720e07a51b8a2d86df2efdfdb7dbe654a5ac4
Dan Redding <125183946+dangotbanned@users.noreply.github.com>
2025-05-07 13:52:36 +0100

    refactor: Add `LazyExpr.alias` (#2506)

--- narwhals/_compliant/expr.py
+++ narwhals/_compliant/expr.py
@@ -900,6 +900,14 @@ class LazyExpr(  # type: ignore[misc]
 
     def _with_callable(self, call: Callable[..., Any], /) -> Self: ...
     def _with_alias_output_names(self, func: AliasNames | None, /) -> Self: ...
+    def alias(self, name: str) -> Self:
+        def fn(names: Sequence[str]) -> Sequence[str]:
+            if len(names) != 1:
+                msg = f"Expected function with single output, found output names: {names}"
+                raise ValueError(msg)
+            return [name]
+
+        return self._with_alias_output_names(fn)
 
     @property
     def name(self) -> LazyExprNameNamespace[Self]:
