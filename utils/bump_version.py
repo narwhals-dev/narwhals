8a792210d4d4c718ba37d0899c645ab83bf015c5
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-08 22:49:34 +0100

    ci: alias `subprocess` to `sp`

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -1,7 +1,7 @@
 # mypy: ignore
 from __future__ import annotations
 
-import subprocess
+import subprocess as sp
 import sys
 
 GIT = "git"
@@ -14,7 +14,7 @@ TAGS = "--tags"
 TAG = "tag"
 VERSION = "version"
 
-out = subprocess.run([GIT, FETCH, UPSTREAM, TAGS], check=False)
+out = sp.run([GIT, FETCH, UPSTREAM, TAGS], check=False)
 if out.returncode != 0:
     print(
         "Something went wrong with the release process, please check the Narwhals Wiki for "
@@ -22,10 +22,10 @@ if out.returncode != 0:
     )
     print(out)
     sys.exit(1)
-subprocess.run([GIT, "reset", "--hard", "upstream/main"], check=False)
+sp.run([GIT, "reset", "--hard", "upstream/main"], check=False)
 
 if (
-    subprocess.run(
+    sp.run(
         [GIT, "branch", "--show-current"], text=True, capture_output=True, check=False
     ).stdout.strip()
     != "bump-version"
@@ -36,29 +36,27 @@ if (
 # Delete local tags, if present
 try:
     # Get the list of all tags
-    result = subprocess.run([GIT, TAG, "-l"], capture_output=True, text=True, check=True)
+    result = sp.run([GIT, TAG, "-l"], capture_output=True, text=True, check=True)
     tags = result.stdout.splitlines()  # Split the tags into a list by lines
 
     # Delete each tag using git tag -d
     for tag in tags:
-        subprocess.run([GIT, TAG, "-d", tag], check=True)
+        sp.run([GIT, TAG, "-d", tag], check=True)
     print("All local tags have been deleted.")
-except subprocess.CalledProcessError as e:
+except sp.CalledProcessError as e:
     print(f"An error occurred: {e}")
 
-subprocess.run([GIT, FETCH, UPSTREAM, TAGS], check=False)
-subprocess.run([GIT, FETCH, UPSTREAM, "--prune", TAGS], check=False)
+sp.run([GIT, FETCH, UPSTREAM, TAGS], check=False)
+sp.run([GIT, FETCH, UPSTREAM, "--prune", TAGS], check=False)
 
 how = sys.argv[1]
 
-subprocess.run([UV, VERSION, "--bump", how], check=False)
-version = subprocess.run(
+sp.run([UV, VERSION, "--bump", how], check=False)
+version = sp.run(
     [UV, VERSION, "--short"], capture_output=True, text=True, check=False
 ).stdout
 
-subprocess.run(
-    [GIT, COMMIT, "-a", "-m", f"release: Bump version to {version}"], check=False
-)
-subprocess.run([GIT, TAG, "-a", f"v{version}", "-m", f"v{version}"], check=False)
-subprocess.run([GIT, PUSH, UPSTREAM, "HEAD", "--follow-tags"], check=False)
-subprocess.run([GIT, PUSH, UPSTREAM, "HEAD:stable", "-f", "--follow-tags"], check=False)
+sp.run([GIT, COMMIT, "-a", "-m", f"release: Bump version to {version}"], check=False)
+sp.run([GIT, TAG, "-a", f"v{version}", "-m", f"v{version}"], check=False)
+sp.run([GIT, PUSH, UPSTREAM, "HEAD", "--follow-tags"], check=False)
+sp.run([GIT, PUSH, UPSTREAM, "HEAD:stable", "-f", "--follow-tags"], check=False)
