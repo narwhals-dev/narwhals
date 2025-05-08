ed06050cd57f9740cb6eb62238c2876494cb7f98
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-05-08 18:24:09 +0200

    use uv version bump instead of manual process

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -7,7 +7,8 @@ import sys
 out = subprocess.run(["git", "fetch", "upstream", "--tags"])
 if out.returncode != 0:
     print(
-        "Something went wrong with the release process, please check the Narwhals Wiki and try again."
+        "Something went wrong with the release process, please check the Narwhals Wiki for "
+        "at https://github.com/narwhals-dev/narwhals/wiki#release-process and try again."
     )
     print(out)
     sys.exit(1)
@@ -42,19 +43,8 @@ subprocess.run(["git", "fetch", "upstream", "--prune", "--tags"])
 
 how = sys.argv[1]
 
-with open("pyproject.toml", encoding="utf-8") as f:
-    content = f.read()
-old_version = re.search(r'version = "(.*)"', content).group(1)  # pyright: ignore[reportOptionalMemberAccess]
-version = old_version.split(".")
-if how == "patch":
-    version = ".".join(version[:-1] + [str(int(version[-1]) + 1)])
-elif how == "minor":
-    version = ".".join(version[:-2] + [str(int(version[-2]) + 1), "0"])
-elif how == "major":
-    version = ".".join([str(int(version[0]) + 1), "0", "0"])
-content = content.replace(f'version = "{old_version}"', f'version = "{version}"')
-with open("pyproject.toml", "w", encoding="utf-8") as f:
-    f.write(content)
+subprocess.run(["uv", "version", "--bump", how]) 
+version = subprocess.run(["uv", "version", "--short"], capture_output=True).stdout.decode("utf-8")
 
 subprocess.run(["git", "commit", "-a", "-m", f"release: Bump version to {version}"])
 subprocess.run(["git", "tag", "-a", f"v{version}", "-m", f"v{version}"])
