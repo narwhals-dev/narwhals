45435ff321fbd5e75c5e551cc838faff69d9c86d
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-13 12:05:20 +0100

    test: Basic `test_sort_by`
    Need to flesh out the cases more, but the structure is good for now

--- tests/expr_and_series/sort_by_test.py
+++ tests/expr_and_series/sort_by_test.py
@@ -26,6 +26,33 @@ data = {
 
 
 @pytest.mark.parametrize(
+    ("cols", "by", "descending", "nulls_last", "expected"),
+    [
+        ("group_1", "d", False, False, {"group_1": ["D", "D", "E", "E", "C"]}),
+        ("group_1", ["c", "b"], False, True, {"group_1": ["E", "D", "E", "C", "D"]}),
+    ],
+)
+def test_sort_by(
+    constructor_eager: ConstructorEager,
+    cols: str | Sequence[str],
+    by: str | Sequence[str],
+    descending: bool | Sequence[bool],
+    nulls_last: bool,
+    expected: dict[str, Sequence[PythonLiteral]],
+    request: pytest.FixtureRequest,
+) -> None:
+    if any(
+        x in str(constructor_eager) for x in ("pyarrow_table", "pandas", "modin", "cudf")
+    ):
+        request.applymarker(pytest.mark.xfail(reason="Not implemented"))
+    df = nw.from_native(constructor_eager(data), eager_only=True)
+    result = df.select(
+        nw.col(cols).sort_by(by, descending=descending, nulls_last=nulls_last)
+    )
+    assert_equal_data(result, expected)
+
+
+@pytest.mark.parametrize(
     ("descending", "nulls_last", "expected"),
     [
         (True, True, {"a": _EXPECTED_A, "b": [3, 2, 1, None, None]}),
