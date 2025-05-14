6c8b7db661f4e877ec091efdd778200e5dc79c5f
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-14 15:07:48 +0100

    feat: Add some `window`

--- narwhals/_plan/window.py
+++ narwhals/_plan/window.py
@@ -1,3 +1,24 @@
 from __future__ import annotations
 
-# TODO @dangotbanned: Investigate
+from narwhals._plan.common import ExprIR
+
+
+class Window(ExprIR): ...
+
+
+class OverWindow(Window): ...
+
+
+class RollingWindow(Window): ...
+
+
+class RollingSum(RollingWindow): ...
+
+
+class RollingMean(RollingWindow): ...
+
+
+class RollingVar(RollingWindow): ...
+
+
+class RollingStd(RollingWindow): ...
