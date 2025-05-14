f637ecd8c9d69e6979f07f06b89f24bf531aefdf
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-14 16:08:15 +0100

    feat: Add `name`

---
+++ narwhals/_plan/name.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from narwhals._plan.common import Function
+
+
+class NameFunction(Function):
+    """`polars` version doesn't represent in the same way here.
+
+    https://github.com/pola-rs/polars/blob/6df23a09a81c640c21788607611e09d9f43b1abc/crates/polars-plan/src/dsl/name.rs
+    """
+
+
+class Keep(NameFunction): ...
+
+
+class Map(NameFunction): ...
+
+
+class Prefix(NameFunction): ...
+
+
+class Suffix(NameFunction): ...
+
+
+class ToLowercase(NameFunction): ...
+
+
+class ToUppercase(NameFunction): ...
