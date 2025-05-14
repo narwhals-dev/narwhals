6e6b2414d19adf73b1cadc5aa5a0c62974ff0a75
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-14 16:34:46 +0100

    feat: Add `Immutable`
    - Will probably want to set up the `object.__setattr__` part somewhere
    - Not decided on how initialization should work

--- narwhals/_plan/common.py
+++ narwhals/_plan/common.py
@@ -1,7 +1,20 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 
-class ExprIR: ...
+if TYPE_CHECKING:
+    from typing_extensions import Never
+
+
+class Immutable:
+    __slots__ = ()
+
+    def __setattr__(self, name: str, value: Never) -> Never:
+        msg = f"{type(self).__name__!r} is immutable, {name!r} cannot be set."
+        raise AttributeError(msg)
+
+
+class ExprIR(Immutable): ...
 
 
 class Function(ExprIR):
