3cbea9203cbdb291e2d2550dfa2fafd5d845cc38
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-14 11:02:41 +0100

    add `_nodes` package
    - Not stressing on the name for now
    - Just want to make a start on modelling ops

---
+++ narwhals/_nodes/__init__.py
@@ -0,0 +1,23 @@
+"""Brainstorming an `Expr` internal node represention.
+
+References:
+    - https://github.com/pola-rs/polars/blob/dafd0a2d0e32b52bcfa4273bffdd6071a0d5977a/crates/polars-python/src/lazyframe/visitor/expr_nodes.rs
+    - https://github.com/pola-rs/polars/blob/dafd0a2d0e32b52bcfa4273bffdd6071a0d5977a/crates/polars-plan/src/dsl/expr.rs
+    - https://github.com/pola-rs/polars/blob/dafd0a2d0e32b52bcfa4273bffdd6071a0d5977a/crates/polars-plan/src/dsl/function_expr/mod.rs
+    - https://github.com/pola-rs/polars/blob/dafd0a2d0e32b52bcfa4273bffdd6071a0d5977a/crates/polars-plan/src/dsl/options/mod.rs#L137-L172
+    - https://github.com/pola-rs/polars/blob/3fd7ecc5f9de95f62b70ea718e7e5dbf951b6d1c/crates/polars-plan/src/plans/options.rs#L35-L106
+    - https://github.com/pola-rs/polars/blob/3fd7ecc5f9de95f62b70ea718e7e5dbf951b6d1c/crates/polars-plan/src/plans/options.rs#L131-L236
+    - https://github.com/pola-rs/polars/blob/3fd7ecc5f9de95f62b70ea718e7e5dbf951b6d1c/crates/polars-plan/src/plans/options.rs#L240-L267
+
+Related:
+    - https://github.com/narwhals-dev/narwhals/pull/2483#issuecomment-2866902903
+    - https://github.com/narwhals-dev/narwhals/pull/2483#issuecomment-2867331343
+    - https://github.com/narwhals-dev/narwhals/pull/2483#issuecomment-2867446959
+    - https://github.com/narwhals-dev/narwhals/pull/2483#issuecomment-2869070157
+    - https://github.com/narwhals-dev/narwhals/pull/2538/commits/a7eeb0d23e67cb70e7cfa73cec2c7b69a15c8bef#r2083562677
+    - https://github.com/narwhals-dev/narwhals/issues/2225
+    - https://github.com/narwhals-dev/narwhals/issues/1848
+    - https://github.com/narwhals-dev/narwhals/issues/2534#issuecomment-2875676729
+"""
+
+from __future__ import annotations
