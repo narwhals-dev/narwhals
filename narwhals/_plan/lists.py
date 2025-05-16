f27e5901f2519ba08be57760b31186132ae4adb1
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-16 11:13:56 +0100

    feat: Fill out `lists`

--- narwhals/_plan/lists.py
+++ narwhals/_plan/lists.py
@@ -1,11 +1,15 @@
-"""TODO: Attributes."""
-
 from __future__ import annotations
 
 from narwhals._plan.common import Function
+from narwhals._plan.options import FunctionOptions
 
 
 class ListFunction(Function): ...
 
 
-class Len(ListFunction): ...
+class Len(ListFunction):
+    """https://github.com/pola-rs/polars/blob/62257860a43ec44a638e8492ed2cf98a49c05f2e/crates/polars-plan/src/dsl/function_expr/list.rs#L32."""
+
+    @property
+    def function_options(self) -> FunctionOptions:
+        return FunctionOptions.elementwise()
