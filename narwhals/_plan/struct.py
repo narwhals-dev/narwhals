7ff6d3c130e5358c7e9967aed86c1f506a8ef0b6
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-16 11:10:05 +0100

    feat: Fill out `struct`

--- narwhals/_plan/struct.py
+++ narwhals/_plan/struct.py
@@ -1,11 +1,19 @@
-"""TODO: Attributes."""
-
 from __future__ import annotations
 
 from narwhals._plan.common import Function
+from narwhals._plan.options import FunctionOptions
 
 
 class StructFunction(Function): ...
 
 
-class FieldByName(StructFunction): ...
+class FieldByName(StructFunction):
+    """https://github.com/pola-rs/polars/blob/62257860a43ec44a638e8492ed2cf98a49c05f2e/crates/polars-plan/src/dsl/function_expr/struct_.rs#L11."""
+
+    __slots__ = ("name",)
+
+    name: str
+
+    @property
+    def function_options(self) -> FunctionOptions:
+        return FunctionOptions.elementwise()
