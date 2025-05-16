ba69ae361d6153030d0321835473cfd551177575
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-16 11:16:36 +0100

    feat: Fill out `categorical`

--- narwhals/_plan/categorical.py
+++ narwhals/_plan/categorical.py
@@ -1,11 +1,16 @@
-"""TODO: Attributes."""
-
 from __future__ import annotations
 
 from narwhals._plan.common import Function
+from narwhals._plan.options import FunctionOptions
 
 
 class CategoricalFunction(Function): ...
 
 
-class GetCategories(CategoricalFunction): ...
+class GetCategories(CategoricalFunction):
+    """https://github.com/pola-rs/polars/blob/62257860a43ec44a638e8492ed2cf98a49c05f2e/crates/polars-plan/src/dsl/function_expr/cat.rs#L7."""
+
+    @property
+    def function_options(self) -> FunctionOptions:
+        """https://github.com/pola-rs/polars/blob/62257860a43ec44a638e8492ed2cf98a49c05f2e/crates/polars-plan/src/dsl/function_expr/cat.rs#L41."""
+        return FunctionOptions.groupwise()
