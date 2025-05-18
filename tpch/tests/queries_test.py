12bdeedbb8b16fc1d5aebdb7de5ebe707e85db5d
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-05-18 19:08:07 +0200

    parametrize backend

--- tpch/tests/queries_test.py
+++ tpch/tests/queries_test.py
@@ -1,10 +1,21 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import TYPE_CHECKING
+from typing import Any
 
 import pytest
 
+from tpch.execute import BACKEND_NAMESPACE_KWARGS_MAP
+from tpch.execute import DUCKDB_SKIPS
+from tpch.execute import _execute_query_single_backend
 from tpch.execute import execute_query
+
+if TYPE_CHECKING:
+    from types import ModuleType
+
+    from pytest_codspeed.plugin import BenchmarkFixture
+
 
 ROOT_PATH = Path(__file__).resolve().parent.parent
 # Directory containing all the query scripts
@@ -17,8 +28,26 @@ def test_execute_query(query_path: Path) -> None:
     _ = execute_query(query_id=query_path.stem)
 
 
-@pytest.mark.benchmark
 @pytest.mark.parametrize("query_path", QUERIES_DIR.glob("q[1-9]*.py"))
-def test_benchmark_query(query_path: Path) -> None:
-    print(f"executing query {query_path.stem}")  # noqa: T201
-    _ = execute_query(query_id=query_path.stem)
+@pytest.mark.parametrize(
+    ("backend", "namespace_and_kwargs"), BACKEND_NAMESPACE_KWARGS_MAP.items()
+)
+def test_benchmark_query(
+    benchmark: BenchmarkFixture,
+    query_path: Path,
+    backend: str,
+    namespace_and_kwargs: tuple[ModuleType, dict[str, Any]],
+) -> None:
+    query_id = query_path.stem
+    native_namespace, kwargs = namespace_and_kwargs
+
+    if (
+        backend in {"duckdb", "sqlframe"} and query_id in DUCKDB_SKIPS
+    ) or backend == "dask":
+        pytest.skip()
+
+    _ = benchmark(
+        lambda: _execute_query_single_backend(
+            query_id=query_id, native_namespace=native_namespace, **kwargs
+        )
+    )
