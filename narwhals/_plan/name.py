ae8cc5c6fcac746d783e98e7eb81af8ef9c15e5f
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-05-21 14:21:51 +0100

    docs: Add more notes on `name.py`
    Stumbled into this when trying to inspect `pl.col("a").name.prefix("-1")`

--- narwhals/_plan/name.py
+++ narwhals/_plan/name.py
@@ -10,9 +10,12 @@ if TYPE_CHECKING:
 
 
 class NameFunction(Function):
-    """`polars` version doesn't represent these as `FunctionExpr`.
+    """`polars` version [doesn't represent as `FunctionExpr`].
 
-    https://github.com/pola-rs/polars/blob/6df23a09a81c640c21788607611e09d9f43b1abc/crates/polars-plan/src/dsl/name.rs
+    Also [doesn't support serialization].
+
+    [doesn't represent as `FunctionExpr`]: https://github.com/pola-rs/polars/blob/6df23a09a81c640c21788607611e09d9f43b1abc/crates/polars-plan/src/dsl/name.rs
+    [doesn't support serialization]: https://github.com/pola-rs/polars/blob/dafd0a2d0e32b52bcfa4273bffdd6071a0d5977a/crates/polars-plan/src/dsl/expr_dyn_fn.rs#L145-L151
     """
 
     @property
@@ -34,16 +37,24 @@ class NameFunction(Function):
         return f"name.{m[tp]}"
 
 
-class Keep(NameFunction): ...
+class Keep(NameFunction):
+    """Returns ``Expr::KeepName``."""
 
 
 class Map(NameFunction):
+    """Returns ``Expr::RenameAlias``.
+
+    https://github.com/pola-rs/polars/blob/6df23a09a81c640c21788607611e09d9f43b1abc/crates/polars-plan/src/dsl/name.rs#L28-L38
+    """
+
     __slots__ = ("function",)
 
     function: AliasName
 
 
 class Prefix(NameFunction):
+    """Each of these depend on `Map`."""
+
     __slots__ = ("prefix",)
 
     prefix: str
