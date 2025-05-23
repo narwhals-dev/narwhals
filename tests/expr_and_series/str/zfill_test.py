dda9485c0c1f83533bb220212ac03036a47a25c6
Will Dean <wd60622@gmail.com>
2025-05-23 16:21:27 -0400

    add additional test cases

--- tests/expr_and_series/str/zfill_test.py
+++ tests/expr_and_series/str/zfill_test.py
@@ -3,18 +3,18 @@ from __future__ import annotations
 import narwhals as nw
 from tests.utils import Constructor, ConstructorEager, assert_equal_data
 
-data = {"a": ["1", "12", "123"]}
+data = {"a": ["-1", "1", "12", "123", "99999", None]}
 
 
 def test_str_zfill(constructor: Constructor) -> None:
     df = nw.from_native(constructor(data))
     result = df.select(nw.col("a").str.zfill(3))
-    expected = {"a": ["001", "012", "123"]}
+    expected = {"a": ["-01", "001", "012", "123", "99999", None]}
     assert_equal_data(result, expected)
 
 
 def test_str_zfill_series(constructor_eager: ConstructorEager) -> None:
     df = nw.from_native(constructor_eager(data), eager_only=True)
     result = df.select(df["a"].str.zfill(3))
-    expected = {"a": ["001", "012", "123"]}
+    expected = {"a": ["-01", "001", "012", "123", "99999", None]}
     assert_equal_data(result, expected)
