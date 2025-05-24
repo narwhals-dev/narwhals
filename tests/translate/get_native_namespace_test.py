c7255b8f3a121603cec1093246854c592ab7b183
Marco Edward Gorelli <marcogorelli@protonmail.com>
2025-05-24 19:55:45 +0100

    ci: fix native namespace test (#2611)

--- tests/translate/get_native_namespace_test.py
+++ tests/translate/get_native_namespace_test.py
@@ -56,8 +56,8 @@ def _get_expected_namespace(constructor_name: str) -> Any | None:  # noqa: PLR09
 
 
 def test_native_namespace_frame(constructor: Constructor) -> None:
-    constructor_name = constructor.__name__
-    if constructor_name == "pyspark_lazy_constructor":
+    constructor_name = str(constructor)
+    if "pyspark" in constructor_name and "sqlframe" not in constructor_name:
         pytest.skip(reason="Requires special handling for spark local vs spark connect")
 
     expected_namespace = _get_expected_namespace(constructor_name=constructor_name)
