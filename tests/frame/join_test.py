e10b11e0daed4d1c7c395e0cd6169601aedefa68
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-06-07 15:23:25 +0100

    test: xfail nightlies pandas `MergeError`
    Fixes https://github.com/narwhals-dev/narwhals/actions/runs/15508156416/job/43665550812?pr=2654

--- tests/frame/join_test.py
+++ tests/frame/join_test.py
@@ -746,6 +746,18 @@ def test_joinasof_by_exceptions(constructor: Constructor) -> None:
 def test_join_duplicate_column_names(
     constructor: Constructor, request: pytest.FixtureRequest
 ) -> None:
+    if any(
+        x in str(constructor)
+        for x in ("pandas", "pandas[pyarrow]", "pandas[nullable]", "dask")
+    ) and PANDAS_VERSION >= (3,):
+        from pandas.errors import MergeError
+
+        request.applymarker(
+            pytest.mark.xfail(
+                reason="Passing suffixes which cause duplicate column names is not allowed.",
+                raises=MergeError,
+            )
+        )
     if "polars" in str(constructor) and POLARS_VERSION < (1, 26):
         pytest.skip()
     if (
