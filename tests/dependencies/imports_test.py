5a82c9e991cdf293b92390d5b0813da4120e9ee5
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-06-08 14:17:54 +0200

    refactor round trip test

--- tests/dependencies/imports_test.py
+++ tests/dependencies/imports_test.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-import sys
+from importlib.util import find_spec
 from types import ModuleType
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pytest
 
@@ -11,7 +11,6 @@ from narwhals.utils import Implementation
 
 if TYPE_CHECKING:
     from narwhals.typing import IntoFrameT
-
 
 data = {"a": [1, 1, 2], "b": [4, 5, 6]}
 
@@ -26,48 +25,29 @@ def _roundtrip_query(frame: IntoFrameT) -> IntoFrameT:
     )
 
 
-def test_import_polars() -> None:
-    # NOTE: Can't do `monkeypatch.delitem` safely
-    #     ImportError: PyO3 modules compiled for CPython 3.8 or older may only be initialized once per interpreter process
-    pytest.importorskip("polars")
-    df = Implementation.POLARS.to_native_namespace().DataFrame(data)
+@pytest.mark.parametrize(
+    ("impl", "frame_constructor", "frame_type", "kwargs"),
+    [
+        (Implementation.CUDF, "DataFrame", "DataFrame", {}),
+        (Implementation.DASK, "from_dict", "DataFrame", {"npartitions": 1}),
+        (Implementation.MODIN, "DataFrame", "DataFrame", {}),
+        (Implementation.PANDAS, "DataFrame", "DataFrame", {}),
+        (Implementation.POLARS, "DataFrame", "DataFrame", {}),
+        (Implementation.PYARROW, "table", "Table", {}),
+    ],
+)
+def test_round_trip(
+    impl: Implementation, frame_constructor: str, frame_type: str, kwargs: dict[str, Any]
+) -> None:
+    if not find_spec(impl.value):
+        reason = f"{impl.value} not installed"
+        pytest.skip(reason=reason)
+
+    module = impl.to_native_namespace()
+    df = getattr(module, frame_constructor)(data, **kwargs)
     result = _roundtrip_query(df)
-    import polars as pl
 
-    assert isinstance(result, pl.DataFrame)
-
-
-def test_import_dask() -> None:
-    pytest.importorskip("dask")
-    df = Implementation.DASK.to_native_namespace().from_dict(data, npartitions=1)
-    result = _roundtrip_query(df)
-    import dask.dataframe as dd
-
-    assert isinstance(result, dd.DataFrame)
-
-
-def test_import_pandas(monkeypatch: pytest.MonkeyPatch) -> None:
-    pytest.importorskip("pandas")
-    if sys.version_info >= (3, 9):
-        monkeypatch.delitem(sys.modules, "pandas")
-    else:  # pragma: no cover
-        # NOTE: AttributeError: partially initialized module 'pandas' has no attribute 'compat' (most likely due to a circular import)
-        ...
-    df = Implementation.PANDAS.to_native_namespace().DataFrame(data)
-    result = _roundtrip_query(df)
-    import pandas as pd
-
-    assert isinstance(result, pd.DataFrame)
-
-
-def test_import_pyarrow(monkeypatch: pytest.MonkeyPatch) -> None:
-    pytest.importorskip("pyarrow")
-    monkeypatch.delitem(sys.modules, "pyarrow")
-    df = Implementation.PYARROW.to_native_namespace().table(data)
-    result = _roundtrip_query(df)
-    import pyarrow as pa
-
-    assert isinstance(result, pa.Table)
+    assert isinstance(result, getattr(module, frame_type))
 
 
 @pytest.mark.parametrize(
@@ -88,7 +68,9 @@ def test_import_pyarrow(monkeypatch: pytest.MonkeyPatch) -> None:
 def test_to_native_namespace(
     monkeypatch: pytest.MonkeyPatch, impl: Implementation
 ) -> None:
-    pytest.importorskip(impl.value)
+    if not find_spec(impl.value):
+        reason = f"{impl.value} not installed"
+        pytest.skip(reason=reason)
 
     assert isinstance(impl.to_native_namespace(), ModuleType)
 
