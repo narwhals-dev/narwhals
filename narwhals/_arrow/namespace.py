5a5f7a7915e217e6169ac33e62a7a648a4b9442f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-06-10 19:28:10 +0100

    imports

--- narwhals/_arrow/namespace.py
+++ narwhals/_arrow/namespace.py
@@ -12,7 +12,6 @@ from narwhals._arrow.dataframe import ArrowDataFrame
 from narwhals._arrow.expr import ArrowExpr
 from narwhals._arrow.selectors import ArrowSelectorNamespace
 from narwhals._arrow.series import ArrowSeries
-from narwhals._arrow.typing import ChunkedArrayAny
 from narwhals._arrow.utils import (
     align_series_full_broadcast,
     cast_to_comparable_string_types,
@@ -25,14 +24,17 @@ from narwhals._expression_parsing import (
 from narwhals._utils import Implementation
 
 if TYPE_CHECKING:
-    from narwhals._arrow.typing import Incomplete
+    from narwhals._arrow.typing import (
+        ChunkedArrayAny,  # noqa: F401
+        Incomplete,
+    )
     from narwhals._utils import Version
     from narwhals.dtypes import DType
     from narwhals.typing import NonNestedLiteral
 
 
 class ArrowNamespace(
-    EagerNamespace[ArrowDataFrame, ArrowSeries, ArrowExpr, pa.Table, ChunkedArrayAny]
+    EagerNamespace[ArrowDataFrame, ArrowSeries, ArrowExpr, pa.Table, "ChunkedArrayAny"]
 ):
     @property
     def _dataframe(self) -> type[ArrowDataFrame]:
