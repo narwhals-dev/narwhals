eaa5ffdc949f59d1241282e865454141ba7a45de
Cameron Riddell <cameron@dutc.io>
2025-06-12 10:58:22 -0700

    remove test coalesce upcast

--- tests/expr_and_series/coalesce_test.py
+++ tests/expr_and_series/coalesce_test.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import pytest
-
 import narwhals as nw
 from tests.utils import Constructor, assert_equal_data
 
@@ -30,36 +28,3 @@ def test_coalesce_strings(constructor: Constructor) -> None:
     )
 
     assert_equal_data(result, {"result": ["0", "xyz", "3", "5", "3"]})
-
-
-def test_coalesce_upcast(
-    constructor: Constructor, request: pytest.FixtureRequest
-) -> None:
-    if any(
-        x in str(constructor)
-        for x in [
-            "pandas_pyarrow",
-            "pandas_nullable",
-            "modin_pyarrow",
-            "pyarrow_table",
-            "ibis",
-        ]
-    ):
-        # backends handle upcasting independently, most of them fail
-        #   due to strict preferences against automatic upcasting
-        request.applymarker(pytest.mark.xfail)
-
-    data = {
-        "a": [0, None, None, None, None],
-        "b": [1, None, None, 5, 3],
-        "c": [5, None, 3, 2, 1],
-    }
-    result = nw.from_native(constructor(data)).select(
-        result=nw.coalesce(*data, nw.lit("xyz"))
-    )
-
-    schema = result.collect_schema()
-    if schema["result"] == nw.String:
-        assert_equal_data(result, {"result": ["0", "xyz", "3", "5", "3"]})
-    elif schema["result"] == nw.Object:
-        assert_equal_data(result, {"result": [0, "xyz", 3, 5, 3]})
