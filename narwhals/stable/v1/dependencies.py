87efbb0238ddb5056211d8b01bc2f463962b92f7
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-06-13 15:47:16 +0100

    fixup

--- narwhals/stable/v1/dependencies.py
+++ narwhals/stable/v1/dependencies.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     import cudf
+    import dask.dataframe as dd
     import ibis
     import modin.pandas as mpd
     import pandas as pd
@@ -14,7 +15,9 @@ if TYPE_CHECKING:
 
 
 from narwhals.dependencies import (
+    IMPORT_HOOKS,
     get_cudf,
+    get_dask_dataframe,
     get_ibis,
     get_modin,
     get_numpy,
@@ -28,8 +31,8 @@ from narwhals.dependencies import (
     is_narwhals_series,
     is_numpy_array,
     is_pandas_index,
-    IMPORT_HOOKS,
 )
+
 
 def is_pandas_dataframe(df: Any) -> TypeIs[pd.DataFrame]:
     """Check whether `df` is a pandas DataFrame without importing pandas."""
@@ -49,7 +52,6 @@ def is_pandas_series(ser: Any) -> TypeIs[pd.Series[Any]]:
     )
 
 
-
 def is_modin_dataframe(df: Any) -> TypeIs[mpd.DataFrame]:
     """Check whether `df` is a modin DataFrame without importing modin."""
     return (mpd := get_modin()) is not None and isinstance(df, mpd.DataFrame)
@@ -58,7 +60,6 @@ def is_modin_dataframe(df: Any) -> TypeIs[mpd.DataFrame]:
 def is_modin_series(ser: Any) -> TypeIs[mpd.Series]:
     """Check whether `ser` is a modin Series without importing modin."""
     return (mpd := get_modin()) is not None and isinstance(ser, mpd.Series)
-
 
 
 def is_cudf_dataframe(df: Any) -> TypeIs[cudf.DataFrame]:
@@ -71,18 +72,9 @@ def is_cudf_series(ser: Any) -> TypeIs[cudf.Series[Any]]:
     return (cudf := get_cudf()) is not None and isinstance(ser, cudf.Series)
 
 
-
-
 def is_dask_dataframe(df: Any) -> TypeIs[dd.DataFrame]:
     """Check whether `df` is a Dask DataFrame without importing Dask."""
     return (dd := get_dask_dataframe()) is not None and isinstance(df, dd.DataFrame)
-
-
-def is_duckdb_relation(df: Any) -> TypeIs[duckdb.DuckDBPyRelation]:
-    """Check whether `df` is a DuckDB Relation without importing DuckDB."""
-    return (duckdb := get_duckdb()) is not None and isinstance(
-        df, duckdb.DuckDBPyRelation
-    )
 
 
 def is_ibis_table(df: Any) -> TypeIs[ibis.Table]:
@@ -115,34 +107,6 @@ def is_pyarrow_table(df: Any) -> TypeIs[pa.Table]:
     return (pa := get_pyarrow()) is not None and isinstance(df, pa.Table)
 
 
-
-def is_pyspark_dataframe(df: Any) -> TypeIs[pyspark_sql.DataFrame]:
-    """Check whether `df` is a PySpark DataFrame without importing PySpark."""
-    return bool(
-        (pyspark_sql := get_pyspark_sql()) is not None
-        and isinstance(df, pyspark_sql.DataFrame)
-    )
-
-
-def is_pyspark_connect_dataframe(df: Any) -> TypeIs[PySparkConnectDataFrame]:
-    """Check whether `df` is a PySpark Connect DataFrame without importing PySpark."""
-    if get_pyspark_connect() is not None:  # pragma: no cover
-        try:
-            from pyspark.sql.connect.dataframe import DataFrame
-        except ImportError:
-            return False
-        return isinstance(df, DataFrame)
-    return False
-
-
-def is_sqlframe_dataframe(df: Any) -> TypeIs[SQLFrameDataFrame]:
-    """Check whether `df` is a SQLFrame DataFrame without importing SQLFrame."""
-    if get_sqlframe() is not None:
-        from sqlframe.base.dataframe import BaseDataFrame
-
-        return isinstance(df, BaseDataFrame)
-    return False  # pragma: no cover
-
 def is_pandas_like_dataframe(df: Any) -> bool:
     """Check whether `df` is a pandas-like DataFrame without doing any imports.
 
@@ -157,7 +121,6 @@ def is_pandas_like_series(ser: Any) -> bool:
     By "pandas-like", we mean: pandas, Modin, cuDF.
     """
     return is_pandas_series(ser) or is_modin_series(ser) or is_cudf_series(ser)
-
 
 
 __all__ = [
