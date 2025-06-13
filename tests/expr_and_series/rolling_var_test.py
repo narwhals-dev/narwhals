b02d508a38154ee5383b8bc72ad826a39a2b2f6b
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-06-13 10:24:46 +0100

    test: Suppress `too_slow` on `test_rolling_var_hypothesis_polars`
    - https://hypothesis.readthedocs.io/en/latest/tutorial/settings.html
    - https://hypothesis.readthedocs.io/en/latest/reference/api.html#hypothesis.HealthCheck
    - https://discord.com/channels/1235257048170762310/1382745360270889131

--- tests/expr_and_series/rolling_var_test.py
+++ tests/expr_and_series/rolling_var_test.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Any
 
 import hypothesis.strategies as st
 import pytest
-from hypothesis import given
+from hypothesis import HealthCheck, given, settings
 
 import narwhals as nw
 from tests.utils import (
@@ -134,6 +134,7 @@ def test_rolling_var_hypothesis(center: bool, values: list[float]) -> None:  # n
 
 
 @given(center=st.booleans(), values=st.lists(st.floats(-10, 10), min_size=5, max_size=10))
+@settings(suppress_health_check=[HealthCheck.too_slow])
 @pytest.mark.skipif(PANDAS_VERSION < (1,), reason="too old for pyarrow")
 @pytest.mark.skipif(POLARS_VERSION < (1,), reason="different null behavior")
 @pytest.mark.filterwarnings("ignore:.*is_sparse is deprecated:DeprecationWarning")
