794919f3f251b8db50b65f36316ef2ef7d88b2eb
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-06-13 17:43:42 +0100

    reuse is_native_*

--- narwhals/_utils.py
+++ narwhals/_utils.py
@@ -649,14 +649,10 @@ def tupleify(arg: Any) -> Any:
 
 
 def _is_iterable(arg: Any | Iterable[Any]) -> bool:
+    from narwhals._namespace import is_native_pandas, is_native_polars
     from narwhals.series import Series
 
-    if (pd := get_pandas()) is not None and isinstance(arg, (pd.Series, pd.DataFrame)):
-        msg = f"Expected Narwhals class or scalar, got: {qualified_type_name(arg)!r}. Perhaps you forgot a `nw.from_native` somewhere?"
-        raise TypeError(msg)
-    if (pl := get_polars()) is not None and isinstance(
-        arg, (pl.Series, pl.Expr, pl.DataFrame, pl.LazyFrame)
-    ):
+    if is_native_pandas(arg) or is_native_polars(arg):
         msg = (
             f"Expected Narwhals class or scalar, got: {qualified_type_name(arg)!r}.\n\n"
             "Hint: Perhaps you\n"
