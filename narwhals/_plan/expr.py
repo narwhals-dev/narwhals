3a1c3759e8a2a97f339411f33022ea8156a97184
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-06-24 19:04:55 +0100

    refactor: Simplify `FunctionExpr` version

--- narwhals/_plan/expr.py
+++ narwhals/_plan/expr.py
@@ -478,8 +478,7 @@ class FunctionExpr(ExprIR, t.Generic[FunctionT]):
             FunctionExpr(..., [Alias(..., name='...'), Literal(...), ...])
             #                  ^^^^^            ^^^
         """
-        yield self
-        for e in self.input:
+        for e in self.input[:1]:
             yield from e.iter_output_name()
 
     def __init__(
