8ffa6a81ce1937533fd792799d615f645d1b17b9
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-06-26 11:20:40 +0100

    docs: Friendly notes

--- narwhals/_constants.py
+++ narwhals/_constants.py
@@ -3,26 +3,35 @@ from __future__ import annotations
 import datetime as dt
 
 MAX_REPR_LENGTH = 40
+"""Length to begin truncating an object repr in an error message."""
 MAX_WHILE_REPEAT = 100
+"""Loop count to trigger an error if we can't generate a unique column name."""
 NON_ELEMENTARY_DEPTH = 2
+"""Threshold for complex group by aggregations in `pandas`, `pyarrow`, `dask`."""
 
 # Temporal (from `polars._utils.constants`)
 SECONDS_PER_DAY = 86_400
 SECONDS_PER_MINUTE = 60
 NS_PER_MINUTE = 60_000_000_000
+"""Nanoseconds (`[ns]`) per minute."""
 US_PER_MINUTE = 60_000_000
+"""Microseconds (`[μs]`) per minute."""
 MS_PER_MINUTE = 60_000
-
+"""Milliseconds (`[ms]`) per minute."""
 NS_PER_SECOND = 1_000_000_000
-"""Nanoseconds per second."""
+"""Nanoseconds (`[ns]`) per second (`[s]`)."""
 US_PER_SECOND = 1_000_000
-"""Microseconds per second."""
+"""Microseconds (`[μs]`) per second (`[s]`)."""
 MS_PER_SECOND = 1_000
-"""Milliseconds per second."""
-
+"""Milliseconds (`[ms]`) per second (`[s]`)."""
 NS_PER_MICROSECOND = 1_000
+"""Nanoseconds (`[ns]`) per microsecond (`[μs]`)."""
 NS_PER_MILLISECOND = 1_000_000
-"""https://github.com/pola-rs/polars/blob/2c7a3e77f0faa37c86a3745db4ef7707ae50c72e/crates/polars-time/src/chunkedarray/duration.rs#L7-L8"""
+"""Nanoseconds (`[ns]`) per millisecond (`[ms]`).
 
+From [polars](https://github.com/pola-rs/polars/blob/2c7a3e77f0faa37c86a3745db4ef7707ae50c72e/crates/polars-time/src/chunkedarray/duration.rs#L7).
+"""
 EPOCH_YEAR = 1970
+"""See [Unix time](https://en.wikipedia.org/wiki/Unix_time)."""
 EPOCH = dt.datetime(EPOCH_YEAR, 1, 1).replace(tzinfo=None)
+"""See [Unix time](https://en.wikipedia.org/wiki/Unix_time)."""
