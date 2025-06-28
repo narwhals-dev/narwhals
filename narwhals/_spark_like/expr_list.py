eb81991eadff57ebeaa7df712ddbbc2236427a16
Dan Redding <125183946+dangotbanned@users.noreply.github.com>
2025-06-28 21:41:41 +0100

    chore: Add `LazyExprNamespace` (#2756)

--- narwhals/_spark_like/expr_list.py
+++ narwhals/_spark_like/expr_list.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from narwhals._compliant.any_namespace import ListNamespace
+from narwhals._compliant.expr import LazyExprNamespace
+
 if TYPE_CHECKING:
     from narwhals._spark_like.expr import SparkLikeExpr
 
 
-class SparkLikeExprListNamespace:
-    def __init__(self, expr: SparkLikeExpr) -> None:
-        self._compliant_expr = expr
-
+class SparkLikeExprListNamespace(
+    LazyExprNamespace["SparkLikeExpr"], ListNamespace["SparkLikeExpr"]
+):
     def len(self) -> SparkLikeExpr:
-        return self._compliant_expr._with_callable(self._compliant_expr._F.array_size)
+        return self.compliant._with_callable(self.compliant._F.array_size)
