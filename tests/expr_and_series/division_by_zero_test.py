5ccb3027c289968e34c93c6e48195238269747dc
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-06-29 20:49:02 +0200

    some polars bugs?

--- tests/expr_and_series/division_by_zero_test.py
+++ tests/expr_and_series/division_by_zero_test.py
@@ -103,27 +103,27 @@ def test_expr_rtruediv_by_zero(
     constructor: Constructor, numerator: float, expected: float
 ) -> None:
     df = nw.from_native(constructor(data))
-    result = df.select(numerator / nw.col("denominator"))
-    assert_equal_data(result, {"literal": [expected] * len(data["denominator"])})
-    assert_equal_data(result.select((~nw.all().is_finite()).all()), {"literal": [True]})
+    result = df.select((numerator / nw.col("denominator")).alias("result"))
+    assert_equal_data(result, {"result": [expected] * len(data["denominator"])})
+    assert_equal_data(result.select((~nw.all().is_finite()).all()), {"result": [True]})
 
 
 @pytest.mark.parametrize("numerator", data["int"])
 def test_series_rfloordiv_by_zero(
     constructor_eager: ConstructorEager, numerator: float
 ) -> None:
-    if "polars" in str(constructor_eager) and POLARS_VERSION < (0, 20, 7):
+    if "polars" in str(constructor_eager) and POLARS_VERSION < (1, 10):
         pytest.skip(reason="bug")
 
     df = nw.from_native(constructor_eager(data), eager_only=True)
     result = {"literal": numerator // df["denominator"]}
     assert_equal_data(result, {"literal": expected_floordiv})
-    assert (~result["literal"].is_finite()).all()
+    assert result["literal"].is_null().all()
 
 
 @pytest.mark.parametrize("numerator", data["int"])
 def test_expr_rfloordiv_by_zero(constructor: Constructor, numerator: float) -> None:
-    if "polars" in str(constructor) and POLARS_VERSION < (0, 20, 7):
+    if "polars" in str(constructor) and POLARS_VERSION < (1, 10):
         pytest.skip(reason="bug")
 
     df = nw.from_native(constructor(data))
