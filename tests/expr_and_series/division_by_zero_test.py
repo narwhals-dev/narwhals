5ff71ce52925c7844dbf87061638d19908c3c5d1
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-06-29 11:39:24 +0200

    rollback pandas completely and xfail for now

--- tests/expr_and_series/division_by_zero_test.py
+++ tests/expr_and_series/division_by_zero_test.py
@@ -3,13 +3,7 @@ from __future__ import annotations
 import pytest
 
 import narwhals as nw
-from tests.utils import (
-    PANDAS_VERSION,
-    POLARS_VERSION,
-    Constructor,
-    ConstructorEager,
-    assert_equal_data,
-)
+from tests.utils import POLARS_VERSION, Constructor, ConstructorEager, assert_equal_data
 
 data = {"int": [-2, 0, 2], "float": [-2.1, 0.0, 2.1]}
 expected_truediv = [float("-inf"), float("nan"), float("inf")]
@@ -30,23 +24,34 @@ def test_expr_truediv_by_zero(constructor: Constructor) -> None:
     assert_equal_data(result, expected)
 
 
-def test_series_floordiv_by_zero(constructor_eager: ConstructorEager) -> None:
-    if "pandas" in str(constructor_eager) and PANDAS_VERSION < (2,):
-        pytest.skip(reason="converts floordiv by zero to 0")
+def test_series_floordiv_by_zero(
+    constructor_eager: ConstructorEager, request: pytest.FixtureRequest
+) -> None:
     if "polars" in str(constructor_eager) and POLARS_VERSION < (0, 20, 6):
         pytest.skip(reason="bug")
     df = nw.from_native(constructor_eager(data), eager_only=True)
+
+    if df.implementation.is_pandas_like():
+        reason = "floordiv either fails or generate different result"
+        request.applymarker(pytest.mark.xfail(reason=reason))
+
     result = {"int": df["int"] // 0}
     expected = {"int": expected_floordiv}
     assert_equal_data(result, expected)
 
 
-def test_expr_floordiv_by_zero(constructor: Constructor) -> None:
-    if "pandas" in str(constructor) and PANDAS_VERSION < (2,):
-        pytest.skip(reason="converts floordiv by zero to 0")
+def test_expr_floordiv_by_zero(
+    constructor: Constructor, request: pytest.FixtureRequest
+) -> None:
     if "polars" in str(constructor) and POLARS_VERSION < (0, 20, 6):
         pytest.skip(reason="bug")
+
     df = nw.from_native(constructor(data))
+
+    if df.implementation.is_pandas_like():
+        reason = "floordiv either fails or generate different result"
+        request.applymarker(pytest.mark.xfail(reason=reason))
+
     result = df.select(nw.col("int") // 0)
     expected = {"int": expected_floordiv}
     assert_equal_data(result, expected)
