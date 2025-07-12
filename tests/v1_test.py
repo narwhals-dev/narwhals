1af547ab4a29ad6fdcaf5319e96f53392cffef93
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-12 13:28:16 +0100

    chore: more v1 test suite cleanups

--- tests/v1_test.py
+++ tests/v1_test.py
@@ -895,26 +895,23 @@ def test_head_aggregation() -> None:
         nw_v1.col("a").mean().head()
 
 
-@pytest.mark.parametrize(
-    ("descending", "nulls_last", "expected"),
-    [
-        (True, True, {"a": [0, 0, 2, -1], "b": [3, 2, 1, None]}),
-        (True, False, {"a": [0, 0, 2, -1], "b": [None, 3, 2, 1]}),
-        (False, True, {"a": [0, 0, 2, -1], "b": [1, 2, 3, None]}),
-        (False, False, {"a": [0, 0, 2, -1], "b": [None, 1, 2, 3]}),
-    ],
-)
-def test_sort_expr(descending: Any, nulls_last: Any, expected: Any) -> None:
+def test_deprecated_expr_methods() -> None:
     pytest.importorskip("polars")
     import polars as pl
 
     data = {"a": [0, 0, 2, -1], "b": [1, 3, 2, None]}
     df = nw_v1.from_native(pl.DataFrame(data), eager_only=True)
     result = df.select(
-        "a", nw_v1.col("b").sort(descending=descending, nulls_last=nulls_last)
+        c=nw_v1.col("a").sort().head(2),
+        d=nw_v1.col("a").sort().tail(2),
+        e=(nw_v1.col("a") == 0).arg_true(),
     )
+    expected = {"c": [-1, 0], "d": [0, 2], "e": [0, 1]}
     assert_equal_data(result, expected)
-    with pytest.deprecated_call(
-        match="is deprecated and will be removed in a future version"
-    ):
-        df.select("a", nw.col("b").sort(descending=descending, nulls_last=nulls_last))
+
+    with pytest.deprecated_call():
+        df.select(
+            c=nw.col("a").sort().head(2),
+            d=nw.col("a").sort().tail(2),
+            e=(nw.col("a") == 0).arg_true(),
+        )
