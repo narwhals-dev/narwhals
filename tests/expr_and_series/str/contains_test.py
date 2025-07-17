115ae950aa3427dca528bf93a5d1ee625c54f25e
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-17 19:04:19 +0100

    fixup

--- tests/expr_and_series/str/contains_test.py
+++ tests/expr_and_series/str/contains_test.py
@@ -20,7 +20,7 @@ def test_contains_case_insensitive(
     result = df.select(
         nw.col("pets").str.contains("(?i)parrot|Dove").alias("case_insensitive_match")
     )
-    if "pandas" in str(constructor) and PANDAS_VERSION >= (3,):
+    if "pandas_constructor" in str(constructor) and PANDAS_VERSION >= (3,):
         # pandas uses 'str' type, and the result is 'bool', which can't contain missing values.
         expected: dict[str, list[Any]] = {
             "case_insensitive_match": [False, False, True, True, True, False]
@@ -38,21 +38,39 @@ def test_contains_series_case_insensitive(
 
     df = nw.from_native(constructor_eager(data), eager_only=True)
     result = df.select(case_insensitive_match=df["pets"].str.contains("(?i)parrot|Dove"))
-    expected = {"case_insensitive_match": [False, False, True, True, True, None]}
+    if "pandas_constructor" in str(constructor_eager) and PANDAS_VERSION >= (3,):
+        # pandas uses 'str' type, and the result is 'bool', which can't contain missing values.
+        expected: dict[str, Any] = {
+            "case_insensitive_match": [False, False, True, True, True, False]
+        }
+    else:
+        expected = {"case_insensitive_match": [False, False, True, True, True, None]}
     assert_equal_data(result, expected)
 
 
 def test_contains_case_sensitive(constructor: Constructor) -> None:
     df = nw.from_native(constructor(data))
     result = df.select(nw.col("pets").str.contains("parrot|Dove").alias("default_match"))
-    expected = {"default_match": [False, False, True, False, False, None]}
+    if "pandas_constructor" in str(constructor) and PANDAS_VERSION >= (3,):
+        # pandas uses 'str' type, and the result is 'bool', which can't contain missing values.
+        expected: dict[str, Any] = {
+            "default_match": [False, False, True, False, False, False]
+        }
+    else:
+        expected = {"default_match": [False, False, True, False, False, None]}
     assert_equal_data(result, expected)
 
 
 def test_contains_series_case_sensitive(constructor_eager: ConstructorEager) -> None:
     df = nw.from_native(constructor_eager(data), eager_only=True)
     result = df.select(default_match=df["pets"].str.contains("parrot|Dove"))
-    expected = {"default_match": [False, False, True, False, False, None]}
+    if "pandas_constructor" in str(constructor_eager) and PANDAS_VERSION >= (3,):
+        # pandas uses 'str' type, and the result is 'bool', which can't contain missing values.
+        expected: dict[str, Any] = {
+            "default_match": [False, False, True, False, False, False]
+        }
+    else:
+        expected = {"default_match": [False, False, True, False, False, None]}
     assert_equal_data(result, expected)
 
 
@@ -82,8 +100,15 @@ def test_contains_series_literal(constructor_eager: ConstructorEager) -> None:
         default_match=df["pets"].str.contains("Parrot|dove"),
         literal_match=df["pets"].str.contains("Parrot|dove", literal=True),
     )
-    expected = {
-        "default_match": [False, False, False, True, True, None],
-        "literal_match": [False, False, False, False, True, None],
-    }
+    if "pandas_constructor" in str(constructor_eager) and PANDAS_VERSION >= (3,):
+        # pandas uses 'str' type, and the result is 'bool', which can't contain missing values.
+        expected: dict[str, Any] = {
+            "default_match": [False, False, False, True, True, False],
+            "literal_match": [False, False, False, False, True, False],
+        }
+    else:
+        expected = {
+            "default_match": [False, False, False, True, True, None],
+            "literal_match": [False, False, False, False, True, None],
+        }
     assert_equal_data(result, expected)
