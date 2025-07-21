df9809d261c83dbc9ded13f1aeebb601d6e9d095
ym-pett <ympettinato@gmail.com>
2025-07-21 12:28:07 +0100

    delete commented out

--- narwhals/_duckdb/expr.py
+++ narwhals/_duckdb/expr.py
@@ -390,10 +390,6 @@ class DuckDBExpr(SQLExpr["DuckDBLazyFrame", "Expression"]):
 
         return self._with_elementwise(_log)
 
-    # def exp(self) -> Self:
-    #     def _exp(expr: Expression) -> Expression:
-    #         return F("exp", expr)
-
         return self._with_elementwise(_exp)
 
     def sqrt(self) -> Self:
