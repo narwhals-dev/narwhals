d09bc89417773eac4542cc55e42b4c01687672f9
Cameron Riddell <cameron@dutc.io>
2025-07-28 12:02:45 -0700

    clarify errors in str.replace/replace_all tests

--- tests/expr_and_series/str/replace_test.py
+++ tests/expr_and_series/str/replace_test.py
@@ -32,7 +32,7 @@ replace_all_data = [
     ),
 ]
 
-replace_data_nonscalar = [
+replace_data_multivalue = [
     (
         {"a": ["123abc", "abc456"], "b": ["ghi", "jkl"]},
         r"abc",
@@ -67,7 +67,7 @@ replace_data_nonscalar = [
     ),
 ]
 
-replace_all_data_nonscalar = [
+replace_all_data_multivalue = [
     (
         {"a": ["123abc", "abc456"], "b": ["ghi", "jkl"]},
         r"abc",
@@ -142,12 +142,13 @@ def test_str_replace_expr_scalar(
     literal: bool,  # noqa: FBT001
     expected: dict[str, list[str]],
 ) -> None:
-    if (
-        ("pyspark" in str(constructor))
-        or "duckdb" in str(constructor)
-        or "ibis" in str(constructor)
-    ):
-        request.applymarker(pytest.mark.xfail)
+    if any(x in str(constructor) for x in ["pyspark", "duckdb", "ibis"]):
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor} only supports `replace_all`.",
+                raises=NotImplementedError,
+            )
+        )
     df = nw.from_native(constructor(data))
     result_df = df.select(
         nw.col("a").str.replace(pattern=pattern, value=value, n=n, literal=literal)
@@ -174,9 +175,9 @@ def test_str_replace_all_expr_scalar(
 
 
 @pytest.mark.parametrize(
-    ("data", "pattern", "value", "n", "literal", "expected"), replace_data_nonscalar
+    ("data", "pattern", "value", "n", "literal", "expected"), replace_data_multivalue
 )
-def test_str_replace_series_nonscalar(
+def test_str_replace_series_multivalue(
     constructor_eager: ConstructorEager,
     data: dict[str, list[str]],
     pattern: str,
@@ -188,7 +189,12 @@ def test_str_replace_series_nonscalar(
 ) -> None:
     df = nw.from_native(constructor_eager(data), eager_only=True)
     if "pyarrow_table" in str(constructor_eager):
-        request.applymarker(pytest.mark.xfail)
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor_eager} does not support multivalue replacement",
+                raises=TypeError,
+            )
+        )
 
     result_series = df["a"].str.replace(
         pattern=pattern, value=df[value], n=n, literal=literal
@@ -197,9 +203,9 @@ def test_str_replace_series_nonscalar(
 
 
 @pytest.mark.parametrize(
-    ("data", "pattern", "value", "literal", "expected"), replace_all_data_nonscalar
+    ("data", "pattern", "value", "literal", "expected"), replace_all_data_multivalue
 )
-def test_str_replace_all_series_nonscalar(
+def test_str_replace_all_series_multivalue(
     constructor_eager: ConstructorEager,
     data: dict[str, list[str]],
     pattern: str,
@@ -209,9 +215,14 @@ def test_str_replace_all_series_nonscalar(
     request: pytest.FixtureRequest,
 ) -> None:
     if "pyarrow_table" in str(constructor_eager):
-        request.applymarker(pytest.mark.xfail)
-    df = nw.from_native(constructor_eager(data), eager_only=True)
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor_eager} only supports `replace_all`.",
+                raises=TypeError,
+            )
+        )
 
+    df = nw.from_native(constructor_eager(data), eager_only=True)
     result_series = df["a"].str.replace_all(
         pattern=pattern, value=df[value], literal=literal
     )
@@ -219,9 +230,9 @@ def test_str_replace_all_series_nonscalar(
 
 
 @pytest.mark.parametrize(
-    ("data", "pattern", "value", "n", "literal", "expected"), replace_data_nonscalar
+    ("data", "pattern", "value", "n", "literal", "expected"), replace_data_multivalue
 )
-def test_str_replace_expr_nonscalar(
+def test_str_replace_expr_multivalue(
     constructor: Constructor,
     request: pytest.FixtureRequest,
     data: dict[str, list[str]],
@@ -231,14 +242,21 @@ def test_str_replace_expr_nonscalar(
     literal: bool,  # noqa: FBT001
     expected: dict[str, list[str]],
 ) -> None:
-    if (
-        ("pyspark" in str(constructor))
-        or "duckdb" in str(constructor)
-        or "ibis" in str(constructor)
-        or "pyarrow_table" in str(constructor)
-        or ("sqlframe" in str(constructor))
-    ):
-        request.applymarker(pytest.mark.xfail)
+    if any(x in str(constructor) for x in ["pyspark", "duckdb", "ibis"]):
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor} only supports `replace_all`.",
+                raises=NotImplementedError,
+            )
+        )
+    elif any(x in str(constructor) for x in ["pyarrow_table", "dask"]):
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor} does not support multivalue replacement",
+                raises=TypeError,
+            )
+        )
+
     df = nw.from_native(constructor(data))
     result_df = df.select(
         nw.col("a").str.replace(
@@ -249,9 +267,9 @@ def test_str_replace_expr_nonscalar(
 
 
 @pytest.mark.parametrize(
-    ("data", "pattern", "value", "literal", "expected"), replace_all_data_nonscalar
+    ("data", "pattern", "value", "literal", "expected"), replace_all_data_multivalue
 )
-def test_str_replace_all_expr_nonscalar(
+def test_str_replace_all_expr_multivalue(
     constructor: Constructor,
     data: dict[str, list[str]],
     pattern: str,
@@ -260,12 +278,14 @@ def test_str_replace_all_expr_nonscalar(
     expected: dict[str, list[str]],
     request: pytest.FixtureRequest,
 ) -> None:
-    if (
-        ("pyarrow_table" in str(constructor))
-        or ("sqlframe" in str(constructor))
-        or ("ibis" in str(constructor))
-    ):
-        request.applymarker(pytest.mark.xfail)
+    if any(x in str(constructor) for x in ["pyarrow_table", "pyspark", "ibis", "dask"]):
+        request.applymarker(
+            pytest.mark.xfail(
+                reason=f"{constructor} does not support multivalue replacement",
+                raises=TypeError,
+            )
+        )
+
     df = nw.from_native(constructor(data))
     result = df.select(
         nw.col("a").str.replace_all(pattern=pattern, value=nw.col(value), literal=literal)
