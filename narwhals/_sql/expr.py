2bf02b4ce4f978094912af155369727d5daf857f
ym-pett <ympettinato@gmail.com>
2025-07-29 10:30:17 +0100

    removed duplicate _when

--- narwhals/_sql/expr.py
+++ narwhals/_sql/expr.py
@@ -181,9 +181,6 @@ class SQLExpr(LazyExpr[SQLLazyFrameT, NativeExprT], Protocol[SQLLazyFrameT, Nati
     def _lit(self, value: Any) -> NativeExprT:
         return self.__narwhals_namespace__()._lit(value)
 
-    def _when(self, condition: NativeExprT, value: NativeExprT) -> NativeExprT:
-        return self.__narwhals_namespace__()._when(condition, value)
-
     def _coalesce(self, *expr: NativeExprT) -> NativeExprT:
         return self.__narwhals_namespace__()._coalesce(*expr)
 
