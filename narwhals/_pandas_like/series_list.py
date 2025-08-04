12611ab279cfa72e58cfc3ef464aba2cfb26f587
raisadz <34237447+raisadz@users.noreply.github.com>
2025-08-04 18:14:17 +0100

    add `pandas`

--- narwhals/_pandas_like/series_list.py
+++ narwhals/_pandas_like/series_list.py
@@ -8,6 +8,7 @@ from narwhals._pandas_like.utils import (
     get_dtype_backend,
     narwhals_to_native_dtype,
 )
+from narwhals._pandas_like.utils import is_dtype_pyarrow
 
 if TYPE_CHECKING:
     from narwhals._pandas_like.series import PandasLikeSeries
@@ -30,3 +31,25 @@ class PandasLikeSeriesListNamespace(
             self.version,
         )
         return self.with_native(result.astype(dtype)).alias(self.native.name)
+    
+    def unique(self) -> PandasLikeSeries:
+        native = self.native
+        if is_dtype_pyarrow(self.native.dtype):
+            import pyarrow as pa  # ignore-banned-import
+
+            compliant = self.compliant
+            ca = pa.chunked_array([compliant.to_arrow()])  # type: ignore[arg-type]
+            result = (
+                compliant._version.namespace.from_backend("pyarrow")
+                .compliant.from_native(ca)
+                .list.unique()
+                .native
+            )
+            return self.with_native(native.__class__(
+                result, dtype=native.dtype, index=native.index, name=native.name
+            ))
+        else:
+            msg = (
+                f"The `unique` method is not implemented for {self.implementation} backend."
+            )
+            raise NotImplementedError(msg)
--- tests/expr_and_series/list/unique_test.py
+++ tests/expr_and_series/list/unique_test.py
@@ -4,32 +4,28 @@ import pandas as pd
 import pytest
 
 import narwhals as nw
-from tests.utils import PANDAS_VERSION, Constructor, ConstructorEager, assert_equal_data
-
-#data = {"a": [[1, 2], [3, 4, None], None, [], [None]]}
-#expected = {"a": [2, 3, None, 0, 1]}
+from tests.utils import Constructor, ConstructorEager, assert_equal_data
 
 data = {"a": [[2, 2, 3, None, None]]}
 expected = {"a": [[None, 2, 3]]}
-expected_sorted = {"a": [None, 2, 3]}
+expected_nulls_first= {"a": [None, 2, 3]}
+expected_nulls_last= {'a': [[2, 3, None]]}
 
 
 def test_unique_expr(request: pytest.FixtureRequest, constructor: Constructor) -> None:
     if any(backend in str(constructor) for backend in ("dask", "modin", "cudf")):
         request.applymarker(pytest.mark.xfail)
 
-    if "pandas" in str(constructor) and PANDAS_VERSION < (2, 2):
-        pytest.skip()
-
     result = nw.from_native(constructor(data)).select(
         nw.col("a").cast(nw.List(nw.Int32())).list.unique()
     )
     if any(backend in str(constructor) for backend in ("duckdb", "sqlframe", "pyspark", "polars", "ibis")):
         result = result.explode("a").sort("a")
-        assert_equal_data(result, expected_sorted)
+        assert_equal_data(result, expected_nulls_first)
+    elif any(backend in str(constructor) for backend in ("pandas", "pyarrow")):
+        assert_equal_data(result, expected_nulls_last)
     else:
         assert_equal_data(result, expected)
-    
 
 
 def test_unique_series(
@@ -38,10 +34,10 @@ def test_unique_series(
     if any(backend in str(constructor_eager) for backend in ("modin", "cudf")):
         request.applymarker(pytest.mark.xfail)
 
-    if "pandas" in str(constructor_eager) and PANDAS_VERSION < (2, 2):
-        pytest.skip()
-
     df = nw.from_native(constructor_eager(data), eager_only=True)
 
     result = df["a"].cast(nw.List(nw.Int32())).list.unique()
-    assert_equal_data({"a": result}, expected)
+    if any(backend in str(constructor_eager) for backend in ("pandas", "pyarrow")):
+        assert_equal_data({"a": result}, expected_nulls_last)
+    else:
+        assert_equal_data({"a": result}, expected)
