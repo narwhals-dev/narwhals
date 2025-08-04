7fd179c69ba1537dc41e912bc9a9cbb95539bc0d
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-08-04 12:55:00 +0000

    test: Split out infer case
    Going to parametrize more for `test_series_from_iterable`
    https://github.com/narwhals-dev/narwhals/pull/2933#discussion_r2249983202

--- tests/series_only/from_iterable_test.py
+++ tests/series_only/from_iterable_test.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pytest
 
@@ -8,21 +8,29 @@ import narwhals as nw
 from tests.utils import assert_equal_series
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+
     from narwhals._namespace import EagerAllowed
+    from narwhals.typing import IntoDType
 
 
+# TODO @dangotbanned: parametrize many https://github.com/narwhals-dev/narwhals/pull/2933#discussion_r2249983202
 def test_series_from_iterable(eager_backend: EagerAllowed) -> None:
     name = "b"
     into_values = 4, 1, 2
     values = list(into_values)
-    result = nw.Series.from_iterable(name, values, backend=eager_backend)
-    # all supported libraries auto-infer this to be int64, we can always special-case
-    # something different if necessary
-    assert result.dtype == nw.Int64
-    assert_equal_series(result, values, name)
-
     result = nw.Series.from_iterable(name, values, nw.Int32, backend=eager_backend)
     assert result.dtype == nw.Int32
+    assert_equal_series(result, values, name)
+
+
+@pytest.mark.parametrize(("values", "expected_dtype"), [((4, 1, 2), nw.Int64)])
+def test_series_from_iterable_infer(
+    eager_backend: EagerAllowed, values: Sequence[Any], expected_dtype: IntoDType
+) -> None:
+    name = "b"
+    result = nw.Series.from_iterable(name, values, backend=eager_backend)
+    assert result.dtype == expected_dtype
     assert_equal_series(result, values, name)
 
 
