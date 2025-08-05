5839b1ca02326e94f1dda3a53b94ca9c52e9ca84
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-08-05 16:43:18 +0200

    WIP

--- narwhals/testing.py
+++ narwhals/testing.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, NoReturn
 
-from narwhals.functions import new_series
+from narwhals import from_native, new_series
 
 if TYPE_CHECKING:
-    from narwhals.typing import SeriesT
+    from narwhals.typing import FrameT, SeriesT
 
 
 def _raise_assertion_error(mismatch_type: str, left: Any, right: Any) -> NoReturn:
@@ -18,35 +18,53 @@ def assert_series_equal(
     left: SeriesT,
     right: SeriesT,
     *,
-    check_backend: bool = True,
     check_dtypes: bool = True,
     check_names: bool = True,
-    # check_order: bool = True,  # Don't allow False due to complex types
+    # check_order: bool = True,  # TODO(FBruzzesi): How do we sort complex types?
     check_exact: bool = False,
     rtol: float = 1e-05,
     atol: float = 1e-08,
     # categorical_as_str: bool = False,  # TODO(FBruzzesi): Should we even support this?
 ) -> None:
-    if check_backend and (l_impl := left.implementation) != (
-        r_impl := right.implementation
-    ):
-        _raise_assertion_error("backend mismatch", l_impl, r_impl)
+    """Assert that the left and right Series are equal.
 
-    if (l_len := len(left)) != (r_len := len(right)):
+    Raises a detailed `AssertionError` if the Series differ.
+    This function is intended for use in unit tests.
+
+    Arguments:
+        left: The first Series to compare.
+        right: The second Series to compare.
+        check_dtypes: Requires data types to match.
+        check_names: Requires names to match.
+        check_exact: Requires float values to match exactly. If set to `False`, values are
+            considered equal when within tolerance of each other (see `rtol` and `atol`).
+            Only affects columns with a Float data type.
+        rtol: Relative tolerance for inexact checking, given as a fraction of the values in
+            `right`.
+        atol: Absolute tolerance for inexact checking.
+
+    """
+    left_ = from_native(left, series_only=True)
+    right_ = from_native(right, series_only=True)
+
+    if (l_impl := left_.implementation) != (r_impl := right_.implementation):
+        _raise_assertion_error("implementation mismatch", l_impl, r_impl)
+
+    if (l_len := len(left_)) != (r_len := len(right_)):
         _raise_assertion_error("length mismatch", l_len, r_len)
 
-    if check_dtypes and (l_dtype := left.dtype) != (r_dtype := right.dtype):
+    if check_dtypes and (l_dtype := left_.dtype) != (r_dtype := right_.dtype):
         _raise_assertion_error("dtype mismatch", l_dtype, r_dtype)
 
-    if check_names and (l_name := left.name) != (r_name := right.name):
+    if check_names and (l_name := left_.name) != (r_name := right_.name):
         _raise_assertion_error("name mismatch", l_name, r_name)
 
-    if ((l_null_mask := left.is_null()) != (r_null_mask := right.is_null())).any() or (
-        l_null_count := left.null_count()
-    ) != (r_null_count := right.null_count()):
+    if ((l_null_mask := left_.is_null()) != (r_null_mask := right_.is_null())).any() or (
+        l_null_count := left_.null_count()
+    ) != (r_null_count := right_.null_count()):
         _raise_assertion_error("null value mismatch", l_null_count, r_null_count)
 
-    l_vals, r_vals = left.filter(~l_null_mask), right.filter(~r_null_mask)
+    l_vals, r_vals = left_.filter(~l_null_mask), right_.filter(~r_null_mask)
 
     # TODO(FBruzzesi): Open points:
     #  1. Handle nan's and infinities for numerical
@@ -61,7 +79,7 @@ def assert_series_equal(
         # Namely: abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol).
         l_abs, r_abs = l_vals.abs(), r_vals.abs()
         m1 = l_abs.zip_with(l_abs > r_abs, r_abs) * rtol
-        m2 = new_series(name="tmp", values=[atol] * l_len, backend=l_impl)
+        m2 = new_series(name="tmp", values=[atol] * l_len, backend=left_.implementation)
 
         is_not_close_mask = (l_vals - r_vals).abs() > m1.zip_with(m1 > m2, m2)
 
@@ -71,3 +89,17 @@ def assert_series_equal(
                 l_vals.filter(is_not_close_mask),
                 r_vals.filter(is_not_close_mask),
             )
+
+
+def assert_frame_equal(
+    left: FrameT,
+    right: FrameT,
+    *,
+    check_row_order: bool = True,
+    check_column_order: bool = True,
+    check_dtypes: bool = True,
+    check_exact: bool = False,
+    rtol: float = 1e-05,
+    atol: float = 1e-08,
+    categorical_as_str: bool = False,
+) -> None: ...
