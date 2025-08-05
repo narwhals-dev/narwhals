44a5f1ccb8fb7dce67b2b735cc259ad26de39507
raisadz <34237447+raisadz@users.noreply.github.com>
2025-08-05 14:56:27 +0100

    simplify `pyspark`

--- narwhals/_spark_like/expr_list.py
+++ narwhals/_spark_like/expr_list.py
@@ -6,8 +6,6 @@ from narwhals._compliant import LazyExprNamespace
 from narwhals._compliant.any_namespace import ListNamespace
 
 if TYPE_CHECKING:
-    from sqlframe.base.column import Column
-
     from narwhals._spark_like.expr import SparkLikeExpr
 
 
@@ -18,12 +16,4 @@ class SparkLikeExprListNamespace(
         return self.compliant._with_elementwise(self.compliant._F.array_size)
 
     def unique(self) -> SparkLikeExpr:
-        def func(expr: Column) -> Column:
-            F = self.compliant._F  # noqa: N806
-            list_distinct = F.array_distinct(expr)
-            return F.when(
-                F.array_position(expr, F.lit(None)).isNotNull(),
-                F.array_append(list_distinct, F.lit(None)),
-            ).otherwise(list_distinct)
-
-        return self.compliant._with_elementwise(func)
+        return self.compliant._with_elementwise(self.compliant._F.array_distinct)
