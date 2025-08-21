330fdd0ef96ebe853c54472eb473a370b00048f9
ym-pett <ympettinato@gmail.com>
2025-08-21 16:01:24 +0100

    wip

--- narwhals/translate.py
+++ narwhals/translate.py
@@ -5,6 +5,7 @@ import sys
 from decimal import Decimal
 from functools import wraps
 from typing import TYPE_CHECKING, Any, Callable, Literal, TypeVar, overload
+import contextlib
 
 from narwhals._constants import EPOCH, MS_PER_SECOND
 from narwhals._namespace import (
@@ -358,10 +359,8 @@ def _from_native_impl(  # noqa: C901, PLR0911, PLR0912, PLR0915
 
         for plugin in discovered_plugins:  # pragma: no cover
             obj = plugin.load()
-            if obj.is_native_object(native_object):
-                native_object = obj.from_native(
-                    native_object, eager_only=False, series_only=False
-                )
+            with contextlib.suppress(TypeError):
+                native_object = obj.from_native(native_object)
                 break
 
     # Extensions
