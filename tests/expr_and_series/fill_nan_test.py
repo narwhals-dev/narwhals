8e61f630ed4b2fe360e0f32ae037372a3df21b0a
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-08-24 11:13:58 +0100

    coverage

--- tests/expr_and_series/fill_nan_test.py
+++ tests/expr_and_series/fill_nan_test.py
@@ -1,13 +1,20 @@
 from __future__ import annotations
 
 import narwhals as nw
-from tests.utils import Constructor, assert_equal_data
+from tests.utils import Constructor, ConstructorEager, assert_equal_data
 
 
 def test_is_nan(constructor: Constructor) -> None:
     df = nw.from_native(
         constructor({"a": [1.1, 2.0, float("nan")], "b": [3.1, 4.0, None]})
     )
+
+    if df.implementation.is_dask():
+        # test both pyarrow-dtypes and numpy-dtypes
+        df = nw.from_native(
+            df.to_native().astype({"a": "Float64[pyarrow]", "b": "float64"})
+        )  # type: ignore[attr-defined]
+
     result = df.select(nw.all().fill_nan(None))
     expected = {"a": [1.1, 2.0, None], "b": [3.1, 4.0, None]}
     assert_equal_data(result, expected)
@@ -22,3 +29,12 @@ def test_is_nan(constructor: Constructor) -> None:
         assert result.lazy().collect()["b"].null_count() == 1
     assert_equal_data(result, expected)
     assert result.lazy().collect()["a"].null_count() == 0
+
+
+def test_is_nan_series(constructor_eager: ConstructorEager) -> None:
+    s = nw.from_native(
+        constructor_eager({"a": [1.1, 2.0, float("nan")]}), eager_only=True
+    )["a"]
+
+    result = s.fill_nan(999)
+    assert_equal_data({"a": result}, {"a": [1.1, 2.0, 999]})
