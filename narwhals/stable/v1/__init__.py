055b8bbd9c6aa4504545ed90d7ff2d8600e3d375
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-09-08 12:16:09 +0100

    Revert "chore: simplify parsing expressions in group-by (#3106)"
    This reverts commit 538973e32c2b9114b74dd2fbe5898015bd85197c.

--- narwhals/stable/v1/__init__.py
+++ narwhals/stable/v1/__init__.py
@@ -6,7 +6,6 @@ from typing import TYPE_CHECKING, Any, Callable, Literal, cast, overload
 import narwhals as nw
 from narwhals import exceptions, functions as nw_f
 from narwhals._exceptions import issue_warning
-from narwhals._expression_parsing import is_expr
 from narwhals._typing_compat import TypeVar, assert_never
 from narwhals._utils import (
     Implementation,
@@ -234,13 +233,17 @@ class LazyFrame(NwLazyFrame[IntoLazyFrameT]):
     def _dataframe(self) -> type[DataFrame[Any]]:
         return DataFrame
 
-    def _parse_into_expr(self, arg: Expr | str) -> Expr:  # type: ignore[override]
+    def _extract_compliant(self, arg: Any) -> Any:
         # After v1, we raise when passing order-dependent, length-changing,
         # or filtration expressions to LazyFrame
-        if isinstance(arg, str):
-            return col(arg)
-        if is_expr(arg):
-            return arg
+        from narwhals.expr import Expr
+        from narwhals.series import Series
+
+        if isinstance(arg, Series):  # pragma: no cover
+            msg = "Mixing Series with LazyFrame is not supported."
+            raise TypeError(msg)
+        if isinstance(arg, (Expr, str)):
+            return self.__narwhals_namespace__().parse_into_expr(arg, str_as_lit=False)
         raise InvalidIntoExprError.from_invalid_type(type(arg))
 
     def collect(
