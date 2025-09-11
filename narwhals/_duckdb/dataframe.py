893135b2dc89e360aa4aecade6877d3b10f2c519
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-09-11 12:14:06 +0100

    fix typing

--- narwhals/_duckdb/dataframe.py
+++ narwhals/_duckdb/dataframe.py
@@ -256,7 +256,7 @@ class DuckDBLazyFrame(
 
     def to_arrow(self) -> pa.Table:
         # only if version is v1, keep around for backcompat
-        return self.lazy().collect(Implementation.PYARROW).native
+        return self.lazy().collect(Implementation.PYARROW).native  # type: ignore[no-any-return]
 
     def _with_version(self, version: Version) -> Self:
         return self.__class__(self.native, version=version)
