2f22890a415500c507278f46769e783db825d773
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-09-16 09:52:41 +0000

    test: Raise a helpful message if fails in ci
    Hoping this addresses https://github.com/narwhals-dev/narwhals/pull/3134#discussion_r2349707956

--- tests/version_test.py
+++ tests/version_test.py
@@ -2,10 +2,16 @@ from __future__ import annotations
 
 import re
 from importlib.metadata import distribution
+from os import environ
 from pathlib import Path
 
 import narwhals as nw
 from narwhals._utils import parse_version
+
+
+def is_in_ci() -> bool:
+    # https://docs.github.com/en/actions/reference/workflows-and-actions/variables#default-environment-variables
+    return environ.get("CI", "") == "true" and environ.get("GITHUB_ACTIONS", "") == "true"
 
 
 def test_package_version() -> None:
@@ -18,7 +24,13 @@ def test_package_version() -> None:
     pyproject_version = match.group(1)
 
     assert isinstance(version, str)
-    if version != pyproject_version:  # pragma: no cover
+    msg = (
+        f"Expected `nw.__version__` to match `pyproject.toml`, unless testing locally.\n\n"
+        f"nw.__version__={version!r}\npyproject.toml={pyproject_version!r}"
+    )
+    if is_in_ci():
+        assert version == pyproject_version, msg
+    elif version != pyproject_version:
         # NOTE: metadata from venv is outdated (https://github.com/narwhals-dev/narwhals/pull/3130#issuecomment-3291578373)
         version_comp = parse_version(version)
         assert version_comp < parse_version(pyproject_version)
