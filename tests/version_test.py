0500e3c3844017e7afb52f6d32043d844ec6a7b7
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-09-16 10:02:09 +0000

    test: Isolate the single assert that differs

--- tests/version_test.py
+++ tests/version_test.py
@@ -15,24 +15,23 @@ def is_in_ci() -> bool:
 
 
 def test_package_version() -> None:
-    version = nw.__version__
-    dist_version = distribution("narwhals").version
-
     content = Path("pyproject.toml").read_text("utf-8")
     match = re.search(r'version = "(.*)"', content)
     assert match is not None
     pyproject_version = match.group(1)
 
+    version = nw.__version__
     assert isinstance(version, str)
+    version_comp = parse_version(version)
+
     msg = (
         f"Expected `nw.__version__` to match `pyproject.toml`, unless testing locally.\n\n"
         f"nw.__version__={version!r}\npyproject.toml={pyproject_version!r}"
     )
     if is_in_ci():
         assert version == pyproject_version, msg
-    elif version != pyproject_version:
-        # NOTE: metadata from venv is outdated (https://github.com/narwhals-dev/narwhals/pull/3130#issuecomment-3291578373)
-        version_comp = parse_version(version)
-        assert version_comp < parse_version(pyproject_version)
-        assert version_comp == parse_version(dist_version)
+    else:  # pragma: no cover
+        # NOTE: metadata from venv may be outdated (https://github.com/narwhals-dev/narwhals/pull/3130#issuecomment-3291578373)
+        assert version_comp <= parse_version(pyproject_version)
+    dist_version = distribution("narwhals").version
     assert version == dist_version
