cb51c6741062f5037c11fd37fbd5a7eb68915f46
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-09-29 14:03:20 +0000

    docs: Leave more useful notes in `group_by`

--- narwhals/_plan/arrow/group_by.py
+++ narwhals/_plan/arrow/group_by.py
@@ -24,6 +24,9 @@ if TYPE_CHECKING:
 
 Incomplete: TypeAlias = Any
 
+# NOTE: Unless stated otherwise, all aggregations have 2 variants:
+# - `<function>` (pc.Function.kind == "scalar_aggregate")
+# - `hash_<function>` (pc.Function.kind == "hash_aggregate")
 SUPPORTED_AGG: Mapping[type[agg.AggExpr], acero.Aggregation] = {
     agg.Sum: "hash_sum",
     agg.Mean: "hash_mean",
@@ -40,18 +43,15 @@ SUPPORTED_AGG: Mapping[type[agg.AggExpr], acero.Aggregation] = {
 }
 SUPPORTED_IR: Mapping[type[ir.ExprIR], acero.Aggregation] = {
     ir.Len: "hash_count_all",
-    ir.Column: "hash_list",
+    ir.Column: "hash_list",  # `hash_aggregate` only
 }
 SUPPORTED_FUNCTION: Mapping[type[ir.Function], acero.Aggregation] = {
     ir.boolean.All: "hash_all",
     ir.boolean.Any: "hash_any",
-    ir.functions.Unique: "hash_distinct",
+    ir.functions.Unique: "hash_distinct",  # `hash_aggregate` only
 }
 
-REQUIRES_PYARROW_20: tuple[Literal["kurtosis"], Literal["skew"]] = (
-    "kurtosis",  # Compute the kurtosis of values in each group
-    "skew",  # Compute the skewness of values in each group
-)
+REQUIRES_PYARROW_20: tuple[Literal["kurtosis"], Literal["skew"]] = ("kurtosis", "skew")
 """They don't show in [our version of the stubs], but are possible in [`pyarrow>=20`].
 
 [our version of the stubs]: https://github.com/narwhals-dev/narwhals/issues/2124#issuecomment-3191374210
