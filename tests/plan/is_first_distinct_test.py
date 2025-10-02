36ddce064e55cd7b4bc87eb27c538c29b6f33712
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-10-02 17:49:52 +0000

    test: Port over `is_first_distinct` tests
    Prep for https://github.com/narwhals-dev/narwhals/pull/3173#discussion_r2399356649

---
+++ tests/plan/is_first_distinct_test.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import pytest
+
+from narwhals import _plan as nwp
+from tests.plan.utils import assert_equal_data, dataframe
+
+XFAIL_NO_IMPL_YET = pytest.mark.xfail(
+    reason="Not added `ArrowExpr.is_first_distinct` yet", raises=AttributeError
+)
+
+
+@XFAIL_NO_IMPL_YET
+def test_is_first_distinct_expr() -> None:
+    data = {"a": [1, 1, 2, 3, 2], "b": [1, 2, 3, 2, 1]}
+    df = dataframe(data)
+    result = df.select(nwp.all().is_first_distinct())
+    expected = {
+        "a": [True, False, True, True, False],
+        "b": [True, True, True, False, False],
+    }
+    assert_equal_data(result, expected)
+
+
+@XFAIL_NO_IMPL_YET
+def test_is_first_distinct_expr_order_by() -> None:
+    data = {"a": [1, 1, 2, 3, 2], "b": [1, 2, 3, 2, 1], "i": [None, 1, 2, 3, 4]}
+    df = dataframe(data)
+    result = (
+        df.select(nwp.col("a", "b").is_first_distinct().over(order_by="i"), "i")
+        .sort("i")
+        .drop("i")
+    )
+    expected = {
+        "a": [True, False, True, True, False],
+        "b": [True, True, True, False, False],
+    }
+    assert_equal_data(result, expected)
