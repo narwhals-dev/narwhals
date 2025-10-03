ff2260429b7375e6ed277f8732f57ca953c65dde
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-10-03 15:40:13 +0100

    make test more unusual

--- tests/expr_and_series/first_last_test.py
+++ tests/expr_and_series/first_last_test.py
@@ -126,7 +126,7 @@ def test_first_expr_over_order_by_partition_by(
     if "pyarrow_table" in str(constructor) and PYARROW_VERSION < (14,):
         pytest.skip()
     frame = nw.from_native(
-        constructor({"a": [1, 1, 2], "b": [4, 5, 6], "c": [None, 7, 8], "i": [0, 1, 2]})
+        constructor({"a": [1, 1, 2], "b": [4, 5, 6], "c": [None, 7, 8], "i": [1, 0, 2]})
     )
     result = frame.with_columns(
         nw.col("b", "c").first().over("a", order_by="i").name.suffix("_first"),
@@ -134,12 +134,12 @@ def test_first_expr_over_order_by_partition_by(
     ).sort("i")
     expected = {
         "a": [1, 1, 2],
-        "b": [4, 5, 6],
-        "c": [None, 7, 8],
+        "b": [5, 4, 6],
+        "c": [7.0, None, 8.0],
         "i": [0, 1, 2],
-        "b_first": [4, 4, 6],
-        "c_first": [None, None, 8],
-        "b_last": [5, 5, 6],
-        "c_last": [7, 7, 8],
+        "b_first": [5, 5, 6],
+        "c_first": [7.0, 7.0, 8.0],
+        "b_last": [4, 4, 6],
+        "c_last": [None, None, 8.0],
     }
     assert_equal_data(result, expected)
