1523dbbff2da60fe6abbedf3aa735a1c74c9d239
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-10-08 12:05:26 +0000

    test: Add some cases from polars

--- tests/plan/frame_filter_test.py
+++ tests/plan/frame_filter_test.py
@@ -20,6 +20,15 @@ def data() -> Data:
     return {"a": [1, 3, 2], "b": [4, 4, 6], "z": [7.0, 8.0, 9.0]}
 
 
+@pytest.fixture
+def data_2() -> Data:
+    """Dataset used [upstream].
+
+    [upstream]: https://github.com/pola-rs/polars/blob/a4522d719de940be3ef99d494ccd1cd6067475c6/py-polars/tests/unit/lazyframe/test_lazyframe.py#L175-L182
+    """
+    return {"a": [1, 1, 1, 2, 2], "b": [1, 1, 2, 2, 2], "c": [1, 1, 2, 3, 4]}
+
+
 @pytest.mark.parametrize(
     "predicate",
     [[False, True, True], series([False, True, True]), nwp.col("a") > 1],
@@ -99,3 +108,67 @@ def test_filter_mask_mixed() -> None:  # pragma: no cover
 
     result = df.filter(mask, nwp.col("b") != 4, b=2)  # type: ignore[arg-type]
     assert_equal_data(result, expected_mixed)
+
+
+def test_filter_multiple_predicates(data_2: Data) -> None:
+    """https://github.com/pola-rs/polars/blob/a4522d719de940be3ef99d494ccd1cd6067475c6/py-polars/tests/unit/lazyframe/test_lazyframe.py#L175-L202."""
+    df = dataframe(data_2)
+
+    # multiple predicates
+    expected = {"a": [1, 1, 1], "b": [1, 1, 2], "c": [1, 1, 2]}
+    for out in (
+        df.filter(nwp.col("a") == 1, nwp.col("b") <= 2),  # positional/splat
+        df.filter([nwp.col("a") == 1, nwp.col("b") <= 2]),  # as list
+    ):
+        assert_equal_data(out, expected)
+
+    # multiple kwargs
+    assert_equal_data(df.filter(a=1, b=2), {"a": [1], "b": [2], "c": [2]})
+
+    # both positional and keyword args
+    assert_equal_data(
+        df.filter(nwp.col("c") < 4, a=2, b=2), {"a": [2], "b": [2], "c": [3]}
+    )
+
+
+def test_filter_string_predicate() -> None:
+    """https://github.com/pola-rs/polars/blob/a4522d719de940be3ef99d494ccd1cd6067475c6/py-polars/tests/unit/lazyframe/test_lazyframe.py#L204-L210."""
+    data = {"description": ["eq", "gt", "ge"], "predicate": ["==", ">", ">="]}
+    expected = {"description": ["eq"], "predicate": ["=="]}
+    df = dataframe(data)
+    result = df.filter(predicate="==")
+    assert_equal_data(result, expected)
+
+
+@pytest.mark.parametrize(
+    "predicate",
+    [
+        [nwp.lit(True)],
+        iter([nwp.lit(True)]),
+        [True, True, True],
+        iter([True, True, True]),
+        (p for p in (nwp.col("z") < 10,)),
+        (p for p in (nwp.col("a") > 0, nwp.col("b") > 0)),
+    ],
+)
+def test_filter_seq_iterable_all_true(predicate: Any, data: Data) -> None:
+    """https://github.com/pola-rs/polars/blob/a4522d719de940be3ef99d494ccd1cd6067475c6/py-polars/tests/unit/lazyframe/test_lazyframe.py#L213-L233."""
+    df = dataframe(data)
+    assert_equal_data(df.filter(predicate), df.to_dict(as_series=False))
+
+
+@pytest.mark.parametrize(
+    "predicate",
+    [
+        [nwp.lit(False)],
+        iter([nwp.lit(False)]),
+        [False, False, False],
+        iter([False, False, False]),
+        (p for p in (nwp.col("z") > 10,)),
+        (p for p in (nwp.col("a") < 0, nwp.col("b") < 0)),
+    ],
+)
+def test_filter_seq_iterable_all_false(predicate: Any, data: Data) -> None:
+    df = dataframe(data)
+    expected = {"a": [], "b": [], "z": []}
+    assert_equal_data(df.filter(predicate), expected)
