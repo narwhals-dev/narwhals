c0d9d006d57627b4115d7aab7d32456d1b66e237
ym-pett <ympettinato@gmail.com>
2025-10-08 18:04:33 +0200

    seeing if no cover works in this file

--- tests/plugins_test.py
+++ tests/plugins_test.py
@@ -12,5 +12,5 @@ def test_plugin() -> None:
     pytest.importorskip("test_plugin")
     df_native = {"a": [1, 1, 2], "b": [4, 5, 6]}
     lf = nw.from_native(df_native)  # type: ignore[call-overload]
-    assert isinstance(lf, nw.LazyFrame)
-    assert lf.columns == ["a", "b"]
+    assert isinstance(lf, nw.LazyFrame)  # pragma: no cover
+    assert lf.columns == ["a", "b"]  # pragma: no cover
