e96f4633c485084cc83d30ba7fe89e230ec41c44
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-10-12 11:30:10 +0000

    refactor(suggestion): Reuse `get_dtype_backend_test` logic
    See https://github.com/narwhals-dev/narwhals/pull/3194/files#r2423653800

--- utils/check_slotted_classes.py
+++ utils/check_slotted_classes.py
@@ -1,55 +1,39 @@
 from __future__ import annotations
 
-import inspect
 import sys
-from itertools import chain
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeVar
 
-import narwhals.dtypes as dtypes_main
-import narwhals.stable.v1.dtypes as v1_dtypes
-import narwhals.stable.v2.dtypes as v2_dtypes
+from narwhals.dtypes import DType, Field
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
-    from types import ModuleType, UnionType
-
-    from typing_extensions import TypeAlias
-
-    if sys.version_info >= (3, 10):
-        _ClassInfo: TypeAlias = type | UnionType | tuple["_ClassInfo", ...]
-    else:
-        _ClassInfo: TypeAlias = type | tuple["_ClassInfo", ...]
 
 
-base_dtype = dtypes_main.DType
-field_type = dtypes_main.Field
+T_co = TypeVar("T_co", covariant=True)
 
 
-def get_unslotted_classes(
-    module: ModuleType, bases: _ClassInfo
-) -> Iterator[tuple[str, ModuleType]]:
-    """Find classes in a `module` that inherit from `bases` but don't define `__slots__`."""
-    return (
-        (name, module)
-        for name, cls in inspect.getmembers(module)
-        if isinstance(cls, type)
-        and issubclass(cls, bases)
-        and "__slots__" not in cls.__dict__
-    )
+def _iter_descendants(*bases: type[T_co]) -> Iterator[type[T_co]]:
+    for base in bases:
+        if children := base.__subclasses__():
+            yield from _iter_descendants(*children)
+        else:
+            yield base
+
+
+def iter_unslotted_classes(*bases: type[T_co]) -> Iterator[str]:
+    """Find classes in that inherit from `bases` but don't define `__slots__`."""
+    for tp in sorted(set(_iter_descendants(*bases)), key=repr):
+        if "__slots__" not in tp.__dict__:
+            yield f"{tp.__module__}.{tp.__name__}"
 
 
 ret = 0
-unslotted_classes = tuple(
-    chain.from_iterable(
-        get_unslotted_classes(mod, bases=(base_dtype, field_type))
-        for mod in (dtypes_main, v1_dtypes, v2_dtypes)
-    )
-)
+unslotted_classes = tuple(iter_unslotted_classes(DType, Field))
 
 if unslotted_classes:
     ret = 1
     msg = "The following classes are expected to define `__slots__` but they don't:\n"
-    cls_list = "\n".join(f"  * {name} from {mod}" for name, mod in unslotted_classes)
+    cls_list = "\n".join(f"  * {name}" for name in unslotted_classes)
     print(f"{msg}{cls_list}")
 
 sys.exit(ret)
