2088eb8fff74b1cc395eb27baa6933645ea36118
Matt Richards <mrichards7@outlook.com.au>
2025-10-13 20:09:36 +1100

    tst: update pandas dtypes test pattern

--- tests/expr_and_series/floor_ceil_test.py
+++ tests/expr_and_series/floor_ceil_test.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import pytest
+
 import narwhals as nw
-from tests.utils import Constructor, ConstructorEager, assert_equal_data
+from tests.utils import PANDAS_VERSION, Constructor, ConstructorEager, assert_equal_data
 
 
 def test_floor(constructor: Constructor) -> None:
@@ -11,10 +13,6 @@ def test_floor(constructor: Constructor) -> None:
 
     expected_data = {"a": [1.0, 2.0, 3.0, -1.0]}
     result_frame = df.select(nw.col("a").floor())
-
-    if "pandas_pyarrow" in str(constructor):
-        pandas_input = df_raw["a"]  # type:ignore[index]
-        assert pandas_input.dtype == result_frame["a"].to_native().dtype
 
     assert_equal_data(result_frame, expected_data)
 
@@ -35,8 +33,7 @@ def test_floor_series(constructor_eager: ConstructorEager) -> None:
     df = nw.from_native(df_raw, eager_only=True)
 
     expected_data = {"a": [1.0, 2.0, 3.0, -1.0]}
-    x = df["a"]
-    result_series = x.floor()
+    result_series = df["a"].floor()
 
     assert_equal_data({"a": result_series}, expected_data)
 
@@ -50,3 +47,71 @@ def test_ceil_series(constructor_eager: ConstructorEager) -> None:
     result_series = df["a"].ceil()
 
     assert_equal_data({"a": result_series}, expected_data)
+
+
+def test_floor_dtype_pandas() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, 2.2], name="a", dtype="float32", index=[8, 7])
+    result = nw.from_native(s, series_only=True).floor().to_native()
+    expected = pd.Series([1.0, 2.0], name="a", dtype="float32", index=[8, 7])
+    pd.testing.assert_series_equal(result, expected)
+
+
+@pytest.mark.skipif(PANDAS_VERSION < (2, 0, 0), reason="nullable types require pandas2+")
+def test_floor_dtype_pandas_nullable() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, None, 2.2], name="a", dtype="Float32", index=[8, 7, 6])
+    result = nw.from_native(s, series_only=True).floor().to_native()
+    expected = pd.Series([1.0, None, 2.0], name="a", dtype="Float32", index=[8, 7, 6])
+    pd.testing.assert_series_equal(result, expected)
+
+
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
+def test_floor_dtype_pandas_pyarrow() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, None, 2.2], name="a", dtype="Float32[pyarrow]", index=[8, 7, 6])
+    result = nw.from_native(s, series_only=True).floor().to_native()
+    expected = pd.Series(
+        [1.0, None, 2.0], name="a", dtype="Float32[pyarrow]", index=[8, 7, 6]
+    )
+    pd.testing.assert_series_equal(result, expected)
+
+
+def test_ceil_dtype_pandas() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, 2.2], name="a", dtype="float32", index=[8, 7])
+    result = nw.from_native(s, series_only=True).ceil().to_native()
+    expected = pd.Series([1.0, 3.0], name="a", dtype="float32", index=[8, 7])
+    pd.testing.assert_series_equal(result, expected)
+
+
+@pytest.mark.skipif(PANDAS_VERSION < (2, 0, 0), reason="nullable types require pandas2+")
+def test_ceil_dtype_pandas_nullable() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, None, 2.2], name="a", dtype="Float32", index=[8, 7, 6])
+    result = nw.from_native(s, series_only=True).ceil().to_native()
+    expected = pd.Series([1.0, None, 3.0], name="a", dtype="Float32", index=[8, 7, 6])
+    pd.testing.assert_series_equal(result, expected)
+
+
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
+def test_ceil_dtype_pandas_pyarrow() -> None:
+    pytest.importorskip("pandas")
+    import pandas as pd
+
+    s = pd.Series([1.0, None, 2.2], name="a", dtype="Float32[pyarrow]", index=[8, 7, 6])
+    result = nw.from_native(s, series_only=True).ceil().to_native()
+    expected = pd.Series(
+        [1.0, None, 3.0], name="a", dtype="Float32[pyarrow]", index=[8, 7, 6]
+    )
+    pd.testing.assert_series_equal(result, expected)
