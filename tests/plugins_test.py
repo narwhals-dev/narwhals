242d8dd91d1c7c0cc46a93b0a7025c85b7c73dee
ym-pett <ympettinato@gmail.com>
2025-10-13 13:55:47 +0200

    removing skip for lower versions as it now runs for all

--- tests/plugins_test.py
+++ tests/plugins_test.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-import sys
-
 import pytest
 
 import narwhals as nw
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="3.10+ required for entrypoints")
 def test_plugin() -> None:
     pytest.importorskip("test_plugin")
     df_native = {"a": [1, 1, 2], "b": [4, 5, 6]}
