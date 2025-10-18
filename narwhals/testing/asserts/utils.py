9417b60b9e1c87e9142aec31c94649d989f1295e
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-10-18 12:38:11 +0200

    try within 'if TYPE_CHECKING'

--- narwhals/testing/asserts/utils.py
+++ narwhals/testing/asserts/utils.py
@@ -8,33 +8,33 @@ from narwhals.dependencies import is_narwhals_series
 if TYPE_CHECKING:
     from typing_extensions import Never, TypeAlias
 
-# NOTE: These aliases are created to facilitate autocompletion.
-# Feel free to extend them as you please when adding new features.
-# See: https://github.com/narwhals-dev/narwhals/pull/2983#discussion_r2337548736
-ObjectName: TypeAlias = Literal["inputs", "Series", "DataFrames"]
-SeriesDetail: TypeAlias = Literal[
-    "dtype mismatch",
-    "exact value mismatch",
-    "implementation mismatch",
-    "length mismatch",
-    "name mismatch",
-    "nested value mismatch",
-    "null value mismatch",
-    "values not within tolerance",
-]
-DataFramesDetail: TypeAlias = (
-    Literal[
-        "columns are not in the same order",
-        "dtypes do not match",
-        "height (row count) mismatch",
+    # NOTE: These aliases are created to facilitate autocompletion.
+    # Feel free to extend them as you please when adding new features.
+    # See: https://github.com/narwhals-dev/narwhals/pull/2983#discussion_r2337548736
+    ObjectName: TypeAlias = Literal["inputs", "Series", "DataFrames"]
+    SeriesDetail: TypeAlias = Literal[
+        "dtype mismatch",
+        "exact value mismatch",
         "implementation mismatch",
+        "length mismatch",
+        "name mismatch",
+        "nested value mismatch",
+        "null value mismatch",
+        "values not within tolerance",
     ]
-    | str
-    # NOTE: `| str` makes # This makes the literals above redundant, but they still show
-    # up when typing as autocompletion.
-    # The reason to have `str` is due to the fact that other details are dynamic
-    # and depend upon which columns lead to the assertion error.
-)
+    DataFramesDetail: TypeAlias = (
+        Literal[
+            "columns are not in the same order",
+            "dtypes do not match",
+            "height (row count) mismatch",
+            "implementation mismatch",
+        ]
+        | str
+        # NOTE: `| str` makes the literals above redundant, but they still show
+        # up when typing as autocompletion.
+        # The reason to have `str` is due to the fact that other details are dynamic
+        # and depend upon which columns lead to the assertion error.
+    )
 
 
 def raise_assertion_error(
