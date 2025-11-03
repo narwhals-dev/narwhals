9fa78ab1b735e72e2c2dd77c5715f9007249bf40
msalvany <81303454+msalvany@users.noreply.github.com>
2025-11-03 14:17:17 +0100

    test_struct: `assert_equal_data(result, expected)`

--- tests/expr_and_series/struct_test.py
+++ tests/expr_and_series/struct_test.py
@@ -3,14 +3,13 @@ from __future__ import annotations
 import pytest
 
 import narwhals as nw
-from tests.utils import POLARS_VERSION, Constructor
+from tests.utils import POLARS_VERSION, Constructor, assert_equal_data
 
 pytest.importorskip("pyarrow")
 
 data = {"a": [1, 2, 3], "b": ["dogs", "cats", None], "c": ["play", "swim", "walk"]}
 
-
-def test_dryrun(constructor: Constructor, *, request: pytest.FixtureRequest) -> None:
+def test_struct(constructor: Constructor, *, request: pytest.FixtureRequest) -> None:
     if "polars" in str(constructor) and POLARS_VERSION < (1, 0, 0):
         request.applymarker(pytest.mark.xfail)
     if any(
@@ -20,3 +19,13 @@ def test_dryrun(constructor: Constructor, *, request: pytest.FixtureRequest) -> 
 
     df = nw.from_native(constructor(data))
     result = df.select(nw.struct([nw.col("a"), nw.col("b"), nw.col("c")]).alias("struct"))
+
+    expected = {
+        "struct": [
+            {"a": 1, "b": "dogs", "c": "play"},
+            {"a": 2, "b": "cats", "c": "swim"},
+            {"a": 3, "b": None,  "c": "walk"},
+        ]
+    }
+
+    assert_equal_data(result, expected)
