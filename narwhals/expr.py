d7b4f2a3d8d17d4a1cbc3e099e0fe3652b9bb035
liamholmes31 <liamholmes1996@gmail.com>
2025-11-07 16:11:11 +0000

    chore: second attempt at documenting ClosedInterval type arguments, to comply with DAR103

--- narwhals/expr.py
+++ narwhals/expr.py
@@ -910,7 +910,7 @@ class Expr:
         Arguments:
             lower_bound: Lower bound value. String literals are interpreted as column names.
             upper_bound: Upper bound value. String literals are interpreted as column names.
-            closed({'left', 'right', 'none', 'both'}): Define which sides of the interval are closed (inclusive).
+            closed: Define which sides of the interval are closed (inclusive). Options are {"left", "right", "none", "both"}.
 
         Examples:
             >>> import pandas as pd
