fix: `with_row_index` was returning incorrect results when used with `order_by` for pandas, pyarrow, and polars

narwhals/_arrow/dataframe.py
@@ -28,6 +28,7 @@ from narwhals._utils import (
 )
 from narwhals.dependencies import is_numpy_array_1d
 from narwhals.exceptions import ShapeError
+from narwhals.functions import col as nw_col
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -531,18 +532,23 @@ class ArrowDataFrame(
         return {ser.name: ser.to_list() for ser in it}
 
     def with_row_index(self, name: str, order_by: Sequence[str] | None) -> Self:
-        import numpy as np  # ignore-banned-import
-
         plx = self.__narwhals_namespace__()
-        data = pa.array(np.arange(len(self)))
-        row_index_s = plx._series.from_iterable(data, context=self, name=name)
-        row_index = plx._expr._from_series(row_index_s)
-        if order_by:
+        if order_by is None:
+            import numpy as np  # ignore-banned-import
+
+            data = pa.array(np.arange(len(self), dtype=np.int64))
             row_index = plx._expr._from_series(
-                self.select(row_index, *(plx.col(x) for x in order_by))
-                .sort(*order_by, descending=False, nulls_last=False)
-                .get_column(name)
+                plx._series.from_iterable(data, context=self, name=name)
             )
+        else:
+            rank = cast(
+                "ArrowExpr",
+                nw_col(order_by[0]).rank(method="ordinal")._to_compliant_expr(plx),
+            )
+            row_index = (
+                rank.over(partition_by=[], order_by=order_by)
+                - plx.lit(1, None).broadcast()
+            ).alias(name)
         return self.select(row_index, plx.all())
 
     def filter(self, predicate: ArrowExpr) -> Self: