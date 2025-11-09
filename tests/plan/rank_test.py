1f406d602cf1c7b76ea5cf163ef2b919c28f991f
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-09 13:01:22 +0000

    test: `rank(descending=True)` as well

--- tests/plan/rank_test.py
+++ tests/plan/rank_test.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 import pytest
@@ -9,13 +8,16 @@ import narwhals._plan as nwp
 from tests.plan.utils import assert_equal_data, dataframe
 
 if TYPE_CHECKING:
-    from collections.abc import Iterator
+    from collections.abc import Iterator, Mapping, Sequence
 
     from typing_extensions import TypeAlias
 
     from narwhals.typing import RankMethod
 
-Data: TypeAlias = dict[str, Sequence[float | None]]
+Data: TypeAlias = "dict[str, Sequence[float | None]]"
+
+ASC = False
+DESC = True
 
 
 @pytest.fixture(params=["average", "min", "max", "dense", "ordinal"])
@@ -38,19 +40,27 @@ def _generate_data() -> Iterator[Data]:
         yield {"a": column, "b": [1, 1, 2, 1, 2, 2], "i": [1, 2, 3, 4, 5, 6]}
 
 
-@pytest.fixture(params=_generate_data(), scope="module")
+@pytest.fixture(params=_generate_data(), scope="module", ids=["int", "float"])
 def data(request: pytest.FixtureRequest) -> Data:
     data_: Data = request.param
     return data_
 
 
-def test_rank_expr(rank_method: RankMethod, data: Data) -> None:
-    expected = {
-        "average": [3.0, 4.5, 1.5, 1.5, None, 4.5],
-        "min": [3, 4, 1, 1, None, 4],
-        "max": [3, 5, 2, 2, None, 5],
-        "dense": [2, 3, 1, 1, None, 3],
-        "ordinal": [3, 4, 1, 2, None, 5],
-    }
-    result = dataframe(data).select(nwp.col("a").rank(rank_method))
-    assert_equal_data(result, {"a": expected[rank_method]})
+EXPECTED: Mapping[tuple[RankMethod, bool], Sequence[float | None]] = {
+    ("average", ASC): [3.0, 4.5, 1.5, 1.5, None, 4.5],
+    ("average", DESC): [3.0, 1.5, 4.5, 4.5, None, 1.5],
+    ("min", ASC): [3, 4, 1, 1, None, 4],
+    ("min", DESC): [3, 1, 4, 4, None, 1],
+    ("max", ASC): [3, 5, 2, 2, None, 5],
+    ("max", DESC): [3, 2, 5, 5, None, 2],
+    ("dense", ASC): [2, 3, 1, 1, None, 3],
+    ("dense", DESC): [2, 1, 3, 3, None, 1],
+    ("ordinal", ASC): [3, 4, 1, 2, None, 5],
+    ("ordinal", DESC): [3, 1, 4, 5, None, 2],
+}
+
+
+@pytest.mark.parametrize("descending", [ASC, DESC], ids=["asc", "desc"])
+def test_rank_expr(rank_method: RankMethod, data: Data, *, descending: bool) -> None:
+    result = dataframe(data).select(nwp.col("a").rank(rank_method, descending=descending))
+    assert_equal_data(result, {"a": EXPECTED[rank_method, descending]})
