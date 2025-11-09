457f6e840d3147a72716d0203f67a64e19eaf6df
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-11-09 22:46:46 +0000

    simpler pyarrow impl (thanks Dan!)

--- narwhals/_arrow/dataframe.py
+++ narwhals/_arrow/dataframe.py
@@ -29,7 +29,6 @@ from narwhals._utils import (
 )
 from narwhals.dependencies import is_numpy_array_1d
 from narwhals.exceptions import ShapeError
-from narwhals.functions import col as nw_col
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -534,28 +533,17 @@ class ArrowDataFrame(
 
     def with_row_index(self, name: str, order_by: Sequence[str] | None) -> Self:
         plx = self.__narwhals_namespace__()
+        data = arange(0, len(self), 1, self._backend_version)
         if order_by is None:
-            data = arange(0, len(self), 1, self._backend_version)
             row_index = plx._expr._from_series(
                 plx._series.from_iterable(data, context=self, name=name)
             )
             return self.select(row_index, plx.all())
+        indices = pc.sort_indices(self.native, [(by, "ascending") for by in order_by])
         if self._backend_version < (20,):
-            rank = cast(
-                "ArrowExpr",
-                nw_col(order_by[0]).rank(method="ordinal")._to_compliant_expr(plx),
-            )
-            row_index = (
-                rank.over(partition_by=[], order_by=order_by)
-                - plx.lit(1, None).broadcast()
-            )
-            return self.select(row_index.alias(name), plx.all())
-        native = self.native
-        indices = pc.sort_indices(native, [(by, "ascending") for by in order_by])
-        new_col = pc.scatter(  # type: ignore[attr-defined]
-            arange(0, len(self), 1, backend_version=self._backend_version),
-            indices.cast(pa.int64()),
-        )
+            new_col = data.take(pc.sort_indices(indices))
+        else:
+            new_col = pc.scatter(data, indices.cast(pa.int64()))  # type: ignore[attr-defined]
         return self._with_native(self.native.add_column(0, name, new_col))
 
     def filter(self, predicate: ArrowExpr) -> Self:
