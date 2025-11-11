40c483fc287e2a347e327affccaf4d4dc8d8f48f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-11-11 15:32:51 +0000

    fixup imports

--- test_plugin/test_plugin/__init__.py
+++ test_plugin/test_plugin/__init__.py
@@ -3,15 +3,15 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from tests.test_plugin.test_plugin.dataframe import DictFrame
-    from tests.test_plugin.test_plugin.namespace import DictNamespace
     from typing_extensions import TypeIs
 
     from narwhals.utils import Version
+    from test_plugin.dataframe import DictFrame
+    from test_plugin.namespace import DictNamespace
 
 
 def __narwhals_namespace__(version: Version) -> DictNamespace:  # noqa: N807
-    from tests.test_plugin.test_plugin.namespace import DictNamespace
+    from test_plugin.namespace import DictNamespace
 
     return DictNamespace(version=version)
 
--- test_plugin/test_plugin/namespace.py
+++ test_plugin/test_plugin/namespace.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-from tests.test_plugin.test_plugin.dataframe import DictFrame, DictLazyFrame
-
 from narwhals._compliant import CompliantNamespace
 from narwhals._utils import not_implemented
+from test_plugin.dataframe import DictFrame, DictLazyFrame
 
 if TYPE_CHECKING:
     from narwhals.utils import Version
