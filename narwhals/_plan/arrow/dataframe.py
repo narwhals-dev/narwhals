b556221b56e8d6c514ed22fc720d1ab97c700e22
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-13 19:09:44 +0000

    revert: (aba9c0262d2e4bc322023eb27717c1ed36672903)

--- narwhals/_plan/arrow/dataframe.py
+++ narwhals/_plan/arrow/dataframe.py
@@ -119,10 +119,12 @@ class ArrowDataFrame(
         options = pa_options.sort(*order_by, nulls_last=nulls_last)
         indices = pc.sort_indices(native, options=options)
         int_range = fn.int_range(len(self))
-        if fn.HAS_SCATTER:
+        if not fn.HAS_SCATTER:
             column = pc.scatter(int_range, indices.cast(pa.int64()))  # type: ignore[attr-defined]
         else:
-            column = int_range.take(indices.sort())
+            # NOTE: Some version in the range (`15<...<=22`)
+            # started returning the correct result via `indices.sort()`
+            column = int_range.take(pc.sort_indices(indices))
         return self._with_native(native.add_column(0, name, column))
 
     def get_column(self, name: str) -> Series:
