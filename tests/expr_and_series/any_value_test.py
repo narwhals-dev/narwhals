777d7f8052b56cfa8cef44eb3fadddc9cd7b7977
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-11-18 07:23:37 +0100

    xfail dask

--- tests/expr_and_series/any_value_test.py
+++ tests/expr_and_series/any_value_test.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
 import narwhals as nw
 from tests.utils import assert_equal_data
 
@@ -11,7 +13,11 @@ if TYPE_CHECKING:
 data = {"a": [1, 1, 1, 2, 2, 3], "b": [1, 2, 3, 4, 5, 6]}
 
 
-def test_any_value_expr(constructor: Constructor) -> None:
+def test_any_value_expr(constructor: Constructor, request: pytest.FixtureRequest) -> None:
+    if "dask" in str(constructor):
+        reason = "sample does not allow n, use frac instead"
+        request.applymarker(pytest.mark.xfail(reason=reason))
+
     df = nw.from_native(constructor(data))
 
     # Aggregation
@@ -36,14 +42,24 @@ def test_any_value_series(constructor_eager: ConstructorEager) -> None:
     assert all(len(r) == 1 for r in result.values())
 
 
-def test_any_value_group_by(constructor: Constructor) -> None:
+def test_any_value_group_by(
+    constructor: Constructor, request: pytest.FixtureRequest
+) -> None:
+    if "dask" in str(constructor):
+        reason = "sample does not allow n, use frac instead"
+        request.applymarker(pytest.mark.xfail(reason=reason))
+
     df = nw.from_native(constructor(data))
 
     result = df.group_by("a").agg(nw.col("b").any_value().alias("any"), nw.col("b").max())
     assert result.lazy().collect().shape == (3, 3)
 
 
-def test_any_value_over(constructor: Constructor) -> None:
+def test_any_value_over(constructor: Constructor, request: pytest.FixtureRequest) -> None:
+    if "dask" in str(constructor):
+        reason = "sample does not allow n, use frac instead"
+        request.applymarker(pytest.mark.xfail(reason=reason))
+
     df = nw.from_native(constructor(data))
 
     result = df.select(nw.col("a"), nw.col("b").any_value().over("a"))
