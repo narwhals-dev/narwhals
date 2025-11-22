2f632b49e4e032c9fa9c9b721b5fc1563873b98a
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-22 16:32:54 +0000

    test: Reveal index out-of-bounds errors

--- tests/plan/fill_null_test.py
+++ tests/plan/fill_null_test.py
@@ -8,6 +8,10 @@ from narwhals import _plan as nwp
 from narwhals._plan import selectors as ncs
 from tests.plan.utils import assert_equal_data, dataframe
 
+pytest.importorskip("pyarrow")
+
+import pyarrow as pa
+
 if TYPE_CHECKING:
     from narwhals._plan.typing import OneOrIterable
     from tests.conftest import Data
@@ -25,8 +29,18 @@ DATA_2 = {
 DATA_LIMITS = {
     "a": [1, None, None, None, 5, 6, None, None, None, 10],
     "b": ["a", None, None, None, "b", "c", None, None, None, "d"],
+    "c": [None, 2.5, None, None, None, None, 3.6, None, 2.2, 3.0],
+    "d": [1, None, None, None, None, None, None, None, 2, None],
     "idx": list(range(10)),
 }
+
+
+# TODO @dangotbanned: Fix this in the new version
+# Then open an issue demonstrating the bug
+XFAIL_INHERITED_INDEX_ERROR = pytest.mark.xfail(
+    reason="Bug in the implementation on `main` for `fill_null(limit=...)`.",
+    raises=pa.ArrowIndexError,
+)
 
 
 @pytest.mark.parametrize(
@@ -61,6 +75,8 @@ DATA_LIMITS = {
             {
                 "a": [1, 5, 5, 5, 5, 6, 10, 10, 10, 10],
                 "b": ["a", "b", "b", "b", "b", "c", "d", "d", "d", "d"],
+                "c": [2.5, 2.5, 3.6, 3.6, 3.6, 3.6, 3.6, 2.2, 2.2, 3.0],
+                "d": [1, 2, 2, 2, 2, 2, 2, 2, 2, None],
             },
         ),
         (  # test_fill_null_limits (1)
@@ -73,13 +89,29 @@ DATA_LIMITS = {
         ),
         (  # test_fill_null_limits (2)
             DATA_LIMITS,
-            nwp.col("a", "b")
-            .fill_null(strategy="backward", limit=2)
-            .over(order_by="idx"),
+            [
+                nwp.col("a", "b")
+                .fill_null(strategy="backward", limit=2)
+                .over(order_by="idx"),
+                nwp.col("c").fill_null(strategy="backward", limit=3).over(order_by="idx"),
+            ],
             {
                 "a": [1, None, 5, 5, 5, 6, None, 10, 10, 10],
                 "b": ["a", None, "b", "b", "b", "c", None, "d", "d", "d"],
+                "c": [2.5, 2.5, None, 3.6, 3.6, 3.6, 3.6, 2.2, 2.2, 3.0],
             },
+        ),
+        pytest.param(
+            DATA_LIMITS,
+            nwp.col("c").fill_null(strategy="forward", limit=3).over(order_by="idx"),
+            {"c": [None, 2.5, 2.5, 2.5, 2.5, None, 3.6, 3.6, 2.2, 3.0]},
+            marks=XFAIL_INHERITED_INDEX_ERROR,
+        ),
+        pytest.param(
+            DATA_LIMITS,
+            nwp.col("d").fill_null(strategy="backward", limit=3).over(order_by="idx"),
+            {"d": [1, None, None, None, None, 2, 2, 2, 2, None]},
+            marks=XFAIL_INHERITED_INDEX_ERROR,
         ),
     ],
 )
