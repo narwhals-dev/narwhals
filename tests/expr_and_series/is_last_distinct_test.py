ba273a03342fce5fec3bd44c7f8fbadd1d232b92
Ehsan Totoni <ehsan.tn@gmail.com>
2025-11-25 12:54:08 -0500

    remove skips

--- tests/expr_and_series/is_last_distinct_test.py
+++ tests/expr_and_series/is_last_distinct_test.py
@@ -15,9 +15,6 @@ data = {"a": [1, 1, 2, 3, 2], "b": [1, 2, 3, 2, 1]}
 
 
 def test_is_last_distinct_expr(constructor_eager: ConstructorEager) -> None:
-    if "bodo" in str(constructor_eager):
-        # BODO fail
-        pytest.skip()
 
     df = nw.from_native(constructor_eager(data))
     result = df.select(nw.all().is_last_distinct())
@@ -29,9 +26,6 @@ def test_is_last_distinct_expr(constructor_eager: ConstructorEager) -> None:
 
 
 def test_is_last_distinct_expr_all(constructor_eager: ConstructorEager) -> None:
-    if "bodo" in str(constructor_eager):
-        # BODO fail
-        pytest.skip()
 
     # https://github.com/narwhals-dev/narwhals/issues/2268
     if "polars" in str(constructor_eager) and POLARS_VERSION < (1, 9):
@@ -51,9 +45,6 @@ def test_is_last_distinct_expr_lazy(constructor: Constructor) -> None:
     if "polars" in str(constructor) and POLARS_VERSION < (1, 10):
         pytest.skip()
     if "duckdb" in str(constructor) and DUCKDB_VERSION < (1, 3):
-        pytest.skip()
-    if "bodo" in str(constructor):
-        # BODO fail
         pytest.skip()
 
     data = {"a": [1, 1, 2, 2, 2], "b": [1, 2, 2, 2, 1], "i": [None, 1, 2, 3, 4]}
@@ -115,9 +106,6 @@ def test_is_last_distinct_expr_lazy_grouped_nulls(
 
 
 def test_is_last_distinct_series(constructor_eager: ConstructorEager) -> None:
-    if "bodo" in str(constructor_eager):
-        # BODO fail
-        pytest.skip()
 
     series = nw.from_native(constructor_eager(data), eager_only=True)["a"]
     result = series.is_last_distinct()
