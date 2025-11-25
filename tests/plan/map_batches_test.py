100d82e7b6375a25304c192712947a24fb6767fc
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-25 14:11:58 +0000

    test: Clean up and doc `map_batches`

--- tests/plan/map_batches_test.py
+++ tests/plan/map_batches_test.py
@@ -7,12 +7,16 @@ import pytest
 import narwhals as nw
 import narwhals._plan as nwp
 from narwhals._plan import selectors as ncs
-from narwhals._utils import Version
 from tests.plan.utils import assert_equal_data, dataframe
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+    from narwhals._plan.compliant.typing import (
+        SeriesAny as CompliantSeriesAny,
+        SeriesT as CompliantSeriesT,
+    )
+    from narwhals.typing import _1DArray, _NumpyScalar
     from tests.conftest import Data
 
 pytest.importorskip("pyarrow")
@@ -23,22 +27,38 @@ import numpy as np
 @pytest.fixture
 def data() -> Data:
     return {
-        "a": ["A", "B", "A"],
-        "b": [1, 2, 3],
+        "a": [1, 2, 3],
+        "b": [4, 5, 6],
         "c": [9, 2, 4],
         "d": [8, 7, 8],
-        "e": [None, 9, 7],
-        "f": [True, False, None],
-        "g": [False, None, False],
-        "h": [None, None, True],
-        "i": [None, None, None],
+        "e": ["A", "B", "A"],
         "j": [12.1, None, 4.0],
         "k": [42, 10, None],
-        "l": [4, 5, 6],
-        "m": [0, 1, 2],
-        "n": ["dogs", "cats", None],
-        "o": ["play", "swim", "walk"],
+        "z": [7.0, 8.0, 9.0],
     }
+
+
+def elementwise_series(s: CompliantSeriesT, /) -> CompliantSeriesT:
+    dtype_name = type(s.dtype).__name__.lower()
+    repeat_name = (dtype_name,) * (len(s) - 1)
+    values = [*repeat_name, "last"]
+    return s.from_iterable(values, version=s.version, name="funky")
+
+
+def elementwise_1d_array(s: CompliantSeriesAny, /) -> _1DArray:
+    return s.to_numpy() + 1
+
+
+def to_numpy(s: CompliantSeriesAny, /) -> _1DArray:
+    return s.to_numpy()
+
+
+def groupwise_1d_array(s: CompliantSeriesAny, /) -> _1DArray:
+    return np.append(s.to_numpy(), [10, 2])
+
+
+def aggregation_np_scalar(s: CompliantSeriesAny, /) -> _NumpyScalar:
+    return s.to_numpy().max()
 
 
 @pytest.mark.parametrize(
@@ -46,41 +66,47 @@ def data() -> Data:
     [
         pytest.param(
             [
-                nwp.col("a")
+                nwp.col("e")
                 .alias("...")
-                .map_batches(
-                    lambda s: s.from_iterable(
-                        [*((len(s) - 1) * [type(s.dtype).__name__.lower()]), "last"],
-                        version=Version.MAIN,
-                        name="funky",
-                    ),
-                    is_elementwise=True,
-                ),
-                nwp.col("a"),
+                .map_batches(elementwise_series, is_elementwise=True),
+                nwp.col("e"),
             ],
-            {"funky": ["string", "string", "last"], "a": ["A", "B", "A"]},
-            id="series",
+            {"funky": ["string", "string", "last"], "e": ["A", "B", "A"]},
+            id="is_elementwise-series",
         ),
         pytest.param(
-            nwp.col("b")
-            .map_batches(lambda s: s.to_numpy() + 1, nw.Float64(), is_elementwise=True)
+            nwp.col("a", "b", "z").map_batches(to_numpy),
+            {"a": [1, 2, 3], "b": [4, 5, 6], "z": [7.0, 8.0, 9.0]},
+            id="to-numpy",
+        ),
+        pytest.param(
+            nwp.col("a")
+            .map_batches(elementwise_1d_array, nw.Float64, is_elementwise=True)
             .sum(),
-            {"b": [9.0]},
-            id="numpy",
+            {"a": [9.0]},
+            id="is_elementwise-1d-array",
         ),
         pytest.param(
-            ncs.by_name("b", "c", "d")
-            .map_batches(lambda s: np.append(s.to_numpy(), [10, 2]), is_elementwise=True)
+            nwp.col("a").map_batches(elementwise_1d_array, nw.Float64).sum(),
+            {"a": [9.0]},
+            id="unknown-1d-array",
+        ),
+        pytest.param(
+            ncs.by_index(0, 2, 3)
+            .map_batches(groupwise_1d_array, is_elementwise=True)
             .sort(),
-            {"b": [1, 2, 2, 3, 10], "c": [2, 2, 4, 9, 10], "d": [2, 7, 8, 8, 10]},
-            id="selector",
+            {"a": [1, 2, 2, 3, 10], "c": [2, 2, 4, 9, 10], "d": [2, 7, 8, 8, 10]},
+            # NOTE: Maybe this should be rejected because of the length change?
+            # It doesn't break broadcasting rules, but uses an optional argument incorrectly
+            # and we only know *after* execution
+            id="is_elementwise-1d-array-groupwise",
         ),
         pytest.param(
             nwp.col("j", "k")
             .fill_null(15)
-            .map_batches(lambda s: (s.to_numpy().max()), returns_scalar=True),
+            .map_batches(aggregation_np_scalar, returns_scalar=True),
             {"j": [15], "k": [42]},
-            id="returns_scalar",
+            id="returns_scalar-np-scalar",
         ),
     ],
 )
