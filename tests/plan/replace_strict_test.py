cd04606fad0df82d5de000b0a062f9f5b488d6c8
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-26 15:20:54 +0000

    test: Tidy up `replace_strict`
    Much clearer what each case is testing, covers a slightly more, + generates nice test ids

--- tests/plan/replace_strict_test.py
+++ tests/plan/replace_strict_test.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from itertools import chain
+from typing import TYPE_CHECKING, Any, Literal
 
 import pytest
 
 import narwhals as nw
 import narwhals._plan as nwp
+from narwhals._utils import no_default
 from narwhals.exceptions import InvalidOperationError
 from tests.plan.utils import assert_equal_data, dataframe
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable, Iterator, Mapping, Sequence
+    from collections.abc import Collection, Iterable, Iterator, Mapping, Sequence
 
     from _pytest.mark import ParameterSet
     from typing_extensions import TypeAlias
 
+    from narwhals._plan.typing import IntoExpr
     from narwhals._typing import NoDefault
-    from narwhals.typing import IntoDType
+    from narwhals.typing import IntoDType, NonNestedLiteral
     from tests.conftest import Data
 
 pytest.importorskip("pyarrow")
@@ -37,40 +40,86 @@ def data() -> Data:
     }
 
 
-def basic_cases(
-    column: str,
+def cases(
+    column: Literal["str", "int", "str-null", "int-null", "str-alt"],
     replacements: Mapping[Any, Any],
     return_dtypes: Iterable[IntoDType | None],
+    *,
+    default: IntoExpr | NoDefault = no_default,
+    expected: list[NonNestedLiteral] | None = None,
+    marks: pytest.MarkDecorator | Collection[pytest.MarkDecorator | pytest.Mark] = (),
 ) -> Iterator[ParameterSet]:
     old, new = list(replacements), tuple(replacements.values())
-    values = list(new)
     base = nwp.col(column)
     alt_name = f"{column}_seqs"
     alt = nwp.col(column).alias(alt_name)
-    expected = {column: values, alt_name: values}
+    if expected:
+        expected_m = {column: expected, alt_name: expected}
+    else:
+        expected_m = {column: list(new), alt_name: list(new)}
+    if default is no_default:
+        suffix = ""
+    else:
+        tp = type(default._ir) if isinstance(default, nwp.Expr) else type(default)
+        suffix = f"-default-{tp.__name__}"
+
     for dtype in return_dtypes:
         exprs = (
-            base.replace_strict(replacements, return_dtype=dtype),
-            alt.replace_strict(old, new, return_dtype=dtype),
+            base.replace_strict(replacements, default=default, return_dtype=dtype),
+            alt.replace_strict(old, new, default=default, return_dtype=dtype),
         )
-        schema = {column: dtype, alt_name: dtype} if dtype is not None else None
-        yield pytest.param(exprs, expected, schema, id=f"{column}-{dtype}")
+        schema = {column: dtype, alt_name: dtype} if dtype else None
+        id = f"{column}-{dtype}{suffix}"
+        yield pytest.param(exprs, expected_m, schema, id=id, marks=marks)
 
 
 @pytest.mark.parametrize(
     ("exprs", "expected", "schema"),
-    [
-        *basic_cases(
+    chain(
+        cases(
             "str",
             {"one": 1, "two": 2, "three": 3, "four": 4},
             [nw.Int8, nw.Float32, None],
         ),
-        *basic_cases(
-            "int", {1: "one", 2: "two", 3: "three", 4: "four"}, [nw.String(), None]
+        cases("int", {1: "one", 2: "two", 3: "three", 4: "four"}, [nw.String(), None]),
+        cases(
+            "int",
+            {1: "one", 2: "two"},
+            [nw.String, None],
+            default=nwp.lit("other"),
+            expected=["one", "two", "other", "other"],
         ),
-    ],
+        cases(
+            "int-null",
+            {1: 10, 2: 20},
+            [nw.Int64, None],
+            default=99,
+            expected=[10, 20, 99, 99],
+        ),
+        cases(
+            "int",
+            {1: "one", 2: "two", 3: None},
+            [nw.String, None],
+            default="other",
+            expected=["one", "two", None, "other"],
+        ),
+        cases(
+            "int",
+            {1: "one", 2: "two"},
+            [nw.String, None],
+            default=nwp.col("str-alt"),
+            expected=["one", "two", "orca", "vaquita"],
+        ),
+        cases(
+            "int",
+            {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"},
+            [None],
+            default="hundred",
+            expected=["one", "two", "three", "four"],
+        ),
+    ),
 )
-def test_replace_strict_expr_basic(
+def test_replace_strict_expr(
     data: Data,
     exprs: Iterable[nwp.Expr],
     expected: Data,
@@ -94,68 +143,6 @@ def test_replace_strict_expr_non_full(data: Data, expr: nwp.Expr) -> None:
         (ValueError, InvalidOperationError), match=r"did not replace all non-null"
     ):
         dataframe(data).select(expr)
-
-
-# TODO @dangotbanned: Share more of the case generation logic from `basic_cases`
-@pytest.mark.parametrize(
-    ("expr", "expected"),
-    [
-        # test_replace_strict_expr_with_default
-        pytest.param(
-            nwp.col("int").replace_strict(
-                [1, 2], ["one", "two"], default=nwp.lit("other"), return_dtype=nw.String
-            ),
-            {"int": ["one", "two", "other", "other"]},
-            id="non-null-1",
-        ),
-        pytest.param(
-            nwp.col("int").replace_strict([1, 2], ["one", "two"], default="other"),
-            {"int": ["one", "two", "other", "other"]},
-            id="non-null-2",
-        ),
-        # test_replace_strict_with_default_and_nulls
-        pytest.param(
-            nwp.col("int-null").replace_strict(
-                [1, 2], [10, 20], default=99, return_dtype=nw.Int64
-            ),
-            {"int-null": [10, 20, 99, 99]},
-            id="null-1",
-        ),
-        pytest.param(
-            nwp.col("int-null").replace_strict([1, 2], [10, 20], default=99),
-            {"int-null": [10, 20, 99, 99]},
-            id="null-2",
-        ),
-        # test_replace_strict_with_default_mapping
-        pytest.param(
-            nwp.col("int").replace_strict(
-                {1: "one", 2: "two", 3: None}, default="other", return_dtype=nw.String()
-            ),
-            {"int": ["one", "two", None, "other"]},
-            # shouldn't be an independent case, the mapping isn't the default
-            id="replace_strict_with_default_mapping",
-        ),
-        # test_replace_strict_with_expressified_default
-        pytest.param(
-            nwp.col("int").replace_strict(
-                {1: "one", 2: "two"}, default=nwp.col("str-alt"), return_dtype=nw.String
-            ),
-            {"int": ["one", "two", "orca", "vaquita"]},
-            id="column",
-        ),
-        # test_mapping_key_not_in_expr
-        pytest.param(
-            nwp.col("int").replace_strict(
-                {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}, default="hundred"
-            ),
-            {"int": ["one", "two", "three", "four"]},
-            id="mapping_key_not_in_expr",
-        ),
-    ],
-)
-def test_replace_strict_expr_default(data: Data, expr: nwp.Expr, expected: Data) -> None:
-    result = dataframe(data).select(expr)
-    assert_equal_data(result, expected)
 
 
 def test_replace_strict_scalar(data: Data) -> None:
