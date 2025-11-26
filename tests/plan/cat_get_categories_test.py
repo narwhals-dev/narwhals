895cbe2c6beae344ff29d3a41c9c0d381c0b7746
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-26 22:36:24 +0000

    test: xfail `pyarrow<15.0`

--- tests/plan/cat_get_categories_test.py
+++ tests/plan/cat_get_categories_test.py
@@ -5,7 +5,9 @@ import pytest
 import narwhals as nw
 import narwhals._plan as nwp  # noqa: F401
 import narwhals._plan.selectors as ncs
+from narwhals._utils import Implementation
 from tests.plan.utils import assert_equal_data, dataframe
+from tests.utils import PYARROW_VERSION
 
 pytest.importorskip("pyarrow")
 
@@ -15,8 +17,18 @@ pytest.importorskip("pyarrow")
     [(["one", "two", "two"], ["one", "two"]), (["A", "B", None, "D"], ["A", "B", "D"])],
     ids=["full", "nulls"],
 )
-def test_get_categories(values: list[str], expected: list[str]) -> None:
+def test_get_categories(
+    values: list[str], expected: list[str], request: pytest.FixtureRequest
+) -> None:
     data = {"a": values}
+    df = dataframe(data)
+    request.applymarker(
+        pytest.mark.xfail(
+            (df.implementation is Implementation.PYARROW and PYARROW_VERSION < (15,)),
+            reason="Unsupported cast from string to dictionary using function cast_dictionary",
+        )
+    )
+    df = dataframe(data)
     df = dataframe(data).select(ncs.first().cast(nw.Categorical))
     result = df.select(ncs.first().cat.get_categories())
     assert_equal_data(result, {"a": expected})
