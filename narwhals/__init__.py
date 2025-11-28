a0b8ae3ae00871dec6e5180a64b7b231205df0d4
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-11-28 13:05:09 +0000

    cov
    fixes https://github.com/narwhals-dev/narwhals/pull/3333#discussion_r2571462120

--- narwhals/__init__.py
+++ narwhals/__init__.py
@@ -189,3 +189,5 @@ if not _t.TYPE_CHECKING:
             return __version__
         msg = f"module {__name__!r} has no attribute {name!r}"
         raise AttributeError(msg)
+else:  # pragma: no cover
+    ...
