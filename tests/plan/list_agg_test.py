a7c9ee13916937276d5710f5dd949647afab8f95
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-12-15 16:19:55 +0000

    test: Make scalar cases less of a disaster
    The line diff might only be small right now, but polars has 8 more list aggs ...

--- tests/plan/list_agg_test.py
+++ tests/plan/list_agg_test.py
@@ -1,28 +1,46 @@
 from __future__ import annotations
 
+from itertools import chain
 from typing import TYPE_CHECKING, Final
 
 import pytest
 
 import narwhals as nw
 import narwhals._plan as nwp
+from narwhals._plan._dispatch import get_dispatch_name
+from narwhals._utils import zip_strict
 from tests.plan.utils import assert_equal_data, dataframe
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator, Sequence
+
+    from _pytest.mark import ParameterSet
+    from typing_extensions import TypeAlias
+
     from narwhals._plan.typing import OneOrIterable
     from tests.conftest import Data
     from tests.plan.utils import SubList
 
+    SubListNumeric: TypeAlias = SubList[int] | SubList[float]
 
-R1: Final[SubList[int]] = [3, None, 2, 2, 4, None]
-R2: Final[SubList[int]] = [-1]
-R3: Final[SubList[float]] = None
-R4: Final[SubList[float]] = [None, None, None]
-R5: Final[SubList[float]] = []
+
+R1: Final[SubListNumeric] = [3, None, 2, 2, 4, None]
+R2: Final[SubListNumeric] = [-1]
+R3: Final[SubListNumeric] = None
+R4: Final[SubListNumeric] = [None, None, None]
+R5: Final[SubListNumeric] = []
 # NOTE: `pyarrow` needs at least 3 (non-null) values to calculate `median` correctly
 # Otherwise it picks the lowest non-null
 # https://github.com/narwhals-dev/narwhals/pull/3332#discussion_r2617508167
-R6: Final[SubList[int]] = [3, 4, None, 4, None, 3]
+R6: Final[SubListNumeric] = [3, 4, None, 4, None, 3]
+
+ROWS: Final[tuple[SubListNumeric, ...]] = R1, R2, R3, R4, R5, R6
+
+EXPECTED_MAX = [4, -1, None, None, None, 4]
+EXPECTED_MEAN = [2.75, -1, None, None, None, 3.5]
+EXPECTED_MIN = [2, -1, None, None, None, 3]
+EXPECTED_SUM = [11, -1, None, 0, 0, 14]
+EXPECTED_MEDIAN = [2.5, -1, None, None, None, 3.5]
 
 
 @pytest.fixture(scope="module")
@@ -37,11 +55,11 @@ cast_a = a.cast(nw.List(nw.Int32))
 @pytest.mark.parametrize(
     ("exprs", "expected"),
     [
-        (a.list.max(), [4, -1, None, None, None, 4]),
-        (a.list.mean(), [2.75, -1, None, None, None, 3.5]),
-        (a.list.min(), [2, -1, None, None, None, 3]),
-        (a.list.sum(), [11, -1, None, 0, 0, 14]),
-        (a.list.median(), [2.5, -1, None, None, None, 3.5]),
+        (a.list.max(), EXPECTED_MAX),
+        (a.list.mean(), EXPECTED_MEAN),
+        (a.list.min(), EXPECTED_MIN),
+        (a.list.sum(), EXPECTED_SUM),
+        (a.list.median(), EXPECTED_MEDIAN),
     ],
     ids=["max", "mean", "min", "sum", "median"],
 )
@@ -53,52 +71,27 @@ def test_list_agg(
     assert_equal_data(result, {"a": expected})
 
 
-first = a.first()
-first_list_max = first.list.max()
-first_list_mean = first.list.mean()
-first_list_min = first.list.min()
-first_list_sum = first.list.sum()
-first_list_median = first.list.median()
+def cases_scalar(
+    expr: nwp.Expr, expected: Sequence[float | None]
+) -> Iterator[ParameterSet]:
+    for idx, row_expected in enumerate(zip_strict(ROWS, expected), start=1):
+        row, out = row_expected
+        name = get_dispatch_name(expr._ir).removeprefix("list.")
+        yield pytest.param(row, expr, out, id=f"{name}-R{idx}")
 
 
-# TODO @dangotbanned: Shrink this
 @pytest.mark.parametrize(
     ("row", "expr", "expected"),
-    [
-        (R1, first_list_max, 4),
-        (R2, first_list_max, -1),
-        (R3, first_list_max, None),
-        (R4, first_list_max, None),
-        (R5, first_list_max, None),
-        (R6, first_list_max, 4),
-        (R1, first_list_mean, 2.75),
-        (R2, first_list_mean, -1),
-        (R3, first_list_mean, None),
-        (R4, first_list_mean, None),
-        (R5, first_list_mean, None),
-        (R6, first_list_mean, 3.5),
-        (R1, first_list_min, 2),
-        (R2, first_list_min, -1),
-        (R3, first_list_min, None),
-        (R4, first_list_min, None),
-        (R5, first_list_min, None),
-        (R6, first_list_min, 3),
-        (R1, first_list_sum, 11),
-        (R2, first_list_sum, -1),
-        (R3, first_list_sum, None),
-        (R4, first_list_sum, 0),
-        (R5, first_list_sum, 0),
-        (R6, first_list_sum, 14),
-        (R1, first_list_median, 2.5),
-        (R2, first_list_median, -1),
-        (R3, first_list_median, None),
-        (R4, first_list_median, None),
-        (R5, first_list_median, None),
-        (R6, first_list_median, 3.5),
-    ],
+    chain(
+        cases_scalar(a.first().list.max(), EXPECTED_MAX),
+        cases_scalar(a.first().list.mean(), EXPECTED_MEAN),
+        cases_scalar(a.first().list.min(), EXPECTED_MIN),
+        cases_scalar(a.first().list.sum(), EXPECTED_SUM),
+        cases_scalar(a.first().list.median(), EXPECTED_MEDIAN),
+    ),
 )
 def test_list_agg_scalar(
-    row: SubList[float], expr: nwp.Expr, expected: float | None
+    row: SubListNumeric, expr: nwp.Expr, expected: float | None
 ) -> None:
     data = {"a": [row]}
     df = dataframe(data).select(cast_a)
