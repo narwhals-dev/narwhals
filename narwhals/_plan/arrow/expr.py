95a85e55f48669deb68114d07ceca490d54e27ec
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-12-17 17:30:24 +0000

    Impl `ArrowExpr.list.sort`

--- narwhals/_plan/arrow/expr.py
+++ narwhals/_plan/arrow/expr.py
@@ -1003,8 +1003,17 @@ class ArrowListNamespace(
         return self.with_native(agg.agg_list(previous.native), name)
 
     def sort(self, node: FExpr[lists.Sort], frame: Frame, name: str) -> Expr | Scalar:
-        msg = "TODO: `ArrowExpr.list.sort`"
-        raise NotImplementedError(msg)
+        previous = node.input[0].dispatch(self.compliant, frame, name)
+        # TODO @dangotbanned: Maybe keep options together?
+        descending = node.function.options.descending
+        nulls_last = node.function.options.nulls_last
+        if isinstance(previous, ArrowScalar):
+            msg = "TODO: `ArrowScalar.list.sort`"
+            raise NotImplementedError(msg)
+        result = fn.list_sort_messy(
+            previous.native, descending=descending, nulls_last=nulls_last
+        )
+        return self.with_native(result, name)
 
     min = aggregate
     max = aggregate
