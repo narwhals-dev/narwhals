0c86346a4ed7bcf40cb783a791b016509dd21b8d
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-12-22 22:49:17 +0000

    test: cleanup a bit

--- tests/plan/unpivot_test.py
+++ tests/plan/unpivot_test.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import pytest
 
@@ -9,10 +9,13 @@ from tests.plan.utils import assert_equal_data, dataframe
 from tests.utils import PYARROW_VERSION
 
 if TYPE_CHECKING:
-    from narwhals.dtypes import DType
+    from tests.conftest import Data
 
 
-data = {"a": [7, 8, 9], "b": [1, 3, 5], "c": [2, 4, 6]}
+@pytest.fixture(scope="module")
+def data() -> Data:
+    return {"a": [7, 8, 9], "b": [1, 3, 5], "c": [2, 4, 6]}
+
 
 expected_on_b_idx_a = {"a": [7, 8, 9], "variable": ["b", "b", "b"], "value": [1, 3, 5]}
 
@@ -50,11 +53,10 @@ expected_on_none_idx_none = {
     ],
 )
 def test_unpivot(
-    on: str | list[str] | None, index: list[str] | None, expected: dict[str, list[float]]
+    data: Data, on: str | list[str] | None, index: list[str] | None, expected: Data
 ) -> None:
-    df = dataframe(data)
     sort_columns = ["variable"] if index is None else ["variable", "a"]
-    result = df.unpivot(on=on, index=index).sort(by=sort_columns)
+    result = dataframe(data).unpivot(on, index=index).sort(sort_columns)
     assert_equal_data(result, expected)
 
 
@@ -66,31 +68,20 @@ def test_unpivot(
         ("custom_variable_name", "custom_value_name"),
     ],
 )
-def test_unpivot_var_value_names(variable_name: str, value_name: str) -> None:
-    df = dataframe(data)
-    result = df.unpivot(
-        on=["b", "c"], index=["a"], variable_name=variable_name, value_name=value_name
+def test_unpivot_var_value_names(data: Data, variable_name: str, value_name: str) -> None:
+    result = dataframe(data).unpivot(
+        ["b", "c"], index=["a"], variable_name=variable_name, value_name=value_name
     )
     assert result.collect_schema().names()[-2:] == [variable_name, value_name]
 
 
-def test_unpivot_default_var_value_names() -> None:
-    df = dataframe(data)
-    result = df.unpivot(on=["b", "c"], index=["a"])
+def test_unpivot_default_var_value_names(data: Data) -> None:
+    result = dataframe(data).unpivot(["b", "c"], index=["a"])
     assert result.collect_schema().names()[-2:] == ["variable", "value"]
 
 
 @pytest.mark.xfail(PYARROW_VERSION < (14, 0, 0), reason="pyarrow<14")
-@pytest.mark.parametrize(
-    ("data", "expected_dtypes"),
-    [
-        (
-            {"idx": [0, 1], "a": [1, 2], "b": [1.5, 2.5]},
-            [nw.Int64(), nw.String(), nw.Float64()],
-        )
-    ],
-)
-def test_unpivot_mixed_types(data: dict[str, Any], expected_dtypes: list[DType]) -> None:
-    df = dataframe(data)
-    result = df.unpivot(on=["a", "b"], index="idx")
-    assert result.collect_schema().dtypes() == expected_dtypes
+def test_unpivot_mixed_types() -> None:
+    df = dataframe({"idx": [0, 1], "a": [1, 2], "b": [1.5, 2.5]})
+    result = df.unpivot(["a", "b"], index="idx")
+    assert result.collect_schema().dtypes() == [nw.Int64(), nw.String(), nw.Float64()]
