2091c911ff0bcd0752baac6c91eda67b31f78f05
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-12-25 15:16:04 +0000

    test: Add most of the remaining tests

--- tests/plan/pivot_test.py
+++ tests/plan/pivot_test.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+# ruff: noqa: FBT001
 from contextlib import nullcontext as does_not_raise
 from typing import TYPE_CHECKING, Any
 
@@ -10,6 +11,14 @@ from tests.plan.utils import assert_equal_data, dataframe, re_compile
 
 if TYPE_CHECKING:
     from tests.conftest import Data
+
+
+XFAIL_NOT_IMPL_PIVOT_AGG = pytest.mark.xfail(
+    reason="TODO: `ArrowDataFrame.pivot_agg`", raises=NotImplementedError
+)
+XFAIL_NOT_IMPL_PIVOT_ON_MULTIPLE = pytest.mark.xfail(
+    reason="TODO: `ArrowDataFrame.pivot(on: list[str])`", raises=NotImplementedError
+)
 
 
 @pytest.fixture(scope="module")
@@ -40,6 +49,74 @@ data_no_dups = {
 }
 
 
+# NOTE: `tests::frame::pivot_test.py::test_pivot`
+@XFAIL_NOT_IMPL_PIVOT_AGG
+@XFAIL_NOT_IMPL_PIVOT_ON_MULTIPLE
+def test_pivot_agg() -> None:
+    raise NotImplementedError
+
+
+# TODO @dangotbanned: Make a version of this that doesn't use `aggregate_function`
+@XFAIL_NOT_IMPL_PIVOT_AGG
+@pytest.mark.parametrize(
+    ("sort_columns", "expected"),
+    [
+        (True, ["ix", "foo_a", "foo_b", "bar_a", "bar_b"]),
+        (False, ["ix", "foo_b", "foo_a", "bar_b", "bar_a"]),
+    ],
+)
+def test_pivot_sort_columns(
+    sort_columns: bool, expected: list[str]
+) -> None:  # pragma: no cover
+    df = dataframe(data)
+    result = df.pivot(
+        on="col",
+        index="ix",
+        values=["foo", "bar"],
+        aggregate_function="sum",
+        sort_columns=sort_columns,
+    )
+    assert result.columns == expected
+
+
+@XFAIL_NOT_IMPL_PIVOT_AGG
+@pytest.mark.parametrize(
+    ("on", "values", "expected"),
+    [
+        (["col"], ["foo"], ["ix", "b", "a"]),
+        (["col"], ["foo", "bar"], ["ix", "foo_b", "foo_a", "bar_b", "bar_a"]),
+        pytest.param(
+            ["col", "col_b"],
+            ["foo"],
+            ["ix", '{"b","x"}', '{"b","y"}', '{"a","x"}', '{"a","y"}'],
+            marks=XFAIL_NOT_IMPL_PIVOT_ON_MULTIPLE,
+        ),
+        pytest.param(
+            ["col", "col_b"],
+            ["foo", "bar"],
+            [
+                "ix",
+                'foo_{"b","x"}',
+                'foo_{"b","y"}',
+                'foo_{"a","x"}',
+                'foo_{"a","y"}',
+                'bar_{"b","x"}',
+                'bar_{"b","y"}',
+                'bar_{"a","x"}',
+                'bar_{"a","y"}',
+            ],
+            marks=XFAIL_NOT_IMPL_PIVOT_ON_MULTIPLE,
+        ),
+    ],
+)
+def test_pivot_names_out(
+    on: list[str], values: list[str], expected: list[str]
+) -> None:  # pragma: no cover
+    df = dataframe(data)
+    result = df.pivot(on=on, values=values, aggregate_function="min", index="ix").columns
+    assert result == expected
+
+
 @pytest.mark.parametrize(
     ("data_", "context"),
     [
@@ -48,7 +125,7 @@ data_no_dups = {
     ],
     ids=["no-duplicates", "duplicated"],
 )
-def test_pivot_no_agg(data_: Any, context: Any) -> None:
+def test_pivot_no_agg(data_: Data, context: Any) -> None:
     expected_no_dups = {
         "ix": [1, 2],
         "foo_a": [1, 3],
@@ -70,6 +147,7 @@ def test_pivot_no_index_no_values() -> None:
         df.pivot(on="col")
 
 
+# NOTE: `tests::frame::pivot_test.py::test_pivot_no_index`
 def test_pivot_implicit_index() -> None:
     inferred_index_names = "ix", "bar"
     df = dataframe(data_no_dups)
