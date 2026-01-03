e3d137a06d320bd30367056bae171afb1f527025
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-01-03 00:03:52 +0000

    docs: Add short `_arithmetic` docstrings

--- narwhals/_plan/arrow/functions/_arithmetic.py
+++ narwhals/_plan/arrow/functions/_arithmetic.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 
 import pyarrow.compute as pc  # ignore-banned-import
 
-from narwhals._arrow.utils import cast_for_truediv, floordiv_compat as floordiv
+from narwhals._arrow.utils import cast_for_truediv, floordiv_compat as _floordiv
 from narwhals._plan.arrow.functions.meta import call
 
 if TYPE_CHECKING:
@@ -34,23 +34,35 @@ __all__ = [
 ]
 
 add = t.cast("BinaryNumericTemporal", pc.add)
+"""Equivalent to `lhs + rhs`."""
 sub = t.cast("BinaryNumericTemporal", pc.subtract)
+"""Equivalent to `lhs - rhs`."""
 multiply = pc.multiply
+"""Equivalent to `lhs * rhs`."""
+floordiv = t.cast("BinaryNumericTemporal", _floordiv)
+"""Equivalent to `lhs // rhs`."""
 power = t.cast("BinaryFunction[NumericScalar, NumericScalar]", pc.power)
+"""Equivalent to `lhs ** rhs`."""
 sqrt = t.cast("UnaryNumeric", pc.sqrt)
+"""Compute the square root of the elements."""
 abs = t.cast("UnaryNumeric", pc.abs)
+"""Compute absolute values."""
 exp = t.cast("UnaryNumeric", pc.exp)
+"""Compute the exponential, element-wise."""
 
 
-def truediv(lhs: Incomplete, rhs: Incomplete) -> Incomplete:
+def truediv(lhs: Incomplete, rhs: Incomplete, /) -> Incomplete:
+    """Equivalent to `lhs / rhs`."""
     return pc.divide(*cast_for_truediv(lhs, rhs))
 
 
-def modulus(lhs: Incomplete, rhs: Incomplete) -> Incomplete:
+def modulus(lhs: Incomplete, rhs: Incomplete, /) -> Incomplete:
+    """Equivalent to `lhs % rhs`."""
     floor_div = floordiv(lhs, rhs)
     return sub(lhs, multiply(floor_div, rhs))
 
 
 def log(native: ChunkedOrScalarAny, base: float = math.e) -> ChunkedOrScalarAny:
+    """Compute the logarithm to a given base."""
     result: ChunkedOrScalarAny = call("logb", native, base)
     return result
