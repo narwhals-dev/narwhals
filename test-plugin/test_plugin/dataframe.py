e9a2a1917d3dd9048427095d3a5321f2b87fe9ad
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-01-14 15:02:41 +0000

    only check for interchange protocol in v1

--- test-plugin/test_plugin/dataframe.py
+++ test-plugin/test_plugin/dataframe.py
@@ -31,6 +31,9 @@ class DictLazyFrame(
     def __narwhals_lazyframe__(self) -> Self:
         return self
 
+    def __dataframe__(self) -> None:
+        return None
+
     @property
     def columns(self) -> list[str]:  # pragma: no cover
         return list(self._native_frame.keys())
