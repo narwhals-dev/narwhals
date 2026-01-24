ca27d638653aa3d654478b1c1570c40a68dab040
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-01-24 23:18:59 +0000

    chore: Don't run `generate_data.py` on import

---
+++ tpch/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from tpch import (
+    generate_data as generate_data,
+    queries as queries,
+    tests as tests,
+    typing_ as typing_,
+)
--- tpch/generate_data.py
+++ tpch/generate_data.py
@@ -2,32 +2,15 @@ from __future__ import annotations
 
 import io
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-import duckdb
-import pyarrow as pa
-import pyarrow.csv as pc
-import pyarrow.parquet as pq
-
-data_path = Path("data")
-data_path.mkdir(exist_ok=True)
-
-SCALE_FACTOR = 0.1
-con = duckdb.connect(database=":memory:")
-con.execute("INSTALL tpch; LOAD tpch")
-con.execute(f"CALL dbgen(sf={SCALE_FACTOR})")
-tables = (
-    "lineitem",
-    "customer",
-    "nation",
-    "orders",
-    "part",
-    "partsupp",
-    "region",
-    "supplier",
-)
+if TYPE_CHECKING:
+    import pyarrow as pa
 
 
 def convert_schema(schema: pa.Schema) -> pa.Schema:
+    import pyarrow as pa
+
     new_schema = []
     for field in schema:
         if pa.types.is_decimal(field.type):
@@ -39,28 +22,52 @@ def convert_schema(schema: pa.Schema) -> pa.Schema:
     return pa.schema(new_schema)
 
 
-for t in tables:
-    tbl = con.query(f"SELECT * FROM {t}")  # noqa: S608
-    tbl_arrow = tbl.to_arrow_table()
-    new_schema = convert_schema(tbl_arrow.schema)
-    tbl_arrow = tbl_arrow.cast(new_schema)
-    pq.write_table(tbl_arrow, data_path / f"{t}.parquet")
+def main(scale_factor: float = 0.1) -> None:
+    import duckdb
+    import pyarrow.csv as pc
+    import pyarrow.parquet as pq
 
-
-results = con.query(
-    f"""
-    SELECT query_nr, answer
-    FROM tpch_answers()
-    WHERE scale_factor={SCALE_FACTOR}
-    """  # noqa: S608
-)
-
-while row := results.fetchmany(1):
-    query_nr, answer = row[0]
-    tbl_answer = pc.read_csv(
-        io.BytesIO(answer.encode("utf-8")), parse_options=pc.ParseOptions(delimiter="|")
+    data_path = Path("data")
+    data_path.mkdir(exist_ok=True)
+    con = duckdb.connect(database=":memory:")
+    con.execute("INSTALL tpch; LOAD tpch")
+    con.execute(f"CALL dbgen(sf={scale_factor})")
+    tables = (
+        "lineitem",
+        "customer",
+        "nation",
+        "orders",
+        "part",
+        "partsupp",
+        "region",
+        "supplier",
     )
-    new_schema = convert_schema(tbl_answer.schema)
-    tbl_answer = tbl_answer.cast(new_schema)
+    for t in tables:
+        tbl = con.query(f"SELECT * FROM {t}")  # noqa: S608
+        tbl_arrow = tbl.to_arrow_table()
+        new_schema = convert_schema(tbl_arrow.schema)
+        tbl_arrow = tbl_arrow.cast(new_schema)
+        pq.write_table(tbl_arrow, data_path / f"{t}.parquet")
 
-    pq.write_table(tbl_answer, data_path / f"result_q{query_nr}.parquet")
+    results = con.query(
+        f"""
+        SELECT query_nr, answer
+        FROM tpch_answers()
+        WHERE scale_factor={scale_factor}
+        """  # noqa: S608
+    )
+
+    while row := results.fetchmany(1):
+        query_nr, answer = row[0]
+        tbl_answer = pc.read_csv(
+            io.BytesIO(answer.encode("utf-8")),
+            parse_options=pc.ParseOptions(delimiter="|"),
+        )
+        new_schema = convert_schema(tbl_answer.schema)
+        tbl_answer = tbl_answer.cast(new_schema)
+
+        pq.write_table(tbl_answer, data_path / f"result_q{query_nr}.parquet")
+
+
+if __name__ == "__main__":
+    main()
