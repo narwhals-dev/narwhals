4f303b3b00f71f32d1d6a427f5f1cf323d089be1
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-01-25 16:00:57 +0000

    fix: Don't yolo the suggestion this time

--- tpch/__init__.py
+++ tpch/__init__.py
@@ -2,9 +2,4 @@ from __future__ import annotations
 
 from tpch import generate_data, queries, tests, typing_
 
- __all__ = (
-    "generate_data",
-    "queries",
-    "tests",
-    "typing_",
-)
+__all__ = ("generate_data", "queries", "tests", "typing_")
