6d289c2c145462289ee0ac93f34ad8a0e7e32235
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-01-30 22:12:46 +0000

    docs: Add `ScaleFactor` alias

--- tpch/typing_.py
+++ tpch/typing_.py
@@ -39,6 +39,28 @@ QueryID: TypeAlias = Literal[
     "q21",
     "q22",
 ]
+ScaleFactor: TypeAlias = Literal[
+    "0.014", "0.052", "0.1", "0.25", "0.51", "1.0", "10.0", "30.0"
+]
+"""Values for `scale_factor` that are known to produce correct results.
+
+These three are blessed by [TPC-H v3.0.1 (Page 79)]:
+
+    "1.0", "10.0", "30.0"
+
+These five are *not*, but represent a [benchmark runtime] between 13-72 seconds:
+
+    "0.014", "0.052", "0.1", "0.25", "0.51"
+
+Warning:
+    Running the higher values can **easily** crash when combined with [`pytest-xdist`].
+    We are effectively running `scale_factor * 6` when all backends are selected.
+
+[TPC-H v3.0.1 (Page 79)]: https://www.tpc.org/TPC_Documents_Current_Versions/pdf/TPC-H_v3.0.1.pdf
+[benchmark runtime]: https://github.com/narwhals-dev/narwhals/pull/3421#discussion_r2743356336
+[`pytest-xdist`]: https://pytest-xdist.readthedocs.io/en/stable/
+"""
+
 XFailRaises: TypeAlias = type[BaseException] | tuple[type[BaseException], ...]
 Artifact: TypeAlias = Literal["database", "answers"]
 
