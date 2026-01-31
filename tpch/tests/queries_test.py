53147b945f0fa1e803752f9fa7a03511667a847c
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-01-31 11:50:37 +0000

    fix: Remove unused `request`

--- tpch/tests/queries_test.py
+++ tpch/tests/queries_test.py
@@ -3,13 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    import pytest
-
     from tpch.classes import Backend, Query
 
 
-def test_execute_query(
-    query: Query, backend: Backend, request: pytest.FixtureRequest
-) -> None:
+def test_execute_query(query: Query, backend: Backend) -> None:
     """Helper function to run a TPCH query test."""
-    query.execute(backend, request)
+    query.execute(backend)
