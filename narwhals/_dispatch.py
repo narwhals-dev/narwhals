ddd5d98c8d8083fdc53bc22129a3ba58d2eb7b81
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-02-16 18:19:06 +0000

    revert: Remove `Impl` alias
    https://github.com/narwhals-dev/narwhals/pull/3410#discussion_r2813431763

--- narwhals/_dispatch.py
+++ narwhals/_dispatch.py
@@ -5,17 +5,15 @@ from functools import partial
 from typing import TYPE_CHECKING, Any, Generic, Protocol, TypeVar, overload
 
 if TYPE_CHECKING:
-    from typing_extensions import TypeAlias
 
     class Deferred(Protocol):
-        def __call__(self, f: Impl[R], /) -> JustDispatch[R]: ...
+        def __call__(self, f: Callable[..., R], /) -> JustDispatch[R]: ...
 
 
 __all__ = ["just_dispatch"]
 
 R = TypeVar("R")
 R_co = TypeVar("R_co", covariant=True)
-Impl: TypeAlias = Callable[..., R]
 Passthrough = TypeVar("Passthrough", bound=Callable[..., Any])
 
 
@@ -24,11 +22,11 @@ class JustDispatch(Generic[R_co]):
 
     __slots__ = ("_registry", "_upper_bound")
 
-    def __init__(self, function: Impl[R_co], /, upper_bound: type[Any]) -> None:
+    def __init__(self, function: Callable[..., R_co], /, upper_bound: type[Any]) -> None:
         self._upper_bound: type[Any] = upper_bound
-        self._registry: dict[type[Any], Impl[R_co]] = {upper_bound: function}
+        self._registry: dict[type[Any], Callable[..., R_co]] = {upper_bound: function}
 
-    def dispatch(self, tp: type[Any], /) -> Impl[R_co]:
+    def dispatch(self, tp: type[Any], /) -> Callable[..., R_co]:
         """Get the implementation for a given type."""
         if f := self._registry.get(tp):
             return f
@@ -55,11 +53,11 @@ class JustDispatch(Generic[R_co]):
 
 
 @overload
-def just_dispatch(function: Impl[R], /) -> JustDispatch[R]: ...
+def just_dispatch(function: Callable[..., R], /) -> JustDispatch[R]: ...
 @overload
 def just_dispatch(*, upper_bound: type[Any] = object) -> Deferred: ...
 def just_dispatch(
-    function: Impl[R] | None = None, /, *, upper_bound: type[Any] = object
+    function: Callable[..., R] | None = None, /, *, upper_bound: type[Any] = object
 ) -> JustDispatch[R] | Deferred:
     """Transform a function into a single-dispatch generic function.
 
