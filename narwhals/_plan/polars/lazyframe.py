2b0bf4ef8d6e19e144d0cef5ede966f13b043f29
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-02-22 00:36:13 +0000

    feat: Impl most of the new `CompliantLaztFrame` for polars

---
+++ narwhals/_plan/polars/lazyframe.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Any, ClassVar
+
+import polars as pl
+
+from narwhals._plan.common import todo
+from narwhals._plan.compliant.lazyframe import CompliantLazyFrame
+from narwhals._utils import Implementation, Version
+from narwhals.schema import Schema
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    import pandas as pd
+    import pyarrow as pa
+    from typing_extensions import Self
+
+    from narwhals._plan.compliant.dataframe import CompliantDataFrame
+    from narwhals._plan.dataframe import DataFrame as NwDataFrame
+    from narwhals._translate import ArrowStreamExportable
+
+MAIN = Version.MAIN
+
+
+class PolarsLazyFrame(CompliantLazyFrame[pl.LazyFrame]):
+    __slots__ = ("_native", "_version")
+    implementation: ClassVar = Implementation.POLARS
+    _native: pl.LazyFrame
+    _version: Version
+
+    @classmethod
+    def from_arrow(  # type: ignore[override]
+        cls, frame: pa.Table | ArrowStreamExportable, /, version: Version = MAIN, **_: Any
+    ) -> Self:
+        return cls.from_polars(pl.DataFrame(frame), version)
+
+    @classmethod
+    def from_native(cls, native: pl.LazyFrame, /, version: Version = MAIN) -> Self:
+        obj = cls.__new__(cls)
+        obj._native = native
+        obj._version = version
+        return obj
+
+    @classmethod
+    def from_pandas(cls, frame: pd.DataFrame, /, version: Version = MAIN) -> Self:
+        return cls.from_polars(pl.from_pandas(frame), version)
+
+    @classmethod
+    def from_polars(cls, frame: pl.DataFrame, /, version: Version = MAIN) -> Self:
+        return cls.from_native(frame.lazy(), version)
+
+    @classmethod
+    def from_narwhals(
+        cls, frame: NwDataFrame[Any, Any] | CompliantDataFrame[Any, Any, Any]
+    ) -> Self:
+        return cls.from_polars(frame.to_polars(), frame.version)
+
+    def collect_native(self) -> pl.DataFrame:
+        return self.native.collect()
+
+    def to_arrow(self) -> pa.Table:
+        return self.collect_native().to_arrow()
+
+    def to_pandas(self) -> pd.DataFrame:
+        return self.collect_native().to_pandas()
+
+    @property
+    def columns(self) -> Sequence[str]:
+        return self.native.collect_schema().names()
+
+    def collect_schema(self) -> Schema:
+        return Schema.from_polars(self.native.collect_schema())
+
+    @property
+    def native(self) -> pl.LazyFrame:
+        return self._native
+
+    @property
+    def version(self) -> Version:
+        return self._version
+
+    from_compliant = from_narwhals
+    from_arrow_c_stream = from_arrow
+    to_polars = collect_native
+
+    __hash__ = todo()
+    to_narwhals = todo()
