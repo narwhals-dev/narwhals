21a135f134e715de78c13cdeded24e6083db6c89
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-03-14 17:31:43 +0000

    refactor: `_expr_meta_massage_namespace` -> `ExprIRMeta._pop_nodes`

--- narwhals/_plan/_meta.py
+++ narwhals/_plan/_meta.py
@@ -4,17 +4,17 @@ from __future__ import annotations
 
 # ruff: noqa: N806
 from itertools import chain
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from narwhals._plan import _nodes
 from narwhals._plan._nodes import _EXPR_NODE_TYPES
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import Any, Final, TypeVar
+    from typing import Final, TypeVar
 
     import _typeshed
-    from typing_extensions import dataclass_transform
+    from typing_extensions import TypeAlias, dataclass_transform
 
     from narwhals._plan import _expr_ir2
     from narwhals._plan.typing import Seq
@@ -47,6 +47,12 @@ else:
 
 
 __all__ = ["ImmutableMeta", "SlottedMeta", "dataclass_transform"]
+
+Ns: TypeAlias = dict[str, Any]
+"""The class `namespace`.
+
+Marked so that mutating it is visible.
+"""
 
 flatten = chain.from_iterable
 _KEYS_NAME: Final = "__immutable_keys__"
@@ -118,11 +124,11 @@ class ExprIRMeta(ImmutableMeta):
         metacls: type[_typeshed.Self],
         cls_name: str,
         bases: tuple[type, ...],
-        namespace: dict[str, Any],
+        namespace: Ns,
         /,
         **kwds: Any,
     ) -> _typeshed.Self:
-        namespace, nodes = _expr_meta_massage_namespace(namespace)
+        namespace, nodes = ExprIRMeta._pop_nodes(namespace)
         tp = super().__new__(metacls, cls_name, bases, namespace, **kwds)  # type: ignore[misc]
         if not TYPE_CHECKING:  # noqa: SIM102
             # `pyright` is quite unhappy w/ this, thinks that `Self` means `object` but it is a `type`!
@@ -131,95 +137,96 @@ class ExprIRMeta(ImmutableMeta):
                 _inherit_traverser(metacls, tp, nodes)
         return tp  # type: ignore[no-any-return]
 
+    @staticmethod
+    def _pop_nodes(namespace: Ns) -> tuple[Ns, _nodes.IntoExprNodes]:
+        """Extract any field specifiers from the class namespace.
 
-# TODO @dangotbanned: probably turn into a `@staticmethod`
-def _expr_meta_massage_namespace(
-    namespace: dict[str, Any],
-) -> tuple[dict[str, Any], _nodes.IntoExprNodes]:
-    """What on earth is this?
+        ## What?
+        These steps are needed *iff* the new class used the following syntax:
 
-    ## What?
+            class Subclass(ExprIR):
+                __slots__ = ("expr", "non_node")
+                #             ^^^^
+                expr: ExprIR = node()
+                #            ^ ^^^^^^
+                non_node: str
 
-    These steps are needed *iff* the new class used the following syntax:
+        Because `expr` appears both in `__slots__` and in `__dict__`, we would normally see:
 
-        class Subclass(ExprIR):
-            __slots__ = ("expr", "non_node")
-            #             ^^^^    (1)
-            expr: ExprIR = node()
-            #            ^ ^^^^^^ (2)
-            non_node: str
+            ValueError: 'expr' in __slots__ conflicts with class variable
 
-    Because `expr` appears both in `__slots__` (1) and in `__dict__` (2),
-    we would normally see:
+        However, what we actually want is to transform the definition into this:
 
-        ValueError: 'expr' in __slots__ conflicts with class variable
+            class Subclass(ExprIR):
+                __slots__ = ("expr", "non_node")
+                #             ^^^^
+                #             The slot is for the `ExprIR`
+                expr: ExprIR
+                #            ^ ^^^^^^
+                #            Poof! The assignment has vanished. We make the connection
+                #            between `"expr"` and the field specifier `node()` and
+                #            move it to `__expr_ir_nodes__` ...
+                non_node: str
 
-    However, what we actually want is to transform the definition into this:
+                __expr_ir_nodes__: ClassVar[ExprTraverser] = ...
 
-        class Subclass(ExprIR):
-            __slots__ = ("expr", "non_node")
-            #             ^^^^
-            #             The slot is for the `ExprIR`
-            expr: ExprIR
-            #            ^ ^^^^^^
-            #            Poof! The assignment has vanished. We make the connection
-            #            between `"expr"` and the field specifier `node()` and
-            #            move it to `__expr_ir_nodes__` ...
-            non_node: str
+        Our end result has a nice repr that looks a lot like our source code:
 
-            __expr_ir_nodes__: ClassVar[ExprTraverser] = ...
-            # ^                                         | ExprTraverser[1]
-            # |                                         |     expr: ExprIR = node()
-            # has a repr that looks like the source definition
+            Subclass.__expr_ir_nodes__
+            ExprTraverser[1]
+                expr: ExprIR = node()
 
-    ## How?
-    A simple, unoptimized version might look like this:
+        ## How?
+        A simple, unoptimized version might look like this:
 
-        def pop_nodes(namespace: dict[str, Any]) -> tuple[dict[str, Any], list[Any]]:
-            slots = namespace.get("__slots__", ())
-            pop = namespace.pop
-            nodes = [pop(name).with_name(name) for name in tuple(namespace) if name in slots]
-            return namespace, nodes
+            def pop_nodes(namespace: Ns) -> tuple[Ns, list[Any]]:
+                slots = namespace.get("__slots__", ())
+                pop = namespace.pop
+                nodes = [
+                    pop(name).with_name(name)
+                    for name in tuple(namespace)
+                    if name in slots
+                ]
+                return namespace, nodes
 
-    *But*, since this code will be [executed for each new subclass] - we take 4 steps
-    to avoid hogging cycles at import-time.
+        *But*, since this code will be [executed for each new subclass] - we take 4 steps
+        to avoid hogging cycles at import-time.
 
-    If either of these are true, we have no work to do:
-    1. we *don't* have new `__slots__`
-    2. we do, but they *don't* intersect with `namespace`
+        If either of these are true, we have no work to do:
+        1. we *don't* have new `__slots__`
+        2. we do, but they *don't* intersect with `namespace`
 
-    If we have some work, we can avoid iterating over `namespace` *iff*:
+        If we have some work, we can avoid iterating over `namespace` *iff*:
 
-    3. Our intersection is a single name
+        3. Our intersection is a single name
 
-    Surprisingly, that covers 87% of cases (34/39) *already*.
+        Surprisingly, that covers 87% of cases (34/39) *already*.
 
-    For everything else (4), we do a `namespace`-ordered pass
-    but work destructively on the intersection to stop as early as we can.
+        For everything else (4), we do a `namespace`-ordered pass
+        but work destructively on the intersection to stop as early as we can.
 
-    [executed for each new subclass]: https://docs.python.org/3/reference/datamodel.html#class-object-creation
-    """
-    slots: tuple[str, ...]
-    nodes = []
-
-    # Steps 1, 2
-    if (slots := namespace.get("__slots__", ())) and (
-        intersection := namespace.keys() & slots
-    ):
-        # Step 3
-        if len(intersection) == 1:
-            name = intersection.pop()
-            nodes.append(_ensure_node(name, namespace.pop(name)))
-            return namespace, nodes
-
-        # Step 4
-        for name in tuple(namespace):
-            if name in intersection:
+        [executed for each new subclass]: https://docs.python.org/3/reference/datamodel.html#class-object-creation
+        """
+        slots: tuple[str, ...]
+        nodes = []
+        # Steps 1, 2
+        if (slots := namespace.get("__slots__", ())) and (
+            intersection := namespace.keys() & slots
+        ):
+            # Step 3
+            if len(intersection) == 1:
+                name = intersection.pop()
                 nodes.append(_ensure_node(name, namespace.pop(name)))
-                intersection.remove(name)
-                if not intersection:
-                    break
-    return namespace, nodes
+                return namespace, nodes
+
+            # Step 4
+            for name in tuple(namespace):
+                if name in intersection:
+                    nodes.append(_ensure_node(name, namespace.pop(name)))
+                    intersection.remove(name)
+                    if not intersection:
+                        break
+        return namespace, nodes
 
 
 def _ensure_node(name: str, node: Any) -> _nodes._ExprNode:
