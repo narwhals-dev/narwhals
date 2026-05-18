0f0922104c6abe7be2312ed22fd579dd6f2734ed
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-05-18 20:28:52 +0000

    refactor: Polish nested literal repr paths

--- narwhals/_plan/_reprs.py
+++ narwhals/_plan/_reprs.py
@@ -33,18 +33,16 @@ from narwhals.dtypes import (
 
 if TYPE_CHECKING:
     import decimal
-    from collections.abc import Callable
+    from collections.abc import Callable, Sequence
 
     from narwhals.dtypes import DType
-    from narwhals.typing import PythonLiteral
+    from narwhals.typing import IntoDType, PythonLiteral
 
 __all__ = ("lit_repr",)
 
 LitRepr: TypeAlias = tuple[str | None, str]
-"""`(<dtype-repr>, <value-repr>)`
-
-Nested data types may discard `<dtype-repr>`.
-"""
+"""`(<dtype-repr>, <value-repr>)`"""
+_MAX_NESTED: Final = 5
 
 
 @overload
@@ -170,50 +168,41 @@ def _(dtype: Struct, value: dict[str, Any] | None) -> LitRepr:
 
 @_lit_repr.register(List)
 def _(dtype: List, value: list[Any] | tuple[Any, ...] | None) -> LitRepr:
-    inner, values = _lit_repr_nested_partial(dtype, value)
+    inner, values = _lit_repr_nested(dtype, value)
     return f"list[{inner}]" if inner else "list", values
 
 
 @_lit_repr.register(Array)
 def _(dtype: Array, value: list[Any] | tuple[Any, ...] | None) -> LitRepr:
-    inner, values = _lit_repr_nested_partial(dtype, value)
-    dtype_s = f"{inner}, " if inner else ""
+    inner, values = _lit_repr_nested(dtype, value)
     shape = dtype.shape
     shape_s = repr(shape[0] if len(shape) == 1 else shape)
-    return f"array[{dtype_s}{shape_s}]", values
+    return f"array[{inner}, {shape_s}]", values
 
 
-# TODO @dangotbanned: Clean up (eventually)
-def _lit_repr_nested_partial(
-    dtype: Array | List, value: list[Any] | tuple[Any, ...] | None
-) -> LitRepr:
-    """Messy shared logic for `Array`/`List`.
+def _repr_seq(inner: DType, value: Sequence[Any]) -> str:
+    return f"[{', '.join(lit_repr(inner, v)[1] for v in value)}]"
 
-    - Array deviates by always displaying the dtype, since it also has shape
-    """
+
+# always like `array[i64, <shape>]
+@overload
+def _lit_repr_nested(dtype: Array, value: Sequence[Any] | None) -> tuple[str, str]: ...
+@overload
+def _lit_repr_nested(dtype: List, value: Sequence[Any] | None) -> LitRepr: ...
+def _lit_repr_nested(dtype: Array | List, value: Sequence[Any] | None) -> LitRepr:
+    """Infers whether to display the *inner* dtype and/or value for nested literals."""
+    # NOTE: Yes, these are subtly different
     if isinstance(dtype, Array):
-        leaf: Any = dtype
-        for _ in dtype.shape:
+        leaf: Array | IntoDType = dtype
+        while isinstance(leaf, Array):
             leaf = leaf.inner
-        into = leaf
-    else:
-        into = dtype.inner
-    dtype_inner = common.into_dtype(into)
-    dtype_always, _ = lit_repr(dtype_inner, None)
-    if value is None or not value:
-        dtype_s = dtype_always
-        values = "None" if value is None else "[]"
-    else:
-        if isinstance(dtype, Array):
-            dtype_s = dtype_always
-            _, first = lit_repr(dtype_inner, value[0])
-        else:
-            dtype_s, first = lit_repr(dtype_inner, value[0])
-        if len(value) >= 5:
-            values = "..."
-            dtype_s = dtype_always
-        else:
-            it = (lit_repr(dtype_inner, v)[1] for v in value[1:])
-            values = ", ".join((first, *it))
-        values = f"[{values}]"
-    return dtype_s, values
+        inner = common.into_dtype(leaf)
+        inner_s, _ = lit_repr(inner, None)
+        if value is None or len(value) >= _MAX_NESTED:
+            return inner_s, "None" if value is None else "[...]"
+        return inner_s, _repr_seq(inner, value)
+    inner = common.into_dtype(dtype.inner)
+    if not value or len(value) >= _MAX_NESTED:
+        values = "None" if value is None else "[...]" if value else "[]"
+        return lit_repr(inner, None)[0], values
+    return None, _repr_seq(inner, value)
