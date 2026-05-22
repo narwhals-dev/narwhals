51dee8ef1b033b83172e1024f7a68eb4b9b87d9c
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-05-22 21:57:50 +0000

    test: Add lazy cases

--- tests/plan/select_test.py
+++ tests/plan/select_test.py
@@ -1,30 +1,61 @@
 from __future__ import annotations
 
+import datetime as dt
 from typing import TYPE_CHECKING
 
 import pytest
 
 import narwhals._plan as nwp
+import narwhals._plan.selectors as ncs
 from tests.plan.utils import assert_equal_data, re_compile
+from tests.utils import PYARROW_VERSION
 
 if TYPE_CHECKING:
     from narwhals._plan.typing import IntoExpr, OneOrIterable
     from tests.conftest import Data
     from tests.plan.utils import Eager, Lazy
+if PYARROW_VERSION and PYARROW_VERSION < (21,):  # pragma: no cover
+    pytest.importorskip("numpy")
 
 
-XFAIL_TODO = pytest.mark.xfail(reason="TODO")
-
-
-@XFAIL_TODO
+@pytest.mark.parametrize(
+    ("exprs", "named_exprs", "expected"),
+    [
+        ((None,), {}, {"literal": [None]}),
+        (
+            (1,),
+            {"two": 2, "three": nwp.lit({"four": 5, "six": 7})},
+            {"literal": [1], "two": [2], "three": [{"four": 5, "six": 7}]},
+        ),
+        ((), {"numbers": nwp.int_range(10)}, {"numbers": list(range(10))}),
+        (
+            (
+                nwp.date_range(
+                    dt.date(2020, 1, 1), dt.date(2021, 1, 1), dt.timedelta(weeks=17)
+                ).alias("dates"),
+                nwp.lit("123"),
+            ),
+            {},
+            {
+                "dates": [
+                    dt.date(2020, 1, 1),
+                    dt.date(2020, 4, 29),
+                    dt.date(2020, 8, 26),
+                    dt.date(2020, 12, 23),
+                ],
+                "literal": ["123"] * 4,
+            },
+        ),
+    ],
+)
 def test_lazy(
     exprs: tuple[OneOrIterable[IntoExpr], ...],
     named_exprs: dict[str, IntoExpr],
     expected: Data,
     lazy: Lazy,
 ) -> None:
-    result = nwp.select(*exprs, lazy=lazy, **named_exprs)
-    assert_equal_data(result.collect(), expected)
+    result = nwp.select(*exprs, lazy=lazy, **named_exprs).collect().sort(ncs.first())
+    assert_equal_data(result, expected)
 
 
 @pytest.mark.parametrize(
