ac4c25e3951362534ea78f4e9e0408fe56fdaa63
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-06-02 15:16:23 +0000

    test: Decouple `DataFrame.unnest` tests from pyarrow
    https://github.com/narwhals-dev/narwhals/pull/3666#discussion_r3340933312

--- tests/plan/unnest_test.py
+++ tests/plan/unnest_test.py
@@ -14,10 +14,6 @@ if TYPE_CHECKING:
     from narwhals._plan.typing import ColumnNameOrSelector, OneOrIterable
     from tests.conftest import Data
 
-pytest.importorskip("pyarrow")
-import pyarrow as pa
-import pyarrow.compute as pc
-
 BEFORE = "before"
 A = "t_a"
 B = "t_b"
@@ -27,7 +23,11 @@ AFTER = "after"
 
 
 @pytest.fixture
-def data_1() -> Data:
+def data() -> Data:
+    """Dataset from [upstream].
+
+    [upstream]: https://github.com/pola-rs/polars/blob/346a793589efd552a6c10c857e0f0434f7e9a7d4/py-polars/src/polars/lazyframe/frame.py#L9010-L9054
+    """
     return {
         BEFORE: ["foo", "bar"],
         A: [1, 2],
@@ -38,21 +38,15 @@ def data_1() -> Data:
     }
 
 
-# TODO @dangotbanned: Finally decouple this test suite from `pyarrow` by replacing with `nwp.struct`
-# https://github.com/narwhals-dev/narwhals/issues/3247
-def pyarrow_struct(native: pa.Table, columns: list[str]) -> pa.StructArray:
-    return pc.make_struct(*native.select(columns).columns, field_names=columns)
-
-
-# TODO @dangotbanned: Decouple test from `pyarrow`
+# TODO @dangotbanned: Rewrite this whole thing (using indices was *because of* pyarrow's api)
 @pytest.mark.parametrize("insert_at", [-1, 0, 1])
 @pytest.mark.parametrize(
     "columns", [[A], [A, B], [A, B, C, D]], ids=["1-column", "2-column", "4-column"]
 )
 def test_unnest_frame_single_struct(
-    data_1: Data, columns: list[str], insert_at: Literal[-1, 0, 1]
+    data: Data, columns: list[str], insert_at: Literal[-1, 0, 1], dataframe: DataFrame
 ) -> None:
-    expected = copy.deepcopy(data_1)
+    expected = copy.deepcopy(data)
     if insert_at in {-1, 0}:
         if insert_at == -1:
             for column in columns:
@@ -63,41 +57,48 @@ def test_unnest_frame_single_struct(
                 _tmp[column] = expected.pop(column)
             _tmp |= expected
             expected = _tmp
-    table = pa.Table.from_pydict(data_1)
-    struct = pyarrow_struct(table, columns)
-    table = table.drop(columns)
-    if insert_at in {0, 1}:
-        table_w_struct = table.add_column(insert_at, "t_struct", struct)
+
+    df = dataframe(data)
+    struct_name = "t_struct"
+    struct = nwp.struct(columns).alias("t_struct")
+
+    df = df.with_columns(struct).drop(columns)
+    if insert_at == 0:
+        df = df.select(struct_name, nwp.exclude(struct_name))
+    elif insert_at == -1:
+        df = df.select(nwp.exclude(struct_name), struct_name)
     else:
-        table_w_struct = table.append_column("t_struct", struct)
-    df = nwp.DataFrame.from_native(table_w_struct)
+        df = df.select(ncs.first(), struct_name, ~(ncs.first() | ncs.last()))
+
     assert_equal_data(df.unnest("t_struct"), expected)
     assert_equal_data(df.unnest(ncs.struct()), expected)
     assert_equal_data(df.unnest(nwp.nth(insert_at).meta.as_selector()), expected)
 
 
-# TODO @dangotbanned: Decouple test from `pyarrow`
-def test_unnest_frame_multi_struct(data_1: Data) -> None:
-    expected = copy.deepcopy(data_1)
-    table = pa.Table.from_pydict(data_1)
-    columns_1 = [A, B]
-    columns_2 = [C, D]
+def test_unnest_frame_multi_struct(data: Data, dataframe: DataFrame) -> None:
+    expected_identical_input = copy.deepcopy(data)
+    expected_reorder = {
+        BEFORE: ["foo", "bar"],
+        C: [True, None],
+        D: [[1, 2], [3]],
+        A: [1, 2],
+        B: ["a", "b"],
+        AFTER: ["baz", "womp"],
+    }
+
     name_1 = "t_struct_1"
     name_2 = "t_struct_2"
-    table_w_structs = (
-        table.drop([*columns_1, *columns_2])
-        .add_column(1, name_1, pyarrow_struct(table, columns_1))
-        .add_column(2, name_2, pyarrow_struct(table, columns_2))
+
+    df = dataframe(data).select(
+        BEFORE, nwp.struct(A, B).alias(name_1), nwp.struct([C, D]).alias(name_2), AFTER
     )
-
-    df = nwp.DataFrame.from_native(table_w_structs)
-    assert_equal_data(df.unnest(name_1, name_2), expected)
-    assert_equal_data(df.unnest(ncs.struct()), expected)
-    assert_equal_data(df.unnest(ncs.by_index(1, 2)), expected)
+    assert_equal_data(df.unnest(name_1, name_2), expected_identical_input)
+    assert_equal_data(df.unnest(ncs.struct()), expected_identical_input)
+    assert_equal_data(df.unnest(ncs.by_index(2, 1)), expected_reorder)
 
 
-def test_unnest_frame_invalid_operation_error(data_1: Data, dataframe: DataFrame) -> None:
-    df = dataframe(data_1)
+def test_unnest_frame_invalid_operation_error(data: Data, dataframe: DataFrame) -> None:
+    df = dataframe(data)
     with pytest.raises(
         InvalidOperationError,
         match=re_compile(r"unnest.+not supported for.+string.+expected.+struct"),
@@ -112,23 +113,21 @@ def test_unnest_frame_invalid_operation_error(data_1: Data, dataframe: DataFrame
     "columns", [ncs.array(), "hello", nwp.nth(29).meta.as_selector(), ["a", "b", "A"]]
 )
 def test_unnest_frame_column_not_found_error(
-    data_1: Data, columns: OneOrIterable[ColumnNameOrSelector], dataframe: DataFrame
+    data: Data, columns: OneOrIterable[ColumnNameOrSelector], dataframe: DataFrame
 ) -> None:
-    df = dataframe(data_1)
+    df = dataframe(data)
     with pytest.raises(ColumnNotFoundError):
         df.unnest(columns)
 
 
-# TODO @dangotbanned: Decouple test from `pyarrow`
-def test_unnest_frame_duplicate_error(data_1: Data) -> None:
-    columns = [A, B, C]
-    table = pa.Table.from_pydict(data_1)
-    table_w_structs = (
-        table.drop(columns)
-        .add_column(1, "one_conflict", pyarrow_struct(table, [AFTER, *columns[1:]]))
-        .add_column(2, "two_conflicts", pyarrow_struct(table, [BEFORE, columns[2], D]))
+def test_unnest_frame_duplicate_error(data: Data, dataframe: DataFrame) -> None:
+    df = dataframe(data).select(
+        BEFORE,
+        nwp.struct(AFTER, B, C).alias("one_conflict"),
+        nwp.struct(BEFORE, C, D).alias("two_conflicts"),
+        D,
+        AFTER,
     )
-    df = nwp.DataFrame.from_native(table_w_structs)
 
     msg_one = r"'after'"
     msg_two = rf"'({D}|{BEFORE})'.+'({D}|{BEFORE})'"
