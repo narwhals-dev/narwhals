549356831933c1ef95a66fa7f064ec6fa3e21700
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-06-09 22:05:27 +0000

    docs: Fix `AggExpr`

--- src/narwhals/_plan/expressions/aggregation.py
+++ src/narwhals/_plan/expressions/aggregation.py
@@ -1,9 +1,11 @@
 """Aggregation expressions.
 
 ## Implementation Notes
-Adapted from [`dsl::expr::AggExpr`], extended with `Arg{Min,Max}`.
+Adapted from [`dsl::expr::AggExpr`](https://github.com/pola-rs/polars/blob/2b825ad7933e4b7ca88556f67f4323caaaa40644/crates/polars-plan/src/dsl/expr/mod.rs#L23-L61),
+extended with `Arg{Min,Max}`.
 
-They're similar to `Function` [upstream] - but also appear in [`GroupByMethod`] like the rest of `AggExpr`.
+They're similar to `Function` [upstream] - but also appear in [`GroupByMethod`](https://github.com/pola-rs/polars/blob/2b825ad7933e4b7ca88556f67f4323caaaa40644/crates/polars-core/src/frame/group_by/mod.rs#L860-L882)
+like the rest of `AggExpr`.
 
 Note that there are a few other aggregating functions that do not fit this definition:
 
@@ -11,9 +13,7 @@ Note that there are a few other aggregating functions that do not fit this defin
 
 In the future it might make sense to deviate from polars and move them here ([(#3353 (comment))]).
 
-[`dsl::expr::AggExpr`]: https://github.com/pola-rs/polars/blob/2b825ad7933e4b7ca88556f67f4323caaaa40644/crates/polars-plan/src/dsl/expr/mod.rs#L23-L61
 [upstream]: https://github.com/pola-rs/polars/blob/2b825ad7933e4b7ca88556f67f4323caaaa40644/crates/polars-plan/src/dsl/function_expr/mod.rs#L165-L166
-[`GroupByMethod`]: https://github.com/pola-rs/polars/blob/2b825ad7933e4b7ca88556f67f4323caaaa40644/crates/polars-core/src/frame/group_by/mod.rs#L860-L882
 [(#3353 (comment))]: https://github.com/narwhals-dev/narwhals/pull/3353#discussion_r2622679274
 """
 
@@ -38,29 +38,23 @@ same_dtype = ResolveDType.expr_ir.same_dtype
 class AggExpr(ExprIR):
     """An aggregation reduces an expression to one that is always scalar.
 
-    >>> import narwhals._plan as nw
-    >>> nw.col("a")._ir.is_scalar()
-    False
-    >>> nw.col("a").last()._ir.is_scalar()
-    True
+    Compared to `Col`:
+
+        >>> import narwhals._plan as nw
+        >>> nw.col("a")._ir.is_scalar()
+        False
+        >>> nw.col("a").last()._ir.is_scalar()
+        True
     """
 
     __slots__ = ("expr",)
     expr: ExprIR = node()
     """The input for this aggregation.
 
-    >>> import narwhals._plan as nw
-    >>> nw.col("a").alias("b").sum()._ir.expr
-    col('a').alias('b')
-
     Note:
-        To match the behavior on main ([1], [2]), scalar input is rejected at construction-time.
-        However, [`CompliantScalar`] offers an alternative - which can allow a backend to
-        more closely resemble `polars`.
-
-    [1]: https://github.com/narwhals-dev/narwhals/blob/061c97f8a01bf9e721835978b039303c5051501c/narwhals/_expression_parsing.py#L495-L498
-    [2]: https://github.com/narwhals-dev/narwhals/blob/061c97f8a01bf9e721835978b039303c5051501c/narwhals/_expression_parsing.py#L515-L517
-    [`CompliantScalar`]: https://github.com/narwhals-dev/narwhals/blob/7eb0d60159dd485af04d49ec9a1b18d99085482c/narwhals/_plan/compliant/scalar.py
+        To match the behavior on `main`, scalar input is rejected at construction-time.
+        However, [`CompliantScalar`](ttps://github.com/narwhals-dev/narwhals/blob/7eb0d60159dd485af04d49ec9a1b18d99085482c/narwhals/_plan/compliant/scalar.py)
+        offers an alternative - which can allow a backend to more closely resemble `polars`.
     """
 
     def is_scalar(self) -> bool:
