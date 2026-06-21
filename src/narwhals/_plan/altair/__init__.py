358bdd05d99aabaadaa32703528a14621d2dbf89
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-06-21 18:14:35 +0000

    chore: Add `altair` package
    here we go

---
+++ src/narwhals/_plan/altair/__init__.py
@@ -0,0 +1,9 @@
+"""Experimental translation from `ExprIR` to [Vega Expressions].
+
+[Vega Expressions]: https://vega.github.io/vega/docs/expressions/
+
+See Also:
+    [Altair docs](https://altair-viz.github.io/user_guide/interactions/expressions.html)
+"""
+
+from __future__ import annotations
