e9ea82477297cabce387b29346f637dc0fa0841e
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-06-24 18:28:34 +0000

    feat(DRAFT): Some progress on `conditional`
    Realise i didn't need `predicate.py`, after reading that
    vega-lite just rewrites that them to expressions anyway

--- src/narwhals/_plan/altair/conditional.py
+++ src/narwhals/_plan/altair/conditional.py
@@ -3,7 +3,7 @@
 So that you can write `nw.when` in the places that `alt.when` currently works.
 
 ## Important
-Depends on both `expression.py` and `predicate.py`.
+Depends on `expression.py`.
 
 ## Notes
 `expression.py` handles `TernaryExpr` (including those that are nested) by mapping them directly to `if_`:
@@ -22,3 +22,123 @@ Depends on both `expression.py` and `predicate.py`.
 """
 
 from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypeAlias, TypedDict
+
+from narwhals._plan import expressions as ir
+from narwhals._plan.altair import typing as alt_t
+from narwhals._plan.altair.expression import into_vega_expr
+from narwhals._plan.expressions import functions as F
+
+
+class _Test(TypedDict):
+    """A predicate inside a condition.
+
+    Important:
+        Only `test`-based predicates can be supported by directly using Narwhals.
+
+        This simplifies the typing for the experiment, but highlights that this can't
+        replace everything.
+    """
+
+    test: alt_t.Predicate
+    """The `when` half of a condition."""
+
+
+class TestDatum(_Test, alt_t.Datum, TypedDict):
+    """When `test`, then `datum`."""
+
+
+class TestField(_Test, alt_t.Field, TypedDict):
+    """When `test`, then `field`."""
+
+
+if TYPE_CHECKING:
+
+    class TestValue(_Test, alt_t.Value, TypedDict, closed=True):
+        """When `test`, then `value`."""
+else:
+
+    class TestValue(_Test, alt_t.Value, TypedDict):
+        """When `test`, then `value`."""
+
+
+class _Conditional(TypedDict):
+    condition: TestValue | list[TestValue]
+
+
+class ConditionalDatum(_Conditional, alt_t.Datum, TypedDict): ...
+
+
+class ConditionalField(_Conditional, alt_t.Field, TypedDict): ...
+
+
+class ConditionalValue(_Conditional, alt_t.Value, TypedDict):
+    condition: TestAny | list[TestValue]
+
+
+TestAny: TypeAlias = TestDatum | TestField | TestValue
+"""Any pair of `when(test).then(...)` terms."""
+
+ConditionalAny: TypeAlias = ConditionalDatum | ConditionalField | ConditionalValue
+"""Any fully constructed conditional encoding."""
+
+
+def ternary(expr: ir.TernaryExpr) -> ConditionalAny:
+    """Recursively translate the output of `nw.when(...).then(...).otherwise(...)`."""
+    when = into_vega_expr(expr.predicate)
+    condition: TestAny
+    truthy = expr.truthy
+    falsy = expr.falsy
+    match truthy:
+        case ir.Lit(value=value):
+            condition = {"test": when, "value": value}
+        case ir.Column(name=name_then):
+            condition = {"test": when, "field": name_then, "type": "nominal"}
+
+        # TODO @dangotbanned: Some parallel to `parse_shorthand`
+        # `falsy` has made a start
+        case _:
+            msg = f"Unsupported {truthy!r}"
+            raise NotImplementedError(msg)
+    match falsy:
+        # TODO @dangotbanned: `When` (chained)
+        # TODO @dangotbanned: here's where we create a list
+        case ir.TernaryExpr():
+            msg = f"todo (chained ternary) {type(falsy).__name__!r}"
+            raise NotImplementedError(msg)
+
+        case ir.Lit(value=value):
+            return {"condition": condition, "value": value}
+        case ir.Column(name=name_otherwise):
+            if "field" in condition:
+                msg_0 = (
+                    "Only one field may be used within a conditional encoding.\n"
+                    f"{falsy!r} followed {truthy!r}, in:\n    {expr!r}"
+                )
+                raise TypeError(msg_0)
+            return {"condition": condition, "field": name_otherwise, "type": "nominal"}
+
+        # TODO @dangotbanned: Aggregate
+        # NOTE: `narwhals._plan.altair.aggregate.AGG_EXPR*`
+        case ir.AggExpr() | ir.Len():
+            msg = f"todo (aggregate) {type(falsy).__name__!r}"
+            raise NotImplementedError(msg)
+
+        # NOTE: `narwhals._plan.altair.aggregate.AGG_FUNC`
+        case ir.FunctionExpr(function=F.NullCount(), args=(ir.Column(name=name),)):
+            if "field" in condition:
+                msg_0 = (
+                    "Only one field may be used within a conditional encoding.\n"
+                    f"{falsy.args[0]!r} followed {truthy!r}, in:\n    {expr!r}"
+                )
+                raise TypeError(msg_0)
+            return {
+                "condition": condition,
+                "field": name,
+                "aggregate": "missing",
+                "type": "quantitative",
+            }
+        case _:
+            msg = f"todo {type(falsy).__name__!r}"
+            raise NotImplementedError(msg)
--- src/narwhals/_plan/altair/typing.py
+++ src/narwhals/_plan/altair/typing.py
@@ -8,18 +8,38 @@ if find_spec("altair") is None:
     msg = "`altair` is required to convert `ExprIR`s"
     raise ModuleNotFoundError(msg)
 
-from typing import TYPE_CHECKING, TypeAlias, Union
+from typing import TYPE_CHECKING, Literal, TypeAlias, TypedDict, Union
 
+from altair.expr import core as alt_ir
 from altair.vegalite.v6.schema import _typing as alt_t
 
 if TYPE_CHECKING:
+    from _typeshed import Incomplete
+    from typing_extensions import NotRequired
+
     import narwhals._plan as nwp
+    from narwhals._plan.typing import OneOrSeq
 
 
 VegaExpr: TypeAlias = str
 """A stringized [Vega Expression].
 
 [Vega Expression]: https://vega.github.io/vega/docs/expressions/
+"""
+
+AltExpr: TypeAlias = alt_ir.Expression
+"""An altair wrapper around a [Vega Expression].
+
+[Vega Expression]: https://vega.github.io/vega/docs/expressions/"""
+
+
+Predicate: TypeAlias = VegaExpr | AltExpr
+"""Anything [^1] that is accepted by `condition(test=...)`.
+
+[^1]: That will be constructed via Narwhals expressions
+
+See Also:
+    [Predicate](https://vega.github.io/vega-lite/docs/predicate.html)
 """
 
 FieldName: TypeAlias = str
@@ -51,3 +71,85 @@ WindowOp: TypeAlias = alt_t.WindowOnlyOp_T
 
 AggOrWindow: TypeAlias = AggregateOp | WindowOp
 """Anything accepted by `WindowFieldDef(op=...)`."""
+
+_Repeater: TypeAlias = Literal["row", "column", "repeat", "layer"]
+
+
+class ArgMin(TypedDict):
+    argmin: FieldName
+
+
+class ArgMax(TypedDict):
+    argmax: FieldName
+
+
+Aggregate: TypeAlias = alt_t.NonArgAggregateOp_T | ArgMax | ArgMin
+"""Anything accepted by `Encoding(aggregate=...)`.
+
+- Probably gonna be annoying to wrap/unwrap (internally).
+- But these are aggs from narwhals that'll be easier to write now as expressions.
+
+See Also:
+    [Argmin / Argmax]: https://vega.github.io/vega-lite/docs/aggregate.html#argmax
+"""
+
+
+TimeUnit: TypeAlias = "Incomplete"
+"""TODO @dangotbanned: for `timeUnit`."""
+
+
+class HasExpr(TypedDict):
+    """`ExprRef`, which is anywhere `Parameter` is annotated."""
+
+    expr: VegaExpr
+
+
+class HasRepeat(TypedDict):
+    """Tie a channel to the row or column within a repeated chart.
+
+    Allowed in `Encoding(field=..., datum=...)`.
+    """
+
+    repeat: _Repeater
+
+
+class HasType(TypedDict):
+    # `"geojson"` is allowed in some places, but not all
+    type: alt_t.StandardType_T
+    """[`type`] has some interesting inference rules.
+
+    [`type`]: https://vega.github.io/vega-lite/docs/type.html
+    """
+
+
+class MaybeTitle(TypedDict):
+    title: NotRequired[OneOrSeq[str] | None]
+
+
+class Datum(HasType, MaybeTitle, TypedDict):
+    """Shared by all datums."""
+
+    datum: HasExpr | alt_t.PrimitiveValue_T | HasRepeat
+    """A constant data value.
+
+    - Can never support transforms
+    - But can support expressions
+    """
+
+
+class Field(HasType, MaybeTitle, TypedDict):
+    """Shared by all fields."""
+
+    # `HasRepeat` is always allowed, but not sure how it would make sense in narwhals
+    field: FieldName  # | HasRepeat
+    aggregate: NotRequired[Aggregate]
+    timeUnit: NotRequired[TimeUnit]
+    # On all fields, but havent mapped expressions over - maybe:
+    # - hist?
+    bin: NotRequired[Incomplete]
+
+
+class Value(TypedDict):
+    """Shared by all values."""
+
+    value: HasExpr | alt_t.PrimitiveValue_T
