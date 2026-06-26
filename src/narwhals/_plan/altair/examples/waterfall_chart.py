6036286d3f695192730b4c285a2c89e6d79cf520
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-06-26 20:52:42 +0000

    chore: Waterfall plumbing
    `nw.when` can be translated in multiple ways, depending on the context 😊

--- src/narwhals/_plan/altair/examples/waterfall_chart.py
+++ src/narwhals/_plan/altair/examples/waterfall_chart.py
@@ -1,11 +1,28 @@
 # (https://altair-viz.github.io/gallery/waterfall_chart.html)
 from __future__ import annotations
 
+import altair as alt
 import polars as pl
 
 import narwhals._plan as nw
+from narwhals._plan import expressions as ir
 from narwhals._plan.altair.aggregate import window_transform
 from narwhals._plan.altair.calculate import calculate_transform
+from narwhals._plan.altair.conditional import (
+    ConditionalField,
+    ConditionalValue,
+    encode_ternary_expr,
+)
+from narwhals._plan.altair.exceptions import unsupported_error
+
+
+# TODO @dangotbanned: Integrate the with `Chart.encode` properly
+def _encode_one(expr: nw.Expr) -> ConditionalValue | ConditionalField:
+    e = expr._ir
+    if isinstance(e, ir.TernaryExpr):
+        return encode_ternary_expr(e)
+    raise unsupported_error(e, "encoding")
+
 
 data = [
     {"label": "Begin", "amount": 4000},
@@ -34,11 +51,8 @@ window_lead_label = nw.col("window_lead_label")
 window_sum_amount = nw.col("window_sum_amount")
 
 # New stuff
-begin = nw.lit("Begin")
-end = nw.lit("End")
-when_end = nw.when(label == end)
-empty = nw.lit("")
-
+when_end = nw.when(label=nw.lit("End"))
+begin_or_end = label.is_in(["Begin", "End"])
 
 # NOTE: Need to be outside the altair api for now - but it is legit!
 tf_window = window_transform(
@@ -57,18 +71,56 @@ tf_calculate = calculate_transform(
     calc_prev_sum=calc_prev_sum,
     calc_amount=calc_amount,
     calc_text_amount=(
-        nw.when(~label.is_in(["Begin", "End"]), calc_amount > 0).then(nw.lit("+"))
-        + calc_amount
+        nw.when(~begin_or_end, calc_amount > 0).then(nw.lit("+")) + calc_amount
     ),
     calc_center=(window_sum_amount + calc_prev_sum) / 2,
-    calc_sum_dec=(
-        nw.when(window_sum_amount < calc_prev_sum)
-        .then(window_sum_amount)
-        .otherwise(empty)
-    ),
-    calc_sum_inc=(
-        nw.when(window_sum_amount > calc_prev_sum)
-        .then(window_sum_amount)
-        .otherwise(empty)
-    ),
+    calc_sum_dec=(nw.when(window_sum_amount < calc_prev_sum).then(window_sum_amount)),
+    calc_sum_inc=(nw.when(window_sum_amount > calc_prev_sum).then(window_sum_amount)),
 )
+
+
+base = (
+    alt.Chart(source)
+    ._add_transform(tf_window, *tf_calculate)
+    .encode(x=alt.X("label:O", axis=alt.Axis(title="Months", labelAngle=0), sort=None))
+)
+
+
+color_coding = _encode_one(
+    nw.when(begin_or_end)
+    .then(nw.lit("#878d96"))
+    .when(calc_amount < 0)
+    .then(nw.lit("#fa4d56"))
+    .otherwise(nw.lit("#24a148"))
+)
+
+
+bar = base.mark_bar(size=45).encode(
+    y=alt.Y("calc_prev_sum:Q", title="Amount"),
+    y2="window_sum_amount:Q",
+    color=color_coding,
+)
+
+# The "rule" chart is for the horizontal lines that connect the bars
+rule = base.mark_rule(xOffset=-22.5, x2Offset=22.5).encode(
+    y="window_sum_amount:Q", x2="calc_lead"
+)
+
+# Add values as text
+text_pos_values_top_of_bar = base.mark_text(baseline="bottom", dy=-4).encode(
+    text=alt.Text("calc_sum_inc:N"), y="calc_sum_inc:Q"
+)
+text_neg_values_bot_of_bar = base.mark_text(baseline="top", dy=4).encode(
+    text=alt.Text("calc_sum_dec:N"), y="calc_sum_dec:Q"
+)
+text_bar_values_mid_of_bar = base.mark_text(baseline="middle").encode(
+    text=alt.Text("calc_text_amount:N"), y="calc_center:Q", color=alt.value("white")
+)
+
+chart = alt.layer(
+    bar,
+    rule,
+    text_pos_values_top_of_bar,
+    text_neg_values_bot_of_bar,
+    text_bar_values_mid_of_bar,
+).properties(width=800, height=450)
