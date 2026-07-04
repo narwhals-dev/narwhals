24c7627cd15a9322d9f4e69c2e7f0906d8882817
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-04 21:31:12 +0000

    ughhhhh parameter insanity
    - trying (with great difficulty) to get the typing working
    - waaaaaaaaaaay more code than I want, but still experimenting

--- src/narwhals/_plan/altair/parameter.py
+++ src/narwhals/_plan/altair/parameter.py
@@ -20,13 +20,29 @@ Interactions are used in many of the complex examples, so I want to show those t
 
 from __future__ import annotations
 
+# mypy: disable-error-code="typeddict-item"
+# > Non-required keys (...) not explicitly found in any ** item"
+# TODO @dangotbanned: Review after PEP 728 support
 import functools
 import hashlib
+from copy import deepcopy
 from importlib.util import find_spec
-from typing import TYPE_CHECKING, Any, Literal, TypeAlias, TypedDict
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    Literal,
+    Protocol,
+    TypeAlias,
+    TypedDict,
+    TypeVar,
+    final,
+)
 
 import altair as alt
-from altair import theme
+from altair import Undefined, theme
+from altair.theme import VariableParameterKwds as VariableParamKwds
+from altair.utils import is_undefined
 
 from narwhals._plan.altair.expression import parse_into_vega_expr
 
@@ -34,14 +50,25 @@ if TYPE_CHECKING:
     from collections.abc import Callable, Sequence
 
     import altair.vegalite.v6.schema._typing as _alt_t
+    import typing_extensions as te
     from _typeshed import Incomplete
-    from typing_extensions import TypedDict, Unpack
+    from typing_extensions import Self, Unpack
 
     import narwhals._plan as nw
     from narwhals._plan.altair import typing as alt_t
 
-    class _Name(TypedDict, total=False, closed=True):  # type: ignore[call-arg]
+    class _CommonBind(te.TypedDict, total=False, closed=True):  # type: ignore[call-arg]
+        debounce: float
+        element: str
         name: str
+
+    class _CommonParam(TypedDict, total=False, closed=True):  # type: ignore[call-arg]
+        """Initial state for a param that could be either variable or selection."""
+
+        name: str
+        """If not provided, must be computed as the final build step."""
+        bind: Binding  # selection is wider, but legend/scales are one-way (also never seen stream stuff used)
+        value: Any
 
 
 Binding: TypeAlias = (
@@ -87,15 +114,43 @@ Important:
     discriminator field `type`. But that isn't captured in the `TypedDict` or `SchemaBase` definitions.
 """
 
-ParamKwds: TypeAlias = theme.VariableParameterKwds | theme.TopLevelSelectionParameterKwds
+
+ParamKwds: TypeAlias = VariableParamKwds | theme.TopLevelSelectionParameterKwds
 """Pre-construction state for a parameter.
 
 No arguments are required yet.
 """
 
+_ElementType: TypeAlias = Literal[
+    "button",
+    "color",
+    "date",
+    "datetime-local",
+    "month",
+    "number",
+    "search",
+    "submit",
+    "text",
+    "time",
+    "week",
+]
+"""A [HTML form input type] that does not have a dedicated `Bind*` wrapper.
+
+`"search"` is demonstrated in [here](https://altair-viz.github.io/gallery/scatter_point_paths_hover.html)
+
+[HTML form input type]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+"""
+
+
+class _WithBind(Protocol):
+    def _with_bind(self, bind: Binding, /) -> Self: ...
+
+
+_PBuild = TypeVar("_PBuild", bound=_WithBind, covariant=True)  # noqa: PLC0105
+
 
 def variable(
-    expr: nw.Expr | alt_t.IntoAltExpr, /, **kwds: Unpack[theme.VariableParameterKwds]
+    expr: nw.Expr | alt_t.IntoAltExpr, /, **kwds: Unpack[VariableParamKwds]
 ) -> VariableParam:
     """Create a named variable parameter."""
     kwds["expr"] = parse_into_vega_expr(expr)
@@ -118,33 +173,242 @@ class _ParamBuilder:
     ## Notes
     - Expose a few arguments at a time
     - Don't let things get into an invalid state
+    - For internal use
     """
 
-    # NOTE: These 3 don't follow the pattern I was thinking of here, since they work in one step
+    _state: _CommonParam
+
+    @property
+    def bind(self) -> _BindBuilder:
+        return _BindBuilder(self)
+
+    def point(self) -> Incomplete:
+        msg = "TODO selection_point"
+        raise NotImplementedError(msg)
+
+    def interval(self) -> Incomplete:
+        msg = "TODO selection_interval"
+        raise NotImplementedError(msg)
+
+    # NOTE: variable parameters do not require an expression
+    # most examples just add a default + binding
+    def expr(
+        self, expr: nw.Expr | alt_t.IntoAltExpr, *, react: bool = True
+    ) -> _VariableParamBuilder:
+        return _VariableParamBuilder._from_param_expr(self, expr, react=react)
+
+    def var(self, default: Any = Undefined) -> _VariableParamBuilder:
+        """Create a variable parameter, optionally with a default value."""
+        return _VariableParamBuilder._from_param_var(self, default)
+
+
+@final
+class _VariableParamBuilder:
+    _state: VariableParamKwds
+
+    def __init__(self, state: VariableParamKwds, /) -> None:
+        self._state = state
+
+    @property
+    def bind(self) -> _CommonBindBuilder[Self]:
+        """Bind the parameter to a UI element/widget."""
+        return _CommonBindBuilder(self)
+
+    def _with_bind(self, bind: Binding, /) -> Self:
+        if self._state.get("bind"):
+            msg = "Keyword 'bind' was provided multiple times"
+            raise TypeError(msg)
+        next_state = deepcopy(self._state)
+        next_state["bind"] = bind
+        return type(self)(next_state)
+
     @staticmethod
-    def bind_scales(
-        *,
-        encodings: Sequence[_alt_t.SingleDefUnitChannel_T] = ("x", "y"),
-        **kwds: Unpack[_Name],
+    def _from_param_var(builder: _ParamBuilder, default: Any) -> _VariableParamBuilder:
+        prev_state = deepcopy(builder._state)
+        next_state: VariableParamKwds = {}
+        if prev_state:
+            if name := prev_state.pop("name", None):
+                next_state["name"] = name
+            if bind := prev_state.pop("bind", None):
+                next_state["bind"] = bind
+            value = prev_state.pop("value", Undefined)
+            if not is_undefined(value):
+                if not is_undefined(default):
+                    msg = "Keyword 'value'/'default' was provided multiple times"
+                    raise TypeError(msg)
+                next_state["value"] = value
+
+            if prev_state:
+                msg = (
+                    f"Keywords {list(prev_state)!r} are not valid for variable parameters"
+                )
+                raise TypeError(msg)
+        elif not is_undefined(default):
+            next_state["value"] = default
+
+        return _VariableParamBuilder(next_state)
+
+    @staticmethod
+    def _from_param_expr(
+        builder: _ParamBuilder, expr: nw.Expr | alt_t.IntoAltExpr, *, react: bool
+    ) -> _VariableParamBuilder:
+        prev_state = deepcopy(builder._state)
+        next_state: VariableParamKwds = {}
+        if prev_state:
+            if name := prev_state.pop("name", None):
+                next_state["name"] = name
+            if bind := prev_state.pop("bind", None):
+                next_state["bind"] = bind
+            value = prev_state.pop("value", Undefined)
+            if not is_undefined(value):
+                next_state["value"] = value
+
+            if prev_state:
+                msg = (
+                    f"Keywords {list(prev_state)!r} are not valid for variable parameters"
+                )
+                raise TypeError(msg)
+        next_state.update({"expr": parse_into_vega_expr(expr), "react": react})
+        return _VariableParamBuilder(next_state)
+
+    def default(self, value: Any, /) -> _VariableParamBuilder:
+        self._state["value"] = value
+        return self
+
+
+def _param_builder(name: str | None = None) -> _ParamBuilder:
+    obj = _ParamBuilder.__new__(_ParamBuilder)
+    obj._state = {"name": name} if name else {}
+    return obj
+
+
+class _CommonBindBuilder(Generic[_PBuild]):
+    _state: _CommonBind
+    _param: _PBuild
+
+    def __init__(self, builder: _PBuild, /) -> None:
+        self._param = builder
+        self._state = {}
+
+    # NOTE: properties shared by all `Bind*`
+    def element(self, css_selector: str, /) -> Self:
+        """CSS selector string indicating the parent element to which the input element should be added.
+
+        By default, all input elements are added within the parent container of the Vega view.
+        """
+        self._state["element"] = css_selector
+        return self
+
+    def debounce(self, delay: float, /) -> Self:
+        """Delays event handling until `delay` (milliseconds) have elapsed since the last event was fired."""
+        self._state["debounce"] = delay
+        return self
+
+    def label(self, name: str, /) -> Self:
+        """Use `name` as custom label for input elements.
+
+        By default, the signal name is used instead.
+        """
+        # (not allowed for bind direct)
+        self._state["name"] = name
+        return self
+
+    # NOTE: The actual bind methods, which return back to where we came from
+    def checkbox(self) -> _PBuild:
+        return self._param._with_bind(
+            theme.BindCheckboxKwds(input="checkbox", **self._state)
+        )
+
+    def radio(self, options: Sequence[Any], labels: Sequence[str] = ()) -> _PBuild:
+        # radio buttons
+        return self._radio_select("radio", options, labels)
+
+    def select(self, options: Sequence[Any], labels: Sequence[str] = ()) -> _PBuild:
+        return self._radio_select("select", options, labels)
+
+    dropdown = select
+
+    def range(
+        self,
+        min: float | None = None,  # noqa: A002
+        max: float | None = None,  # noqa: A002
+        step: float | None = None,
+    ) -> _PBuild:
+        bind = theme.BindRangeKwds(input="range", **self._state)
+        if min is not None:
+            bind["min"] = min
+        if max is not None:
+            bind["max"] = max
+        if step is not None:
+            bind["step"] = step
+        return self._param._with_bind(bind)
+
+    def input(
+        self,
+        input_type: _ElementType,
+        /,
+        autocomplete: str | None = None,
+        placeholder: str | None = None,
+    ) -> _PBuild:
+        bind = theme.BindInputKwds(input=input_type, **self._state)
+        if autocomplete is not None:
+            bind["autocomplete"] = autocomplete
+        if placeholder is not None:
+            bind["placeholder"] = placeholder
+        return self._param._with_bind(bind)
+
+    def _radio_select(
+        self,
+        input_type: Literal["radio", "select"],
+        options: Sequence[Any],
+        labels: Sequence[str],
+        /,
+    ) -> _PBuild:
+        bind = theme.BindRadioSelectKwds(input=input_type, options=options, **self._state)
+        if labels:
+            bind["labels"] = labels
+        return self._param._with_bind(bind)
+
+
+# TODO @dangotbanned: Need the input element stuff too
+class _BindBuilder:
+    def __init__(self, builder: _ParamBuilder, /) -> None:
+        self._builder: _ParamBuilder = builder
+
+    # NOTE: Should not be accessible from `_VariableParamBuilder`
+    def scales(
+        self, *, encodings: Sequence[_alt_t.SingleDefUnitChannel_T] = ("x", "y")
     ) -> SelectionParam:
         """Equivalent to `alt.Chart().interactive()`, but without adding the param to the chart."""
-        return _selection(
-            **kwds, select=_config_interval(encodings=encodings), bind="scales"
-        )
+        # NOTE: Obvious now that `_selection` is the wrong API, when combined with this
+        select = _config_interval(encodings=encodings)
+        if name := self._builder._state.get("name"):
+            return _selection(bind="scales", select=select, name=name)
+        return _selection(bind="scales", select=select)
 
     # NOTE: 1 encoding or 1 field -> one method each
     # https://github.com/vega/altair/blob/48b388f140c79d29056d6ea56e519b27e2ed8838/tests/examples_methods_syntax/interactive_legend.py#L5-L6
-    @staticmethod
-    def bind_legend_encoding(
-        encoding: _alt_t.SingleDefUnitChannel_T = "color", /, **kwds: Unpack[_Name]
-    ) -> SelectionParam:
-        return _selection(
-            **kwds, select=_config_point(encodings=[encoding]), bind="legend"
-        )
 
-    @staticmethod
-    def bind_legend_field(field: str, /, **kwds: Unpack[_Name]) -> SelectionParam:
-        return _selection(**kwds, select=_config_point(fields=[field]), bind="legend")
+    def legend_encoding(
+        self, encoding: _alt_t.SingleDefUnitChannel_T = "color", /
+    ) -> SelectionParam:
+        select = _config_point(encodings=[encoding])
+        if name := self._builder._state.get("name"):
+            return _selection(bind="legend", select=select, name=name)
+        return _selection(bind="legend", select=select)
+
+    def legend_field(self, field: str, /) -> SelectionParam:
+        select = _config_point(fields=[field])
+        if name := self._builder._state.get("name"):
+            return _selection(bind="legend", select=select, name=name)
+        return _selection(bind="legend", select=select)
+
+
+def slider_cutoff() -> _VariableParamBuilder:
+    _p1 = _param_builder().var().bind.range(0, 100, 1)
+    _p2 = _param_builder().var(50).bind.range(0, 100, 1)
+    p3 = _param_builder("slider").var(50).bind.range(0, 100, 1)
+    return p3  # noqa: RET504
 
 
 def _variable(
