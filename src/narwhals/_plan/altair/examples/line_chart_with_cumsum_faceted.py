326f567c754afc06fdf87987c04f09f5cf5332c7
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-07 16:50:10 +0000

    chore: Add "Faceted Line Chart with Cumulative Sum" example
    https://altair-viz.github.io/gallery/line_chart_with_cumsum_faceted.html

---
+++ src/narwhals/_plan/altair/examples/line_chart_with_cumsum_faceted.py
@@ -0,0 +1,46 @@
+"""[Faceted Line Chart with Cumulative Sum].
+
+[Faceted Line Chart with Cumulative Sum]: https://altair-viz.github.io/gallery/line_chart_with_cumsum_faceted.html
+"""
+
+from __future__ import annotations
+
+import altair as alt
+
+from narwhals._plan.altair import api
+
+source = api.datasets.load("disasters", backend="polars")
+columns_sorted = ["Drought", "Epidemic", "Earthquake", "Flood"]
+
+chart = (
+    alt.Chart(source)
+    .transform_filter(
+        alt.FieldOneOfPredicate(field="Entity", oneOf=columns_sorted),
+        alt.FieldRangePredicate(field="Year", range=[1900, 2000]),
+    )
+    .transform_window(
+        cumulative_deaths="sum(Deaths)",
+        groupby=["Entity"],  # Calculate cumulative sum of Deaths by Entity
+    )
+    .mark_line()
+    .encode(
+        alt.X("Year:Q", title=None).axis(format="d"),
+        alt.Y("cumulative_deaths:Q", title=None),
+        alt.Color("Entity:N", legend=None),
+    )
+    .properties(width=300, height=150)
+    .facet(
+        facet=alt.Facet(
+            "Entity:N",
+            title=None,
+            sort=columns_sorted,
+            header=alt.Header(labelAnchor="start", labelFontStyle="italic"),
+        ),
+        title=alt.Title(
+            text=["Cumulative casualties by type of disaster", "in the 20th century"],
+            anchor="middle",
+        ),
+        columns=2,
+    )
+    .resolve_axis(y="independent", x="independent")
+)
