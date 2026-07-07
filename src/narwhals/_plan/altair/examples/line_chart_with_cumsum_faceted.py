6838ef4d74cb29b11da1336ba71b4b86e2a44c1b
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-07 17:08:24 +0000

    swap namespace

--- src/narwhals/_plan/altair/examples/line_chart_with_cumsum_faceted.py
+++ src/narwhals/_plan/altair/examples/line_chart_with_cumsum_faceted.py
@@ -13,7 +13,7 @@ source = api.datasets.load("disasters", backend="polars")
 columns_sorted = ["Drought", "Epidemic", "Earthquake", "Flood"]
 
 chart = (
-    alt.Chart(source)
+    api.Chart(source)
     .transform_filter(
         # TODO @dangotbanned: Use `col("Entity").is_in(columns_sorted)`
         alt.FieldOneOfPredicate(field="Entity", oneOf=columns_sorted),
@@ -24,25 +24,25 @@ chart = (
     # - [ ] Support `over` here (like `transform_aggregate`)
     # - [ ] Support `cum_*` functions in `transform_window` only
     #   - They need to split out with `frame=(None, 0)`
-    .transform_window(cumulative_deaths="sum(Deaths)", groupby=["Entity"])
+    .transform_window(cumulative_deaths="sum(Deaths)", groupby=["Entity"])  # pyright: ignore[reportArgumentType]
     .mark_line()
     .encode(
-        alt.X("Year:Q", title=None).axis(format="d"),
-        alt.Y("cumulative_deaths:Q", title=None),
-        alt.Color("Entity:N", legend=None),
+        api.X("Year:Q", title=None).axis(format="d"),
+        api.Y("cumulative_deaths:Q", title=None),
+        api.Color("Entity:N", legend=None),
     )
     .properties(width=300, height=150)
     # TODO @dangotbanned: Add `Chart.facet`
     # TODO @dangotbanned: Re-export `Facet`, `Header`, `Title` (although )
     # TODO @dangotbanned: Consider re-wrapping `Title` as it can take `Parameter`s
-    .facet(
-        facet=alt.Facet(
+    .facet(  # pyright: ignore[reportAttributeAccessIssue]
+        facet=api.Facet(  # pyright: ignore[reportAttributeAccessIssue]
             "Entity:N",
             title=None,
             sort=columns_sorted,
-            header=alt.Header(labelAnchor="start", labelFontStyle="italic"),
+            header=api.Header(labelAnchor="start", labelFontStyle="italic"),  # pyright: ignore[reportAttributeAccessIssue]
         ),
-        title=alt.Title(
+        title=api.Title(  # pyright: ignore[reportAttributeAccessIssue]
             text=["Cumulative casualties by type of disaster", "in the 20th century"],
             anchor="middle",
         ),
