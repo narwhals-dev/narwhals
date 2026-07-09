3967e74966618e63c7ba155945f17908268c191a
Cangyuan Li <76500213+CangyuanLi@users.noreply.github.com>
2026-07-09 04:31:19 -0700

    feat: add nw.list (#3694)
    ---------
    
    Co-authored-by: MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- src/narwhals/_arrow/utils.py
+++ src/narwhals/_arrow/utils.py
@@ -49,6 +49,7 @@ if TYPE_CHECKING:
         options: Any = None,
         memory_pool: Any = None,
     ) -> ChunkedArrayStructArray: ...
+
 else:
     from pyarrow.compute import extract_regex
     from pyarrow.types import (
@@ -89,6 +90,35 @@ def extract_py_scalar(value: Any, /) -> Any:
 def is_array_or_scalar(obj: Any) -> TypeIs[ArrayOrScalar]:
     """Return True for any base `pyarrow` container."""
     return isinstance(obj, (pa.ChunkedArray, pa.Array, pa.Scalar))
+
+
+def arange(start: int, end: int, step: int) -> ArrayAny:
+    if BACKEND_VERSION < (21,):
+        import numpy as np  # ignore-banned-import
+
+        return pa.array(np.arange(start, end, step))
+    # NOTE: Added in https://github.com/apache/arrow/pull/46778
+    return pa.arange(start, end, step)  # type: ignore[attr-defined]
+
+
+def build_list_array(arrays: list[pa.Array[Any]]) -> pa.ChunkedArray[Any]:
+    # This works by using concat_arrays to vertically stack the arrays.
+    # Then we use take to grab the data by index and to horizontally
+    # pack the arrays.
+    n = len(arrays[0])
+    num_cols = len(arrays)
+    i = arange(0, n, 1)
+    j = arange(0, num_cols, 1)
+    k = arange(0, n * num_cols, 1)
+    row_idx = cast("pa.Int64Array", pc.divide(k, lit(num_cols)))
+    col_idx = cast("pa.Int64Array", pc.subtract(k, pc.multiply(row_idx, lit(num_cols))))
+    i_rep = pc.take(i, row_idx)
+    j_tile = pc.take(j, col_idx)
+    indices = cast("pa.Int64Array", pc.add(pc.multiply(j_tile, lit(n)), i_rep))
+    interleaved = pc.take(pa.concat_arrays(arrays), indices)
+    offsets = cast("pa.Int64Array", arange(0, n * num_cols + 1, num_cols))
+    # Typing suggests only allow Int32Array is valid for offsets, but Int64Array is fine.
+    return pa.chunked_array([pa.ListArray.from_arrays(offsets, interleaved)])  # type: ignore[call-overload]
 
 
 def chunked_array(
@@ -491,15 +521,6 @@ def concat_tables(
 
 
 class ArrowSeriesNamespace(EagerSeriesNamespace["ArrowSeries", "ChunkedArrayAny"]): ...
-
-
-def arange(start: int, end: int, step: int) -> ArrayAny:
-    if BACKEND_VERSION < (21,):
-        import numpy as np  # ignore-banned-import
-
-        return pa.array(np.arange(start, end, step))
-    # NOTE: Added in https://github.com/apache/arrow/pull/46778
-    return pa.arange(start, end, step)  # type: ignore[attr-defined]
 
 
 def list_agg(
