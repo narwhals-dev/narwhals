0c99b1715ea05b947f89c0f543ab53f3b58e5806
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-08-13 09:23:15 +0100

    chore: Bump pyrefly 1.2, remove some unnecessary `pyrefly: ignore`s (#3848)

--- tests/expr_and_series/pandas_str_dtypes_test.py
+++ tests/expr_and_series/pandas_str_dtypes_test.py
@@ -104,7 +104,7 @@ STRING_DTYPE_PYTHON_NA = pd.StringDtype("python", na_value=pd.NA)  # type: ignor
 def test_pandas_str_types(left_dtype: Any, right_dtype: Any, result_dtype: Any) -> None:
     df = pd.DataFrame({"fruit": ["apple", "banana"]}, dtype=left_dtype)
     df["new_str_col"] = "!"
-    df["new_str_col"] = df["new_str_col"].astype(right_dtype)  # pyrefly: ignore[missing-attribute] https://github.com/facebook/pyrefly/issues/3299
+    df["new_str_col"] = df["new_str_col"].astype(right_dtype)
     res = nw.from_native(df).with_columns(
         concat_col=nw.concat_str([nw.col("fruit"), nw.col("new_str_col")])
     )
