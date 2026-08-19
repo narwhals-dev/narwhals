fcb435798c4cc1a7f3dd477a07d8d562f306f2ca
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-08-19 17:53:23 +0100

    :art:

--- src/narwhals/_pandas_like/namespace.py
+++ src/narwhals/_pandas_like/namespace.py
@@ -335,17 +335,14 @@ class PandasLikeNamespace(
         # Follow the left-hand-rule documented in `docs/concepts/pandas_index.md`.
         target_index = dfs[0].index
         arange = import_array_module(self._implementation).arange
-        # Value of type variable "NativeNDFrameT" of "set_index" cannot be "NativeDataFrameT"
-        reset_dfs = cast(
-            "list[NativeDataFrameT]",
-            [
-                set_index(  # type: ignore[type-var]
-                    df, arange(len(df)), implementation=self._implementation
-                )
-                for df in dfs
-            ],
-        )
-        concatenated = self._concat_by_index(reset_dfs)
+
+        def reset(df: NativeDataFrameT) -> NativeDataFrameT:
+            # Value of type variable "NativeNDFrameT" of "set_index" cannot be "NativeDataFrameT"
+            return set_index(  # type: ignore[type-var]
+                df, arange(len(df)), implementation=self._implementation
+            )
+
+        concatenated = self._concat_by_index([reset(df) for df in dfs])
         # Value of type variable "NativeNDFrameT" of "set_index" cannot be "NativeDataFrameT"
         return set_index(  # type: ignore[type-var]
             concatenated, target_index, implementation=self._implementation
