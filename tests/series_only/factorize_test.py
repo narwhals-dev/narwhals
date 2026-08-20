0b8c8fb251d10a2546cdb3dd49a46a45c2495153
Cameron Riddell <cameron@dutc.io>
2026-08-20 14:08:16 -0700

    factorize drop support for pandas<2.0 since we require Nullable types

--- tests/series_only/factorize_test.py
+++ tests/series_only/factorize_test.py
@@ -7,6 +7,7 @@ import pytest
 
 import narwhals as nw
 from tests.utils import (
+    PANDAS_VERSION,
     POLARS_VERSION,
     ConstructorEager,
     assert_equal_data,
@@ -42,6 +43,7 @@ NULL_ARGS = [
     ],
 )
 @pytest.mark.parametrize("null_policy_args", NULL_ARGS)
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_nonnull(
     constructor_eager: ConstructorEager,
     *,
@@ -104,6 +106,7 @@ def test_factorize_nonnull(
         ([1.1, 2.2, 3.3, 2.2, None, None], NULL_SENTINEL, [1.1, 2.2, 3.3]),
     ],
 )
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_null(
     constructor_eager: ConstructorEager,
     *,
@@ -156,6 +159,7 @@ def test_factorize_null(
     ],
 )
 @pytest.mark.parametrize("null_policy_args", NULL_ARGS)
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_sort_nonnull(
     constructor_eager: ConstructorEager,
     *,
@@ -228,6 +232,7 @@ def test_factorize_sort_nonnull(
         ),
     ],
 )
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_sort_null(
     constructor_eager: ConstructorEager,
     *,
@@ -292,6 +297,7 @@ def test_factorize_sort_null(
         ),
     ],
 )
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_nan_semantics(
     constructor_eager: ConstructorEager,
     *,
@@ -313,9 +319,13 @@ def test_factorize_nan_semantics(
     assert_equal_series(result.uniques, expected, name="uniques")
 
 
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_non_sentinel_policy_w_sentinel_arg() -> None:
     pytest.importorskip("polars")
     import polars as pl
+
+    if polars_lt_v1:
+        pytest.skip(reason=pl_skip_reason)
 
     s = nw.from_native(pl.Series("a", ["x", "y", "z"]), series_only=True)
     with pytest.raises(
@@ -324,9 +334,13 @@ def test_factorize_non_sentinel_policy_w_sentinel_arg() -> None:
         s.factorize(null_policy="preserve", sentinel=-1)
 
 
+@pytest.mark.skipif(PANDAS_VERSION < (2, 1, 0), reason="nullable types require pandas2+")
 def test_factorize_sentinel_policy_w_no_sentinel_arg() -> None:
     pytest.importorskip("polars")
     import polars as pl
+
+    if polars_lt_v1:
+        pytest.skip(reason=pl_skip_reason)
 
     s = nw.from_native(pl.Series("a", ["x", "y", "z"]), series_only=True)
     with pytest.raises(
@@ -337,14 +351,15 @@ def test_factorize_sentinel_policy_w_no_sentinel_arg() -> None:
 
 @pytest.mark.parametrize(("values", "expected_unique"), [([*"abcabc"], ["a", "b", "c"])])
 @pytest.mark.parametrize("null_policy_args", NULL_ARGS)
-def test_factorize_pyarrow_dictionary_array(
+def test_factorize_categoricals(
     constructor_eager: ConstructorEager,
     *,
     values: list[Any],
     expected_unique: list[Any],
     null_policy_args: dict[str, Any],
 ) -> None:
-    pytest.importorskip("pyarrow")
+    if "polars" in str(constructor_eager) and polars_lt_v1:
+        pytest.skip(reason=pl_skip_reason)
 
     df_native = constructor_eager({"a": values})
     df = nw.from_native(df_native).select(nw.col("a").cast(nw.Categorical))
