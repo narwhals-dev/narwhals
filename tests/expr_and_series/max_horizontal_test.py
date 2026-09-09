d7f434b3f29434d4a648bbd1179203e8418deb09
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-09-09 20:58:06 +0100

    fix: avoid name collision in `{max,min}_horizontal` for pandas-like (#3935)

--- tests/expr_and_series/max_horizontal_test.py
+++ tests/expr_and_series/max_horizontal_test.py
@@ -23,3 +23,16 @@ def test_maxh_all(constructor: Constructor) -> None:
     result = df.select(nw.max_horizontal(nw.all()), c=nw.max_horizontal(nw.all()))
     expected = {"a": expected_values, "c": expected_values}
     assert_equal_data(result, expected)
+
+
+def test_maxh_duplicate_output_names(constructor: Constructor) -> None:
+    # https://github.com/narwhals-dev/narwhals/issues/3934
+    df = nw.from_native(constructor(data))
+    exprs = [
+        nw.when(nw.col("a") > 1).then(nw.lit(1)),
+        nw.when(nw.col("b") > 1).then(nw.lit(2)),
+        nw.when(nw.col("z") > 1).then(nw.lit(3)),
+    ]
+    result = df.select(horizontal_max=nw.max_horizontal(*exprs))
+    expected = {"horizontal_max": [3, 1, 2, None]}
+    assert_equal_data(result, expected)
