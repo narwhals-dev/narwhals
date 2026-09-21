32747338605d3c6c34a43c243a4d8e02631e11da
Max Zwager <maxzwager+github@gmail.com>
2026-09-21 16:37:03 +0200

    Expose __version__ from stable namespaces

--- tests/getattr_test.py
+++ tests/getattr_test.py
@@ -23,3 +23,19 @@ def test_package_getattr() -> None:
 
     with pytest.raises(AttributeError):
         very_bad = nw.not_real  # type: ignore[attr-defined]  # noqa: F841
+
+
+def test_stable_package_getattr() -> None:
+    pytest.importorskip("typing_extensions")
+    from typing_extensions import assert_type
+
+    import narwhals.stable.v1 as nw_v1
+    import narwhals.stable.v2 as nw_v2
+
+    ok_v1 = nw_v1.__version__
+    assert_type(ok_v1, str)
+    assert ok_v1 == nw.__version__
+
+    ok_v2 = nw_v2.__version__
+    assert_type(ok_v2, str)
+    assert ok_v2 == nw.__version__
